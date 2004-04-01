ac7434916a7d97d267052ff583b011b8dd6e94ad
Michael Gogins <gogins@users.sourceforge.net>
2004-04-01 03:25:01 +0000

    Warning riddance.

--- frontends/CsoundVST/CsoundFile.cpp
+++ frontends/CsoundVST/CsoundFile.cpp
@@ -19,7 +19,9 @@
 * License along with this software; if not, write to the Free Software
 * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
+#if defined(MSVC)
 #pragma warning(disable: 4786)
+#endif
 #include "CsoundFile.hpp"
 #include <ctime>
 #include <algorithm>
