cd270ef9edd0c6054ef068e873bb76c8261e0a48
John ffitch <jpff@users.sourceforge.net>
2004-07-17 18:28:38 +0000

    pconvolve

--- OOps/ugens9.c
+++ OOps/ugens9.c
@@ -309,40 +309,77 @@ int convolve(CONVOLVE *p)
 }
 
 
-/* partitioned (low latency) overlap-save convolution.  We break up the
-   IR into separate blocks before doing the FFT.  For this reason we ONLY
-   accept soundfiles as input, and do the FFT at i-time [code cut N pasted
-   from cvanal.c].
-   Eventually it would nice to change cvanal to do the FFT as a preprocess
+/* partitioned (low latency) overlap-save convolution. 
+	we break up the IR into separate blocks, then perform
+	an FFT on each partition.  For this reason we ONLY accept 
+	soundfiles as input, and do all of the traditional 'cvanal' 
+	processing at i-time.  it would be nice to eventually 
+	have cvanal create a partitioned format, which in turn would
+	allow this opcode to accept .con files.
 	-ma++ april 2004 */
 
 extern int sndinset(SOUNDIN *p);
 extern int soundin(SOUNDIN *p);
-extern SNDFILE *SAsndgetset(char*,SOUNDIN **,MYFLT*,MYFLT*,MYFLT*,int);
+extern SNDFILE *sndgetset(SOUNDIN *);
+extern long getsndin(SNDFILE *, MYFLT *, long, SOUNDIN *);
 
 int pconvset(PCONVOLVE *p)
 {
-    int      IRfileChanls, channel = *(p->channel);
-    MYFLT    beg_time = FL(0.0), input_dur = FL(0.0), sr = FL(0.0);
+    int      channel = (*(p->channel) <= 0 ? ALLCHNLS : *(p->channel));
     SNDFILE *infd;
-    SOUNDIN *IRfile;
+    SOUNDIN IRfile;
     complex *basis;
     MYFLT   *inbuf, *fp1,*fp2;
-    long    i, read_in, part;
+    long    i, j, read_in, part;
     MYFLT   *IRblock;
+    MYFLT   ainput_dur;
 
-    /* open impulse response soundfile */
-    if ((infd = SAsndgetset(unquote(p->STRARG),&IRfile,&beg_time,
-                            &input_dur,&sr,channel))<0) {
-      sprintf(errmsg, "pconvolve: error while opening %s", retfilnam);
+    static  MYFLT    format = FL(0.0), skptm = FL(0.0);
+    static  MYFLT    sstrcod = (MYFLT)SSTRCOD;
+    static  ARGOFFS  argofs = {0};
+    static  OPTXT    optxt;
+
+	/* open impulse response soundfile [code derived from SAsndgetset()] */
+	optxt.t.outoffs = &argofs;    /* point to dummy OUTOCOUNT       */
+    IRfile.h.optext = &optxt;
+    IRfile.ifilno = &sstrcod;
+    IRfile.iskptim = &skptm;
+    IRfile.iformat = &format;
+    IRfile.STRARG = p->STRARG;
+    IRfile.sr = 0;
+    if (channel < 1 || ((channel > 4) && (channel != ALLCHNLS))) {
+      sprintf(errmsg, "channel request %d illegal\n", channel);
       return perferror(errmsg);
     }
+    IRfile.channel = channel;
+    IRfile.analonly = 1;
+    if ((infd = sndgetset(&IRfile))==NULL) {
+      return perferror("pconvolve: error while impulse file");
+    }
 
-    IRfileChanls = IRfile->nchanls;
- /*   p->nchanls = (channel != ALLCHNLS ? 1 : IRfileChanls); */
+    if ((IRfile.framesrem < 0) && (O.msglevel & WARNMSG)) {
+      printf(Str(X_1318,
+                 "WARNING: undetermined file length, will attempt "
+                 "requested duration"));
+      ainput_dur = FL(0.0);     /* This is probably wrong -- JPff */
+    }
+    else {
+        IRfile.getframes = IRfile.framesrem;
+        ainput_dur = (MYFLT) IRfile.getframes / IRfile.sr;
+      }
 
-    if (sr != esr && (O.msglevel & WARNMSG)) {
-      printf("WARNING: IR srate = %8.0f, orch's srate = %8.0f", sr, esr);
+    printf(Str(X_598,"analyzing %ld sample frames (%3.1f secs)\n"),
+           IRfile.getframes, ainput_dur);
+
+    p->nchanls = (channel != ALLCHNLS ? 1 : IRfile.nchanls);
+    if (p->nchanls != p->OUTOCOUNT) {
+      return perferror("PCONVOLVE: number of output channels not equal "
+                "to input channels");
+    }
+
+    if (IRfile.sr != esr && (O.msglevel & WARNMSG)) {
+      /* ## RWD suggests performing sr conversion here! */  
+      printf("WARNING: IR srate != orch's srate");
     }
 
     /* make sure the partition size is nonzero and a power of 2  */
@@ -355,56 +392,41 @@ int pconvset(PCONVOLVE *p)
 
     p->Hlenpadded = 2*p->Hlen;
 
-
     /* determine the number of partitions */
-    p->numPartitions = ceil((MYFLT)(IRfile->getframes) / (MYFLT)p->Hlen);
+    p->numPartitions = ceil((MYFLT)(IRfile.getframes) / (MYFLT)p->Hlen);
 
     /* set up FFT tables */
-    basis = AssignBasis(NULL, p->Hlenpadded);  /*##MEMLEAK?? */
-    inbuf = (MYFLT *)mmalloc(p->Hlen /** p->nchanls*/ * sizeof(MYFLT));
+    basis = AssignBasis(NULL, p->Hlenpadded);
+    inbuf = (MYFLT *)mmalloc(p->Hlen * p->nchanls * sizeof(MYFLT));
     auxalloc(p->numPartitions * (p->Hlenpadded + 2) *
-             sizeof(MYFLT) /** p->nchanls*/, &p->H);
+             sizeof(MYFLT) * p->nchanls, &p->H);
     IRblock = (MYFLT *)p->H.auxp;
 
     /* form each partition and take its FFT */
     for (part = 0; part < p->numPartitions; part++) {
       /* get the block of input samples and normalize -- soundin code 
          handles finding the right channel */
-      if ((read_in = getsndin(infd, inbuf, p->Hlen, IRfile)) <= 0)
-        die(Str(X_965,"less sound than expected!"));
+      if ((read_in = getsndin(infd, inbuf, p->Hlen*p->nchanls, &IRfile)) <= 0)
+        die("PCONVOLVE: less sound than expected!");
 
-      fp1 = inbuf;
-      fp2 = IRblock;
-      for (i = 0; i < read_in; i++)
-        *fp2++ = *fp1++ * dbfs_to_float;
-
-      FFT2realpacked((complex *)IRblock, p->Hlenpadded , basis);
-
-      /*	 would need to do something like this if multichannel is supported
-
-      if (channel > 1) -- needed? 		 cue up to first sample
-      fp1 += channel - 1; -- needed?
-
-      for (i = 0; i < p->nchanls; i++) {
-      fp2 = IRblock + i * (p->Hlenpadded + 2);
-
-      for (j = p->Hlen; j > 0; j--) {
-      *fp2++ = *fp1 * dbfs_to_float;
-      fp1 += nchanls;
+      /* take FFT of each channel */
+      for (i = 0; i < p->nchanls; i++) { 
+        fp1 = inbuf + i;
+        fp2 = IRblock;
+        for (j = 0; j < read_in/p->nchanls; j++) {
+          *fp2++ = *fp1 * dbfs_to_float;
+          fp1 += p->nchanls;
+        }
+        FFT2realpacked((complex *)IRblock, p->Hlenpadded , basis);
+        IRblock += (p->Hlenpadded + 2);
       }
-
-      fp1 = inbuf + i + 1;
-      FFT2realpacked((complex *)IRblock, p->Hlenpadded , basis);
-      } /* end i loop */
-
-      IRblock += (p->Hlenpadded + 2) /** p->nchanls*/;
     }
 
     mfree(inbuf);
     sf_close(infd);
 
     /* Initialise input FFT lookup table */
-    p->cvlut = (complex *)AssignBasis(NULL, p->Hlenpadded);  /*##MEMLEAK?? */
+    p->cvlut = (complex *)AssignBasis(NULL, p->Hlenpadded);
 
     /* allocate the buffer saving recent input samples */
     auxalloc(p->Hlen * sizeof(MYFLT), &p->savedInput);
@@ -415,14 +437,28 @@ int pconvset(PCONVOLVE *p)
     p->workWrite = (MYFLT *)p->workBuf.auxp + p->Hlen;
 
     /* allocate the buffer holding recent past convolutions */
-    auxalloc((p->Hlenpadded+2) * p->numPartitions * sizeof(MYFLT), &p->convBuf);
+    auxalloc((p->Hlenpadded+2) * p->numPartitions
+             * p->nchanls * sizeof(MYFLT), &p->convBuf);
     p->curPart = 0;
 
     /* allocate circular output sample buffer */
-    p->outBufSiz = sizeof(MYFLT) * (p->Hlen >= ksmps ? p->Hlenpadded : 2*ksmps);
+    p->outBufSiz = sizeof(MYFLT) * p->nchanls *
+      (p->Hlen >= ksmps ? p->Hlenpadded : 2*ksmps);
     auxalloc(p->outBufSiz, &p->output);
-    p->outWrite = p->outRead = (MYFLT *)p->output.auxp;
-    p->outCount = 0;
+    p->outRead = (MYFLT *)p->output.auxp;
+
+    /* if ksmps < hlen, we have to pad initial output to prevent a possible
+       empty ksmps pass	after a few initial generated buffers.  There is
+       probably an equation to figure this out to reduce the delay, but
+       I can't seem to figure it out */
+    if (p->Hlen > ksmps) {
+      p->outCount = p->Hlen + ksmps;
+      p->outWrite = p->outRead + (p->nchanls * p->outCount);
+    }
+    else {
+      p->outCount = 0;
+      p->outWrite = p->outRead;
+    }
     return OK;
 }
 
@@ -431,9 +467,10 @@ int pconvolve(PCONVOLVE *p)
     int    nsmpsi = ksmps;
     MYFLT  *ai = p->ain;
     MYFLT *buf, *input = p->savedInput.auxp, *workWrite = p->workWrite;
-    MYFLT  *a1 = p->ar1;
+    MYFLT  *a1 = p->ar1, *a2 = p->ar2, *a3 = p->ar3, *a4 = p->ar4;
     long hlenp1 = p->Hlen + 1;
-    long   i, count = p->inCount;
+    long   i, j, count = p->inCount;
+    long   hlenpaddedplus2 = p->Hlenpadded+2;
 
     while (nsmpsi-- > 0) {
       /* Read input audio and place into buffer. */
@@ -442,17 +479,17 @@ int pconvolve(PCONVOLVE *p)
       /* We have enough audio for a convolution. */
       if (count == p->Hlen) {
         complex *dest = (complex *)((MYFLT *)p->convBuf.auxp +
-                                    p->curPart * (p->Hlenpadded+2) /* * p->nchanls*/);
+                                    p->curPart *(p->Hlenpadded+2) *p->nchanls);
         complex *h = (complex *)p->H.auxp;
         complex *workBuf = (complex *)p->workBuf.auxp;
 
         /* FFT the input (to create X) */
-        *workWrite = FL(0.0); /* zero out nyquist bin from last fft result -
-                                 maybe is ignored for input(?) but just in case.. */
+        *workWrite = FL(0.0); /* zero out nyquist bin from last fft result - maybe
+                                 is ignored for input(?) but just in case.. */
         FFT2realpacked(workBuf, p->Hlenpadded, p->cvlut);
 
         /* for every IR partition convolve and add to previous convolves */
-        for (i = 0; i < p->numPartitions; i++) {
+        for (i = 0; i < p->numPartitions*p->nchanls; i++) {
           complex *src = workBuf;
           long n = hlenp1;
           while (n--) {
@@ -466,47 +503,68 @@ int pconvolve(PCONVOLVE *p)
         }
 
         /* Perform inverse FFT of the ondeck partion block */
-        buf = (MYFLT *)p->convBuf.auxp + p->curPart * (p->Hlenpadded+2) /* p->nchanls*/;
-        FFT2torlpacked((complex *)buf, p->Hlenpadded, FL(1.0)/((MYFLT)p->Hlenpadded), p->cvlut);
-
+        buf = (MYFLT *)p->convBuf.auxp + p->curPart * p->nchanls * hlenpaddedplus2;
+        for (i = 0; i < p->nchanls; i++) 
+          FFT2torlpacked((complex *)(buf + i*hlenpaddedplus2),
+                         p->Hlenpadded,
+                         FL(1.0)/((MYFLT)p->Hlenpadded),
+                         /* *p->numPartitions --needed??? above */
+                         p->cvlut);
         /* We only take only the last Hlen output samples so we first zero out
            the first half for next time, then we copy the rest to output buffer */
-        for (i = 0; i < p->Hlen; i++)
-          *buf++ = FL(0.0);
-        for (i = 0; i < p->Hlen; i++) {
-          *p->outWrite++ = *buf;
-          *buf++ = FL(0.0);
-          if (p->outWrite == p->output.endp)
-            p->outWrite = p->output.auxp;
+        for (j = 0; j < p->nchanls; j++) {
+          MYFLT *outp = p->outWrite + j;
+          for (i = 0; i < p->Hlen; i++)		
+            *buf++ = FL(0.0);
+          for (i = 0; i < p->Hlen; i++) {
+            *outp = *buf;
+            *buf++ = FL(0.0);
+            outp += p->nchanls;
+            if (outp >= (MYFLT *)p->output.endp)
+              outp = (MYFLT *)p->output.auxp + j;
+          }
+          buf += 2;
         }
-
+	p->outWrite += p->Hlen*p->nchanls;
+        if (p->outWrite >= (MYFLT *)p->output.endp)
+          p->outWrite -= p->outBufSiz/sizeof(MYFLT);
         p->outCount += p->Hlen;
-
-        if (++p->curPart == p->numPartitions) /* advance to the next partition */
+	if (++p->curPart == p->numPartitions) /* advance to the next partition */
           p->curPart = 0;
-
         /* copy the saved input into the work buffer for next time around */
         memcpy(p->workBuf.auxp, input, p->Hlen * sizeof(MYFLT));
-
-
-        /* make sure we zero out the extra 2 nyquist vals for next time??? */
-        /*  ((MYFLT *)p->workBuf.auxp)[p->Hlenpadded] = 0; */
-/*           ((MYFLT *)p->workBuf.auxp)[p->Hlenpadded + 1] = 0; /\* technically nyquist->im should always be zero, but just in case.. *\/ */
-
         count = 0;
         workWrite = (MYFLT *)p->workBuf.auxp + p->Hlen;
       }
-
     } /* end while */
 
 
     /* copy to output if we have enough samples [we always should
        except the first Hlen samples] */
     if (p->outCount >= ksmps) {
-      int n = 0;
+      int n;
       p->outCount -= ksmps;
-      for (n; n < ksmps; n++) {
-        *a1++ = *p->outRead++;
+      for (n=0; n < ksmps; n++) {
+        switch (p->nchanls) {
+        case 1:
+          *a1++ = *p->outRead++;
+          break;
+        case 2:
+          *a1++ = *p->outRead++;
+          *a2++ = *p->outRead++;
+          break;
+        case 3:
+          *a1++ = *p->outRead++;
+          *a2++ = *p->outRead++;
+          *a3++ = *p->outRead++;
+          break;
+        case 4:
+          *a1++ = *p->outRead++;
+          *a2++ = *p->outRead++;
+          *a3++ = *p->outRead++;
+          *a4++ = *p->outRead++;
+          break;
+        }
         if (p->outRead == p->output.endp)
           p->outRead = p->output.auxp;
       }
