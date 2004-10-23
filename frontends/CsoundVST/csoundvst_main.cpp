8668f23fdf507b13c8e47bb0c1ec5587871edf23
Michael Gogins <gogins@users.sourceforge.net>
2004-10-23 17:15:40 +0000

    Improved diagnostic messages for exported functions.

--- frontends/CsoundVST/csoundvst_main.cpp
+++ frontends/CsoundVST/csoundvst_main.cpp
@@ -21,22 +21,31 @@
 */
 #include <CsoundVST.hpp>
 #include <CsoundVstFltk.hpp>
-#include <iostream>
+#include <cstdio>
+#include <cstdlib>
 
 int main(int argc, char **argv)
 {
+        std::fprintf(stderr, "Starting CsoundVST...\n" );
 #if defined(WIN32)
         HINSTANCE lib = LoadLibrary("_CsoundVST.dll");
+        if(!lib) {
+            DWORD lastError = GetLastError();
+            std::fprintf(stderr, "DLL load error: %d.\n", lastError);
+        }
+        std::fprintf(stderr, "lib = 0x%x\n", lib);
         AEffect* (*plugin_main)(audioMasterCallback audioMaster) = (AEffect* (*)(audioMasterCallback audioMaster)) GetProcAddress(lib, "main");
-        std::cout << "lib = " << lib << " main = " << (void *)plugin_main << std::endl;
+        std::fprintf(stderr, "plugin_main = 0x%x\n", plugin_main);
         int (*init_CsoundVST)() = (int (*)()) GetProcAddress(lib, "init_CsoundVST");
-        std::cout << "lib = " << lib << " init_CsoundVST = " << (void *)init_CsoundVST << std::endl;
+        std::fprintf(stderr, "init_CsoundVST = 0x%x\n", init_CsoundVST);
+        CsoundVST *(*CreateCsoundVST_)() = (CsoundVST *(*)()) GetProcAddress(lib, "CreateCsoundVST");
+        std::fprintf(stderr, "CreateCsoundVST_ = 0x%x\n", CreateCsoundVST_);
 #endif
 		CsoundVST *csoundVST = CreateCsoundVST();
+        std::fprintf(stderr, "csoundVST = 0x%x\n", csoundVST);
 		AEffEditor *editor = csoundVST->getEditor();
 		editor->open(0);
-		if(argc == 2)
-		{
+		if(argc == 2) {
 		  csoundVST->openFile(argv[1]);
 		}
 		return csoundVST->run();
