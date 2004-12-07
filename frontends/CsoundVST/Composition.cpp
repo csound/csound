5be8fcd9b8d90517c830dca718c29d59fbc7184c
Michael Gogins <gogins@users.sourceforge.net>
2004-12-07 03:52:29 +0000

    Added createCsoundScore function for generating without rendering, looking at Csound score before rendering, etc.

--- frontends/CsoundVST/Composition.cpp
+++ frontends/CsoundVST/Composition.cpp
@@ -1,24 +1,24 @@
 /**
-* C S O U N D   V S T 
-*
-* A VST plugin version of Csound, with Python scripting.
-*
-* L I C E N S E
-*
-* This software is free software; you can redistribute it and/or
-* modify it under the terms of the GNU Lesser General Public
-* License as published by the Free Software Foundation; either
-* version 2.1 of the License, or (at your option) any later version.
-*
-* This software is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-* Lesser General Public License for more details.
-*
-* You should have received a copy of the GNU Lesser General Public
-* License along with this software; if not, write to the Free Software
-* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*/
+ * C S O U N D   V S T 
+ *
+ * A VST plugin version of Csound, with Python scripting.
+ *
+ * L I C E N S E
+ *
+ * This software is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 #if defined(_MSC_VER) && !defined(__GNUC__)
 #pragma warning (disable:4786) 
 #endif
@@ -27,98 +27,103 @@
 
 namespace csound 
 {
-	Composition::Composition() : 
-        tonesPerOctave(12.0),
-        conformPitches(false),
-		cppSound(&cppSound_)
-	{
-	}
+  Composition::Composition() : 
+    tonesPerOctave(12.0),
+    conformPitches(false),
+    cppSound(&cppSound_)
+  {
+  }
 
-	Composition::~Composition()
-	{
-	}
+  Composition::~Composition()
+  {
+  }
 
-	void Composition::render()
-	{
-		generate();
-		perform();
-	}
+  void Composition::render()
+  {
+    generate();
+    createCsoundScore();
+    perform();
+  }
 
-	void Composition::perform()
-	{
-		cppSound->removeScore();
-		score.sort();
-		for(Score::iterator it = score.begin(); it != score.end(); ++it)
-		{
-			if(getConformPitches())
-			{
-				csound::Event e = *it;
-				e.conformToPitchClassSet();
-				cppSound->addScoreLine(e.toCsoundIStatement(tonesPerOctave));
-			}
-			else
-			{
-				cppSound->addScoreLine(it->toCsoundIStatement(tonesPerOctave));
-			}
-		}
-		cppSound->exportForPerformance();
-		cppSound->perform();
-	}
+  void Composition::createCsoundScore()
+  {
+    cppSound->removeScore();
+    score.sort();
+    for(Score::iterator it = score.begin(); it != score.end(); ++it)
+      {
+	if(getConformPitches())
+	  {
+	    csound::Event e = *it;
+	    e.conformToPitchClassSet();
+	    cppSound->addScoreLine(e.toCsoundIStatement(tonesPerOctave));
+	  }
+	else
+	  {
+	    cppSound->addScoreLine(it->toCsoundIStatement(tonesPerOctave));
+	  }
+      }
+    cppSound->exportForPerformance();
+  }
 
-	void Composition::generate()
-	{
-	}
+  void Composition::perform()
+  {
+    cppSound->perform();
+  }
 
-	void Composition::clear()
-	{
-		score.clear();
-		cppSound->removeScore();
-	}
+  void Composition::generate()
+  {
+  }
 
-	Score &Composition::getScore()
-	{
-		return score;
-	}
+  void Composition::clear()
+  {
+    score.clear();
+    cppSound->removeScore();
+  }
 
-	void Composition::setCppSound(CppSound *cppSound)
-	{
-		if(!cppSound)
-		{
-			this->cppSound = &cppSound_;
-		}
-		else
-		{
-			this->cppSound = cppSound;
-		}
-	}
+  Score &Composition::getScore()
+  {
+    return score;
+  }
 
-	CppSound *Composition::getCppSound()
-	{
-		return cppSound;
-	}
+  void Composition::setCppSound(CppSound *cppSound)
+  {
+    if(!cppSound)
+      {
+	this->cppSound = &cppSound_;
+      }
+    else
+      {
+	this->cppSound = cppSound;
+      }
+  }
 
-	void Composition::write(const char *text)
-	{
-		System::message(text);
-	}
+  CppSound *Composition::getCppSound()
+  {
+    return cppSound;
+  }
 
-	void Composition::setTonesPerOctave(double tonesPerOctave)
-	{
-		this->tonesPerOctave = tonesPerOctave;
-	}
+  void Composition::write(const char *text)
+  {
+    System::message(text);
+  }
 
-	double Composition::getTonesPerOctave() const
-	{
-		return tonesPerOctave;
-	}
+  void Composition::setTonesPerOctave(double tonesPerOctave)
+  {
+    this->tonesPerOctave = tonesPerOctave;
+  }
 
-	void Composition::setConformPitches(bool conformPitches)
-	{
-		this->conformPitches = conformPitches;
-	}
+  double Composition::getTonesPerOctave() const
+  {
+    return tonesPerOctave;
+  }
+
+  void Composition::setConformPitches(bool conformPitches)
+  {
+    this->conformPitches = conformPitches;
+  }
 	
-	bool Composition::getConformPitches() const
-	{
-		return conformPitches;
-	}
+  bool Composition::getConformPitches() const
+  {
+    return conformPitches;
+  }
 }	
--- frontends/CsoundVST/Composition.hpp
+++ frontends/CsoundVST/Composition.hpp
@@ -1,24 +1,24 @@
 /*
-* C S O U N D   V S T 
-*
-* A VST plugin version of Csound, with Python scripting.
-*
-* L I C E N S E
-*
-* This software is free software; you can redistribute it and/or
-* modify it under the terms of the GNU Lesser General Public
-* License as published by the Free Software Foundation; either
-* version 2.1 of the License, or (at your option) any later version.
-*
-* This software is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-* Lesser General Public License for more details.
-*
-* You should have received a copy of the GNU Lesser General Public
-* License along with this software; if not, write to the Free Software
-* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*/
+ * C S O U N D   V S T 
+ *
+ * A VST plugin version of Csound, with Python scripting.
+ *
+ * L I C E N S E
+ *
+ * This software is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 #ifndef COMPOSITION_H
 #define COMPOSITION_H
 
@@ -27,7 +27,7 @@
 %{
 #include "CppSound.hpp"
 #include "Score.hpp"
-%}
+  %}
 #else
 #include "CppSound.hpp"
 #include "Score.hpp"
@@ -35,61 +35,65 @@
 
 namespace csound 
 {
-	/**
-	* Base class for user-derived musical compositions.
-	* Contains a Score object for collecting generated Events such as notes and control messages,
-	* and an Orchestra object for rendering the generated scores.
-	*/
-	class Composition 
-	{
-	protected:
-		Score score;
-		double tonesPerOctave;
-		bool conformPitches;
-		CppSound cppSound_;
-		CppSound *cppSound;
-	public:
-		Composition();
-		virtual ~Composition();
-		/**
-		* Convenience function that erases the existing score, 
-		* invokes generate(), and invokes perform().
-		*/
-		virtual void render();
-		/**
-		* Uses csound to perform the current score.
-		*/
-		virtual void perform();
-		/**
-		* Generate performance events and store them in the score. 
-		* Must be overidden in derived classes.
-		*/
-		virtual void generate();
-		/**
-		* Clear all contents of this. Probably should be overridden in derived classes.
-		*/
-		virtual void clear();
-		/**
-		* Return the self-contained Score.
-		*/
-		virtual Score &getScore();
-		/**
-		* Sets the self-contained Orchestra.
-		*/
-		virtual void setCppSound(CppSound *orchestra);
-		/**
-		* Return the self-contained Orchestra.
-		*/
-		virtual CppSound *getCppSound();
-		/**
-		* Write as if to stdout or stderr.
-		*/
-		virtual void write(const char *text);
-		virtual void setTonesPerOctave(double tonesPerOctave);
-		virtual double getTonesPerOctave() const;
-		virtual void setConformPitches(bool conformPitches);
-		virtual bool getConformPitches() const;
-	};
+  /**
+   * Base class for user-derived musical compositions.
+   * Contains a Score object for collecting generated Events such as notes and control messages,
+   * and an Orchestra object for rendering the generated scores.
+   */
+  class Composition 
+  {
+  protected:
+    Score score;
+    double tonesPerOctave;
+    bool conformPitches;
+    CppSound cppSound_;
+    CppSound *cppSound;
+  public:
+    Composition();
+    virtual ~Composition();
+    /**
+     * Generate performance events and store them in the score. 
+     * Must be overidden in derived classes.
+     */
+    virtual void generate();
+    /**
+     * Translate the generated score to a Csound score and export it for performance.
+     */
+    virtual void createCsoundScore();
+    /**
+     * Convenience function that erases the existing score, 
+     * invokes generate(), and invokes perform().
+     */
+    virtual void render();
+    /**
+     * Uses csound to perform the current score.
+     */
+    virtual void perform();
+    /**
+     * Clear all contents of this. Probably should be overridden in derived classes.
+     */
+    virtual void clear();
+    /**
+     * Return the self-contained Score.
+     */
+    virtual Score &getScore();
+    /**
+     * Sets the self-contained Orchestra.
+     */
+    virtual void setCppSound(CppSound *orchestra);
+    /**
+     * Return the self-contained Orchestra.
+     */
+    virtual CppSound *getCppSound();
+    /**
+     * Write as if to stdout or stderr.
+     */
+    virtual void write(const char *text);
+    virtual void setTonesPerOctave(double tonesPerOctave);
+    virtual double getTonesPerOctave() const;
+    virtual void setConformPitches(bool conformPitches);
+    virtual bool getConformPitches() const;
+  };
 }
 #endif
 
