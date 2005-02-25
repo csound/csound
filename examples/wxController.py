cad5aefb44d918fa270995865ac4c50ad7fedf15
Michael Gogins <gogins@users.sourceforge.net>
2005-02-25 03:40:26 +0000

    Simplified this example. It works at low latencies on Windows, at least, perhaps more.

--- examples/wxController.py
+++ examples/wxController.py
@@ -1,140 +1,96 @@
-# my demo app of a button and slider that sends events to some functions 
-# this one uses the style from the wxPython wiki
-# I would like to embed csound so that the functions can instead make api calls to csound
-
-
-# import wx
-from wxPython.wx import *
-
-# Automatically creates global csound object.
-
+# Importing CsoundVST automatically creates a global 'csound' object.
 import CsoundVST 
-
-ID_BUTTON1 = 10
-ID_SLIDER1 = 20
-
-# Csound MUST run in its own thread
-
+from wxPython.wx import *
+# Csound MUST run in its own thread.
 import threading
 
-def csoundThreadRoutine():
-    # Embed an orchestra in this script.
-    csound.setOrchestra('''
-    sr=44100
-    kr=441
-    ksmps=100
-    nchnls=2
-
-        instr	1
-        print p1, p2, p3, p4, p5
-        ; Slider sends note with MIDI key number.
-        ; Convert it to octaves.
-        ioctave = p4 / 12.0 + 3.0
-        print ioctave
-        ; Convert octave to frequency.
-        ihertz = cpsoct(ioctave)
-    a1	oscili	ampdb(p5), ihertz, 1
-        outs	a1,a1
-        endin
-    ''')
-    # And a score.
-    csound.setScore('''
-    f1 0 8192 10 1
-    f0 60
-    i 1 0 10 60 60
-    e
-    ''')
-
-    # Real-time audio output, don't forget to enable line events.
-    
-    csound.setCommand('''csound -d -Lstdin -odac6 temp.orc temp.sco''')
-
-    # Export the orc and sco.
-
-    csound.exportForPerformance()
-
-    # Start the performance.
-
-    csound.compile()
-    while(True):
-        csound.performKsmps()
-        
-        # Absolutely MUST yield to wxWindows or the app will freeze!
-        
-        wx.wxYield()
-
-# Create the Csound thread, but don't start it just yet.
-
-csoundThread = threading.Thread(None, csoundThreadRoutine)
-csoundThread.start()
-
-# declare my ControlPanel class, inherit from standard wx.Panel
-class ControlPanel( wxPanel ):
-
-	# overide the standard wx.Panel constructor	
+# Create a panel to hold widgets that will control a Csound performance.
+class ControlPanel(wxPanel):
+        # Override the base class constructor.
 	def __init__(self, parent):
+		wxPanel.__init__(self, parent, -1)	
+                # Create a button to send a note to Csound.
+                self.ID_BUTTON1 = 10
+ 		self.button1 = wxButton(self, self.ID_BUTTON1, "Send Note", (20, 20))
+                # Bind the button to its event handler.
+		EVT_BUTTON(self, self.ID_BUTTON1, self.OnClickButton1)
+		# Create a slider to change the pitch of the note.
+		self.ID_SLIDER1 = 20
+		self.slider1 = wxSlider(self, self.ID_SLIDER1, 63, 0, 127, (20, 50), (200,50),
+				   wxSL_HORIZONTAL | wxSL_LABELS)
+        	self.slider1.SetTickFreq(5, 1)
+        	# Bind the slider to its event handler.
+		EVT_SLIDER(self, self.ID_SLIDER1, self.OnSlider1Move)
+		# Default pitch.
+		self.pitch = 60
+                # Create the Csound thread and start it.
+                self.csoundThread = threading.Thread(None, self.csoundThreadRoutine)
+                self.csoundThread.start()
 
-		# call the parent classes constructor to begin with	
-		wxPanel.__init__( self, parent, -1 )	
-		
-		# add a button, event is ID_BUTTON1		
-		button1 = wxButton(self, ID_BUTTON1, "Send Note", (20, 20))
-		EVT_BUTTON(self, ID_BUTTON1, self.OnClickButton1 )
-		
-		# below is method that works in windows but not on my linux version, weird
-		# on linux I get a no method called Bind message
-		# self.Bind(wx.EVT_BUTTON, self.OnClickButton1, button1 )
-	
-	
-		# add a slider that sends an event on any change
-		slider1 = wxSlider(self, ID_SLIDER1, 63, 0, 127, (20, 50), (200,50),
-				   wxSL_HORIZONTAL | wxSL_LABELS )
-        	slider1.SetTickFreq(5, 1)
-		EVT_SLIDER(self, ID_SLIDER1, self.OnSlider1Move )
+        # Initialize Csound and start performing, in a separate thread.
+        # Csound will play notes sent in from wxWindows.
+        def csoundThreadRoutine(self):
+            # Embed an orchestra in this script.
+            csound.setOrchestra('''
+            sr=44100
+            ksmps=400
+            nchnls=2
+                instr	1
+                ; print p1, p2, p3, p4, p5
+                ; Slider sends note with MIDI key number.
+                ; Convert it to octaves.
+                ioctave = p4 / 12.0 + 3.0
+                print ioctave
+                ; Convert octave to frequency.
+                ihertz = cpsoct(ioctave)
+            a1	oscili	ampdb(p5), ihertz, 1
+                kenv linseg 0, p3/2, 1, p3/2, 0
+                a1 = kenv * a1
+                outs	a1,a1
+                endin
+            ''')
+            # And a score.
+            csound.setScore('''
+            f1 0 8192 10 1
+            f0 600
+            e
+            ''')
+            # Real-time audio output.
+            # It is not necessary to enable line events.
+            csound.setCommand('''csound -d -m0 -b800 -B800 -odac1 temp.orc temp.sco''')
+            # Export the orc and sco.
+            csound.exportForPerformance()
+            # Start the performance.
+            csound.compile()
+            # Perform in blocks of ksmps
+            # (Csound will yield implicitly to wxWindows).
+            while True:
+                csound.performKsmps()
 
-	# methods of the ControlPanel class
+        # Handle the button click -- send a note to Csound
+        # with the pitch set by the slider.
 	def OnClickButton1(self, event):
-		# call my external function
-		# send it the current value of the slider
-		button1( self.slider1_val )
+            print "You pressed button 1"
+            # Send a line event.
+            csound.inputMessage("i 1 0 8 %i 70" % self.pitch)
 
+        # Handle the slider movement -- change the pitch.
 	def OnSlider1Move(self, event):
-		slider1()
-		self.slider1_val = event.GetInt()
+            print "You moved the slider to %d" % event.GetInt()
+            self.pitch = event.GetInt()
 
-	# the slider value data member of the ControlPanel
-	slider1_val = 0
-
-
-#--------------------------------------------------------------------------------
-# Functions outside of the control class
-# these would be functions to interface with the csound API
-
-def button1( value ):
-	print "You pressed button 1"
-	print "The value sent is: %i" % value
-	# Send a line event.
-	csound.inputMessage("i 1 0 1 %i 70" % value)
-
-def slider1():
-	print "You moved slider 1"
-	
-	
-
-#--------------------------------------------------------------------------------
-# Main body of app
-
-ctrl_app = wxPySimpleApp()
-
-# start_pos = ( 400,20 )
-# start_size = ( 400,300 )
-# frame = wxFrame( None, -1, "Csound Controller", start_pos, start_size )
-frame = wxFrame( None, -1, "Csound Controller" )
-ctrl_panel = ControlPanel( frame )
+# Create a wx application.
+application = wxPySimpleApp()
+# Create a frame to hold the control panel.
+frame = wxFrame(None, -1, "Csound Controller")
+# Create the control panel as a child of the frame.
+controlPanel = ControlPanel(frame)
+# Display the frame.
 frame.Show(True)
-ctrl_app.MainLoop()
+# Run the application.
+application.MainLoop()
 
-# End of main body
+
 
 
 
