5f81946f44a17ea67e10bacfe101d1128ebe2fa0
Michael Gogins <gogins@users.sourceforge.net>
2005-02-27 20:50:59 +0000

    Simplified and reworked for current SoundFont opcodes and samples/VintageDreams SoundFont.

--- examples/Koch.py
+++ examples/Koch.py
@@ -11,7 +11,15 @@ import CsoundVST
 # will get a smaller copy of the motive nested atop it,
 # and so on.
 
-programsForInstruments = {6:6, 5:5, 4:4, 3:3, 2:2, 1:1}
+gainsForLevels = {}
+gainsForLevels[0] = 0.0
+gainsForLevels[1] = 1.5
+gainsForLevels[2] = 1.0
+gainsForLevels[3] = 1.0
+gainsForLevels[4] = 1.0
+gainsForLevels[5] = 1.0
+gainsForLevels[6] = 1.0
+gainsForLevels[7] = 1.0
 
 def Koch(generator, t, d, c, k, v, level, score):
 	t1 = t
@@ -22,20 +30,12 @@ def Koch(generator, t, d, c, k, v, level, score):
 	pan = 0
 	if level > 0:
 		for i in range(0, generator.__len__(), 4):
-			k1 = 2 + k1 + generator[i]
+			k1 = k + generator[i]
 			v1 = generator[i + 1]
 			d1 = d  * generator[i + 2]
 			d2 = d1 * generator[i + 3]
-			if level == 0:
-				pan = 0
-			elif level == 1:
-				pan = -.5
-			elif level == 2:
-				pan = .5
-			else:
-				pan == 0
-			score.addNote(level, t1, d2, k1 - 12, 70 + v1, programsForInstruments[level], pan)
-			Koch(generator, t1, d1, c1, k1, v1, level - 1, score)
+			score.addNote(level, t1, d2, k1, (70.0 + v1) * gainsForLevels[level], 0, pan)
+			Koch(generator, t1, d1, c1, k1 + 12, v1, level - 1, score)
 			t1 = t1 + d1
 
 # Normalizes the times in a generator.
@@ -58,7 +58,6 @@ g = [  7,  0,  8,  1,
       -4, -5,  8,  1,
       15,  4,  3,  .875,
       -9,  1,  6,  1
-#    -13,  0,  8,  1 
     ]
 
 normalizeGeneratorTimes(g)
@@ -83,77 +82,62 @@ normalizeGeneratorTimes(h)
 
 # Generate events for 3 layers of each generator.
 
-Koch(g, 0, 240, 0, 30, 32, 3, csound)
+Koch(g, 0, 240, 0, 24, 32, 3, csound)
 
 # The second generator makes a canon relative to the first.
 
-Koch(h, 0.5, 240, 0, 37, 32, 3, csound);
+Koch(h, 0.5, 240, 0, 30, 32, 3, csound);
 
 csound.setOrchestra('''
-sr = 44100
-kr = 441
-ksmps = 100   
-nchnls = 2
-0dbfs = 1.0
+sr 			= 			44100
+ksmps 			= 			100 
+nchnls 			= 			2
+0dbfs 			= 			3
 
-; Load a bunch of cool SoundFonts.
+giFluidsynth		fluidEngine
+giFluidGM		fluidLoad		"../samples/VintageDreamsWaves-v2.sf2", giFluidsynth, 1
+			fluidProgramSelect	giFluidsynth, 0, giFluidGM,	 0, 119
+			fluidProgramSelect	giFluidsynth, 1, giFluidGM,	 0,   8
+			fluidProgramSelect	giFluidsynth, 2, giFluidGM,	 0,  12
+			fluidProgramSelect	giFluidsynth, 3, giFluidGM,	 0,   0
+			fluidProgramSelect	giFluidsynth, 4, giFluidGM,	 0,  15
+			fluidProgramSelect	giFluidsynth, 5, giFluidGM,	 0,  15
+			fluidProgramSelect	giFluidsynth, 6, giFluidGM,	 0,  15
 
-			fluidload		"C:/tools/SoundFonts/SoundSite CD6/Piano Steinway Grand Model C (21,738KB).sf2", 1, 1, 1
-			fluidload		"C:/tools/SoundFonts/SoundSite CD1/Bonus/63.3mg The Sound Site Album Bank V1.0.SF2", 50, 2, 1
-			fluidload		"C:/tools/SoundFonts/SoundSite CD6/Piano Steinway Grand Model C (21,738KB).sf2", 2, 3, 1
-			fluidload		"C:/tools/SoundFonts/SoundSite CD5/Organ JJ's English Chamber (4,418KB).SF2", 154, 4, 1
 
-
-instr 1,3,5 ; FluidSynth Steinway Rev
+instr 1,2,3,4,5,6; FluidSynth
 ; INITIALIZATION
-			;print 			p2, p3, p4, p5, p6, p7, p8, p9, p10, p11
 			mididefault 		60, p3
 			midinoteonkey		p4, p5
 ; Use channel assigned in fluidload.
-ichannel		=			3
+ichannel		=			p1 - 1
 ikey	 		= 			p4
 ivelocity 		= 			p5 
-ijunk6 			= 			p6
-ijunk7			=			p7
-ijunk8			=			p8
-ijunk9			=			p9
-ijunk10			=			p10
-ijunk11			=			p11
+ijunk			=			p6
+ijunk			=			p7
 istatus			=			144
-				fluidcontrol	istatus, ichannel, ikey, ivelocity
-endin
-
-instr 2,4,6 ; FluidSynth full organ
-; INITIALIZATION
-			;print 			p2, p3, p4, p5, p6, p7, p8, p9, p10, p11
-			mididefault 		60, p3
-			midinoteonkey		p4, p5
-; Use channel assigned in fluidload.
-ichannel		=			4
-ikey	 		= 			p4
-ivelocity 		= 			p5 / 4.0
-ijunk6 			= 			p6
-ijunk7			=			p7
-ijunk8			=			p8
-ijunk9			=			p9
-ijunk10			=			p10
-ijunk11			=			p11
-istatus			=			144
-			fluidcontrol	        istatus, ichannel, ikey, ivelocity
+			fluidControl		giFluidsynth, istatus, ichannel, ikey, ivelocity
 endin
 
 instr 100 ; Fluidsynth output
 ; AUDIO
-iamplitude              =                       5.0
-aleft, aright 		fluidout
+iamplitude              =                       0.75
+aleft, aright 		fluidOut		giFluidsynth
 			outs 			aleft * iamplitude, aright * iamplitude
 endin
 ''')
 
-csound.setCommand("csound --opcode-lib=c:/projects/csound5/fluid.dll -RWdfo ./koch1.wav ./temp.orc ./temp.sco")
+#csound.setCommand("csound -b100 -B100 -odac2 ./temp.orc ./temp.sco")
+csound.setCommand("csound -RWdfo koch.wav ./temp.orc ./temp.sco")
 csound.addScoreLine("i 100 0 -1")
 csound.exportForPerformance()
 csound.perform()
+
+
+
+
+
+
 
 
 
