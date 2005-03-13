e0412bf3797e5a206dcfee25f3e91f7f3dc12099
John ffitch <jpff@users.sourceforge.net>
2005-03-13 09:46:18 +0000

    Added tuning gen

--- Opcodes/ftest.c
+++ Opcodes/ftest.c
@@ -12,8 +12,47 @@ void tanhtable(FUNC *ftp, FGDATA *ff)
       *fp++ = (MYFLT)tanh(x);
 }
 
+static void gentune(void)             /* Gab 1/3/2005 */
+{
+    int j;
+    int notenum;
+    int grade;
+    int numgrades;
+    int basekeymidi;
+    MYFLT basefreq, factor,interval;
+
+    MYFLT	*fp = ftp->ftable, *pp = &e->p[5];
+    int	nvals = nargs;
+
+    numgrades = (int) *pp++;
+    interval  = *pp++;
+    basefreq  = *pp++;
+    basekeymidi=  (int) *pp++;
+
+    nvals = flenp1 - 1;
+
+    for (j =0; j < nvals; j++) {
+      notenum = j;
+      if (notenum < basekeymidi) {
+        notenum = basekeymidi - notenum;
+        grade  = (numgrades-(notenum % numgrades)) % numgrades;
+        factor = - (MYFLT)(int) ((notenum+numgrades-1) / numgrades) ;
+      }
+      else {
+        notenum = notenum - basekeymidi;
+        grade  = notenum % numgrades;
+        factor = (MYFLT)(int) (notenum / numgrades);
+      }
+      factor = (MYFLT)pow((double)interval, (double)factor);
+      fp[j] = pp[grade] * factor * basefreq;
+      
+    }
+}
+
+
 static NGFENS localfgens[] = {
    { "tanh", (void(*)(void))tanhtable},
+   { "cpstune", (void(*)(void))gentune},
    { NULL, NULL}
 };
 
