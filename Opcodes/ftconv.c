ace1ff68e83452b3bd69619b15000b9aa85295a6
Istvan Varga <istvanv@users.sourceforge.net>
2005-03-20 12:16:05 +0000

    Changed opcode to use equal sized partitions

--- Opcodes/ftconv.c
+++ Opcodes/ftconv.c
@@ -27,80 +27,129 @@
 #define FTCONV_MAXCHN   8
 
 typedef struct {
-    int     nSamples;   /* IR length (not padded) in samples                */
-    int     cnt;        /* buffer position, counts from 0 to nSamples       */
-    int     nChannels;  /* number of channels (1 to 8)                      */
-    MYFLT   *inBuffer;  /* input/FFT buffer (size = nSamples*2)             */
-    MYFLT   *tmpBuf;    /* temporary buffer for FFT (size = nSamples*2)     */
-    MYFLT   *outBuffers[FTCONV_MAXCHN];
-                        /* output buffer (size = nSamples*2)                */
-    MYFLT   *IR_Data[FTCONV_MAXCHN];
-                        /* FFT of impulse response (size = nSamples*2)      */
-    MYFLT   data[1];
-} CONVOLVE_FFT;
-
-typedef struct {
-    OPDS          h;
-    MYFLT         *aOut[FTCONV_MAXCHN];
-    MYFLT         *aIn;
-    MYFLT         *iFTNum;
-    MYFLT         *iMinLen;
-    MYFLT         *iSkipInit;
+    OPDS    h;
+    MYFLT   *aOut[FTCONV_MAXCHN];
+    MYFLT   *aIn;
+    MYFLT   *iFTNum;
+    MYFLT   *iPartLen;
+    MYFLT   *iSkipSamples;
+    MYFLT   *iTotLen;
+    MYFLT   *iSkipInit;
  /* ------------------------- */
-    int           initDone;
-    int           nChannels;
-    int           nFFTConv;
-    CONVOLVE_FFT  *fftConvolve[32];
-    AUXCH         auxData;
+    int     initDone;
+    int     nChannels;
+    int     cnt;                /* buffer position, 0 to partSize - 1       */
+    int     nPartitions;        /* number of convolve partitions            */
+    int     partSize;           /* partition length in sample frames        */
+    int     rbCnt;              /* ring buffer index, 0 to nPartitions - 1  */
+    MYFLT   *tmpBuf;            /* temporary buffer for accumulating FFTs   */
+    MYFLT   *ringBuf;           /* ring buffer of FFTs of input partitions  */
+    MYFLT   *IR_Data[FTCONV_MAXCHN];    /* impulse responses (scaled)       */
+    MYFLT   *outBuffers[FTCONV_MAXCHN]; /* output buffer (size=partSize*2)  */
+    AUXCH   auxData;
 } FTCONV;
 
-static int fft_convolve_bytes_alloc(int nChannels, int nSamples)
+static void multiply_fft_buffers(MYFLT *outBuf, MYFLT *ringBuf,
+                                 MYFLT *IR_Data, int partSize, int nPartitions,
+                                 int ringBuf_startPos)
 {
-    int nBytes;
+    MYFLT   re, im, re1, re2, im1, im2;
+    MYFLT   *rbPtr, *irPtr, *outBufPtr, *outBufEndPm2, *rbEndP;
 
-    nBytes = (nSamples << 1);                   /* inBuffer   */
-    nBytes += (nSamples << 1);                  /* tmpBuf     */
-    nBytes += ((nSamples << 1) * nChannels);    /* outBuffers */
-    nBytes += ((nSamples << 1) * nChannels);    /* IR_Data    */
-    nBytes *= (int) sizeof(MYFLT);
-    nBytes += (int) (sizeof(CONVOLVE_FFT) - sizeof(MYFLT));
-    nBytes = (nBytes + 15) & (~15);
-    return nBytes;
+    /* note: partSize must be at least 2 samples */
+    partSize <<= 1;
+    outBufEndPm2 = (MYFLT*) outBuf + (int) (partSize - 2);
+    rbEndP = (MYFLT*) ringBuf + (int) (partSize * nPartitions);
+    rbPtr = &(ringBuf[ringBuf_startPos]);
+    irPtr = IR_Data;
+    outBufPtr = outBuf;
+    /* clear output buffer to zero */
+    do {
+      *(outBufPtr++) = FL(0.0);
+      *(outBufPtr++) = FL(0.0);
+    } while (outBufPtr <= outBufEndPm2);
+    /* multiply FFTs for each partition, and mix to output buffer */
+    /* note: IRs are stored in reverse partition order */
+    do {
+      /* wrap ring buffer position */
+      if (rbPtr >= rbEndP)
+        rbPtr = ringBuf;
+      outBufPtr = outBuf;
+      *(outBufPtr++) += *(rbPtr++) * *(irPtr++);    /* convolve DC */
+      *(outBufPtr++) += *(rbPtr++) * *(irPtr++);    /* convolve Nyquist */
+      re1 = *(rbPtr++);
+      im1 = *(rbPtr++);
+      re2 = *(irPtr++);
+      im2 = *(irPtr++);
+      re = re1 * re2 - im1 * im2;
+      im = re1 * im2 + re2 * im1;
+      while (outBufPtr < outBufEndPm2) {
+        /* complex multiply */
+        re1 = rbPtr[0];
+        im1 = rbPtr[1];
+        re2 = irPtr[0];
+        im2 = irPtr[1];
+        outBufPtr[0] += re;
+        outBufPtr[1] += im;
+        re = re1 * re2 - im1 * im2;
+        im = re1 * im2 + re2 * im1;
+        re1 = rbPtr[2];
+        im1 = rbPtr[3];
+        re2 = irPtr[2];
+        im2 = irPtr[3];
+        outBufPtr[2] += re;
+        outBufPtr[3] += im;
+        re = re1 * re2 - im1 * im2;
+        im = re1 * im2 + re2 * im1;
+        outBufPtr += 4;
+        rbPtr += 4;
+        irPtr += 4;
+      }
+      outBufPtr[0] += re;
+      outBufPtr[1] += im;
+    } while (--nPartitions);
 }
 
-static void fft_convolve_init(ENVIRON *csound, CONVOLVE_FFT *fp, MYFLT *ftData,
-                              int nChannels, int nSamples, int skipSamples)
+static int buf_bytes_alloc(int nChannels, int partSize, int nPartitions)
 {
-    int i, j, n, nsmps;
+    int nSmps;
 
-    fp->nSamples = nSamples;
-    fp->cnt = 0;
-    fp->nChannels = nChannels;
-    nsmps = 0;
-    fp->inBuffer = &(fp->data[nsmps]); nsmps += (nSamples << 1);
-    fp->tmpBuf = &(fp->data[nsmps]); nsmps += (nSamples << 1);
-    for (n = 0; n < nChannels; n++) {
-      fp->outBuffers[n] = &(fp->data[nsmps]); nsmps += (nSamples << 1);
-      fp->IR_Data[n] = &(fp->data[nsmps]); nsmps += (nSamples << 1);
-      /* clear output buffer */
-      for (i = 0; i < (nSamples << 1); i++)
-        fp->outBuffers[n][i] = FL(0.0);
-      /* calculate FFT of impulse response */
-      j = (skipSamples * nChannels) + n;
-      for (i = 0; i < nSamples; i++) {
-        fp->IR_Data[n][i] = ftData[j];
-        j += nChannels;
-      }
-      for (i = nSamples; i < (nSamples << 1); i++)
-        fp->IR_Data[n][i] = FL(0.0);    /* pad to double length */
-      csound->RealFFT(csound, &(fp->IR_Data[n][0]), (nSamples << 1));
+    nSmps = (partSize << 1);                                /* tmpBuf     */
+    nSmps += ((partSize << 1) * nPartitions);               /* ringBuf    */
+    nSmps += ((partSize << 1) * nChannels * nPartitions);   /* IR_Data    */
+    nSmps += ((partSize << 1) * nChannels);                 /* outBuffers */
+
+    return ((int) sizeof(MYFLT) * nSmps);
+}
+
+static void set_buf_pointers(FTCONV *p,
+                             int nChannels, int partSize, int nPartitions)
+{
+    MYFLT *ptr;
+    int   i;
+
+    ptr = (MYFLT*) (p->auxData.auxp);
+    p->tmpBuf = ptr;
+    ptr += (partSize << 1);
+    p->ringBuf = ptr;
+    ptr += ((partSize << 1) * nPartitions);
+    for (i = 0; i < nChannels; i++) {
+      p->IR_Data[i] = ptr;
+      ptr += ((partSize << 1) * nPartitions);
+    }
+    for (i = 0; i < nChannels; i++) {
+      p->outBuffers[i] = ptr;
+      ptr += (partSize << 1);
     }
 }
+
+#define RNDINT(x) ((int) ((double) (x) + ((double) (x) < 0.0 ? -0.5 : 0.5)))
 
 static int ftconv_init(ENVIRON *csound, FTCONV *p)
 {
     FUNC    *ftp;
-    int     i, n, nBytes, minFFTlen, maxFFTlen;
+    int     i, j, k, n, nBytes, skipSamples;
+    MYFLT   FFTscale;
 
     /* check parameters */
     p->nChannels = (int) p->OUTOCOUNT;
@@ -108,41 +157,39 @@ static int ftconv_init(ENVIRON *csound, FTCONV *p)
       initerror(Str("ftconv: invalid number of channels"));
       return NOTOK;
     }
-    /* minFFTlen: length of shortest FFT convolve unit */
-    /* maxFFTlen: total length = length of longest FFT convolve unit * 2 */
-    minFFTlen = (int) (*(p->iMinLen) + FL(0.5));
-    if (minFFTlen < 1 || (minFFTlen & (minFFTlen - 1)) != 0) {
-      initerror(Str("ftconv: invalid number of samples to skip"));
+    /* partition length */
+    p->partSize = RNDINT(*(p->iPartLen));
+    if (p->partSize < 4 || (p->partSize & (p->partSize - 1)) != 0) {
+      initerror(Str("ftconv: invalid impulse response partition length"));
       return NOTOK;
     }
     ftp = csound->ftfind_(csound, p->iFTNum);
     if (ftp == NULL)
       return NOTOK; /* ftfind should already have printed the error message */
-    maxFFTlen = (int) ftp->flen / p->nChannels;
-    /* round down to the nearest power of two and divide by two to get */
-    /* the length of the longest impulse response partition */
-    for (i = 1; i <= maxFFTlen; i <<= 1);
-    maxFFTlen = i >> 2;
-    if (maxFFTlen < minFFTlen) {
-      initerror(Str("ftconv: insufficient data in table for convolution"));
+    /* calculate total length / number of partitions */
+    n = (int) ftp->flen / p->nChannels;
+    skipSamples = RNDINT(*(p->iSkipSamples));
+    n -= skipSamples;
+    if (RNDINT(*(p->iTotLen)) > 0 && n > RNDINT(*(p->iTotLen)))
+      n = RNDINT(*(p->iTotLen));
+    if (n <= 0) {
+      initerror(Str("ftconv: invalid length, "
+                    "or insufficient IR data for convolution"));
       return NOTOK;
     }
-    p->nFFTConv = 0;
-    for (i = minFFTlen; i <= maxFFTlen; i <<= 1)
-      p->nFFTConv++;    /* number of parallel FFT convolve units */
+    p->nPartitions = (n + (p->partSize - 1)) / p->partSize;
     /* calculate the amount of aux space to allocate (in bytes) */
-    nBytes = 0;
-    n = minFFTlen;
-    for (i = 0; i < p->nFFTConv; i++) {
-      nBytes += fft_convolve_bytes_alloc(p->nChannels, n);
-      n <<= 1;
-    }
+    nBytes = buf_bytes_alloc(p->nChannels, p->partSize, p->nPartitions);
     if (nBytes != (int) p->auxData.size)
       csound->auxalloc_(csound, (long) nBytes, &(p->auxData));
     else if (p->initDone > 0 && *(p->iSkipInit) != FL(0.0))
       return OK;    /* skip initialisation if requested */
-    if (csound->oparms_->msglevel & 4) {
-      for (i = 0; i < (minFFTlen * p->nChannels); i++) {
+    /* if skipping samples: check for possible truncation of IR */
+    if (skipSamples > 0 && (csound->oparms_->msglevel & 4)) {
+      n = skipSamples * p->nChannels;
+      if (n > (int) ftp->flen)
+        n = (int) ftp->flen;
+      for (i = 0; i < n; i++) {
         if (ftp->ftable[i] != FL(0.0)) {
           csound->Message(csound,
                           Str("ftconv: warning: skipped non-zero samples, "
@@ -151,16 +198,41 @@ static int ftconv_init(ENVIRON *csound, FTCONV *p)
         }
       }
     }
-    /* set up all FFT convolve units */
-    nBytes = 0;
-    n = minFFTlen;
-    for (i = 0; i < p->nFFTConv; i++) {
-      p->fftConvolve[i] = (CONVOLVE_FFT*) ((unsigned char*) (p->auxData.auxp)
-                                           + (int) nBytes);
-      nBytes += fft_convolve_bytes_alloc(p->nChannels, n);
-      fft_convolve_init(csound, p->fftConvolve[i], &(ftp->ftable[0]),
-                        p->nChannels, n, n);
-      n <<= 1;
+    /* initialise buffer pointers */
+    set_buf_pointers(p, p->nChannels, p->partSize, p->nPartitions);
+    /* clear ring buffer to zero */
+    n = (p->partSize << 1) * p->nPartitions;
+    for (i = 0; i < n; i++)
+      p->ringBuf[i] = FL(0.0);
+    /* initialise buffer index */
+    p->cnt = 0;
+    p->rbCnt = 0;
+    /* calculate FFT of impulse response partitions, in reverse order */
+    /* also apply FFT amplitude scale here */
+    FFTscale = csound->GetInverseRealFFTScale(csound, (p->partSize << 1));
+    for (j = 0; j < p->nChannels; j++) {
+      i = (skipSamples * p->nChannels) + j;           /* table read position */
+      n = (p->partSize << 1) * (p->nPartitions - 1);  /* IR write position */
+      do {
+        for (k = 0; k < p->partSize; k++) {
+          if (i >= 0 && i < (int) ftp->flen)
+            p->IR_Data[j][n + k] = ftp->ftable[i] * FFTscale;
+          else
+            p->IR_Data[j][n + k] = FL(0.0);
+          i += p->nChannels;
+        }
+        /* pad second half of IR to zero */
+        for (k = p->partSize; k < (p->partSize << 1); k++)
+          p->IR_Data[j][n + k] = FL(0.0);
+        /* calculate FFT */
+        csound->RealFFT(csound, &(p->IR_Data[j][n]), (p->partSize << 1));
+        n -= (p->partSize << 1);
+      } while (n >= 0);
+    }
+    /* clear output buffers to zero */
+    for (j = 0; j < p->nChannels; j++) {
+      for (i = 0; i < (p->partSize << 1); i++)
+        p->outBuffers[j][i] = FL(0.0);
     }
     p->initDone = 1;
 
@@ -169,56 +241,49 @@ static int ftconv_init(ENVIRON *csound, FTCONV *p)
 
 static int ftconv_perf(ENVIRON *csound, FTCONV *p)
 {
-    CONVOLVE_FFT  *fp;
-    MYFLT         inSig, *x, *x1, *x2;
-    int           i, j, n, nn, nSamples;
+    MYFLT         *x, *rBuf;
+    int           i, n, nn, nSamples, rBufPos;
 
     if (p->initDone <= 0) {
       perferror(Str("ftconv: not initialised"));
       return NOTOK;
     }
+    nSamples = p->partSize;
+    rBuf = &(p->ringBuf[p->rbCnt * (nSamples << 1)]);
     for (nn = 0; nn < csound->ksmps_; nn++) {
-      /* get input signal */
-      inSig = p->aIn[nn];
-      /* clear output to zero */
+      /* store input signal in buffer */
+      rBuf[p->cnt] = p->aIn[nn];
+      /* copy output signals from buffer */
       for (n = 0; n < p->nChannels; n++)
-        p->aOut[n][nn] = FL(0.0);
-      /* -------- FFT convolution -------- */
-      for (j = 0; j < p->nFFTConv; j++) {
-        fp = p->fftConvolve[j];
-        nSamples = fp->nSamples;
-        /* is input buffer full ? */
-        if (fp->cnt >= nSamples) {
-          x = &(fp->tmpBuf[0]);
-          x1 = &(fp->inBuffer[0]);
-          /* yes, calculate FFT */
-          for (i = nSamples; i < (nSamples << 1); i++)
-            x1[i] = FL(0.0);        /* pad to double length */
-          csound->RealFFT(csound, x1, (nSamples << 1));
-          /* for each channel: */
-          for (n = 0; n < fp->nChannels; n++) {
-            /* multiply complex arrays */
-            x2 = &(fp->IR_Data[n][0]);
-            csound->RealFFTMult(csound, x, x1, x2, (nSamples << 1),
-                                csound->GetInverseRealFFTScale(csound,
-                                                               nSamples << 1));
-            /* inverse FFT */
-            csound->InverseRealFFT(csound, x, (nSamples << 1));
-            /* copy to output buffer, overlap with "tail" of previous block */
-            x2 = &(fp->outBuffers[n][0]);
-            for (i = 0; i < nSamples; i++) {
-              x2[i] = x[i] + x2[i + nSamples];
-              x2[i + nSamples] = x[i + nSamples];
-            }
-          }
-          /* start again from beginning of buffer */
-          fp->cnt = 0;
+        p->aOut[n][nn] = p->outBuffers[n][p->cnt];
+      /* is input buffer full ? */
+      if (++p->cnt < nSamples)
+        continue;                   /* no, continue with next sample */
+      /* reset buffer position */
+      p->cnt = 0;
+      /* calculate FFT of input */
+      for (i = nSamples; i < (nSamples << 1); i++)
+        rBuf[i] = FL(0.0);          /* pad to double length */
+      csound->RealFFT(csound, rBuf, (nSamples << 1));
+      /* update ring buffer position */
+      p->rbCnt++;
+      if (p->rbCnt >= p->nPartitions)
+        p->rbCnt = 0;
+      rBufPos = p->rbCnt * (nSamples << 1);
+      rBuf = &(p->ringBuf[rBufPos]);
+      /* for each channel: */
+      for (n = 0; n < p->nChannels; n++) {
+        /* multiply complex arrays */
+        multiply_fft_buffers(p->tmpBuf, p->ringBuf, p->IR_Data[n],
+                             nSamples, p->nPartitions, rBufPos);
+        /* inverse FFT */
+        csound->InverseRealFFT(csound, p->tmpBuf, (nSamples << 1));
+        /* copy to output buffer, overlap with "tail" of previous block */
+        x = &(p->outBuffers[n][0]);
+        for (i = 0; i < nSamples; i++) {
+          x[i] = p->tmpBuf[i] + x[i + nSamples];
+          x[i + nSamples] = p->tmpBuf[i + nSamples];
         }
-        /* copy output signal on all channels */
-        for (n = 0; n < fp->nChannels; n++)
-          p->aOut[n][nn] += fp->outBuffers[n][fp->cnt];
-        /* store input signal in buffer */
-        fp->inBuffer[fp->cnt++] = inSig;
       }
     }
     return OK;
@@ -236,7 +301,7 @@ int csoundModuleInit(void *csound_)
     ENVIRON *csound;
     csound = (ENVIRON*) csound_;
     return (csound->AppendOpcode(csound, "ftconv",
-                                 (int) sizeof(FTCONV), 5, "mmmmmmmm", "aiio",
+                                 (int) sizeof(FTCONV), 5, "mmmmmmmm", "aiiooo",
                                  (int (*)(void*, void*)) ftconv_init,
                                  (int (*)(void*, void*)) NULL,
                                  (int (*)(void*, void*)) ftconv_perf,
