1f26fc6cc22724bf86f81d4a1b5bfeb3a5f9e843
Istvan Varga <istvanv@users.sourceforge.net>
2005-04-15 13:51:02 +0000

    Fixed incorrect definitions of opcode_size() and opcode_init()

--- Opcodes/fluidOpcodes/fluidOpcodes.cpp
+++ Opcodes/fluidOpcodes/fluidOpcodes.cpp
@@ -648,7 +648,7 @@ extern "C"
    * Called by Csound to obtain the size of
    * the table of OENTRY structures defined in this shared library.
    */
-  PUBLIC int opcode_size()
+  PUBLIC long opcode_size(void)
   {
     return sizeof(OENTRY) * 9;
   }
