be5e75be5579fc22bc88481553063f71c78ac935
Michael Gogins <gogins@users.sourceforge.net>
2005-05-28 17:01:38 +0000

    Changed to use new deinit callback, removed deinit OENTRY member.

--- H/OpcodeBase.hpp
+++ H/OpcodeBase.hpp
@@ -4,94 +4,98 @@
 #include <cstdarg>
 
 /**
-* Template base class, or pseudo-virtual base class,
-* for writing Csound opcodes in C++.
-* Derive opcode implementation classes like this:
-*
-* DerivedClass : public OpcodeBase<DerivedClass>
-* {
-* public:
-*     // All output fields must be declared first as MYFLT *:
-*     MYFLT *aret1;
-*     // All input fields must be declared next as MYFLT *:
-*     MYFLT *iarg1;
-*     MYFLT *karg2;
-*     MYFLT *aarg3;
-*     // All internal state variables must be declared after that:
-*     size_t state1;
-*     double state2;
-*     MYFLT state3;
-*     // Declare and implement only whichever of these are required:
-*     void initialize();
-*     void kontrol();
-*     void audio;
-*     void deinitialize();
-* };
-*/
+ * Template base class, or pseudo-virtual base class,
+ * for writing Csound opcodes in C++.
+ * Derive opcode implementation classes like this:
+ *
+ * DerivedClass : public OpcodeBase<DerivedClass>
+ * {
+ * public:
+ *     // All output fields must be declared first as MYFLT *:
+ *     MYFLT *aret1;
+ *     // All input fields must be declared next as MYFLT *:
+ *     MYFLT *iarg1;
+ *     MYFLT *karg2;
+ *     MYFLT *aarg3;
+ *     // All internal state variables must be declared after that:
+ *     size_t state1;
+ *     double state2;
+ *     MYFLT state3;
+ *     // Declare and implement only whichever of these are required:
+ *     void init();
+ *     void kontrol();
+ *     void audio;
+ *     void noteoff();
+ *     void deinit();
+ * };
+ */
 template<typename T>
 class OpcodeBase
 {
 public:
-    int init(ENVIRON *csound)
-    {
-        return NOTOK;
+  int init(ENVIRON *csound)
+  {
+    return NOTOK;
+  }
+  static int init_(void *csound_, void *opcode_)
+  {
+    ENVIRON *csound = reinterpret_cast<ENVIRON *>(csound_);
+    T *opcode = reinterpret_cast<T *>(opcode_);
+    csound->RegisterDeinitCallback(csound, &opcode->h, OpcodeBase<T>::noteoff_);
+    return opcode->init(csound);
+  }
+  int kontrol(ENVIRON *csound)
+  {
+    return NOTOK;
+  }
+  static int kontrol_(void *csound, void *opcode)
+  {
+    return reinterpret_cast<T *>(opcode)->kontrol(reinterpret_cast<ENVIRON *>(csound));
+  }
+  int audio(ENVIRON *csound)
+  {
+    return NOTOK;
+  }
+  static int audio_(void *csound, void *opcode)
+  {
+    return reinterpret_cast<T *>(opcode)->audio(reinterpret_cast<ENVIRON *>(csound));
+  }
+  int noteoff(ENVIRON *csound)
+  {
+    return OK;
+  }
+  static int noteoff_(void *csound, void *opcode)
+  {
+    return reinterpret_cast< OpcodeBase<T> *>(opcode)->noteoff(reinterpret_cast<ENVIRON *>(csound));
+  }
+  void log(ENVIRON *csound, const char *format,...)
+  {
+    va_list args;
+    va_start(args, format);
+    if(csound) {
+      csound->MessageV(csound, 0, format, args);
     }
-    static int init_(void *csound, void *opcode)
-    {
-        return reinterpret_cast<T *>(opcode)->init(reinterpret_cast<ENVIRON *>(csound));
+    else {
+      vfprintf(stdout, format, args);
     }
-    int kontrol(ENVIRON *csound)
-    {
-        return NOTOK;
-    }
-    static int kontrol_(void *csound, void *opcode)
-    {
-        return reinterpret_cast<T *>(opcode)->kontrol(reinterpret_cast<ENVIRON *>(csound));
-    }
-    int audio(ENVIRON *csound)
-    {
-        return NOTOK;
-    }
-    static int audio_(void *csound, void *opcode)
-    {
-        return reinterpret_cast<T *>(opcode)->audio(reinterpret_cast<ENVIRON *>(csound));
-    }
-    int deinit(void *csound)
-    {
-        return NOTOK;
-    }
-    static int deinit_(void *csound, void *opcode)
-    {
-        return reinterpret_cast<T *>(opcode)->deinit(reinterpret_cast<ENVIRON *>(csound));
-    }
-    void log(ENVIRON *csound, const char *format,...)
-    {
-      va_list args;
-      va_start(args, format);
-      if(csound) {
-            csound->MessageV(csound, 0, format, args);
+    va_end(args);
+  }
+  void warn(ENVIRON *csound, const char *format,...)
+  {
+    va_list args;
+    va_start(args, format);
+    if(csound) {
+      if(csound->GetMessageLevel(csound) & WARNMSG ||
+	 csound->GetDebug(csound)) {
+	csound->MessageV(csound, 0, format, args);
       }
-      else {
-            vfprintf(stdout, format, args);
-      }
-      va_end(args);
     }
-    void warn(ENVIRON *csound, const char *format,...)
-    {
-      va_list args;
-      va_start(args, format);
-      if(csound) {
-          if(csound->GetMessageLevel(csound) & WARNMSG ||
-             csound->GetDebug(csound)) {
-              csound->MessageV(csound, 0, format, args);
-          }
-      }
-      else {
-            vfprintf(stdout, format, args);
-      }
-      va_end(args);
+    else {
+      vfprintf(stdout, format, args);
     }
-    OPDS h;
+    va_end(args);
+  }
+  OPDS h;
 };
 
 #endif
