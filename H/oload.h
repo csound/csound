b45482d67d4de0e0efbf79969613c0fef69157c3
Istvan Varga <istvanv@users.sourceforge.net>
2005-05-31 13:14:03 +0000

    Fixed crash on 64 bits

--- H/oload.h
+++ H/oload.h
@@ -21,77 +21,35 @@
     02111-1307 USA
 */
 
+#ifndef CSOUND_OLOAD_H
+#define CSOUND_OLOAD_H
+
 #define NCONSTS 400    /* gbl */                /*      OLOAD.H         */
-#define LNAMES  400    /* lcl */
-#define GNAMES  100    /* gbl */
 #define NLABELS 5      /* lcl */
 #define NGOTOS  40
 
 #define LABELOFS    (-0x40000000)
 #define LABELIM     (-0x38000000)
-#ifndef MIN_SHORT
-#define MIN_SHORT (-32768)
-#endif
+#define STR_OFS      (0x78000000)       /* string constant index base */
 
-#define STR_OFS 0x78000000      /* string constant index base */
-#define VMAGIC  0x56444154      /* "VDAT" */
-
-typedef struct  {
-        char    *namep;
-        int     type, count;
-} NAME;
-
-typedef struct  {
+typedef struct {
         char    *lbltxt;
         int     *ndxp;
 } LBLARG;
 
-typedef struct  {
+typedef struct {
         int     lblno;
         MYFLT   **argpp;
 } LARGNO;
 
-typedef struct  {
-        MYFLT   *ndx, *string;
-} STRNG;
-
-typedef struct  {
+typedef struct {
         MYFLT   *sets[PMAX];
 } PVSET;
 
-typedef struct  {
-        MYFLT   *dimen;
-} VDIMEN;
-
-typedef struct  {
-        OPDS    h;
-        MYFLT   *pgm, *vals[32];
-} PGM_INIT;
-
-typedef struct inx {
-        struct inx *nxtinx;
-        int ctrlno;
-        int inscnt;
-        int inslst[1];
-} INX;
-
 typedef struct {
-        OPDS   h;
-        MYFLT  *insno, *itime;
+        OPDS    h;
+        MYFLT   *insno, *itime;
 } TURNON;
 
-typedef struct  pgmbnk {
-        struct  pgmbnk *nxtbnk;
-        long    bankno;
-        MYFLT   *vpgdat;
-        MYFLT   *spldat;
-} PGMBNK;
-
-typedef struct  {
-        INSTRTXT *instxt;
-        PGMBNK  *banks;
-} VPGLST;
-
-PGMBNK *getchnbnk(MCHNBLK*);
-MYFLT *getkeyparms(MCHNBLK*,int);
+#endif  /* CSOUND_OLOAD_H */
 
