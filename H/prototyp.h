2b4981ef2d0e790d6a03aa427182ac082860838b
Istvan Varga <istvanv@users.sourceforge.net>
2005-09-01 22:00:31 +0000

    Minor code changes

--- H/prototyp.h
+++ H/prototyp.h
@@ -1,7 +1,7 @@
 /*
     prototyp.h:
 
-    Copyright (C) 1991 Barry Vercoe, John ffitch
+    Copyright (C) 1991-2005 Barry Vercoe, John ffitch
 
     This file is part of Csound.
 
@@ -83,203 +83,195 @@ void    *SAsndgetset(CSOUND *, char *, void *, MYFLT *, MYFLT *, MYFLT *, int);
 int     getsndin(CSOUND *, void *, MYFLT *, int, void *);
 void    *sndgetset(CSOUND *, void *);
 void    dbfs_init(CSOUND *, MYFLT dbfs);
+int     csoundLoadExternal(CSOUND *, const char *libraryPath);
+int     csoundLoadExternals(CSOUND *);
 SNDMEMFILE  *csoundLoadSoundFile(CSOUND *, const char *name, SF_INFO *sfinfo);
-int         PVOCEX_LoadFile(CSOUND *, const char *fname, PVOCEX_MEMFILE *p);
+int     PVOCEX_LoadFile(CSOUND *, const char *fname, PVOCEX_MEMFILE *p);
 
-  /**
-   * Register a function to be called at note deactivation.
-   * Should be called from the initialisation routine of an opcode.
-   * 'p' is a pointer to the OPDS structure of the opcode, and 'func'
-   * is the function to be called, with the same arguments and return
-   * value as in the case of opcode init/perf functions.
-   * The functions are called in reverse order of registration.
-   * Returns zero on success.
-   */
-  int csoundRegisterDeinitCallback(CSOUND *, void *p,
-                                   int (*func)(CSOUND *, void *));
+/**
+ * Register a function to be called at note deactivation.
+ * Should be called from the initialisation routine of an opcode.
+ * 'p' is a pointer to the OPDS structure of the opcode, and 'func'
+ * is the function to be called, with the same arguments and return
+ * value as in the case of opcode init/perf functions.
+ * The functions are called in reverse order of registration.
+ * Returns zero on success.
+ */
+int csoundRegisterDeinitCallback(CSOUND *, void *p,
+                                 int (*func)(CSOUND *, void *));
 
-  /**
-   * Register a function to be called by csoundReset(), in reverse order
-   * of registration, before unloading external modules. The function takes
-   * the Csound instance pointer as the first argument, and the pointer
-   * passed here as 'userData' as the second, and is expected to return zero
-   * on success.
-   * The return value of csoundRegisterResetCallback() is zero on success.
-   */
-  int csoundRegisterResetCallback(CSOUND *, void *userData,
-                                  int (*func)(CSOUND *, void *));
+/**
+ * Register a function to be called by csoundReset(), in reverse order
+ * of registration, before unloading external modules. The function takes
+ * the Csound instance pointer as the first argument, and the pointer
+ * passed here as 'userData' as the second, and is expected to return zero
+ * on success.
+ * The return value of csoundRegisterResetCallback() is zero on success.
+ */
+int csoundRegisterResetCallback(CSOUND *, void *userData,
+                                int (*func)(CSOUND *, void *));
 
-  /**
-   * Returns the name of the opcode of which the data structure
-   * is pointed to by 'p'.
-   */
-  char *csoundGetOpcodeName(void *p);
+/**
+ * Returns the name of the opcode of which the data structure
+ * is pointed to by 'p'.
+ */
+char *csoundGetOpcodeName(void *p);
 
-  /**
-   * Returns the number of input arguments for opcode 'p'.
-   */
-  int csoundGetInputArgCnt(void *p);
+/**
+ * Returns the number of input arguments for opcode 'p'.
+ */
+int csoundGetInputArgCnt(void *p);
 
-  /**
-   * Returns a binary value of which bit 0 is set if the first input
-   * argument is a-rate, bit 1 is set if the second input argument is
-   * a-rate, and so on.
-   * Only the first 31 arguments are guaranteed to be reported correctly.
-   */
-  unsigned long csoundGetInputArgAMask(void *p);
+/**
+ * Returns a binary value of which bit 0 is set if the first input
+ * argument is a-rate, bit 1 is set if the second input argument is
+ * a-rate, and so on.
+ * Only the first 31 arguments are guaranteed to be reported correctly.
+ */
+unsigned long csoundGetInputArgAMask(void *p);
 
-  /**
-   * Returns a binary value of which bit 0 is set if the first input
-   * argument is a string, bit 1 is set if the second input argument is
-   * a string, and so on.
-   * Only the first 31 arguments are guaranteed to be reported correctly.
-   */
-  unsigned long csoundGetInputArgSMask(void *p);
+/**
+ * Returns a binary value of which bit 0 is set if the first input
+ * argument is a string, bit 1 is set if the second input argument is
+ * a string, and so on.
+ * Only the first 31 arguments are guaranteed to be reported correctly.
+ */
+unsigned long csoundGetInputArgSMask(void *p);
 
-  /**
-   * Returns the name of input argument 'n' (counting from 0) for opcode 'p'.
-   */
-  char *csoundGetInputArgName(void *p, int n);
+/**
+ * Returns the name of input argument 'n' (counting from 0) for opcode 'p'.
+ */
+char *csoundGetInputArgName(void *p, int n);
 
-  /**
-   * Returns the number of output arguments for opcode 'p'.
-   */
-  int csoundGetOutputArgCnt(void *p);
+/**
+ * Returns the number of output arguments for opcode 'p'.
+ */
+int csoundGetOutputArgCnt(void *p);
 
-  /**
-   * Returns a binary value of which bit 0 is set if the first output
-   * argument is a-rate, bit 1 is set if the second output argument is
-   * a-rate, and so on.
-   * Only the first 31 arguments are guaranteed to be reported correctly.
-   */
-  unsigned long csoundGetOutputArgAMask(void *p);
+/**
+ * Returns a binary value of which bit 0 is set if the first output
+ * argument is a-rate, bit 1 is set if the second output argument is
+ * a-rate, and so on.
+ * Only the first 31 arguments are guaranteed to be reported correctly.
+ */
+unsigned long csoundGetOutputArgAMask(void *p);
 
-  /**
-   * Returns a binary value of which bit 0 is set if the first output
-   * argument is a string, bit 1 is set if the second output argument is
-   * a string, and so on.
-   * Only the first 31 arguments are guaranteed to be reported correctly.
-   */
-  unsigned long csoundGetOutputArgSMask(void *p);
+/**
+ * Returns a binary value of which bit 0 is set if the first output
+ * argument is a string, bit 1 is set if the second output argument is
+ * a string, and so on.
+ * Only the first 31 arguments are guaranteed to be reported correctly.
+ */
+unsigned long csoundGetOutputArgSMask(void *p);
 
-  /**
-   * Returns the name of output argument 'n' (counting from 0) for opcode 'p'.
-   */
-  char *csoundGetOutputArgName(void *p, int n);
+/**
+ * Returns the name of output argument 'n' (counting from 0) for opcode 'p'.
+ */
+char *csoundGetOutputArgName(void *p, int n);
 
-  /**
-   * Set release time in control periods (1 / csound->ekr second units)
-   * for opcode 'p' to 'n'. If the current release time is longer than
-   * the specified value, it is not changed.
-   * Returns the new release time.
-   */
-  int csoundSetReleaseLength(void *p, int n);
+/**
+ * Set release time in control periods (1 / csound->ekr second units)
+ * for opcode 'p' to 'n'. If the current release time is longer than
+ * the specified value, it is not changed.
+ * Returns the new release time.
+ */
+int csoundSetReleaseLength(void *p, int n);
 
-  /**
-   * Set release time in seconds for opcode 'p' to 'n'.
-   * If the current release time is longer than the specified value,
-   * it is not changed.
-   * Returns the new release time in seconds.
-   */
-  MYFLT csoundSetReleaseLengthSeconds(void *p, MYFLT n);
+/**
+ * Set release time in seconds for opcode 'p' to 'n'.
+ * If the current release time is longer than the specified value,
+ * it is not changed.
+ * Returns the new release time in seconds.
+ */
+MYFLT csoundSetReleaseLengthSeconds(void *p, MYFLT n);
 
-  /**
-   * Returns MIDI channel number (0 to 15) for the instrument instance
-   * that called opcode 'p'.
-   * In the case of score notes, -1 is returned.
-   */
-  int csoundGetMidiChannelNumber(void *p);
+/**
+ * Returns MIDI channel number (0 to 15) for the instrument instance
+ * that called opcode 'p'.
+ * In the case of score notes, -1 is returned.
+ */
+int csoundGetMidiChannelNumber(void *p);
 
-  /**
-   * Returns a pointer to the MIDI channel structure for the instrument
-   * instance that called opcode 'p'.
-   * In the case of score notes, NULL is returned.
-   */
-  MCHNBLK *csoundGetMidiChannel(void *p);
+/**
+ * Returns a pointer to the MIDI channel structure for the instrument
+ * instance that called opcode 'p'.
+ * In the case of score notes, NULL is returned.
+ */
+MCHNBLK *csoundGetMidiChannel(void *p);
 
-  /**
-   * Returns MIDI note number (in the range 0 to 127) for opcode 'p'.
-   * If the opcode was not called from a MIDI activated instrument
-   * instance, the return value is undefined.
-   */
-  int csoundGetMidiNoteNumber(void *p);
+/**
+ * Returns MIDI note number (in the range 0 to 127) for opcode 'p'.
+ * If the opcode was not called from a MIDI activated instrument
+ * instance, the return value is undefined.
+ */
+int csoundGetMidiNoteNumber(void *p);
 
-  /**
-   * Returns MIDI velocity (in the range 0 to 127) for opcode 'p'.
-   * If the opcode was not called from a MIDI activated instrument
-   * instance, the return value is undefined.
-   */
-  int csoundGetMidiVelocity(void *p);
+/**
+ * Returns MIDI velocity (in the range 0 to 127) for opcode 'p'.
+ * If the opcode was not called from a MIDI activated instrument
+ * instance, the return value is undefined.
+ */
+int csoundGetMidiVelocity(void *p);
 
-  /**
-   * Returns non-zero if the current note (owning opcode 'p') is releasing.
-   */
-  int csoundGetReleaseFlag(void *p);
+/**
+ * Returns non-zero if the current note (owning opcode 'p') is releasing.
+ */
+int csoundGetReleaseFlag(void *p);
 
-  /**
-   * Returns the note-off time in seconds (measured from the beginning of
-   * performance) of the current instrument instance, from which opcode 'p'
-   * was called. The return value may be negative if the note has indefinite
-   * duration.
-   */
-  double csoundGetOffTime(void *p);
+/**
+ * Returns the note-off time in seconds (measured from the beginning of
+ * performance) of the current instrument instance, from which opcode 'p'
+ * was called. The return value may be negative if the note has indefinite
+ * duration.
+ */
+double csoundGetOffTime(void *p);
 
-  /**
-   * Returns the array of p-fields passed to the instrument instance
-   * that owns opcode 'p', starting from p0. Only p1, p2, and p3 are
-   * guaranteed to be available. p2 is measured in seconds from the
-   * beginning of the current section.
-   */
-  MYFLT *csoundGetPFields(void *p);
+/**
+ * Returns the array of p-fields passed to the instrument instance
+ * that owns opcode 'p', starting from p0. Only p1, p2, and p3 are
+ * guaranteed to be available. p2 is measured in seconds from the
+ * beginning of the current section.
+ */
+MYFLT *csoundGetPFields(void *p);
 
-  /**
-   * Returns the instrument number (p1) for opcode 'p'.
-   */
-  int csoundGetInstrumentNumber(void *p);
+/**
+ * Returns the instrument number (p1) for opcode 'p'.
+ */
+int csoundGetInstrumentNumber(void *p);
 
-  /**
-   * Returns pointer to a string constant storing an error massage
-   * for error code 'errcode'.
-   */
-  char *csoundExternalMidiErrorString(CSOUND *, int errcode);
+/**
+ * Returns pointer to a string constant storing an error massage
+ * for error code 'errcode'.
+ */
+char *csoundExternalMidiErrorString(CSOUND *, int errcode);
 
-  /**
-   * Appends a list of opcodes implemented by external software to Csound's
-   * internal opcode list. The list should either be terminated with an entry
-   * that has a NULL opname, or the number of entries (> 0) should be specified
-   * in 'n'.
-   * Returns zero on success.
-   */
-  int csoundAppendOpcodes(CSOUND *, const OENTRY *opcodeList, int n);
+/**
+ * Appends a list of opcodes implemented by external software to Csound's
+ * internal opcode list. The list should either be terminated with an entry
+ * that has a NULL opname, or the number of entries (> 0) should be specified
+ * in 'n'.
+ * Returns zero on success.
+ */
+int csoundAppendOpcodes(CSOUND *, const OENTRY *opcodeList, int n);
 
-  /**
-   * Registers all opcodes in the library.
-   */
-  int csoundLoadExternal(CSOUND *, const char *libraryPath);
+/**
+ * Check system events, yielding cpu time for coopertative multitasking, etc.
+ */
+int csoundYield(CSOUND *);
 
-  /**
-   * Registers all opcodes in all libraries in the opcodes directory.
-   */
-  int csoundLoadExternals(CSOUND *);
+/**
+ * Register utility with the specified name.
+ * Returns zero on success.
+ */
+int csoundAddUtility(CSOUND *, const char *name,
+                               int (*UtilFunc)(CSOUND *, int, char **));
 
-  /**
-   * Check system events, yielding cpu time for coopertative multitasking, etc.
-   */
-  int csoundYield(CSOUND *);
-
-  /**
-   * Register utility with the specified name.
-   * Returns zero on success.
-   */
-  int csoundAddUtility(CSOUND *, const char *name,
-                                 int (*UtilFunc)(CSOUND *, int, char **));
-
-  /**
-   * Set description text for the specified utility.
-   * Returns zero on success.
-   */
-  int csoundSetUtilityDescription(CSOUND *, const char *utilName,
-                                            const char *utilDesc);
+/**
+ * Set description text for the specified utility.
+ * Returns zero on success.
+ */
+int csoundSetUtilityDescription(CSOUND *, const char *utilName,
+                                          const char *utilDesc);
 
 #ifdef __cplusplus
 }
