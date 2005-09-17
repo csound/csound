b1c866bc871437bcdae0410fc596085f6247bdce
Istvan Varga <istvanv@users.sourceforge.net>
2005-09-17 21:39:53 +0000

    Minor changes

--- InOut/widgets.cpp
+++ InOut/widgets.cpp
@@ -19,7 +19,7 @@
     License along with Csound; if not, write to the Free Software
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
     02111-1307 USA
-*/ 
+*/
 
 #include <cstdio>
 #include <cstdlib>
