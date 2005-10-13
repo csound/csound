0b1c6ef4014190a370b314bcd1e033d878259311
John ffitch <jpff@users.sourceforge.net>
2005-10-13 17:18:03 +0000

    installer

---
+++ installer/linux/f64.sh
@@ -0,0 +1,6 @@
+cp -upv ../../{linseg,lpanal,brkpt,lpc_export,lpc_import,cs,makecsd,csb64enc,cscore,csound,cvanal,dnoise,mixer,envext,extract,extractor,pvanal,pvlook,het_export,het_import,hetro,scale,scot,scsort,sdif2ad,srconv,tabdes} ../../../CSDIST/binf64
+
+cp -upv ../../li*.so ../../../CSDIST/opcf64
+
+cp -upv ../../libcsound.a ../../libustub.a ../../../CSDIST/libf64
+
