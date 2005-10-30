8fd42ada1c573932b2783c5a3b04374b90a2b9f5
Michael Gogins <gogins@users.sourceforge.net>
2005-10-30 02:27:24 +0000

    Fixes for interfaces, added Java GUI front end in csnd.jar.

--- interfaces/test.lua
+++ interfaces/test.lua
@@ -1,5 +1,10 @@
-my_init=loadlib("c:/utah/home/mkg/projects/csound5/_csnd.dll","Csnd_Init") -- for Unix/Linux
-assert(my_init) -- name sure its not nil
-my_init()       -- call the init fn of the lib
-cs = csnd.csoundCreate(nil)
-print(cs)
+Csnd_Init =loadlib("c:/utah/home/mkg/projects/csound5/_csnd.dll","Csnd_Init") 
+Csnd_Init()
+csound = csnd.CppCsound()
+print("csound = ", csound)
+csound:Compile('-o', 'dac', '../examples/trapped.csd')
+do
+	repeat
+		result = csound:PerformBuffer()
+	until result ~= 0
+end
