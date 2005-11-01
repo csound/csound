bc3e6f045a8cc7c3a7db1f75b9ec6ee2bb868225
Michael Gogins <gogins@users.sourceforge.net>
2005-11-01 04:32:55 +0000

    no message

---
+++ interfaces/test.lisp
@@ -0,0 +1,13 @@
+;; Brief example indicating a minimal use of the Csound API from CLisp.
+;; This file must be run from the csound5 directory,
+;; not the csound5/examples directory, like this:
+;; "clisp examples/test.lisp"
+
+(load "csound.lisp")
+(load "filebuilding.lisp")
+(setq cs (csound::csoundCreate nil))
+(filebuilding::csoundCompileCsd cs "../examples/trapped.csd")
+(setq *keep-playing* t result 0)
+(loop while (and *keep-playing* (eq result 0)) do
+      (setq result (csound::csoundPerformKsmps cs)))
+
