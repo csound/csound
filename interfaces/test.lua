b6fad9d6f5f24e2daa008e8876c16dddcaf1ab23
Michael Gogins <gogins@users.sourceforge.net>
2005-11-01 04:11:54 +0000

    no message

--- interfaces/test.lua
+++ interfaces/test.lua
@@ -1,9 +1,10 @@
 print (_VERSION)
-Csnd_Init =loadlib("c:/utah/home/mkg/projects/csound5/_csnd.dll","Csnd_Init") 
+Csnd_Init = loadlib("_csnd.dll","Csnd_Init")
+print(Csnd_Init)
 Csnd_Init()
-csound = csnd.CppCsound()
+csound = csnd.Csound()
 print("csound = ", csound)
-csound:Compile('-o', 'dac', '../examples/trapped.csd')
+csound:Compile('-o', 'dac', 'examples/trapped.csd')
 do
 	repeat
 		result = csound:PerformBuffer()
