377aaff4ca3caecd8c3ea3f60391f8043c78ad04
John ffitch <jpff@users.sourceforge.net>
2005-11-22 17:35:31 +0000

    Revised installer

--- installer/linux/d32.sh
+++ installer/linux/d32.sh
@@ -1,6 +1,15 @@
-cp -upv ../../{linseg,lpanal,brkpt,lpc_export,lpc_import,cs,makecsd,csb64enc,cscore,csound,cvanal,dnoise,mixer,envext,extract,extractor,pvanal,pvlook,het_export,het_import,hetro,scale,scot,scsort,sdif2ad,srconv,tabdes} ../../../CSDIST/bind
+cp -upv ../../{linseg,lpanal,brkpt,lpc_export,lpc_import,cs,makecsd,csb64enc,cscore,csound,cvanal,dnoise,mixer,envext,extract,extractor,pvanal,pvlook,het_export,het_import,hetro,scale,scot,scsort,sdif2ad,srconv,tabdes,cstclsh,cswish} ../../../CSDIST/linux_d32/bin
 
-cp -upv ../../li*.so ../../../CSDIST/opcd
+cp -upv ../../li*.so ../../../CSDIST/linux_d32/opc
 
-cp -upv ../../libcsound.a  ../../../CSDIST/libd
+cp -upv ../../libcsound.a  ../../../CSDIST/linux_d32/lib
 
+cp -rupv ../../../manual/html ../../../CSDIST/linux_d32/doc
+
+cat > ../../../CSDIST/linux_d32/def.ins <<'EOF'
+Linux Doubles
+/usr/local/bin
+/usr/local/csound
+/usr/local/doc/csound
+/usr/local/lib
+'EOF'
--- installer/linux/d64.sh
+++ installer/linux/d64.sh
@@ -1,6 +1,15 @@
-cp -upv ../../{linseg,lpanal,brkpt,lpc_export,lpc_import,cs,makecsd,csb64enc,cscore,csound,cvanal,dnoise,mixer,envext,extract,extractor,pvanal,pvlook,het_export,het_import,hetro,scale,scot,scsort,sdif2ad,srconv,tabdes} ../../../CSDIST/bind64
+cp -upv ../../{linseg,lpanal,brkpt,lpc_export,lpc_import,cs,makecsd,csb64enc,cscore,csound,cvanal,dnoise,mixer,envext,extract,extractor,pvanal,pvlook,het_export,het_import,hetro,scale,scot,scsort,sdif2ad,srconv,tabdes,cstclsh,cswish} ../../../CSDIST/linux_d64/bin
 
-cp -upv ../../li*.so ../../../CSDIST/opcd64
+cp -upv ../../li*.so ../../../CSDIST/linux_d64/opc
 
-cp -upv ../../libcsound.a ../../../CSDIST/libd64
+cp -upv ../../libcsound.a ../../../CSDIST/linux_d64/lib
 
+cp -rupv ../../../manual/html ../../../CSDIST/linux_d64/doc
+
+cat > ../../../CSDIST/linux_d64/def.ins <<'EOF'
+Linux i86_64 Doubles
+/usr/local/bin
+/usr/local/csound64
+/usr/local/doc/csound
+/usr/local/lib64
+'EOF'
