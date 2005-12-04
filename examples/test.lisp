c474052986ea2766a8200f3b95ce4462f5110909
Michael Gogins <gogins@users.sourceforge.net>
2005-12-04 02:12:31 +0000

    Changed for CFFI version of LISP interface.

--- examples/test.lisp
+++ examples/test.lisp
@@ -1,12 +1,17 @@
-;; Brief example indicating a minimal use of the Csound API from CLisp.
-;; This file must be run from the csound5 directory,
-;; not the csound5/examples directory, like this:
-;; "clisp examples/test.lisp"
+;; Rudimentary demonstration of how to use the Csound API from LISP.
+;; This file assumes that Common Music and CFFI have been installed
+;; for your LISP. You will have to change pathnames appropriately,
+;; and change the cffi:load-foreign-library call to suit your installation.
+;; Author: Michael Gogins
 
-(load "csound.lisp")
-(setq cs (csound::csoundCreate nil))
-(csound::csoundCompileCsd cs "trapped.csd")
+(load "c:/utah/home/mkg/projects/cm/src/cm.lisp")
+(use-system :cffi)
+(cffi:load-foreign-library "c:/utah/home/mkg/projects/csound5/_csnd.dll")
+(load "c:/utah/home/mkg/projects/csound5/interfaces/csound5.lisp")
+(setq cs (csound5::csound-create nil))
+(cffi:with-foreign-string (csd "c:/utah/home/mkg/projects/csound5/examples/trapped.csd")
+			  (csound5::csound-compile-csd cs csd))
 (setq *keep-playing* t result 0)
 (loop while (and *keep-playing* (eq result 0)) do
-      (setq result (csound::csoundPerformKsmps cs)))
+      (setq result (csound5::csound-perform-ksmps-1 cs)))
 
