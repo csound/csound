4c114e92ecbd7b6945ede043f8cccd0ea05f05c9
John ffitch <jpff@users.sourceforge.net>
2005-12-20 11:21:47 +0000

    installer files

--- installer/linux/d32.sh
+++ installer/linux/d32.sh
@@ -1,5 +1,7 @@
 cp -upv ../../{linseg,lpanal,brkpt,lpc_export,lpc_import,cs,makecsd,csb64enc,cscore,csound,cvanal,dnoise,mixer,envext,extract,extractor,pvanal,pvlook,het_export,het_import,hetro,scale,scot,scsort,sdif2ad,srconv,tabdes,cstclsh,cswish} ../../../CSDIST/linux_d32/bin
 
+strip ../../../CSDIST/linux_d32/bin/*
+
 cp -upv ../../li*.so ../../../CSDIST/linux_d32/opc
 
 cp -upv ../../libcsound.a  ../../../CSDIST/linux_d32/lib
@@ -13,4 +15,5 @@ Linux Doubles
 /usr/local/doc/csound
 /usr/local/lib
 OPCODEDIR64
+libcsound64.a
 'EOF'
