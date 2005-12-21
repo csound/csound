cafc479fb3947653470598f6f90c2bf5a709ac2a
Michael Gogins <gogins@users.sourceforge.net>
2005-12-21 04:22:48 +0000

    Fixed up to current state of the API.

--- examples/wxController.py
+++ examples/wxController.py
@@ -8,32 +8,33 @@ import threading
 class ControlPanel(wxPanel):
         # Override the base class constructor.
         def __init__(self, parent):
-                wxPanel.__init__(self, parent, -1)
-                # Create a button to send a note to Csound.
-                self.ID_BUTTON1 = 10
-                self.button1 = wxButton(self, self.ID_BUTTON1, "Send Note", (20, 20))
-                # Bind the button to its event handler.
-                EVT_BUTTON(self, self.ID_BUTTON1, self.OnClickButton1)
-                # Create a slider to change the pitch of the note.
-                self.ID_SLIDER1 = 20
-                self.slider1 = wxSlider(self, self.ID_SLIDER1, 63, 0, 127, (20, 50), (200,50),
-                                   wxSL_HORIZONTAL | wxSL_LABELS)
-                self.slider1.SetTickFreq(5, 1)
-                # Bind the slider to its event handler.
-                EVT_SLIDER(self, self.ID_SLIDER1, self.OnSlider1Move)
-                # Default pitch.
-                self.pitch = 60
-                # Create the Csound thread and start it.
-                self.csoundThread = threading.Thread(None, self.csoundThreadRoutine)
-                self.csoundThread.start()
+            wxPanel.__init__(self, parent, -1)
+            # Create a button to send a note to Csound.
+            self.ID_BUTTON1 = 10
+            self.button1 = wxButton(self, self.ID_BUTTON1, "Send Note", (20, 20))
+            # Bind the button to its event handler.
+            EVT_BUTTON(self, self.ID_BUTTON1, self.OnClickButton1)
+            # Create a slider to change the pitch of the note.
+            self.ID_SLIDER1 = 20
+            self.slider1 = wxSlider(self, self.ID_SLIDER1, 63, 0, 127, (20, 50), (200,50),
+                               wxSL_HORIZONTAL | wxSL_LABELS)
+            self.slider1.SetTickFreq(5, 1)
+            # Bind the slider to its event handler.
+            EVT_SLIDER(self, self.ID_SLIDER1, self.OnSlider1Move)
+            # Default pitch.
+            self.pitch = 60
+            # Create the Csound thread and start it.
+            self.csoundThread = threading.Thread(None, self.csoundThreadRoutine)
+            self.csoundThread.start()
 
         # Initialize Csound and start performing, in a separate thread.
         # Csound will play notes sent in from wxWindows.
         def csoundThreadRoutine(self):
             # Embed an orchestra in this script.
+            csound.setPythonMessageCallback()
             csound.setOrchestra('''
             sr=44100
-            ksmps=400
+            ksmps=128
             nchnls=2
                 instr   1
                 ; print p1, p2, p3, p4, p5
@@ -57,7 +58,7 @@ class ControlPanel(wxPanel):
             ''')
             # Real-time audio output.
             # It is not necessary to enable line events.
-            csound.setCommand('''csound -d -m0 -b800 -B800 -odac1 temp.orc temp.sco''')
+            csound.setCommand('''csound -h -m128 -d -odac -B512 -b256 temp.orc temp.sco''')
             # Export the orc and sco.
             csound.exportForPerformance()
             # Start the performance.
@@ -65,14 +66,14 @@ class ControlPanel(wxPanel):
             # Perform in blocks of ksmps
             # (Csound will yield implicitly to wxWindows).
             while True:
-                csound.performKsmps()
+                csound.PerformKsmps()
 
         # Handle the button click -- send a note to Csound
         # with the pitch set by the slider.
         def OnClickButton1(self, event):
             print "You pressed button 1"
             # Send a line event.
-            csound.inputMessage("i 1 0 8 %i 70" % self.pitch)
+            csound.InputMessage("i 1 0 8 %i 70" % self.pitch)
 
         # Handle the slider movement -- change the pitch.
         def OnSlider1Move(self, event):
