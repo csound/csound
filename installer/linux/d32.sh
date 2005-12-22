75b980c0c9bb8ea5d28bb4abaf92496fa8751d52
John ffitch <jpff@users.sourceforge.net>
2005-12-22 07:04:09 +0000

    improved scripts

--- installer/linux/d32.sh
+++ installer/linux/d32.sh
@@ -1,15 +1,20 @@
-cp -upv ../../{linseg,lpanal,brkpt,lpc_export,lpc_import,cs,makecsd,csb64enc,cscore,csound,cvanal,dnoise,mixer,envext,extract,extractor,pvanal,pvlook,het_export,het_import,hetro,scale,scot,scsort,sdif2ad,srconv,tabdes,cstclsh,cswish} ../../../CSDIST/linux_d32/bin
+#!/bin/tcsh
+setenv DIR linux_d32
 
-strip ../../../CSDIST/linux_d32/bin/*
+cp -upv ../../{linseg,lpanal,brkpt,lpc_export,lpc_import,cs,makecsd,csb64enc,cscore,csound,cvanal,dnoise,mixer,envext,extract,extractor,pvanal,pvlook,het_export,het_import,hetro,scale,scot,scsort,sdif2ad,srconv,tabdes,cstclsh,cswish} ../../../CSDIST/$DIR/bin
 
-cp -upv ../../li*.so ../../../CSDIST/linux_d32/opc
+strip ../../../CSDIST/$DIR/bin/*
 
-cp -upv ../../libcsound.a  ../../../CSDIST/linux_d32/lib
+cp -upv ../../li*.so ../../../CSDIST/$DIR/opc
 
-cp -rupv ../../../manual/html ../../../CSDIST/linux_d32/doc
-find ../../../CSDIST/linux_d32/ -name CVS -exec rm -rf {} \;
+cp -upv ../../libcsound.a  ../../../CSDIST/$DIR/lib
 
-cat > ../../../CSDIST/linux_d32/def.ins <<'EOF'
+cp -upv ../../H/cfgvar.h ../../H/cscore.h ../../H/csdl.h ../../H/csound.h ../../H/csound.hpp ../../H/csoundCore.h ../../H/cwindow.h ../../H/msg_attr.h ../../H/OpcodeBase.hpp ../../H/pstream.h ../../H/pvfileio.h ../../H/soundio.h ../../H/sysdep.h ../../H/text.h ../../H/version.h ../../interfaces/CsoundFile.hpp ../../interfaces/CppSound.hpp ../../interfaces/filebuilding.h ../../../CSDIST/$DIR/hdr
+
+cp -rupv ../../../manual/html ../../../CSDIST/$DIR/doc
+find ../../../CSDIST/$DIR/ -name CVS -exec rm -rf {} \;
+
+cat > ../../../CSDIST/$DIR/def.ins <<'EOF'
 Linux Doubles
 /usr/local/bin
 /usr/local/csound
