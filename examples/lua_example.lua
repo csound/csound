1c6e66993313c7f7197f4b2de854dffecf13d788
Michael Gogins <gogins@users.sourceforge.net>
2006-04-20 01:38:49 +0000

    Updated for Lua 5.1 and to use class method notation.

--- examples/lua_example.lua
+++ examples/lua_example.lua
@@ -1,23 +1,28 @@
 #!/usr/bin/lua
 
-csnd_init = loadlib("./lib_csnd.so", "Csnd_Init")
+-- On Linux this should work:
+-- require "csnd"
+
+-- On Windows this is needed because the _csnd.dll name
+-- does not match the csnd module:
+csnd_init = package.loadlib("_csnd.dll", "Csnd_Init")
 csnd_init()
 
 csound = csnd.Csound()
 args = csnd.CsoundArgVList()
 args_ = {"csound", "-o", "dac", "examples/trapped.csd"}
-for i in args_ do
-    args.Append(args, args_[i])
+for i,v in ipairs(args_) do
+    args:Append(v)
 end
-csound.Compile(csound, args.argc(args), args.argv(args))
+csound:Compile(args.argc(args), args.argv(args))
 pt = csnd.CsoundPerformanceThread(csound)
-pt.SetScoreOffsetSeconds(pt, 21.5)
-pt.Play(pt)
+pt:SetScoreOffsetSeconds(21.5)
+pt:Play()
 csnd.csoundSleep(10000)
-pt.SetScoreOffsetSeconds(pt, 152)
+pt:SetScoreOffsetSeconds(152)
 csnd.csoundSleep(10000)
-pt.Stop(pt)
-pt.Join(pt)
-csound.Reset(csound)
-args.Clear(args)
+pt:Stop()
+pt:Join()
+csound:Reset()
+args:Clear()
 
