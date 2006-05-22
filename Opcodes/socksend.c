5bf33ca1a346434815ac7c809d2e6f1029ef85cf
Schampijer <schampijer@users.sourceforge.net>
2006-05-22 18:15:34 +0000

    Added a two channel version of the udp opcodes.

--- Opcodes/socksend.c
+++ Opcodes/socksend.c
@@ -40,6 +40,14 @@ typedef struct {
     struct sockaddr_in server_addr;
 } SOCKSEND;
 
+typedef struct {
+    OPDS   h;
+    MYFLT  *asigl, *asigr, *ipaddress, *port;
+    AUXCH  aux;
+    int sock, conn, frag;
+    struct sockaddr_in server_addr;
+} SOCKSENDS;
+
 #define MTU (1456)
 
 /* UDP version */
@@ -81,10 +89,84 @@ int send_send(CSOUND *csound, SOCKSEND* p)
     return OK;
 }
 
+/* UDP version 2 channels */
+int init_sendS(CSOUND *csound, SOCKSENDS *p)
+{
+    int n = MTU;
+    MYFLT* buf;
+
+    p->sock = socket(AF_INET, SOCK_DGRAM, 0);
+    if (p->sock < 0) {
+      csound->InitError(csound, "creating socket");
+      return NOTOK;
+    }
+    /* create server address: where we want to send to and clear it out */
+    memset(&p->server_addr, 0, sizeof(p->server_addr));
+    p->server_addr.sin_family = AF_INET;    /* it is an INET address */
+    inet_aton((const char*)p->ipaddress,
+              &p->server_addr.sin_addr);    /* the server IP address */
+    p->server_addr.sin_port = htons((int)*p->port);      /* the port */
+    p->frag = (sizeof(MYFLT)*csound->ksmps>MTU); /* fragment packets? */
+
+    /* create a buffer to store the received interleaved audio data*/
+    if (p->aux.auxp == NULL || (long) (n * sizeof(MYFLT)) > p->aux.size)
+        /* allocate space for the buffer */
+        csound->AuxAlloc(csound, n * sizeof(MYFLT), &p->aux);
+    else {
+        buf = (MYFLT *)p->aux.auxp;  /* make sure buffer is empty */
+        do {
+	    *buf++ = FL(0.0);
+        } while (--n);
+    }
+    return OK;
+}
+
+int send_sendS(CSOUND *csound, SOCKSENDS* p)
+{
+    const struct sockaddr *to = (const struct sockaddr *)(&p->server_addr);
+    MYFLT *asigl = p->asigl;
+    MYFLT *asigr = p->asigr;
+    MYFLT *out = (MYFLT *) p->aux.auxp;
+    int i, j;
+
+    int n = ((sizeof(MYFLT)*csound->ksmps)*2);
+    int m = (csound->ksmps*2);
+    int mtu_in_samples = (MTU/sizeof(MYFLT));
+    if (p->frag) {
+      while (n>MTU) {	
+	  /* store the samples of the channels interleaved in the packet (left, right) */
+	  for(i=0, j=0; i<mtu_in_samples; i+=2, j++)
+	  {
+	      out[i] = asigl[j];
+	      out[i+1] = asigr[j];
+	  }  
+	  if (sendto(p->sock, out, MTU, 0, to, sizeof(p->server_addr)) < 0) {
+	      csound->PerfError(csound, "sendto failed");
+	      return NOTOK;
+	  }
+	  n -= MTU;
+	  m -= mtu_in_samples;
+      }
+    }
+
+    /* store the samples of the channels interleaved in the packet (left, right) */
+    for(i=0, j=0; i<m; i+=2, j++)
+    {
+	out[i] = asigl[j];
+	out[i+1] = asigr[j];
+    }
+    if (sendto(p->sock, out, n, 0, to, sizeof(p->server_addr)) < 0) {
+      csound->PerfError(csound, "sendto failed");
+      return NOTOK;
+    }
+    return OK;
+}
+
+
 /* TCP version */
 int init_ssend(CSOUND *csound, SOCKSEND* p)
 {
-    int clilen;
+    socklen_t clilen;
     /* create a STREAM (TCP) socket in the INET (IP) protocol */
     p->sock = socket(PF_INET, SOCK_STREAM, 0);
 
@@ -143,6 +225,7 @@ int send_ssend(CSOUND *csound, SOCKSEND* p)
 
 static OENTRY localops[] = {
   { "socksend", S(SOCKSEND), 5, "", "aSi", (SUBR)init_send, NULL, (SUBR)send_send},
+  { "socksends", S(SOCKSENDS), 5, "", "aaSi", (SUBR)init_sendS, NULL, (SUBR)send_sendS},
   { "stsend", S(SOCKSEND), 5, "", "aSi", (SUBR)init_ssend, NULL, (SUBR)send_ssend}
 };
 
