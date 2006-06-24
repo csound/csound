069c30561c94b87bbd66bf6ca30f440a06ffe8da
Istvan Varga <istvanv@users.sourceforge.net>
2006-06-24 09:41:44 +0000

    minor

--- InOut/midifile.c
+++ InOut/midifile.c
@@ -561,8 +561,7 @@ int csoundMIDIFileOpen(CSOUND *csound, const char *name)
     if (strcmp(name, "stdin") == 0)
       f = stdin;
     else {
-      fd = csound->FileOpen(csound, &f, CSFILE_STD, name, "rb",
-                            "SFDIR;SSDIR;MFDIR");
+      fd = csound->FileOpen(csound, &f, CSFILE_STD, name, "rb", "SFDIR;SSDIR");
       if (fd == NULL) {
         csound->ErrorMsg(csound, Str(" *** error opening MIDI file '%s': %s"),
                                  name, strerror(errno));
