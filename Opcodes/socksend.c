47dc76c59fa21bde407b13386953d112d89d72dd
John ffitch <jpff@users.sourceforge.net>
2006-07-06 04:38:59 +0000

    Changing copyright notice

--- Opcodes/socksend.c
+++ Opcodes/socksend.c
@@ -3,7 +3,7 @@
 
     Copyright (C) 2006 by John ffitch
 
-    This file is not yet part of Csound.
+    This file is part of Csound.
 
     The Csound Library is free software; you can redistribute it
     and/or modify it under the terms of the GNU Lesser General Public
