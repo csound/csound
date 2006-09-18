20a64bf629f29826f51fa4319eed32c7e7e3bbd8
John ffitch <jpff@users.sourceforge.net>
2006-09-18 18:27:47 +0000

    Strings in UDO; faster code; smaller code

--- util/lpci_main.c
+++ util/lpci_main.c
@@ -1,5 +1,91 @@
+/*
+    lpc_import.c
 
-#include "utilmain.h"
+    Copyright (C) 1995 John ffitch
 
-UTIL_MAIN("lpc_import")
+    This file is part of Csound.
+
+    Csound is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    Csound is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Csound; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+/* ***************************************************************** */
+/* ******** Program to import lpanal files in tabular format. ****** */
+/* ***************************************************************** */
+
+/* ***************************************************************** */
+/* John ffitch 1998 Nov 15                                           */
+/* Version that does not need to load rest of csound5                */
+/* ***************************************************************** */
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#ifndef MYFLT
+#include "sysdep.h"
+#endif
+#include "lpc.h"
+
+void lpc_import_usage(void)
+{
+    printf("Usage: lpc_import cstext_file lpc_file\n");
+}
+
+int main(int argc, char **argv)
+{
+    FILE *inf;
+    FILE *outf;
+    LPHEADER hdr;
+    int i, j;
+    char *str;
+    MYFLT *coef;
+
+    if (argc != 3) {
+      lpc_import_usage();
+      return 1;
+    }
+    inf = fopen(argv[1], "rb");
+    if (inf == NULL) {
+      fprintf(stderr, "Cannot open input file %s\n", argv[1]);
+      exit(1);
+    }
+    outf = fopen(argv[2], "w");
+    if (outf == NULL) {
+      fprintf(stderr, "Cannot open output file %s\n", argv[2]);
+      return 1;
+    }
+    if (fread(&hdr, sizeof(LPHEADER)-4, 1, inf) != 1 ||
+        (hdr.lpmagic != LP_MAGIC && hdr.lpmagic != LP_MAGIC2)) {
+      fprintf(stderr, "Failed to read LPC header\n");
+      return 1;
+    }
+    fprintf(outf, "%ld,%ld,%ld,%ld,%f,%f,%f",
+            hdr.headersize, hdr.lpmagic, hdr.npoles, hdr.nvals,
+            hdr.framrate, hdr.srate, hdr.duration);
+    str = (char *)malloc(hdr.headersize-sizeof(LPHEADER)+4);
+    fread(&hdr, sizeof(char), hdr.headersize-sizeof(LPHEADER)+4, inf);
+    for (i=0; i<hdr.headersize-sizeof(LPHEADER)+4; i++)
+      putc(str[i],outf);
+    putc('\n', outf);
+    coef = (MYFLT *)malloc((hdr.npoles+hdr.nvals)*sizeof(MYFLT));
+    for (i = 0; i<hdr.nvals; i++) {
+      fread(&coef[0], sizeof(MYFLT), hdr.npoles, inf);
+      for (j=0; j<hdr.npoles; j++)
+        fprintf(outf, "%f%c", coef[j], (j==hdr.npoles-1 ? '\n' : ','));
+    }
+    fclose(outf);
+    fclose(inf);
+    free(coef); free(str);
+    return 0;
+}
 
