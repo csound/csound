0eb0b00c996aa2e82255d3279a1f17c821e143bc
Michael Gogins <gogins@users.sourceforge.net>
2006-10-13 00:09:24 +0000

    Brought functions into static class as "namespace".

--- frontends/CsoundVST/Voicelead.hpp
+++ frontends/CsoundVST/Voicelead.hpp
@@ -27,124 +27,127 @@
 
 namespace csound 
 {
-  /**
-   * Returns the pitch-class of the pitch,
-   * in 12-tone equal temperament.
-   */
-  double pc(double pitch);
+  class Voicelead
+  {
+  public:
+    /**
+     * Returns the pitch-class of the pitch,
+     * in 12-tone equal temperament.
+     */
+    static double pc(double pitch);
 
 
-  /**
-   * Returns the voice-leading vector between
-   * chord1 and chord2.
-   */   
-  std::vector<double> voiceleading(const std::vector<double> &chord1, 
-				   const std::vector<double> &chord2);
+    /**
+     * Returns the voice-leading vector between
+     * chord1 and chord2.
+     */   
+    static std::vector<double> voiceleading(const std::vector<double> &chord1, 
+					    const std::vector<double> &chord2);
 
-  /**
-   * Returns the simpler (fewer motions) of the voiceleadings
-   * between source chord and either destination1 or destination2,
-   * optionally avoiding parallel fifths.
-   */
-  const std::vector<double> &simpler(const std::vector<double> &source, 
-				     const std::vector<double> &destination1, 
-				     const std::vector<double> &destination2, 
-				     bool avoidParallels);
+    /**
+     * Returns the simpler (fewer motions) of the voiceleadings
+     * between source chord and either destination1 or destination2,
+     * optionally avoiding parallel fifths.
+     */
+    static const std::vector<double> &simpler(const std::vector<double> &source, 
+					      const std::vector<double> &destination1, 
+					      const std::vector<double> &destination2, 
+					      bool avoidParallels);
 
-  /**
-   * Returns the smoothness (distance by taxicab or L1 norm)
-   * of the voiceleading between chord1 and chord2.
-   */
-  double smoothness(const std::vector<double> &chord1, 
-		    const std::vector<double> &chord1);
+    /**
+     * Returns the smoothness (distance by taxicab or L1 norm)
+     * of the voiceleading between chord1 and chord2.
+     */
+    static double smoothness(const std::vector<double> &chord1, 
+			     const std::vector<double> &chord1);
                 
-  /*
-   * Returns whether the progression between chord1 and chord2
-   * contains a parallel fifth.
-   */
-  bool areParallel(const std::vector<double> &chord1, 
-		   const std::vector<double> &chord2);
+    /*
+     * Returns whether the progression between chord1 and chord2
+     * contains a parallel fifth.
+     */
+    static bool areParallel(const std::vector<double> &chord1, 
+			    const std::vector<double> &chord2);
 
-  /**
-   * Returns the closer, first by smoothness then by simplicity.,
-   * of the voiceleadings between source and either 
-   * destination1 or destination2, optionally avoiding
-   * parallel fifths.
-   */
-  const std::vector<double> &closer(const std::vector<double> &source, 
-				    const std::vector<double> &destination1, 
-				    const std::vector<double> &destination2, 
-				    bool avoidParallels);
+    /**
+     * Returns the closer, first by smoothness then by simplicity.,
+     * of the voiceleadings between source and either 
+     * destination1 or destination2, optionally avoiding
+     * parallel fifths.
+     */
+    static const std::vector<double> &closer(const std::vector<double> &source, 
+					     const std::vector<double> &destination1, 
+					     const std::vector<double> &destination2, 
+					     bool avoidParallels);
 
-  /**
-   * Returns the chord with the first note rotated to the last note.
-   */
-  std::vector<double> rotate(const std::vector<double> &chord);
+    /**
+     * Returns the chord with the first note rotated to the last note.
+     */
+    static std::vector<double> rotate(const std::vector<double> &chord);
 
-  /**
-   * Returns the set of all rotations of the chord.
-   */
-  void rotations(std::vector<double> chord, 
-		 std::vector< std::vector<double> > &rotations_);
+    /**
+     * Returns the set of all rotations of the chord.
+     */
+    static void rotations(std::vector<double> chord, 
+			  std::vector< std::vector<double> > &rotations_);
   
-  /**
-   * Returns the chord as the list of its pitch-classes.
-   */
-  std::vector<double> tones(const std::vector<double> &chord);
+    /**
+     * Returns the chord as the list of its pitch-classes.
+     */
+    static std::vector<double> tones(const std::vector<double> &chord);
 
-  /**
-   * Returns all voicings of the chord
-   * within the specified range.
-   */
-  std::vector< std::vector<double> > voicings(const std::vector<double> &chord, 
-					      double lowest, 
-					      double range);
+    /**
+     * Returns all voicings of the chord
+     * within the specified range.
+     */
+    static std::vector< std::vector<double> > voicings(const std::vector<double> &chord, 
+						       double lowest, 
+						       double range);
  
-  /**
-   * Returns the closest voiceleading within the specified range, 
-   * first by smoothness then by simplicity, 
-   * between the source chord any of the destination chords,
-   * optionally avoiding parallel fifths.
-   */
-  const std::vector<double> &closest(const std::vector<double> &source, 
-				     const std::set< std::vector<double> > &destinations, 
-				     bool avoidParallels);
+    /**
+     * Returns the closest voiceleading within the specified range, 
+     * first by smoothness then by simplicity, 
+     * between the source chord any of the destination chords,
+     * optionally avoiding parallel fifths.
+     */
+    static const std::vector<double> &closest(const std::vector<double> &source, 
+					      const std::set< std::vector<double> > &destinations, 
+					      bool avoidParallels);
    
-  /**
-   * Returns the closest voiceleading within the specified range, 
-   * first by smoothness then by simplicity, 
-   * between the source chord and the target pitch-class set,
-   * optionally avoiding parallel fifths.
-   * The algorithm uses a brute-force search through all
-   * unordered chords fitting the target pitch-class set within
-   * the specified range. For up to 6 voices this is usable
-   * for non-real-time computation.
-   */
-  std::vector<double> voicelead(const std::vector<double> &source, 
-				const std::vector<double> &targetPitchClassSet, 
-				double lowest, 
-				double range, 
-				bool avoidParallels);
+    /**
+     * Returns the closest voiceleading within the specified range, 
+     * first by smoothness then by simplicity, 
+     * between the source chord and the target pitch-class set,
+     * optionally avoiding parallel fifths.
+     * The algorithm uses a brute-force search through all
+     * unordered chords fitting the target pitch-class set within
+     * the specified range. For up to 6 voices this is usable
+     * for non-real-time computation.
+     */
+    static std::vector<double> voicelead(const std::vector<double> &source, 
+					 const std::vector<double> &targetPitchClassSet, 
+					 double lowest, 
+					 double range, 
+					 bool avoidParallels);
 
 #ifndef TEST
-  /**
-   * Returns the closest voiceleading within the specified range, 
-   * first by smoothness then by simplicity, 
-   * between the source chord and the target pitch-class set,
-   * optionally avoiding parallel fifths.
-   * The algorithm uses a brute-force search through all
-   * unordered chords fitting the target pitch-class set within
-   * the specified range. For up to 6 voices this is usable
-   * for non-real-time computation.
-   */
-  std::vector<Event> voiceleadEvents(const std::vector<Event> &source,
-				     const std::vector<Event> &target,
-				     double lowest,
-				     double range,
-				     bool avoidParallels);
+    /**
+     * Returns the closest voiceleading within the specified range, 
+     * first by smoothness then by simplicity, 
+     * between the source chord and the target pitch-class set,
+     * optionally avoiding parallel fifths.
+     * The algorithm uses a brute-force search through all
+     * unordered chords fitting the target pitch-class set within
+     * the specified range. For up to 6 voices this is usable
+     * for non-real-time computation.
+     */
+    static std::vector<Event> voiceleadEvents(const std::vector<Event> &source,
+					      const std::vector<Event> &target,
+					      double lowest,
+					      double range,
+					      bool avoidParallels);
 #endif
+  };
   
 }
-
 #endif
 
