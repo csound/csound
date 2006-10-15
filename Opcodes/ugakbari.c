05659f0ba9d14a92bcc792cb6bcef97444e6fe3c
Steven Yi <kunstmusik@users.sourceforge.net>
2006-10-15 19:36:36 +0000

    modified as discussed on Csound list

--- Opcodes/ugakbari.c
+++ Opcodes/ugakbari.c
@@ -25,7 +25,7 @@
 #include <math.h>
 
 #define LOGCURVE(x,y) ((log(x * (y-1)+1))/(log(y)))
-#define EXPCURVE(x,y) (exp(x * log(y))-1/(y-1))
+#define EXPCURVE(x,y) ((exp(x * log(y))-1)/(y-1))
 #define GAINSLIDER(x) (0.000145 * exp(x * 0.06907))
 
 typedef struct _scale {
@@ -54,14 +54,6 @@ typedef struct _gainslider {
 
 /*  scale opcode  */
 
-int scale_init(CSOUND *csound, scale *p)
-{
-    /* This does little as k values not available yet */
-    *p->koutval = (*p->kinval * (*p->kmax - *p->kmin) + *p->kmin);
-
-    return OK;
-}
-
 int scale_process(CSOUND *csound, scale *p)
 {
 
@@ -74,7 +66,6 @@ int scale_process(CSOUND *csound, scale *p)
 }
 
 /*  expcurve opcode  */
-/* Also used as initialisation */
 
 int expcurve_perf(CSOUND *csound, expcurve *p)
 {
@@ -84,7 +75,6 @@ int expcurve_perf(CSOUND *csound, expcurve *p)
 }
 
 /*  logcurve opcode  */
-/* Also used as initialisation */
 
 int logcurve_perf(CSOUND *csound, logcurve *p)
 {
@@ -97,28 +87,25 @@ int logcurve_perf(CSOUND *csound, logcurve *p)
 
 int gainslider_perf(CSOUND *csound, gainslider *p)
 {
+	if(*p->kindex <= FL(0.0))
+	  {
+         *p->koutsig = FL(0.0);
+      }
+    else
+      {
+         *p->koutsig = GAINSLIDER((MYFLT) *p->kindex);
+      }
 
-    if (*p->kindex <= FL(0.0)) {
-      *p->koutsig = FL(0.0);
-    }
-    else {
-      *p->koutsig = GAINSLIDER((MYFLT) *p->kindex);
-    }
-
-    return OK;
+  return OK;
 }
 
 /* opcode library entries */
 
 static OENTRY localops[] = {
-  { "scale", sizeof(scale), 3, "k", "kkk",
-    (SUBR)scale_init, (SUBR)scale_process, NULL },
-  { "expcurve", sizeof(expcurve), 3, "k", "kk",
-    (SUBR)expcurve_perf, (SUBR)expcurve_perf, NULL },
-  { "logcurve", sizeof(logcurve), 3, "k", "kk",
-    (SUBR)logcurve_perf, (SUBR)logcurve_perf, NULL },
-  { "gainslider", sizeof(gainslider), 3, "k", "k",
-    NULL, (SUBR)gainslider_perf, NULL }
+  { "scale", sizeof(scale), 2, "k", "kkk", NULL, (SUBR)scale_process, NULL },
+  { "expcurve", sizeof(expcurve), 2, "k", "kk", NULL, (SUBR)expcurve_perf, NULL },
+  { "logcurve", sizeof(logcurve), 2, "k", "kk", NULL, (SUBR)logcurve_perf, NULL },
+  { "gainslider", sizeof(gainslider), 2, "k", "k", NULL, (SUBR)gainslider_perf, NULL }
 };
 
 LINKAGE
