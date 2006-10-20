ae7fc03dcaaf7358ff204bf0f42c80465c4e62f7
Michael Gogins <gogins@users.sourceforge.net>
2006-10-20 02:25:56 +0000

    no message

--- frontends/CsoundVST/CsoundVstUnitTests.py
+++ frontends/CsoundVST/CsoundVstUnitTests.py
@@ -5,6 +5,10 @@ import random
 print 'UNIT TESTS FOR CsoundVST.Voicelead and CsoundVST.Score'
 print
 
+lowest = 48
+range = 36
+highest = lowest + range
+
 for i in xrange(36, 96, 1):
     p = float(i)
     print '%8.3f = Voicelead_pc(%8.3f)' % (CsoundVST.Voicelead_pc(p), p)
@@ -31,8 +35,8 @@ print '%8.3f = Voiclead_numberFromChord(%s)' % (CsoundVST.Voicelead_numberFromCh
 print
 print '%s = Voicelead_pcsFromNumber(%8.3f)' % (CsoundVST.Voicelead_pcsFromNumber(2193), 2193)
 print
-voicings = CsoundVST.Voicelead_voicings(a, 36, 60, 12)
-print 'Voicelead_voicings(%s, %s, %s, %d):' % (a, 36, 60, 12)
+voicings = CsoundVST.Voicelead_voicings(a, lowest, range, 12)
+print 'Voicelead_voicings(%s, %s, %s, %d):' % (a, lowest, range, 12)
 for chord in voicings:
     print chord
 print
@@ -42,39 +46,43 @@ FM7 = [65., 69., 72., 76.]
 print 'FM7: %s' % (FM7)
 G7 =  [67., 71., 74., 77.]
 print 'G7:  %s' % (G7)
-t1 = CsoundVST.Voicelead_voicelead(CM7, FM7, 0., 96., False)
-print '%s = CsoundVST.Voicelead_voicelead(%s, %s, 0, 96., True)' % (t1, CM7, FM7)
-t2 = CsoundVST.Voicelead_voicelead(t1, G7, 0., 96., False)
-print '%s = CsoundVST.Voicelead_voicelead(%s, %s, 36., 96., True)' % (t2, t1, G7)
+t1 = CsoundVST.Voicelead_voicelead(CM7, FM7, lowest, range, False)
+print '%s = CsoundVST.Voicelead_voicelead(%s, %s, lowest, range, True)' % (t1, CM7, FM7)
+t2 = CsoundVST.Voicelead_voicelead(t1, G7, lowest, range, False)
+print '%s = CsoundVST.Voicelead_voicelead(%s, %s, lowest, range, True)' % (t2, t1, G7)
 print
-for i in xrange(36, 96, 1):
+for i in xrange(lowest, range, 1):
 	print '%8.3f = Voiclead_closestPitch(Voicelead_pc(%d), %s)' %(CsoundVST.Voicelead_closestPitch(i, CM7), i, CM7)
 print
-for i in xrange(36, 96, 1):
+for i in xrange(lowest, range, 1):
 	print '%8.3f = Voiclead_conformToPitchClassSet(%d, %s)' %(CsoundVST.Voicelead_conformToPitchClassSet(i, a), i, a)
 print
 score = CsoundVST.Score()
 for i in xrange(2000):
 	time = i * 0.125
 	duration = 0.5
-	key = random.randint(36, 96)
+	key = random.randint(lowest, highest)
 	velocity = 80.0
 	score.append(time, duration, 144.0, 1.0, key, velocity)
 score.save('CsoundVstUnitTest.py.1.mid')
 score.setPitchClassSet(0, len(score), a)
 score.save('CsoundVstUnitTest.py.2.mid')
-pcsI = CsoundVST.Voicelead_numberFromChord([0.,4.,7.,11.,14.]) - 1.0
+pcsN = CsoundVST.Voicelead_numberFromChord([0.,4.,7.,11.,14.])
+pcsI = pcsN - 1
 for i in xrange(0, len(score), 20):
-	pcsI = pcsI + 5.0
-	pcsI = pcsI % 4095
-	pcsN = pcsI + 1
 	CsoundVST.Voicelead.pcsFromNumber(pcsN)
 	score.setPitchClassSet(i, i + 20, CsoundVST.Voicelead_pcsFromNumber(pcsN, 12), 12)
+	pcs = score.getPitches(i, i + 20)
+	print pcsN, pcsI, pcs
+	pcsI = pcsI * 32
+	pcsI = pcsI % 4095
+	pcsN = pcsI + 1
 score.save('CsoundVstUnitTest.py.3.mid')
-for i in xrange(0, len(score), 20):
-	pcs = CsoundVST.Voicelead_pcs(score.getPitches(i, i + 20))
-	#print pcs
-	score.voicelead(i, i + 20, i + 20, i + 40, 36, 96, True)
+for i in xrange(0, 1300, 20):
+	prepitches = score.getPitches(i, i + 20)
+	score.voicelead(i, i + 20, i + 20, i + 40, lowest, range, True)
+	postpitches = score.getPitches(i + 20, i + 40)
+	print i, prepitches, postpitches
 score.save('CsoundVstUnitTest.py.4.mid')
 
 	
@@ -84,6 +92,23 @@ score.save('CsoundVstUnitTest.py.4.mid')
 
 	
 	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
