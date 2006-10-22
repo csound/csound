474e8301eaef364a397505596bbc44d12277e779
Michael Gogins <gogins@users.sourceforge.net>
2006-10-22 20:38:57 +0000

    no message

--- interfaces/lisp-swig.sh
+++ interfaces/lisp-swig.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 swig -cffi -module csound -v -extern-all -nocwrap -generate-typedef -ID:/utah/opt/swigwin-1.3.29/lib/cffi -I. -I.. -ID:/utah/home/mkg/projects/csoundd/H D:/utah/home/mkg/projects/csoundd/H/csound.h 
+# Then add to the top of csound.lisp:
+#(defpackage :csound
+#   (:use :common-lisp :cffi))
+#
+#(in-package :csound)
+#
+# and replace incorrect SWIG variadic args (e.g. "(arg4 )" with "&rest".
 swig -cffi -module filebuilding -v -extern-all -nocwrap -generate-typedef -ID:/utah/opt/swigwin-1.3.29/lib/cffi -I. -I.. -ID:/utah/home/mkg/projects/csoundd/H D:/utah/home/mkg/projects/csoundd/interfaces/filebuilding.h
+# Then add to the top of filebuilding.lisp:
+#(defpackage :filebuilding
+#   (:use :common-lisp :cffi))
+#
+#(in-package :filebuilding)
