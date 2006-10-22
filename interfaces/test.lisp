d5e7c688c9fe375d7d3bfae8a29889ff005f8c6d
Michael Gogins <gogins@users.sourceforge.net>
2006-10-22 20:38:57 +0000

    no message

--- interfaces/test.lisp
+++ interfaces/test.lisp
@@ -1,14 +1,14 @@
 ;; Brief example indicating a minimal use of the Csound API from CLisp.
-;; This file must be run from the csound5 directory,
-;; not the csound5/examples directory, like this:
-;; "clisp examples/test.lisp"
+;; This file must be run from the interfaces directory,
+;; e.g. "cd interfaces; clisp test.lisp"
+;; You may need to edit directories specified below.
 (load "d:/utah/opt/cm/src/cm.lisp")
 (use-system :cffi)
 (load "csound.lisp")
 (load "filebuilding.lisp")
-(cffi:load-foreign-library "d:/utah/home/mkg/projects/csoundd/_csnd.dll")
+(cffi:load-foreign-library "_csnd.dll")
 (setq cs (csound::csoundCreate nil))
-(cffi:with-foreign-string (csd "d:/utah/home/mkg/projects/csoundd/examples/trapped.csd")
+(cffi:with-foreign-string (csd "../examples/trapped.csd")
 			  (filebuilding::csoundCompileCsd cs csd))
 (setq *keep-playing* t result 0)
 (loop while (and *keep-playing* (eq result 0)) do
