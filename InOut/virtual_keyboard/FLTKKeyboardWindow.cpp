d84c39c29f0af14eeaab0c1c1e8cfb434701007d
Steven Yi <kunstmusik@users.sourceforge.net>
2006-10-28 16:50:44 +0000

    initial work on keyboard handling

--- InOut/virtual_keyboard/FLTKKeyboardWindow.cpp
+++ InOut/virtual_keyboard/FLTKKeyboardWindow.cpp
@@ -7,6 +7,26 @@ FLTKKeyboardWindow::FLTKKeyboardWindow(CSOUND *csound, int W, int H, const char 
     this->begin();
     this->keyboard = new FLTKKeyboard(csound, 0,0,W, H, "Keyboard");
 
+	this->csound = csound;
+
     this->end();
 
 }
+
+int FLTKKeyboardWindow::handle(int event) {
+    //this->csound->Message(this->csound, "Keyboard event: %d\n", event);
+
+    switch(event) {
+    	case FL_KEYDOWN:
+    		return 0;
+    	case FL_KEYUP:
+      		if (Fl::focus() == this) {
+
+      		}
+        	return 0;
+        default:
+            return Fl_Double_Window::handle(event);
+    }
+
+}
+
