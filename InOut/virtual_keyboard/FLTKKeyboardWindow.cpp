0d2800db2e69100bb43164d19069de963a1833b6
Steven Yi <kunstmusik@users.sourceforge.net>
2006-10-28 20:14:56 +0000

    added all notes off button

--- InOut/virtual_keyboard/FLTKKeyboardWindow.cpp
+++ InOut/virtual_keyboard/FLTKKeyboardWindow.cpp
@@ -1,12 +1,19 @@
 #include "FLTKKeyboardWindow.hpp"
 
+
+static void allNotesOff(Fl_Widget *widget, void * v) {
+	FLTKKeyboardWindow *win = (FLTKKeyboardWindow *)v;
+	win->keyboard->allNotesOff();
+}
+
 FLTKKeyboardWindow::FLTKKeyboardWindow(CSOUND *csound, int W, int H, const char *L = 0)
     : Fl_Double_Window(W, H, L)
 {
 
     this->begin();
-    this->keyboard = new FLTKKeyboard(csound, 0,0,W, H, "Keyboard");
-
+    this->allNotesOffButton = new Fl_Button(0, 0, W, 20, "All Notes Off");
+    this->allNotesOffButton->callback((Fl_Callback*) allNotesOff, this);
+    this->keyboard = new FLTKKeyboard(csound, 0, 20, W, 80, "Keyboard");
 	this->csound = csound;
 
     this->end();
