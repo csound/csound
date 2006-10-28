5b51a39a990eeab7f27dfc567d3e29a2cd3590aa
Steven Yi <kunstmusik@users.sourceforge.net>
2006-10-28 09:42:31 +0000

    added csound pointer to constructor to pass to FLTKKeyboard for mutex locking

--- InOut/virtual_keyboard/FLTKKeyboardWindow.cpp
+++ InOut/virtual_keyboard/FLTKKeyboardWindow.cpp
@@ -1,11 +1,11 @@
 #include "FLTKKeyboardWindow.hpp"
 
-FLTKKeyboardWindow::FLTKKeyboardWindow(int W, int H, const char *L = 0)
+FLTKKeyboardWindow::FLTKKeyboardWindow(CSOUND *csound, int W, int H, const char *L = 0)
     : Fl_Double_Window(W, H, L)
 {
 
     this->begin();
-    this->keyboard = new FLTKKeyboard(0,0,W, H, "Keyboard");
+    this->keyboard = new FLTKKeyboard(csound, 0,0,W, H, "Keyboard");
 
     this->end();
 
