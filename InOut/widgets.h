f8ab9a89ecb869f2dbba5dfea35798db5c312fad
Andres Cabrera <mantaraya36@users.sourceforge.net>
2007-05-05 16:34:44 +0000

    Added opcode FLsetSnapGroup by Gabriel Maldonado. Fixed problem when loading snapshot before saving. Moved contents of newwidgets.h to widgets.h and removed file newwidgets.h. Fix to remove compiler warning.

--- InOut/widgets.h
+++ InOut/widgets.h
@@ -504,6 +504,62 @@ class Fl_Value_Slider_Input : public Fl_Value_Slider {
     int textboxsize() const {return txtboxsize;}
 };
 
+
+// New widgets for Csound5.06 by Gabriel Maldonado
+// Ported by Andres Cabrera. This section below comes
+// from the file newwidgets.h
+
+// Parts below are commented out because thay have not yet been implemented
+typedef struct	{
+	OPDS	h;
+	//MYFLT *ktrig, *kvalue, *ihandle;
+	MYFLT *chan, *cc, *ihandle;
+	int	ccVal, oldCCval;
+	MYFLT log_base, min, max, range;
+	void *WidgAddress, *opcode, *addrSetVal;
+	int exp, widg_type;
+} FL_MIDI_WIDGET_VALUE;
+// 
+// 
+// typedef struct	{
+// 	OPDS	h;
+// 	MYFLT   *kascii,*ifn;//, *ifnMap;
+// 	MYFLT	*table;//, *tableMap;
+// 	int		flag;
+// } FLKEYIN;
+
+typedef struct	{
+	OPDS	h;
+	MYFLT  *group;
+} FLSETSNAPGROUP;
+
+// typedef struct	{
+// 	OPDS	h;
+// 	MYFLT   *x,*y, *b1, *b2, *b3, *flagRaw;
+// 	MYFLT height,width;
+// } FLMOUSE;
+// 
+// 
+// typedef struct	{
+// 	OPDS	h;
+// 	MYFLT *ihandle, *numlinesX, *numlinesY, *iwidth, *iheight, *ix, *iy,*image;
+// 	int width, height;
+// } FL_HVSBOX;
+// 
+// typedef struct	{
+// 	OPDS	h;
+// 	MYFLT *kx, *ky, *ihandle;
+// 	void *WidgAddress, *opcode;
+// 	MYFLT old_x, old_y;
+// } FL_SET_HVS_VALUE;
+// 
+typedef struct	{
+	OPDS	h;
+	MYFLT max[MAXCHNLS];
+	unsigned long widg_address[MAXCHNLS];
+	int dummycycles, dummycyc;
+} VUMETER;
+
 #endif          /* CSOUND_WIDGETS_CPP */
 #endif          /* CSOUND_WIDGETS_H */
 
