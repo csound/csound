5913b83987847b9fd43c550059371853fba325b0
Andres Cabrera <mantaraya36@users.sourceforge.net>
2007-05-13 16:38:11 +0000

    Fixed problem

--- Opcodes/stdopcod.h
+++ Opcodes/stdopcod.h
@@ -61,6 +61,7 @@ typedef struct STDOPCOD_GLOBALS_ {
     /* space.c */
     void        *spaceaddr;
     /* gab/gab.c */
+    MYFLT       *tb_ptrs[16];       /* Left here while the rest is implemented */
     MYFLT       *tb[16];       /* gab: updated */
     int         tb_ixmode[16]; /* gab: added */
     long        tb_size[16];   /* gab: added */
