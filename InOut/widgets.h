161cbc2fca4f48f2b6b638da1a8e51d2c79227e4
Andres Cabrera <mantaraya36@users.sourceforge.net>
2007-05-14 23:16:46 +0000

    Added changes by G. Maldonado. Added optional iclose argument to FLpanel. Added opcodes FLkeyin, FLmouse, FLhsvBox, FLhsvBoxSetValue by G. Maldonado. Removed FLkeyb because it was never functional and marked experimental.

--- InOut/widgets.h
+++ InOut/widgets.h
@@ -200,7 +200,7 @@ typedef struct {
 
 typedef struct {
     OPDS    h;
-    MYFLT   *name, *iwidth, *iheight, *ix, *iy, *border, *ikbdsense;
+    MYFLT   *name, *iwidth, *iheight, *ix, *iy, *border, *ikbdsense, *iclose;
 } FLPANEL;
 
 typedef struct {
@@ -532,40 +532,39 @@ typedef struct	{
 	void *WidgAddress, *opcode, *addrSetVal;
 	int exp, widg_type;
 } FL_MIDI_WIDGET_VALUE;
-// 
-// 
-// typedef struct	{
-// 	OPDS	h;
-// 	MYFLT   *kascii,*ifn;//, *ifnMap;
-// 	MYFLT	*table;//, *tableMap;
-// 	int		flag;
-// } FLKEYIN;
+
+
+typedef struct	{
+	OPDS	h;
+	MYFLT   *kascii,*ifn;//, *ifnMap;
+	MYFLT	*table;//, *tableMap;
+	int		flag;
+} FLKEYIN;
 
 typedef struct	{
 	OPDS	h;
 	MYFLT  *group;
 } FLSETSNAPGROUP;
 
-// typedef struct	{
-// 	OPDS	h;
-// 	MYFLT   *x,*y, *b1, *b2, *b3, *flagRaw;
-// 	MYFLT height,width;
-// } FLMOUSE;
-// 
-// 
-// typedef struct	{
-// 	OPDS	h;
-// 	MYFLT *ihandle, *numlinesX, *numlinesY, *iwidth, *iheight, *ix, *iy,*image;
-// 	int width, height;
-// } FL_HVSBOX;
-// 
-// typedef struct	{
-// 	OPDS	h;
-// 	MYFLT *kx, *ky, *ihandle;
-// 	void *WidgAddress, *opcode;
-// 	MYFLT old_x, old_y;
-// } FL_SET_HVS_VALUE;
-// 
+typedef struct	{
+	OPDS	h;
+	MYFLT   *x,*y, *b1, *b2, *b3, *flagRaw;
+	MYFLT height,width;
+} FLMOUSE;
+
+typedef struct	{
+	OPDS	h;
+	MYFLT *ihandle, *numlinesX, *numlinesY, *iwidth, *iheight, *ix, *iy,*image;
+	int width, height;
+} FL_HVSBOX;
+
+typedef struct	{
+	OPDS	h;
+	MYFLT *kx, *ky, *ihandle;
+	void *WidgAddress, *opcode;
+	MYFLT old_x, old_y;
+} FL_SET_HVS_VALUE;
+
 typedef struct	{
 	OPDS	h;
 	MYFLT max[MAXCHNLS];
