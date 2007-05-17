a7fbe886d71d4fe8176ba777c3b053791dda4c80
John ffitch <jpff@users.sourceforge.net>
2007-05-17 18:12:54 +0000

    added copyright message

--- Opcodes/eqfil.c
+++ Opcodes/eqfil.c
@@ -1,3 +1,26 @@
+/*
+    eqfil.c:
+
+    Copyright (C) 2007 Victor Lazzarini
+
+    This file is part of Csound.
+
+    The Csound Library is free software; you can redistribute it
+    and/or modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    Csound is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with Csound; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+    02111-1307 USA
+*/
+
 #include "csdl.h"
 
 typedef struct _equ {
@@ -6,19 +29,19 @@ typedef struct _equ {
   MYFLT *sig, *fr, *bw, *g, *ini;  /* in, freq, bw, gain, ini */
   double z1,z2;              /* delay memory */
   MYFLT frv, bwv;            /* bandwidth and frequency */
-  double c,d;                /* filter vars */
+  double c,d;                /* filter vars */  
 
 } equ;
 
 static int equ_init(CSOUND *csound, equ *p)
 {
-    if(*p->ini==0){
-        MYFLT sr = csound->GetSr(csound);
-    p->z1 = p->z2 = 0.0;
-    p->frv = *p->fr; p->bwv = *p->bw;
-    p->d = cos(2*PI*p->frv/sr);
-    p->c = tan(PI*p->bwv/sr);
-        }
+    if(*p->ini==0) {
+      MYFLT sr = csound->GetSr(csound);
+      p->z1 = p->z2 = 0.0;
+      p->frv = *p->fr; p->bwv = *p->bw;
+      p->d = cos(2*PI*p->frv/sr);
+      p->c = tan(PI*p->bwv/sr);
+    }
 
     return OK;
 }
@@ -29,7 +52,7 @@ static int equ_process(CSOUND *csound, equ *p)
     MYFLT  *in= p->sig,*out=p->out,g;
     int i, ksmps = csound->GetKsmps(csound);
 
-    if (*p->bw != p->bwv || *p->fr != p->frv) {
+    if(*p->bw != p->bwv || *p->fr != p->frv){
       MYFLT sr = csound->GetSr(csound);
       p->frv = *p->fr; p->bwv = *p->bw;
       p->d = cos(2*PI*p->frv/sr);
@@ -40,15 +63,17 @@ static int equ_process(CSOUND *csound, equ *p)
     a = (1.0-c)/(1.0+c);
     g = *p->g;
 
-    for (i=0; i < ksmps; i++) {
+    for(i=0; i < ksmps; i++){
       w = in[i] + d*(1.0 + a)*z1 - a*z2;
       y = w*a - d*(1.0 + a)*z1 + z2;
       z2 = z1;
       z1 = w;
       out[i] = (MYFLT) (0.5*(y + in[i] + g*(in[i] - y)));
+
     }
     p->z1 = z1;
     p->z2 = z2;
+
     return OK;
 }
 
