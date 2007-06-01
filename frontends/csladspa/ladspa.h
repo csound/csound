470788812bde2d29877e7e5ad7ffca09ccb6171d
John ffitch <jpff@users.sourceforge.net>
2007-06-01 09:19:41 +0000

    Fixed/bidged turnoff

--- frontends/csladspa/ladspa.h
+++ frontends/csladspa/ladspa.h
@@ -3,7 +3,7 @@
    Linux Audio Developer's Simple Plugin API Version 1.1[LGPL].
    Copyright (C) 2000-2002 Richard W.E. Furse, Paul Barton-Davis,
    Stefan Westerfeld.
-
+   
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Lesser General Public License
    as published by the Free Software Foundation; either version 2.1 of
