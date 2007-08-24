cfee03f543a1556999d712e50ce70fc95b43a013
Victor Lazzarini <veplaini@users.sourceforge.net>
2007-08-24 10:21:45 +0000

    allowing for Python 2.5 on OSX

--- custom.py
+++ custom.py
@@ -21,7 +21,6 @@ elif sys.platform == 'darwin':
     platform = 'darwin'
     customCPPPATH.append('/usr/include/malloc')
     customCPPPATH.append('/opt/local/include/boost-1_32/')
-    customCPPPATH.append('/usr/include/python2.3')
     customCXXFLAGS.append('-fabi-version=0')
 elif sys.platform[:3] == 'win':
     # For the basic build you need MinGW, MSys, and libsndfile.
