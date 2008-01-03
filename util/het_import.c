527191ee930ebfd0ac0e23681a850139a66b0598
John ffitch <jpff@users.sourceforge.net>
2008-01-03 13:05:53 +0000

    Spelling in messages

--- util/het_import.c
+++ util/het_import.c
@@ -41,7 +41,7 @@
 
 void het_import_usage(CSOUND *csound)
 {
-    csound->Message(csound, Str("Usage: het_import cstext_file het_file\n"));
+    csound->Message(csound, Str("Usage: het_import csvtext_file het_file\n"));
 }
 
 short getnum(FILE* inf, char *term)
