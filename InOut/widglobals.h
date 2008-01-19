9824bd983e07048b8336b7b7e9538c3140c127ad
Michael Gogins <gogins@users.sourceforge.net>
2008-01-19 21:08:25 +0000

    no message

--- InOut/widglobals.h
+++ InOut/widglobals.h
@@ -52,7 +52,9 @@
 
 using namespace std;
 
-# Undefine CS_VSTHOST to build FLTK without VST.
+/* 
+** Undefine CS_VSTHOST to build FLTK without VST. 
+*/
 #ifdef CS_VSTHOST
 #include "Opcodes/vst4cs/src/vsthost.h"
 #endif
