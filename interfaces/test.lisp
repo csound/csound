e1abe0c9e6fc98630b1ebe52ccde3c047ea70ac9
Michael Gogins <gogins@users.sourceforge.net>
2008-02-25 00:41:07 +0000

    Modified to run from the Csound development root directory.

--- interfaces/test.lisp
+++ interfaces/test.lisp
@@ -2,13 +2,14 @@
 ;; This file must be run from the interfaces directory,
 ;; e.g. "cd interfaces; clisp test.lisp"
 ;; You may need to edit directories specified below.
+(print "Run from Csound root directory.")
 (load "d:/utah/opt/cm/src/cm.lisp")
 (use-system :cffi)
-(load "csound.lisp")
-(load "filebuilding.lisp")
+(load "interfaces/csound.lisp")
+(load "interfaces/filebuilding.lisp")
 (cffi:load-foreign-library "csnd.dll")
 (setq cs (csound::csoundCreate nil))
-(cffi:with-foreign-string (csd "../examples/trapped.csd")
+(cffi:with-foreign-string (csd "examples/trapped.csd")
 			  (filebuilding::csoundCompileCsd cs csd))
 (setq *keep-playing* t result 0)
 (loop while (and *keep-playing* (eq result 0)) do
