5102f3ac7baa3744ce9c09a91d3ea134fddefdf1
Michael Gogins <gogins@users.sourceforge.net>
2008-03-01 20:19:15 +0000

    Sets up debug environment for Csound for MSVC.

---
+++ csound-msvc.bat
@@ -0,0 +1,7 @@
+echo SETTING ENVIRONMENT FOR CSOUNDD-MSVC...
+
+set PYTHONPATH=D:\utah\home\mkg\projects\csound-msvc-release
+set OPCODEDIR64=D:\utah\home\mkg\projects\csound-msvc-release
+set PATH=U:\liblo;%PATH%
+set PATH=U:\libsndfile-1_0_17;%PATH%
+set PATH=U:\pthreads\Pre-built.2\lib;%PATH%
