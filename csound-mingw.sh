3ac8a3ae83a49228d8b4c748baf20d1240617357
Michael Gogins <gogins@users.sourceforge.net>
2008-03-04 03:48:43 +0000

    no message

---
+++ csound-mingw.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+export PATH=/u/libsndfile-1_0_17:${PATH}
+export PATH=/u/portaudio/build/msvc/Win32/Release:${PATH}
+export PATH=/u/fluidsynth/winbuild:${PATH}
+export PATH=/u/Tcl/bin:${PATH}
+export PATH=/u/swigwin-1.3.33:${PATH}
+export PATH=/u/pd/bin:${PATH}
+export PATH=/u/liblo:${PATH}
+#export PATH=/u/Python25:/u//Python25/Scripts:${PATH}
+export RAWWAVE_PATH=/d/utah/home/mkg/projects/csound-mingw-release/Opcodes/stk/rawwaves
+export OPCODEDIR64=/d/utah/home/mkg/projects/csound-mingw-release
+export PYTHONPATH=/d/utah/home/mkg/projects/csound-mingw-release
+export PATH=/d/utah/home/mkg/projects/csound-mingw-release:${PATH}
+
+
