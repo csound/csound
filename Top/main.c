c84b5cdbfedac9a0aedaa10020f7b9947ed0c3c1
Victor Lazzarini <veplaini@users.sourceforge.net>
2008-04-23 17:51:19 +0000

    added message about .csoundrc

--- Top/main.c
+++ Top/main.c
@@ -142,16 +142,22 @@ PUBLIC int csoundCompile(CSOUND *csound, int argc, char **argv)
         if (fd == NULL)
           csoundMessage(csound, Str("WARNING: cannot open csoundrc file %s\n"),
                                 csrcname);
+        else
+          csound->Message(csound, "Reading options from $CSOUNDRC: %s \n", 
+                           csrcname);
       }
       if (fd == NULL && ((home_dir = csoundGetEnv(csound, "HOME")) != NULL &&
                          home_dir[0] != '\0')) {
         s = csoundConcatenatePaths(csound, home_dir, ".csoundrc");
         fd = csound->FileOpen2(csound, &csrc, CSFILE_STD, s, "r", NULL,
                                CSFTYPE_OPTIONS, 0);
+        if(fd != NULL) 
+	  csound->Message(csound, "Reading options from $HOME/.csoundrc\n");
         mfree(csound, s);
       }
       /* read global .csoundrc file (if exists) */
       if (fd != NULL) {
+        
         readOptions(csound, csrc, 0);
         csound->FileClose(csound, fd);
       }
@@ -160,6 +166,7 @@ PUBLIC int csoundCompile(CSOUND *csound, int argc, char **argv)
                              CSFTYPE_OPTIONS, 0);
       if (fd != NULL) {
         readOptions(csound, csrc, 0);
+        csound->Message(csound, "Reading options from local directory .csoundrc \n");
         csound->FileClose(csound, fd);
       }
     }
