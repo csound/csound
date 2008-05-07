402e5135b23a4e3e7d8b2696314c7eadb396c6b1
Victor Lazzarini <veplaini@users.sourceforge.net>
2008-05-07 17:04:52 +0000

    *** empty log message ***

--- Opcodes/py/pycall-gen.py
+++ Opcodes/py/pycall-gen.py
@@ -2,19 +2,22 @@
 
 # Copyright (C) 2002 Maurizio Umberto Puxeddu
 
-# This software is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either
-# version 2 of the License, or (at your option) any later version.
-#
-# This software is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this software; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+#    This file is part of Csound.
+
+#    The Csound Library is free software; you can redistribute it
+#    and/or modify it under the terms of the GNU Lesser General Public
+#    License as published by the Free Software Foundation; either
+#    version 2.1 of the License, or (at your option) any later version.
+
+#    Csound is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Lesser General Public License for more details.
+
+#    You should have received a copy of the GNU Lesser General Public
+#    License along with Csound; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+#    02111-1307 USA
 
 '''Automatically generate opcode structures, methods [and table entries]. Just run it.'''
 
