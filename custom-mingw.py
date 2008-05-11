247e32d2730263c027b98edfeacca6c1292b40f3
Michael Gogins <gogins@users.sourceforge.net>
2008-05-11 16:07:31 +0000

    Added include path to Gmm++ for linear algebra opcodes.

--- custom-mingw.py
+++ custom-mingw.py
@@ -129,11 +129,10 @@ if sys.platform[:3] == 'win':
 	customCPPPATH.append(r'U:\pure-data\trunk\pd\src')
 	customLIBPATH.append(r'U:\pd\bin')
 	################################################################
-	# If you want to use ATLAS BLAS and LAPACK,
-	# you need ATLAS built with LAPACK. 
-	# add it here (you do NOT need to build it first):
-	customCPPPATH.append(r'U:\ATLAS\WinNT_P4SSE2\include')
-	customLIBPATH.append(r'U:\ATLAS\WinNT_P4SSE2\lib')
+	# If you want to build the linear algebra opcodes,
+	# you need Gmm++ (it is only header files and doesn't need to be
+        # built first).
+	customCPPPATH.append(r'U:\gmm-3.0\include')
 	################################################################
 	# If you want to build the Loris opcodes,
 	# then copy Loris to csound5\Opcodes\Loris. Loris also
