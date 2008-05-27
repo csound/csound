cbd5980d483d5f5e345ddd5f3407e4d9618b0716
Victor Lazzarini <veplaini@users.sourceforge.net>
2008-05-27 12:29:33 +0000

    new plugin ftable

--- Opcodes/dcblockr.c
+++ Opcodes/dcblockr.c
@@ -1,7 +1,7 @@
 /*
     dcblockr.c:
 
-    Copyright (C) 1998 John ffitch
+    Copyright (C) 1998 John ffitch, 2008 V Lazzarini 
 
     This file is part of Csound.
 
@@ -64,10 +64,121 @@ static int dcblockr(CSOUND *csound, DCBlocker* p)
     return OK;
 }
 
+/*******************************************/
+/*  DC Blocking Filter                     */
+/*  Improved DC attenuation                */
+/*  V Lazzarini                            */
+/*******************************************/
+
+typedef struct _dcblk2 {
+  OPDS h;
+  MYFLT *output;
+  MYFLT *input, *order, *iskip;
+  AUXCH delay1;
+  AUXCH iirdelay1, iirdelay2, iirdelay3, iirdelay4;
+  double ydels[4];
+  int dp1,dp2;
+  double scaler;
+
+} DCBlock2;
+
+
+static int dcblock2set(CSOUND *csound, DCBlock2* p)
+{
+  int order = (int) *p->order;
+  if(order == 0) order = 128;
+  else if(order < 4) order = 4;
+
+  if(p->delay1.auxp == NULL ||
+     p->delay1.size < (order-1)*2*sizeof(double))
+      csound->AuxAlloc(csound, (order-1)*2*sizeof(double), 
+            &p->delay1);
+     if(p->iirdelay1.auxp == NULL ||
+      p->iirdelay1.size < (order)*sizeof(double))
+      csound->AuxAlloc(csound,  
+		       (order)*sizeof(double), &p->iirdelay1);
+
+     if(p->iirdelay2.auxp == NULL ||
+       p->iirdelay2.size < (order)*sizeof(double))
+      csound->AuxAlloc(csound,  
+		       (order)*sizeof(double), &p->iirdelay2);
+
+
+     if(p->iirdelay3.auxp == NULL ||
+      p->iirdelay3.size < (order)*sizeof(double))
+      csound->AuxAlloc(csound,  
+		       (order)*sizeof(double), &p->iirdelay3);
+
+     if(p->iirdelay4.auxp == NULL ||
+       p->iirdelay4.size < (order)*sizeof(double))
+      csound->AuxAlloc(csound,  
+		       (order)*sizeof(double), &p->iirdelay4);
+
+   p->scaler = 1./order;
+  if(!*p->iskip){
+    p->ydels[0] = 0.0;   p->ydels[1] = 0.0;
+    p->ydels[2] = 0.0;   p->ydels[3] = 0.0;
+    memset(p->delay1.auxp, 0, sizeof(double)*(order-1)*2);
+    memset(p->iirdelay1.auxp, 0, sizeof(double)*(order));
+    memset(p->iirdelay2.auxp, 0, sizeof(double)*(order));
+    memset(p->iirdelay3.auxp, 0, sizeof(double)*(order));
+    memset(p->iirdelay4.auxp, 0, sizeof(double)*(order));
+    p->dp1 = 0; p->dp2 = 0;
+   }
+    return OK;
+}
+
+static int dcblock2(CSOUND *csound, DCBlock2* p)
+{
+  MYFLT *in = p->input;
+  MYFLT *out = p->output;
+  double *del1 = (double *)p->delay1.auxp;
+  double *iirdel[4],x1,x2,y,del;
+  double  *ydels = p->ydels;
+  double scale = p->scaler; 
+  int *dp1 = &(p->dp1);
+  int *dp2 = &(p->dp2);
+  int i,j,del1size, iirdelsize;
+
+  iirdel[0] = (double *) p->iirdelay1.auxp; 
+  iirdel[1] = (double *) p->iirdelay2.auxp; 
+  iirdel[2] = (double *) p->iirdelay3.auxp; 
+  iirdel[3] = (double *) p->iirdelay4.auxp;
+ 
+  del1size = p->delay1.size/sizeof(double);
+  iirdelsize = p->iirdelay1.size/sizeof(double);
+
+  for(i=0; i < csound->ksmps; i++){
+ 
+   /* long delay */
+   del = del1[*dp1];
+   del1[*dp1] = x1 = in[i];
+
+   /* IIR cascade */
+   for(j=0; j < 4; j++){
+     x2 = iirdel[j][*dp2];
+     iirdel[j][*dp2] = x1;
+     y = x1 - x2 + ydels[j];
+     ydels[j] = y;
+     x1 = y*scale;
+   }
+   out[i] = del - x1; 
+
+   *dp1 = (*dp1 == del1size-1 ? 0 : *dp1 + 1);
+   *dp2 = (*dp2 == iirdelsize-1 ? 0 : *dp2 + 1);
+}
+  return OK;
+
+}
+
+
+
+
 #define S(x)    sizeof(x)
 
 static OENTRY localops[] = {
-{ "dcblock", S(DCBlocker), 5, "a", "ao", (SUBR)dcblockrset, NULL, (SUBR)dcblockr}
+  { "dcblock", S(DCBlocker), 5, "a", "ao", (SUBR)dcblockrset, NULL, (SUBR)dcblockr},
+{ "dcblock2", S(DCBlock2), 5, "a", "aoo", (SUBR)dcblock2set, NULL, (SUBR)dcblock2}
 };
 
 int dcblockr_init_(CSOUND *csound)
--- Opcodes/ftest.c
+++ Opcodes/ftest.c
@@ -17,8 +17,28 @@ static int tanhtable(FGDATA *ff, FUNC *ftp)
     return OK;
 }
 
+static int exptable(FGDATA *ff, FUNC *ftp)
+{
+ /* CSOUND  *csound = ff->csound; */
+    MYFLT   *fp = ftp->ftable;
+    MYFLT   start = ff->e.p[5];
+	MYFLT   end   = ff->e.p[6];
+    MYFLT   resc = ff->e.p[7];
+    double  step = (double) (end - start) / (double) ftp->flen, x;
+    int     i;
+  
+    for (i = 0, x = start; i <= (int) ftp->flen; i++, x += step)
+      fp[i] = (MYFLT) exp(x);
+
+	if(resc!=0.0) ff->e.p[4] = -1;
+    return OK;
+}
+
+
+
 static NGFENS localfgens[] = {
    { "tanh", tanhtable },
+   { "exp", exptable },
    { NULL, NULL }
 };
 
