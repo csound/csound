9cfd759384ba318ce360c13d467e884e6c9e1b1e
Felipe Sateler Perez <fsateler@users.sourceforge.net>
2008-07-25 21:55:17 +0000

    Optimize clean script

--- cleanup.sh
+++ cleanup.sh
@@ -16,32 +16,55 @@ fi
 "$0" remove ".sconf_temp"
 "$0" remove "config.log"
 
-find . -type f -iname ".scons*" -exec "$0" remove '{}' \;
-find . -type f -iname "*.xmg"   -exec "$0" remove '{}' \;
-find . -type f -iname "*.o"     -exec "$0" remove '{}' \;
-find . -type f -iname "*.os"    -exec "$0" remove '{}' \;
-find . -type f -iname "*.so"    -exec "$0" remove '{}' \;
-find . -type f -iname "*.a"     -exec "$0" remove '{}' \;
-find . -type f -iname "*.wav"   -exec "$0" remove '{}' \;
-find . -type f -iname "*.aif*"  -exec "$0" remove '{}' \;
-find . -type f -iname "*.sf"    -exec "$0" remove '{}' \;
-find . -type f -iname "*.pcm"   -exec "$0" remove '{}' \;
-find . -type f -iname "*.exe"   -exec "$0" remove '{}' \;
-find . -type f -iname "*.dll"   -exec "$0" remove '{}' \;
-find . -type f -iname "*.obj"   -exec "$0" remove '{}' \;
-find . -type f -iname "*.lib"   -exec "$0" remove '{}' \;
-find "./interfaces" -type f -iname "*.java" -exec "$0" remove '{}' \;
-find "./interfaces" -type f -iname "*.class" -exec "$0" remove '{}' \;
+patterns='*.xmg *.o *.os *.so *.a *.wav *.aif* *.sf *.pcm *.exe *.dll
+*.obj *.lib *.class'
+findexpr='-iname .scons*'
+for pat in $patterns; do
+	findexpr="$findexpr -o -iname $pat"
+done
+findexpr="( $findexpr )"
 
-find . -type f -exec file '{}' \; | grep -G -e ':.*\<ELF\>' | cut -d ':' -f 1 | xargs -n 1 "$0" remove
-find . -type f -exec file '{}' \; | grep -G -e ':.*\<80386\>' | cut -d ':' -f 1 | xargs -n 1 "$0" remove
-find . -type f -exec file '{}' \; | grep -G -e ':.*\<PE\>' | cut -d ':' -f 1 | xargs -n 1 "$0" remove
+find . -type f $findexpr -exec "$0" remove '{}'  \;
+
+#find . -type f -iname ".scons*" -exec "$0" remove '{}' \;
+#find . -type f -iname "*.xmg"   -exec "$0" remove '{}' \;
+#find . -type f -iname "*.o"     -exec "$0" remove '{}' \;
+#find . -type f -iname "*.os"    -exec "$0" remove '{}' \;
+#find . -type f -iname "*.so"    -exec "$0" remove '{}' \;
+#find . -type f -iname "*.a"     -exec "$0" remove '{}' \;
+#find . -type f -iname "*.wav"   -exec "$0" remove '{}' \;
+#find . -type f -iname "*.aif*"  -exec "$0" remove '{}' \;
+#find . -type f -iname "*.sf"    -exec "$0" remove '{}' \;
+#find . -type f -iname "*.pcm"   -exec "$0" remove '{}' \;
+#find . -type f -iname "*.exe"   -exec "$0" remove '{}' \;
+#find . -type f -iname "*.dll"   -exec "$0" remove '{}' \;
+#find . -type f -iname "*.obj"   -exec "$0" remove '{}' \;
+#find . -type f -iname "*.lib"   -exec "$0" remove '{}' \;
+find "./interfaces" -type f -iname "*.java" -exec "$0" remove '{}' \;
+#find "./interfaces" -type f -iname "*.class" -exec "$0" remove '{}' \;
+
+notexecpatterns="*.cpp *.cxx *.h *.hpp *.fl *.i *.csd *.py *.sh *.sco *.orc
+	*.txt *.tcl"
+notexecexpr="-iname *.c"
+for pat in $notexecpatterns; do
+	notexecexpr="$notexecexpr -o -iname $pat"
+done
+notexecexpr="( $notexecexpr )"
+
+find . -type f \! $notexecexpr -exec file '{}' \; | grep -E -e ':.*\<(ELF|80386|PE)\>' | cut -d ':' -f 1 | xargs -n 1 "$0" remove
+#find . -type f \! $notexecexpr -exec file '{}' \; | grep -G -e ':.*\<80386\>' | cut -d ':' -f 1 | xargs -n 1 "$0" remove
+#find . -type f \! $notexecexpr -exec file '{}' \; | grep -G -e ':.*\<PE\>' | cut -d ':' -f 1 | xargs -n 1 "$0" remove
 
 "$0" remove "./frontends/CsoundVST/CsoundVST_wrap.cc"
 "$0" remove "./frontends/CsoundVST/CsoundVST_wrap.h"
 "$0" remove "./CsoundVST.py"
 "$0" remove "./CsoundVST.pyc"
 "$0" remove "./CsoundVST.pyo"
+"$0" remove "./frontends/CsoundAC/CsoundAC_wrap.cc"
+"$0" remove "./frontends/CsoundAC/CsoundAC_wrap.h"
+"$0" remove "./CsoundAC.py"
+"$0" remove "./CsoundAC.pyc"
+"$0" remove "./CsoundAC.pyo"
 "$0" remove "./interfaces/python_interface_wrap.cc"
 "$0" remove "./interfaces/python_interface_wrap.h"
 "$0" remove "./interfaces/java_interface_wrap.cc"
@@ -61,6 +84,7 @@ find . -type f -exec file '{}' \; | grep -G -e ':.*\<PE\>' | cut -d ':' -f 1 | x
 "$0" remove "./loris.pyo"
 "$0" remove "./frontends/winsound/winsound.cxx"
 "$0" remove "./frontends/winsound/winsound.h"
+"$0" remove "./frontends/winsound/winsound.cpp"
 "$0" remove "./frontends/CsoundVST/ScoreGeneratorVST_wrap.cc"
 "$0" remove "./frontends/CsoundVST/ScoreGeneratorVST_wrap.h"
 "$0" remove "./scoregen.py"
