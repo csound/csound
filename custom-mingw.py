a05eda95977fddd6c6e7034d1c15293364d0bb4d
Michael Gogins <gogins@users.sourceforge.net>
2008-09-11 22:53:09 +0000

    no message

--- custom-mingw.py
+++ custom-mingw.py
@@ -65,6 +65,9 @@ if sys.platform[:3] == 'win':
 	# If you want to build the FLTK widgets, csound5gui, cseditor,
 	# CsoundAC, or CsoundVST, FLTK 1.1x is required.
 	customCPPPATH.append(r'U:\fltk-mingw')
+        # Import libraries MUST come first.
+        customLIBPATH.append(r'U:]fltk-mingw\src')
+        # Then static libraries.
 	customLIBPATH.append(r'U:\fltk-mingw\lib')
 	################################################################
 	# If you want to build the image opcodes,
@@ -103,15 +106,15 @@ if sys.platform[:3] == 'win':
 	################################################################
 	# If you want to build the FluidSynth opcodes,
 	# you need the FluidSynth DLL (not .lib).
-	customLIBPATH.append(r'U:\fluidsynth\winbuild')
-	customCPPPATH.append(r'U:\fluidsynth\include')
+	customLIBPATH.append(r'U:\fluidsynth.patched\winbuild')
+	customCPPPATH.append(r'U:\fluidsynth.patched\include')
 	# And the dsound library from the Microsoft DirectX SDK.
 	customLIBPATH.append(r'U:\dxsdk\Lib\x86')
 	################################################################
 	# If you want to build the Java wrapper for CsoundVST
 	# you need Java. 
-	customCPPPATH.append(r'U:\jdk1.5.0\include')
-	customCPPPATH.append(r'U:\jdk1.5.0\include\win32')
+	customCPPPATH.append(r'U:\Java\jdk1.6.0_10\include')
+	customCPPPATH.append(r'U:\Java\jdk1.6.0_10\include\win32')
 	################################################################
 	# If you want to build the OSC opcodes,
 	# you need liblo.
