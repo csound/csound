6b6cfd169d0b14f08db431453dd8a50b0e787640
Michael Gogins <gogins@users.sourceforge.net>
2008-09-21 16:27:12 +0000

    no message

---
+++ csound-mingw-float.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+export PATH=/u/Mega-Nerd/libsndfile:${PATH}
+export PATH=/u/fltk-mingw/src:${PATH}
+export PATH=/u/portaudio/build/msvc/Win32/Release:${PATH}
+export PATH=/u/fluidsynth/winbuild:${PATH}
+export PATH=/u/Tcl/bin:${PATH}
+export PATH=/u/swigwin-1.3.36:${PATH}
+export PATH=/u/pd/bin:${PATH}
+export PATH=/u/liblo:${PATH}
+export PATH=/u/Java/jdk1.6.0_10/bin:${PATH}
+#export PATH=/u/Python25:/u//Python25/Scripts:${PATH}
+export PATH=/d/utah/home/mkg/projects/csound-mingw-release:${PATH}
+export PATH=${PATH}:/d/utah/usr/bin
+export PATH=${PATH}:/u/ATT/Graphviz/bin
+export PATH=${PATH}:/u/MiKTeX-2.5/miktex/bin
+export PATH=${PATH}:/u/doxygen/bin
+export PATH=${PATH}:/u/ImageMagick-6.0.5-Q16
+export RAWWAVE_PATH=/d/utah/home/mkg/projects/csoundf/Opcodes/stk/rawwaves
+export OPCODEDIR=/d/utah/home/mkg/projects/csoundf
+export PYTHONPATH=/d/utah/home/mkg/projects/csoundf
+
+
