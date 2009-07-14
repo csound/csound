cf1d3a553b56577324319eb7d63a49931dab2209
Michael Gogins <gogins@users.sourceforge.net>
2009-07-14 23:16:02 +0000

    *** empty log message ***

--- examples/lua/lua_example.lua
+++ examples/lua/lua_example.lua
@@ -1,23 +1,20 @@
 #!/usr/bin/lua
 
--- On Linux this should work:
--- require "csnd"
+require "luaCsnd"
 
-require "csnd"
-
-csound = csnd.Csound()
-args = csnd.CsoundArgVList()
+csound = luaCsnd.Csound()
+args = luaCsnd.CsoundArgVList()
 args_ = {"csound", "-o", "test.wav", "examples/trapped.csd"}
 for i,v in ipairs(args_) do
     args:Append(v)
 end
 csound:Compile(args.argc(args), args.argv(args))
-pt = csnd.CsoundPerformanceThread(csound)
+pt = luaCsnd.CsoundPerformanceThread(csound)
 pt:SetScoreOffsetSeconds(21.5)
 pt:Play()
-csnd.csoundSleep(10000)
+luaCsnd.csoundSleep(10000)
 pt:SetScoreOffsetSeconds(152)
-csnd.csoundSleep(10000)
+luaCsnd.csoundSleep(10000)
 pt:Stop()
 pt:Join()
 csound:Reset()
