b38000a23d0a4ee555acb83fd8f5fd2bd34f7e36
Michael Gogins <gogins@users.sourceforge.net>
2009-08-25 01:02:13 +0000

    no message

--- Opcodes/doppler.cpp
+++ Opcodes/doppler.cpp
@@ -344,7 +344,7 @@ extern "C"
   
   PUBLIC int csoundModuleDestroy(CSOUND *csound)
   {
-    csound->Message(csound, "Deleting C++ objects from doppler...\n");
+    //csound->Message(csound, "Deleting C++ objects from doppler...\n");
     for (std::list<RCLowpassFilter *>::iterator it = smoothingFilterInstances.begin();
 	 it != smoothingFilterInstances.end();
 	 ++it) {
