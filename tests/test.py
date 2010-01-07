56b10542f91827470ee57759abd4c4bfe97e874f
John ffitch <jpff@users.sourceforge.net>
2010-01-07 16:39:04 +0000

    conditional expresiontest

--- tests/test.py
+++ tests/test.py
@@ -70,6 +70,7 @@ def runTest():
         ["test31.csd", "i-rate conditional test"],
         ["test32.csd", "continuation lines test"],
         ["test33.csd", "using named instrument from score (testing score strings)"],
+        ["test34.csd", "conditional expressions",
     ]
 
 
---
+++ tests/test34.csd
@@ -0,0 +1,24 @@
+<CsoundSynthesizer>
+<CsOptions>
+-n
+</CsOptions>
+<CsInstruments>
+
+sr = 44100
+ksmps = 128
+nchnls = 1
+0dbfs = 1
+
+instr 1
+ii = (p3<10 ? 1 : 2)
+print ii
+endin
+
+</CsInstruments>
+<CsScore>
+f 1 0 1024 10 1
+i 1 0 20
+e
+
+</CsScore>
+</CsoundSynthesizer>
