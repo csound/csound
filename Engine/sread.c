f7f45d72dd9de65ce72bac5f66a39d57828cff17
John ffitch <jpff@users.sourceforge.net>
2010-06-02 17:54:22 +0000

    fiddling with multiple strings

--- Engine/sread.c
+++ Engine/sread.c
@@ -1294,11 +1294,12 @@ static void ifa(CSOUND *csound)
       else switch (ST(bp)->pcnt) {      /*  watch for p1,p2,p3, */
       case 1:                           /*   & MYFLT, setinsno..*/
         if ((ST(op) == 'i' || ST(op) == 'q') && *ST(sp) == '"') {
-          ST(bp)->p1val = ((int[4]){SSTRCOD,SSTRCOD1,SSTRCOD2,SSTRCOD3})[csound->scnt0++];
-          if (csound->scnt0>3) {
-            csound->scnt0 = 3;
-          }
-          /* ST(bp)->p1val = SSTRCOD;      /\* allow string name *\/ */
+        /*   printf("***Entering second dubious code scnt=%d\n", csound->scnt0); */
+        /*   ST(bp)->p1val = ((int[4]){SSTRCOD,SSTRCOD1,SSTRCOD2,SSTRCOD3})[csound->scnt0++]; */
+        /*   if (csound->scnt0>3) { */
+        /*     csound->scnt0 = 3; */
+        /*   } */
+          ST(bp)->p1val = SSTRCOD;      /* allow string name */
         }
         else
           ST(bp)->p1val = stof(csound, ST(sp));
