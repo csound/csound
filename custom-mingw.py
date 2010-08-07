28d7b355a9a0993660929badebc43e906f5b07cf
Michael Gogins <gogins@users.sourceforge.net>
2010-08-07 02:19:24 +0000

    no message Committed on the Free edition of March Hare Software CVSNT Client. Upgrade to CVS Suite for more features and support: http://march-hare.com/cvsnt/

--- custom-mingw.py
+++ custom-mingw.py
@@ -20,12 +20,7 @@ Usage (for example):
 scons custom=custom-mingw.py useDouble=1 etc...
 
 This file assumes that all of Csound's third-party dependencies have
-been installed, or built, in "U:\". On my computer, this is a network
-mapping to the actual directory.
-
-If you also have installed or built all your open source libraries
-in one directory, you also can map that directory to "U:\" and 
-use this customization file without modification.
+been installed, or built, in "C:\utah\opt\". 
 '''
 import sys
 
@@ -44,113 +39,114 @@ if sys.platform[:3] == 'win':
  	################################################################
 	# If you want to build Csound,
 	# libsndfile is REQUIRED.
-	customCPPPATH.append(r'U:\Mega-Nerd\libsndfile\include')
-	customLIBPATH.append(r'U:\Mega-Nerd\libsndfile')
+	customCPPPATH.append(r'C:\utah\opt\Mega-Nerd\libsndfile\include')
+	customLIBPATH.append(r'C:\utah\opt\Mega-Nerd\libsndfile')
 	################################################################
 	# If you want to build PortAudio,
 	# for real-time audio performance.
-	customCPPPATH.append(r'U:\portaudio\include')
-	customLIBPATH.append(r'U:\portaudio')
+	customCPPPATH.append(r'C:\utah\opt\portaudio\include')
+	customLIBPATH.append(r'C:\utah\opt\portaudio')
 	#################################################################
 	# If you want to build PortMidi,
 	# for real-time MIDI performance.,
 	# you need both portmidi and porttime.
-	customCPPPATH.append(r'U:\portmidi\pm_common')
-	customCPPPATH.append(r'U:\portmidi\pm_win')
-	customCPPPATH.append(r'U:\portmidi\porttime')
-	customLIBPATH.append(r'U:\portmidi')
+	customCPPPATH.append(r'C:\utah\opt\portmidi\pm_common')
+	customCPPPATH.append(r'C:\utah\opt\portmidi\pm_win')
+	customCPPPATH.append(r'C:\utah\opt\portmidi\porttime')
+	customLIBPATH.append(r'C:\utah\opt\portmidi')
 	################################################################
 	# If you want to build the FLTK widgets, csound5gui, cseditor,
 	# CsoundAC, or CsoundVST, FLTK 1.1x is required.
-	customCPPPATH.append(r'U:\fltk-mingw')
+	customCPPPATH.append(r'C:\utah\opt\fltk-mingw')
         # Import libraries MUST come first.
-        customLIBPATH.append(r'U:\fltk-mingw\src')
+        customLIBPATH.append(r'C:\utah\opt\fltk-mingw\src')
         # Then static libraries.
-	customLIBPATH.append(r'U:\fltk-mingw\lib')
+	customLIBPATH.append(r'C:\utah\opt\fltk-mingw\lib')
 	################################################################
 	# If you want to build the image opcodes,
 	# libpng and libz are required.
-	customCPPPATH.append(r'U:\zlib-1.2.3.win32\include')
-	customLIBPATH.append(r'U:\zlib-1.2.3.win32\lib')
-	customLIBPATH.append(r'U:\zlib-1.2.3.win32\bin')
-	customCPPPATH.append(r'U:\libpng-1.2.24')
-	customLIBPATH.append(r'U:\libpng-1.2.24\.libs')
+	customCPPPATH.append(r'C:\utah\opt\zlib-1.2.3.win32\include')
+	customLIBPATH.append(r'C:\utah\opt\zlib-1.2.3.win32\lib')
+	customLIBPATH.append(r'C:\utah\opt\zlib-1.2.3.win32\bin')
+	customCPPPATH.append(r'C:\utah\opt\libpng-1.2.24')
+	customLIBPATH.append(r'C:\utah\opt\libpng-1.2.24\.libs')
 	################################################################
 	# If you want to build the Python opcodes, the Python interfaces
 	# to Csound, or CsoundAC, Python 2.5 is required.
-	customCPPPATH.append(r'U:\Python26\include')
-	customLIBPATH.append(r'U:\Python26\libs')
+	customCPPPATH.append(r'C:\utah\opt\Python26\include')
+	customLIBPATH.append(r'C:\utah\opt\Python26\libs')
 	################################################################
 	# If you want to build CsoundVST, you need the 
 	# Steinberg VST SDK version 2.4.
-	customCPPPATH.append(r'U:\vstsdk2.4')    
+	customCPPPATH.append(r'C:\utah\opt\vstsdk2.4')    
 	################################################################
 	# If you want to build CsoundAC, you need the 
 	# boost C++ libraries.
-	customCPPPATH.append(r'U:\boost')
+	customCPPPATH.append(r'C:\utah\opt\boost')
 	################################################################
 	# If you want to build scoregen, you need the include
 	# path to VST MIDI plugin SDK header files. 
-	customCPPPATH.append(r'U:\VSTModuleArchitectureSDK\pluginterfaces\base')    
+	customCPPPATH.append(r'C:\utah\opt\VSTModuleArchitectureSDK\pluginterfaces\base')    
 	################################################################
 	# If you want to build Lua interfaces, you need Lua.
-	customCPPPATH.append(r'U:\Lua5.1')
-	customLIBPATH.append(r'U:\Lua5.1')
+	customCPPPATH.append(r'C:\utah\opt\Lua5.1')
+	customLIBPATH.append(r'C:\utah\opt\Lua5.1')
 	################################################################
 	# If you want to build Tcl\Tk interfaces
 	# or Tclcsound, you need Tcl\Tk.
 	# Add it here:
-	customCPPPATH.append(r'U:\Tcl\include')
-	customLIBPATH.append(r'U:\Tcl\lib')
+	customCPPPATH.append(r'C:\utah\opt\Tcl\include')
+	customLIBPATH.append(r'C:\utah\opt\Tcl\lib')
 	################################################################
 	# If you want to build the FluidSynth opcodes,
 	# you need the FluidSynth DLL (not .lib).
-	customLIBPATH.append(r'U:\fluidsynth-1.0.9\src\.libs')
-	customCPPPATH.append(r'U:\fluidsynth-1.0.9\include')
+	customLIBPATH.append(r'C:\utah\opt\fluidsynth-1.0.9\src\.libs')
+	customCPPPATH.append(r'C:\utah\opt\fluidsynth-1.0.9\include')
 	# And the dsound library from the Microsoft DirectX SDK.
-	customLIBPATH.append(r'U:\dxsdk\Lib\x86')
+	customLIBPATH.append(r'C:\utah\opt\dxsdk\Lib\x86')
 	################################################################
 	# If you want to build the Java wrapper for CsoundVST
 	# you need Java. 
-	customCPPPATH.append(r'U:\Java\jdk1.6.0_14\include')
-	customCPPPATH.append(r'U:\Java\jdk1.6.0_14\include\win32')
+	customCPPPATH.append(r'C:\utah\opt\Java\jdk1.6.0_14\include')
+	customCPPPATH.append(r'C:\utah\opt\Java\jdk1.6.0_14\include\win32')
 	################################################################
 	# If you want to build the OSC opcodes,
 	# you need liblo.
-	customCPPPATH.append(r'U:\liblo-0.26')
-	customCPPPATH.append(r'U:\liblo-0.26\lo')
-	customCPPPATH.append(r'U:\liblo-0.26\src')
-	customLIBPATH.append(r'U:\liblo-0.26')
+	customCPPPATH.append(r'C:\utah\opt\liblo')
+	customCPPPATH.append(r'C:\utah\opt\liblo\lo')
+	customCPPPATH.append(r'C:\utah\opt\liblo\src')
+	customLIBPATH.append(r'C:\utah\opt\liblo')
 	# For OSC on Windows, you will also need a Windows pthread library.
-	customCPPPATH.append(r'U:\pthreads\Pre-built.2\include')
-	customLIBPATH.append(r'U:\pthreads\Pre-built.2\lib')
+	customCPPPATH.append(r'C:\utah\opt\pthreads\Pre-built.2\include')
+	customLIBPATH.append(r'C:\utah\opt\pthreads\Pre-built.2\lib')
 	################################################################
 	# If you want to build the Pure Data external csoundapi~,
 	# you need Pure Data. 
 	# Add it here (you do NOT need to build it first):
-	customCPPPATH.append(r'U:\Pd-0.41.4-extended\pd\src')
-	customLIBPATH.append(r'U:\pd\bin')
+	customCPPPATH.append(r'C:\utah\opt\pure-data\pd\src')
+	# But this should installed from the pre-built version:
+	customLIBPATH.append(r'C:\utah\opt\pd\bin')
 	################################################################
 	# If you want to build the linear algebra opcodes, 
 	# you need Gmm++ (headers only).
-	customCPPPATH.append(r'U:\gmm-3.1\include')
+	customCPPPATH.append(r'C:\utah\opt\gmm-4.1\include')
 	################################################################
 	# If you want to build the Loris opcodes,
 	# then copy Loris to csound5\Opcodes\Loris. Loris also
 	# requires FFTW, if it is not in a standard location,
 	# add it here (you do need to build it first):
-	#customCPPPATH.append(r'U:\fftw-3.0.1\api')
-	#customLIBPATH.append(r'U:\fftw-3.0.1\.libs')
+	#customCPPPATH.append(r'C:\utah\opt\fftw-3.0.1\api')
+	#customLIBPATH.append(r'C:\utah\opt\fftw-3.0.1\.libs')
 	################################################################
 	# If you want to build MusicXML import and export, 
 	# you need the MusicXML library.
         # Add it here (the CodeBlocks build seems to work).
-	customCPPPATH.append(r'U:\musicxml-v2\src\elements') 
-	customCPPPATH.append(r'U:\musicxml-v2\src\files') 
-	customCPPPATH.append(r'U:\musicxml-v2\src\visitors') 
-	customCPPPATH.append(r'U:\musicxml-v2\src\lib') 
-	customCPPPATH.append(r'U:\musicxml-v2\src\parser') 
-	customLIBPATH.append(r'U:\musicxml-v2\win32\codeblocks')
+	customCPPPATH.append(r'C:\utah\opt\musicxml-v2\src\elements') 
+	customCPPPATH.append(r'C:\utah\opt\musicxml-v2\src\files') 
+	customCPPPATH.append(r'C:\utah\opt\musicxml-v2\src\visitors') 
+	customCPPPATH.append(r'C:\utah\opt\musicxml-v2\src\lib') 
+	customCPPPATH.append(r'C:\utah\opt\musicxml-v2\src\parser') 
+	customLIBPATH.append(r'C:\utah\opt\musicxml-v2\win32\codeblocks')
 	# print "Adding custom path."
 else:
 	platform = 'unsupported platform'
