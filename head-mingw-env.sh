9e255403daaa7cdbb19d10d39c3d727165829224
Michael Gogins <gogins@users.sourceforge.net>
2010-08-17 23:24:36 +0000

    no message Committed on the Free edition of March Hare Software CVSNT Client. Upgrade to CVS Suite for more features and support: http://march-hare.com/cvsnt/

---
+++ head-mingw-env.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+echo Setting up environment to run Csound from build area...
+export PATH=/c/utah/opt/Mega-Nerd/libsndfile:${PATH}
+export PATH=/c/utah/opt/fltk/branch-1.3/bin:${PATH}
+export PATH=/c/utah/opt/fltk/branch-1.3/lib:${PATH}
+export PATH=/c/utah/opt/portaudio:${PATH}
+export PATH=/c/utah/opt/portmidi:${PATH}
+export PATH=/c/utah/opt/Tcl/bin:${PATH}
+export PATH=/c/utah/opt/swigwin-2.0.0:${PATH}
+export PATH=/c/utah/opt/pd/bin:${PATH}
+export PATH=/c/utah/opt/liblo:${PATH}
+export PATH=/c/utah/opt/Java/jdk1.6.0_21/bin:${PATH}
+export PATH=/c/utah/opt/fluidsynth/src/Release:${PATH}
+export PATH=/c/utah/opt/luajit-2.0/src:${PATH}
+export PATH=/d/utah/home/mkg/csound/head:${PATH}
+export PATH=${PATH}:/c/utah/opt/Graphviz/bin
+export PATH=${PATH}:/c/utah/opt/MiKTeX-2.8/miktex/bin
+export PATH=${PATH}:/c/utah/opt/doxygen/bin
+export PATH=${PATH}:/c/utah/opt/ImageMagick-6.6.3-Q16
+export PATH=${PATH}:/c/utah/opt/Python26
+export PATH=${PATH}:/c/utah/opt/Python26/Scripts
+export RAWWAVE_PATH=D:/utah/home/mkg/csound/head/Opcodes/stk/rawwaves
+export OPCODEDIR64=D:/utah/home/mkg/csound/head
+export PYTHONPATH=D:/utah/home/mkg/csound/head
+
