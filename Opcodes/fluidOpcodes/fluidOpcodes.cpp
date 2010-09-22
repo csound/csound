bbbb2b27725e8347998cf000f9b945892ca62ebc
Michael Gogins <gogins@users.sourceforge.net>
2010-09-22 00:40:51 +0000

    From Andy Fillebrown -- finish cleaning up in csoundModuleDestroy. Committed on the Free edition of March Hare Software CVSNT Client. Upgrade to CVS Suite for more features and support: http://march-hare.com/cvsnt/

--- Opcodes/fluidOpcodes/fluidOpcodes.cpp
+++ Opcodes/fluidOpcodes/fluidOpcodes.cpp
@@ -739,6 +739,7 @@ PUBLIC int csoundModuleDestroy(CSOUND *csound)
                 delete_fluid_synth(fluidSynth);
                 delete_fluid_settings(fluidSettings);
             }
+			fluidSynths.clear();
         }
     }
     return 0;
