7e847a7763700910eee043bde6e346b5bc83c7fa
John ffitch <jpff@users.sourceforge.net>
2011-01-17 07:03:41 +0000

    Version 5.13

--- H/version.h
+++ H/version.h
@@ -27,16 +27,16 @@
 #define CS_PACKAGE_NAME     "Csound"
 
 /* Define to the full name and version of this package. */
-#define CS_PACKAGE_STRING   "Csound 5.12"
+#define CS_PACKAGE_STRING   "Csound 5.13"
 
 /* Define to the one symbol short name of this package. */
 #define CS_PACKAGE_TARNAME  "csound"
 
 /* Define to the version of this package. */
-#define CS_PACKAGE_VERSION  "5.12"
+#define CS_PACKAGE_VERSION  "5.13"
 #define CS_VERSION          (5)
-#define CS_SUBVER           (12)
-#define CS_PATCHLEVEL       (93)
+#define CS_SUBVER           (13)
+#define CS_PATCHLEVEL       (0)
 
 #define CS_APIVERSION       2   /* should be increased anytime a new version
                                    contains changes that an older host will
