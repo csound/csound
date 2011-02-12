3a7c9eebeb73a8b489d16e95b9eb9727158b87be
Michael Gogins <gogins@users.sourceforge.net>
2011-02-12 00:25:20 +0000

    Updated for Csound 5.13 Windows installer. Committed on the Free edition of March Hare Software CVSNT Client. Upgrade to CVS Suite for more features and support: http://march-hare.com/cvsnt/

--- head-mingw-build.sh
+++ head-mingw-build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-scons custom=custom-mingw.py buildLuaWrapper=1 buildPythonWrapper=1 usePortMIDI=1 useJack=0 buildCsoundVST=1 buildvst4cs=1 buildInterfaces=1 buildCsoundAC=1 buildJavaWrapper=1 useOSC=1 buildPythonOpcodes=1 buildLoris=0 buildStkOpcodes=1 buildWinsound=1 noFLTKThreads=1 buildPDClass=1 buildVirtual=1 buildTclcsound=1 tclversion=85 buildLua=1 useDouble=1 dynamicCsoundLibrary=1 buildCSEditor=1 gcc4opt=core2 buildRelease=1 bufferoverflowu=0 noDebug=0 useOpenMP=1 install=1 $1 $2 $3 $4
+scons custom=custom-mingw.py buildLuaWrapper=1 buildPythonWrapper=1 usePortMIDI=1 useJack=0 buildCsoundVST=1 buildvst4cs=1 buildInterfaces=1 buildCsoundAC=1 buildJavaWrapper=1 useOSC=1 buildPythonOpcodes=1 buildLoris=0 buildStkOpcodes=1 buildWinsound=1 noFLTKThreads=0 buildPDClass=1 buildVirtual=1 buildTclcsound=1 tclversion=85 buildLua=1 useDouble=1 dynamicCsoundLibrary=1 buildCSEditor=1 gcc4opt=core2 buildRelease=1 bufferoverflowu=0 noDebug=0 useOpenMP=1 install=1 $1 $2 $3 $4
 
 # doxygen
--- head-mingw-env.sh
+++ head-mingw-env.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 echo Setting up environment to run Csound head branch from build area...
-echo Order can be vitally important!
+echo Order of paths can be vitally important!
 export PATH=/c/utah/opt/Mega-Nerd/libsndfile:${PATH}
 export PATH=/c/utah/opt/portaudio:${PATH}
 export PATH=/c/utah/opt/portmidi:${PATH}
 export PATH=/c/utah/opt/Tcl/bin:${PATH}
+export PATH=/c/utah/opt/clisp-2.49:${PATH}
 export PATH=/c/utah/opt/swigwin-2.0.1:${PATH}
 export PATH=/c/utah/opt/pd/bin:${PATH}
 export PATH=/c/utah/opt/liblo:${PATH}
 export PATH=/c/utah/opt/Java/jdk1.6.0_23/bin:${PATH}
 export PATH=/c/utah/opt/fluidsynth-1.1.3/src:${PATH}
 export PATH=/c/utah/opt/luajit-2.0/src:${PATH}
+export PATH=/c/utah/opt/Graphviz2.26.3/bin:${PATH}
 export PATH=/d/utah/home/mkg/csound/head:${PATH}
-export PATH=${PATH}:/c/utah/opt/Graphviz/bin
 export PATH=${PATH}:/c/utah/opt/doxygen/bin
 export PATH=${PATH}:/c/utah/opt/ImageMagick-6.6.3-Q16
 export PATH=${PATH}:/c/utah/opt/Python27
@@ -32,4 +33,5 @@ export PATH=${PATH}:/c/utah/opt/MiKTeX-2.9/miktex/bin
 export RAWWAVE_PATH=D:/utah/home/mkg/csound/head/Opcodes/stk/rawwaves
 export OPCODEDIR64=D:/utah/home/mkg/csound/head
 export PYTHONPATH=D:/utah/home/mkg/csound/head
+echo $PATH
 
