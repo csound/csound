d44315e0b1a6481027bf74225f204bde0df2330d
John ffitch <jpff@codemist.co.uk>
2011-02-16 08:59:41 +0000

    new version of loris

--- Opcodes/Loris/lorisgens5.h
+++ Opcodes/Loris/lorisgens5.h
@@ -5,7 +5,7 @@
  * manipulation, and synthesis of digitized sounds using the Reassigned
  * Bandwidth-Enhanced Additive Sound Model.
  *
- * Loris is Copyright (c) 1999-2004 by Kelly Fitz and Lippold Haken
+ * Loris is Copyright (c) 1999-2010 by Kelly Fitz and Lippold Haken
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
