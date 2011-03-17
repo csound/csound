4f5dd632537ec025510046daf0dcf6d690c1a4f5
John ffitch <jpff@codemist.co.uk>
2011-03-17 17:19:08 +0000

    fix if-then-else in new parser

--- H/version.h
+++ H/version.h
@@ -36,7 +36,7 @@
 #define CS_PACKAGE_VERSION  "5.13"
 #define CS_VERSION          (5)
 #define CS_SUBVER           (13)
-#define CS_PATCHLEVEL       (3)
+#define CS_PATCHLEVEL       (4)
 
 #define CS_APIVERSION       2   /* should be increased anytime a new version
                                    contains changes that an older host will
