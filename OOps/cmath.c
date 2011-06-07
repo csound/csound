bb52458378f7a7804d6b1e4c03b1d68cd22a447c
John ffitch <jpff@cs.bath.ac.uk>
2011-06-07 17:33:52 +0100

    minor improvements

--- OOps/cmath.c
+++ OOps/cmath.c
@@ -49,7 +49,7 @@ int apow(CSOUND *csound, POW *p)        /* Power routine for a-rate  */
     MYFLT norm = (p->norm!=NULL ? *p->norm : FL(1.0));
     if (norm==FL(0.0)) norm = FL(1.0);
     if (UNLIKELY(powerOf == FL(0.0))) {
-      MYFLT yy = FL(1.0) / *p->norm;
+      MYFLT yy = FL(1.0) / norm;
       for (n = 0; n < nsmps; n++) {
         MYFLT xx = in[n];
         if (UNLIKELY(xx == FL(0.0))) {
@@ -141,20 +141,18 @@ static MYFLT exprand(CSOUND *csound, MYFLT l)
 
 /* bilateral exponential distribution routine */
 
-static MYFLT biexprand(CSOUND *csound, MYFLT l)
+static MYFLT biexprand(CSOUND *csound, MYFLT range)
 {
     int32_t r1;
 
-    if (l < FL(0.0)) return (FL(0.0)); /* For safety */
+    if (range < FL(0.0)) return (FL(0.0)); /* For safety */
 
-    do {
-      r1 = (int32_t)csoundRandMT(&(csound->randState_));
-    } while (!r1);
+    while ((r1 = (int32_t)csoundRandMT(&(csound->randState_)))==0);
 
     if (r1 < (int32_t)0) {
-      return -(LOG(-(r1) * (FL(1.0) / FL(2147483648.0))) * l);
+      return -(LOG(-(r1) * (FL(1.0) / FL(2147483648.0))) * range);
     }
-    return (LOG(r1 * (FL(1.0) / FL(2147483648.0))) * l);
+    return (LOG(r1 * (FL(1.0) / FL(2147483648.0))) * range);
 }
 
 /* gaussian distribution routine */
@@ -196,7 +194,8 @@ static MYFLT pcauchrand(CSOUND *csound, MYFLT a)
     do {
       r1 = csoundRandMT(&(csound->randState_));
     } while (r1 > (uint32_t)4286377121U);      /* Limit range artificially */
-    x = TAN((MYFLT)r1 * (PI_F * FL(0.5) / FL(4294967295.0))) * (FL(1.0) / FL(318.3));
+    x = TAN((MYFLT)r1 * (PI_F * FL(0.5) / FL(4294967295.0)))
+      * (FL(1.0) / FL(318.3));
     return (x * a);
 }
 
@@ -205,20 +204,21 @@ static MYFLT pcauchrand(CSOUND *csound, MYFLT a)
 static MYFLT betarand(CSOUND *csound, MYFLT range, MYFLT a, MYFLT b)
 {
     double  r1, r2;
-
+    double aa, bb;
     if (a <= FL(0.0) || b <= FL(0.0))
       return FL(0.0);
 
+    aa = (double)a; bb = (double)b;
     do {
       uint32_t  tmp;
       do {
         tmp = csoundRandMT(&(csound->randState_));
       } while (!tmp);
-      r1 = pow(UInt32toFlt(tmp), 1.0 / (double)a);
+      r1 = pow(UInt32toFlt(tmp), 1.0 / aa);
       do {
         tmp = csoundRandMT(&(csound->randState_));
       } while (!tmp);
-      r2 = r1 + pow(UInt32toFlt(tmp), 1.0 / (double)b);
+      r2 = r1 + pow(UInt32toFlt(tmp), 1.0 / bb);
     } while (r2 > 1.0);
 
     return (((MYFLT)r1 / (MYFLT)r2) * range);
@@ -244,14 +244,14 @@ static MYFLT weibrand(CSOUND *csound, MYFLT s, MYFLT t)
 
 /* Poisson distribution routine */
 
-static MYFLT poissrand(CSOUND *csound, MYFLT l)
+static MYFLT poissrand(CSOUND *csound, MYFLT lambda)
 {
     MYFLT r1, r2, r3;
 
-    if (l < FL(0.0)) return FL(0.0);
+    if (lambda < FL(0.0)) return FL(0.0);
 
     r1 = unirand(csound);
-    r2 = EXP(-l);
+    r2 = EXP(-lambda);
     r3 = FL(0.0);
 
     while (r1 >= r2) {
@@ -542,8 +542,8 @@ int kcauchyi(CSOUND *csound, PRANDI *p)
     /* IV - Jul 11 2002 */
     *p->ar = (p->num1 + (MYFLT)p->phs * p->dfdmax) * *p->xamp;
     p->phs += (int32)(*p->xcps * csound->kicvt); /* phs += inc           */
-    if (p->phs >= MAXLEN) {                     /* when phs overflows,  */
-      p->phs &= PHMASK;                         /*      mod the phs     */
+    if (UNLIKELY(p->phs >= MAXLEN)) {         /* when phs overflows,  */
+      p->phs &= PHMASK;                       /*      mod the phs     */
       p->num1 = p->num2;                      /*      & new num vals  */
       p->num2 = cauchrand(csound, *p->arg1); 
       p->dfdmax = (p->num2 - p->num1) / FMAXLEN;
@@ -566,10 +566,10 @@ int acauchyi(CSOUND *csound, PRANDI *p)
       ar[n] = (p->num1 + (MYFLT)phs * p->dfdmax) * *ampp;
       if (p->ampcod)
         ampp++;
-      phs += inc;                               /* phs += inc       */
+      phs += inc;                                /* phs += inc       */
       if (p->cpscod)
         inc = (int32)(*cpsp++ * csound->sicvt);  /*   (nxt inc)      */
-      if (phs >= MAXLEN) {                      /* when phs o'flows, */
+      if (UNLIKELY(phs >= MAXLEN)) {             /* when phs o'flows */
         phs &= PHMASK;
         p->num1 = p->num2;
         p->num2 = cauchrand(csound, *p->arg1);
