3b1eec4d3ed6b2e3810792dd91d27dbf6ca74582
John ffitch <jpff@codemist.co.uk>
2011-07-05 09:44:21 +0100

    Reentrance and macros

--- Engine/parse_param.h
+++ Engine/parse_param.h
@@ -1,9 +1,20 @@
+typedef struct MACRO {          /* To store active macros */
+    char          *name;        /* Use is by name */
+    int           acnt;         /* Count of arguments */
+    char          *body;        /* The text of the macro */
+    struct MACRO  *next;        /* Chain of active macros */
+    int           margs;        /* amount of space for args */
+    char          *arg[MARGS];  /* With these arguments */
+} MACRO;
+
 typedef struct parse_parm_s {
     void            *yyscanner;
     char            *buffer;
 //  int             pos;
 //  int             length;
 //  double          result;
+//  MACRO           *macros;
+//  unsigned int    macro_stack_ptr;
 } PARSE_PARM;
 
 #define lMaxBuffer (1000)
