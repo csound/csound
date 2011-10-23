96a565837afcdbf7f30b78db89100a0ebd73b36e
John ffitch <jpff@codemist.co.uk>
2011-10-23 09:32:04 +0100

    new file for zak-safe

---
+++ H/interlocks.h
@@ -0,0 +1,34 @@
+/*
+    interlocks.h:
+
+    Copyright (C) 2011 John ffitch
+
+    This file is part of Csound.
+
+    The Csound Library is free software; you can redistribute it
+    and/or modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    Csound is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with Csound; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+    02111-1307 USA
+*/
+
+#define ZR (0x08)
+#define ZW (0x10)
+#define ZB (0x18)
+
+#define TR (0x20)
+#define TW (0x40)
+#define TB (0x60)
+
+#define CR (0x080)
+#define CW (0x100)
+#define CB (0x180)
