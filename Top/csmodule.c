15ed390051173f7daa1acd6c0b916640188c2c5b
Victor Lazzarini <Victor.Lazzarini@nuim.ie>
2011-11-13 21:31:44 +0000

    minmax opcodes

--- Top/csmodule.c
+++ Top/csmodule.c
@@ -1355,13 +1355,11 @@ extern long crossfm_localops_init(CSOUND *, void *);
 extern long pvlock_localops_init(CSOUND *, void *);
 extern long fareyseq_localops_init(CSOUND *, void *);
 
-
 extern int stdopc_ModuleInit(CSOUND *csound);
 extern int pvsopc_ModuleInit(CSOUND *csound);
 extern int sfont_ModuleInit(CSOUND *csound);
 extern int sfont_ModuleCreate(CSOUND *csound);
 extern int newgabopc_ModuleInit(CSOUND *csound);
-
 
 const INITFN staticmodules[] = { hrtfopcodes_localops_init, babo_localops_init,
                                  bilbar_localops_init, vosim_localops_init,
