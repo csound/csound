e9ec345aa09b45964d7ff263ad71499374bf7ae5
John ffitch <jpff@codemist.co.uk>
2011-11-20 15:05:26 +0000

    Code tidying and licence

--- H/corfile.h
+++ H/corfile.h
@@ -1,3 +1,30 @@
+/*
+    corfiles.c:
+
+    Copyright (C) 2011 John ffitch
+
+    This file is part of Csound.
+
+    The Csound Library is free software; you can redistribute it
+    and/or modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    Csound is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with Csound; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+    02111-1307 USA
+*/
+
+#ifndef __corfil
+
+#define __corfil
+
 CORFIL *corfile_create_w(void);
 CORFIL *corfile_create_r(char *text);
 void corfile_putc(int c, CORFIL *f);
@@ -18,3 +45,5 @@ CORFIL *copy_to_corefile(char *);
 //CORFIL *copy_to_corefile_path(CSOUND *, char *);
 int corfile_length(CORFIL *f);
 #define corfile_length(f) (strlen(f->body))
+
+#endif
