13fe9bd32a4f556d7304699695a0299e9a7d6464
John ffitch <jpff@codemist.co.uk>
2011-12-30 08:22:03 +0000

    fixed resetting of orchstr

--- Top/one_file.c
+++ Top/one_file.c
@@ -350,7 +350,7 @@ static int createOrchestra(CSOUND *csound, FILE *unf)
       p = buffer;
       while (*p == ' ' || *p == '\t') p++;
       if (strstr(p, "</CsInstruments>") == p) {
-        corfile_flush(incore);
+        //corfile_flush(incore);
         csound->orchstr = incore;
         return TRUE;
       }
