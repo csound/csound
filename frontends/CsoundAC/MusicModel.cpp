d6f53692211abd6e7398662c302a342163380abe
Michael Gogins <michael.gogins@gmail.com>
2012-02-22 20:54:20 -0500

    MusicModel clears out Csound command it has set, after use, to prevent spurious re-use.

--- frontends/CsoundAC/MusicModel.cpp
+++ frontends/CsoundAC/MusicModel.cpp
@@ -77,6 +77,9 @@ namespace csound
     cppSound->setCommand(getCsoundCommand());
     createCsoundScore(csoundScoreHeader);
     cppSound->perform();
+    // The Csound command is managed from MusicModel,
+    // not from CppSound. So we clear out what we set.
+    cppSound->setCommand("");
   }
 
   void MusicModel::clear()
