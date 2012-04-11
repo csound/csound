420622885e5398065e9f6a4c374e1b7b8d95c598
Michael Gogins <michael.gogins@gmail.com>
2012-04-11 19:35:49 -0400

    Syncing CsoundAC with recent changes to csound 5.

--- head-mingw-build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-python c:/utah/opt/Python27/Scripts/scons.py custom=custom-mingw.py useLrint=1 buildLuaWrapper=1 buildPythonWrapper=1 usePortMIDI=1 useJack=0 buildCsoundVST=1 buildvst4cs=1 buildInterfaces=1 buildCsoundAC=1 buildJavaWrapper=1 useOSC=1 buildPythonOpcodes=1 buildLuaOpcodes=1 buildLoris=0 buildStkOpcodes=1 buildWinsound=1 noFLTKThreads=0 buildPDClass=1 buildVirtual=1 buildTclcsound=1 tclversion=85 buildLua=1 useDouble=1 dynamicCsoundLibrary=1 buildCSEditor=1 gcc4opt=core2 buildRelease=1 bufferoverflowu=0 noDebug=0 useOpenMP=1 buildMultiCore=1 install=1 $1 $2 $3 $4
-
-# doxygen
