af66d8f1980a06d49d0f199bb10ec0f0719a979b
Steven Yi <stevenyi@gmail.com>
2012-09-01 18:24:32 +0100

    merge iOS changes from Csound5

--- iOS/build.sh
+++ iOS/build.sh
@@ -2,7 +2,7 @@
 rm -rf cs5iOS
 mkdir cs5iOS
 cd cs5iOS
-cmake ../.. -G Xcode -DUSE_GETTEXT=0 -DUSE_DOUBLE=0 -DBUILD_STATIC_LIBRARY=1
+cmake ../.. -G Xcode -DUSE_GETTEXT=0 -DUSE_DOUBLE=0 -DBUILD_STATIC_LIBRARY=1 -DBUILD_CSOUND_AC=0 -DBUILD_RELEASE=1 -DCMAKE_BUILD_TYPE=Release
 xcodebuild -sdk iphoneos -xcconfig ../device.xcconfig -target csound-static -configuration Release
 cp Release/libcsound.a ./libcsound-device.a
 xcodebuild -sdk iphonesimulator5.1 -xcconfig ../simulator.xcconfig -target csound-static -configuration Release 
