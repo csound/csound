628b46db586f9362fa74d7eb166c7e2a302afdec
Steven Yi <stevenyi@gmail.com>
2012-11-26 16:53:46 +0000

    removed inline to get around compiler error on Mac OSX

--- H/cs_par_dispatch.h
+++ H/cs_par_dispatch.h
@@ -11,9 +11,9 @@ TREE *csp_locks_insert(CSOUND * csound, TREE *root);
 /* build the cache of global locks */
 void csp_locks_cache_build(CSOUND *csound);
 /* lock global with index */
-inline void csp_locks_lock(CSOUND * csound, int global_index);
+void csp_locks_lock(CSOUND * csound, int global_index);
 /* unlock global with index */
-inline void csp_locks_unlock(CSOUND * csound, int global_index);
+void csp_locks_unlock(CSOUND * csound, int global_index);
 
 /* fetch a weight for opcode name */
 uint32_t csp_opcode_weight_fetch(CSOUND *csound, char *name);
