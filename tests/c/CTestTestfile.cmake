ef59b74c72773512821aed13d08b8f5347d79e0d
Steven Yi <stevenyi@gmail.com>
2012-12-01 16:21:25 +0000

    removed cmake generated files from test/c, updated .gitignore for when building in-source

--- tests/c/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /jpff/Sourceforge/csound/csound6/tests/c
-# Build directory: /jpff/Sourceforge/csound/csound6/tests/c
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-ADD_TEST(TestCsoundTypeSystem "/jpff/Sourceforge/csound/csound6/tests/c/csoundTypeSystemTest" "-+env:OPCODEDIR64=/../..")
