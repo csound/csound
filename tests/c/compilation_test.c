eda817352990fe16a10a1cd5f2aab7c06d0e57d1
Victor Lazzarini <Victor.Lazzarini@nuim.ie>
2012-12-20 13:56:53 +0000

    added the compilation test program

---
+++ tests/c/compilation_test.c
@@ -0,0 +1,32 @@
+#include "csound.h"
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    CSOUND  *csound;
+    int     result, compile_again=0;
+    char  *instrument = 
+    "instr 2 \n"
+    "k1 expon p4, p3, p4*0.001 \n"
+    "a1 vco2  k1, p5   \n"
+    "out  a1   \n"
+    "endin \n";
+
+    csoundInitialize(&argc, &argv, 0);
+    csound = csoundCreate(NULL);
+    result = csoundCompile(csound, argc, argv);
+    while(!result){
+    result = csoundPerformKsmps(csound);
+    if(!compile_again){ 
+         /* new compilation */
+    	csoundCompileOrc(csound, instrument);
+	/* schedule an event on instr 2 */
+        csoundInputMessage(csound, "i2 0 8 10000 110 \n");
+        compile_again = 1;
+      }
+    }
+    /* delete Csound instance */
+     csoundDestroy(csound);
+    return 0;
+}
+
