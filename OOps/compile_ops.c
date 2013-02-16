f5647dad3c49a3459e523d25212cdb3c57f859cf
Victor Lazzarini <Victor.Lazzarini@nuim.ie>
2013-02-16 23:18:34 +0000

    some improvements to compile ops

--- OOps/compile_ops.c
+++ OOps/compile_ops.c
@@ -5,20 +5,35 @@
 int compile_orc_i(CSOUND *csound, COMPILE *p){
   FILE *fp;
   int size=0;
-  char *orc, c;
-  fp = fopen(csound->strarg2name(csound,NULL,p->str,"",1), "rb"); 
-  
+  char *orc, c, *name = csound->strarg2name(csound,NULL,p->str,"",1);
+
+  fp = fopen(name, "rb"); 
+   
+  if(fp == NULL) {
+    csound->Warning(csound, Str("compileorc: could not open %s\n"), name);
+    *p->res = CSOUND_ERROR;
+    return NOTOK;
+  }
+
   while(!feof(fp)) 
      size += fread(&c,1,1,fp);
+
+  if(size==0) {
+   fclose(fp);
+   *p->res = CSOUND_ERROR;
+   csound->InitError(csound, Str("compileorc: could not read %s\n"), name);
+   return NOTOK;
+  }					  
 
   orc = (char *) mcalloc(csound, size+1);
   fseek(fp, 0, SEEK_SET);    
   fread(orc,1,size,fp);
+
   /* we need to notify the lock so that merge can occur 
      this might possibly compromise thread-safety
   */
   csoundNotifyThreadLock(csound->API_lock);
-  csoundCompileOrc(csound, orc);
+  *p->res = csoundCompileOrc(csound, orc);
   fclose(fp);
   mfree(csound,orc);
   return OK;
@@ -29,6 +44,6 @@ int compile_str_i(CSOUND *csound, COMPILE *p){
      this might possibly compromise thread-safety
   */
   csoundNotifyThreadLock(csound->API_lock);
-  csoundCompileOrc(csound, (char *)p->str);
+  *p->res = csoundCompileOrc(csound, (char *)p->str);
   return OK;
 }
