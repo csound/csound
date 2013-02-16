19ea8599bcd2764b83c4ad0fcf4d8d70c23bf0fe
John ffitch <jpff@codemist.co.uk>
2013-02-16 14:21:35 +0000

    got lpanal/lpread right

--- util/lpanal.c
+++ util/lpanal.c
@@ -553,10 +553,9 @@ static int lpanal(CSOUND *csound, int argc, char **argv)
 
     /* Write header to disk */
     if (new_format) {
-      fprintf(oFd, "LPANAL\n%d %d %d %d\n%f %f %f %d %d %d %d\n",
+      fprintf(oFd, "LPANAL\n%d %d %d %d\n%f %f %f\n",
               lph->headersize, lph->lpmagic, lph->npoles, lph->nvals,
-              lph->framrate, lph->srate, lph->duration,
-              lph->text[0], lph->text[1], lph->text[2], lph->text[3]);
+              lph->framrate, lph->srate, lph->duration);
     }
     else if ((nb = write(ofd,(char *)lph,(int)lph->headersize)) <
         lph->headersize)
