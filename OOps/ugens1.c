b3b1846dda8941085f5e65c9e33a61c572faef60
Victor Lazzarini <Victor.Lazzarini@nuim.ie>
2013-03-01 11:14:27 +0000

    fixed aline bug

--- OOps/ugens1.c
+++ OOps/ugens1.c
@@ -52,16 +52,16 @@ int aline(CSOUND *csound, LINE *p)
     uint32_t offset = p->h.insdshead->ksmps_offset;
     uint32_t early  = p->h.insdshead->ksmps_no_end;
     uint32_t n, nsmps = CS_KSMPS;
+    ar = p->xr;
+    val = p->val;
+    inc = p->incr;
 
     if (offset) memset(ar, '\0', offset*sizeof(MYFLT));
     if (early) {
       nsmps -= early;
       memset(&ar[nsmps], '\0', early*sizeof(MYFLT));
     }
-
-    val = p->val;
-    inc = p->incr;
-    ar = p->xr;
+    
     p->val += inc;/* nxtval = val + inc */
     inc /= (nsmps - offset);           
     for (n=offset; n<nsmps; n++) {
