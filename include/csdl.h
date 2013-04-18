3f8cae0f77351bd0b1388db6e6ff950151c11ca7
Andres Cabrera <mantaraya36@gmail.com>
2013-04-17 21:19:54 -0700

    Improved docs for cscore.h, csound.h and csdl.h

--- include/csdl.h
+++ include/csdl.h
@@ -23,6 +23,82 @@
 
 #ifndef CSOUND_CSDL_H
 #define CSOUND_CSDL_H
+/**
+* \file csdl.h
+*
+* \brief Declares the Csound plugin opcode interface.
+* \author John P. ffitch, Michael Gogins, Matt Ingalls, John D. Ramsdell,
+*         Istvan Varga, Victor Lazzarini.
+*
+* Plugin opcodes can extend the functionality of Csound, providing new
+* functionality that is exposed as opcodes in the Csound language.
+* Plugins need to include this header file only, as it will bring all necessary
+* data structures to interact with Csound. It is not necessary for plugins
+* to link to the libcsound library, as plugin opcodes will always receive a
+* CSOUND* pointer which contains all the API functions inside.
+*
+* This is the basic template for a plugin opcode. See the manual for further
+* details on accepted types and function call rates. The use of the LINKAGE
+* macro is highly recommended, rather than calling the functions directly.
+*
+* \code
+#include "csdl.h"
+
+typedef struct {
+   OPDS h;
+   MYFLT *out;
+   MYFLT *in1, *in2;
+} OPCODE;
+
+static int op_init(CSOUND *csound, OPCODE *p)
+{
+// Intialization code goes here
+    return OK;
+}
+
+static int op_k(CSOUND *csound, OPCODE *p)
+{
+// Intialization code goes here
+    return OK;
+}
+
+PUBLIC int csoundModuleCreate(CSOUND *csound)
+{
+    return 0;
+}
+
+PUBLIC int csoundModuleInit(CSOUND *csound)
+{
+    OENTRY  *ep = (OENTRY *) &(localops[0]);
+    int     err = 0;
+    while (ep->opname != NULL) {
+      err |= csound->AppendOpcode(csound,
+                                  ep->opname, ep->dsblksiz, ep->thread,
+                                  ep->outypes, ep->intypes,
+                                  (int (*)(CSOUND *, void *)) ep->iopadr,
+                                  (int (*)(CSOUND *, void *)) ep->kopadr,
+                                  (int (*)(CSOUND *, void *)) ep->aopadr);
+      ep++;
+    }
+    return err;
+}
+
+PUBLIC int csoundModuleDestroy(CSOUND *csound)
+{
+    // Called when the plugin opcode is unloaded, usually when Csound terminates.
+    return 0;
+}
+
+static OENTRY localops[] =
+{
+    { "opcode",   sizeof(OPCODE),  0, 3, "i",    "ii", (SUBR)op_init, (SUBR)op_k  }}
+};
+
+LINKAGE(localops)
+
+*
+* \endcode
+**/
 
 #ifdef __BUILDING_LIBCSOUND
 #undef __BUILDING_LIBCSOUND
@@ -53,7 +129,7 @@ PUBLIC  const char  *csoundModuleErrorCodeToString(int);
 
 PUBLIC  int     csoundModuleInfo(void);
 
-/* The LINKAGE macro sets up linking of opcode list*/
+/** The LINKAGE macro sets up linking of opcode list*/
 
 #define LINKAGE                                                         \
 PUBLIC long csound_opcode_init(CSOUND *csound, OENTRY **ep)             \
@@ -61,7 +137,7 @@ PUBLIC long csound_opcode_init(CSOUND *csound, OENTRY **ep)             \
 PUBLIC int csoundModuleInfo(void)                                       \
 { return ((CS_APIVERSION << 16) + (CS_APISUBVER << 8) + (int) sizeof(MYFLT)); }
 
-/* The LINKAGE_BUILTIN macro sets up linking of opcode list for builtin opcodes
+/** The LINKAGE_BUILTIN macro sets up linking of opcode list for builtin opcodes
  * which must have unique function names */
 
 #undef LINKAGE_BUILTIN
@@ -71,7 +147,7 @@ PUBLIC long csound_opcode_init(CSOUND *csound, OENTRY **ep)             \
 PUBLIC int csoundModuleInfo(void)                                       \
 { return ((CS_APIVERSION << 16) + (CS_APISUBVER << 8) + (int) sizeof(MYFLT)); }
 
-/* LINKAGE for f-table plugins */
+/** LINKAGE for f-table plugins */
 
 #define FLINKAGE                                                        \
 PUBLIC NGFENS *csound_fgen_init(CSOUND *csound)                         \
