bcc36c1a058a4dd02a72aa218b27fc6e15114d1f
John ffitch <jpff@codemist.co.uk>
2013-04-28 12:32:54 +0100

    long lines/tabs

--- Engine/csound_orc_compile.c
+++ Engine/csound_orc_compile.c
@@ -917,7 +917,7 @@ void add_to_deadpool(CSOUND *csound, INSTRTXT *instrtxt)
 /** Insert INSTRTXT into an engineState list of INSTRTXT's,
     checking to see if number is greater than number of pointers currently
     allocated and if so expand pool of instruments
- */ 
+ */
 void insert_instrtxt(CSOUND *csound, INSTRTXT *instrtxt,
                      int32 instrNum, ENGINE_STATE *engineState)
 {
@@ -967,13 +967,14 @@ void insert_instrtxt(CSOUND *csound, INSTRTXT *instrtxt,
       }
       /* no active instances */
       if (active == NULL) {
-	if(csound->oparms->odebug) csound->Message(csound, "no active instances \n");
+        if (csound->oparms->odebug)
+          csound->Message(csound, Str("no active instances \n"));
         free_instrtxt(csound, engineState->instrtxtp[instrNum]);
       }
       /* err++; continue; */
     }
  end:
-    
+
     instrtxt->instance = instrtxt->act_instance = instrtxt->lst_instance = NULL;
     engineState->instrtxtp[instrNum] = instrtxt;
 
@@ -1040,7 +1041,8 @@ int engineState_merge(CSOUND *csound, ENGINE_STATE *engineState)
     INSTRTXT *current;
     int count;
 
-    cs_hash_table_merge(csound, current_state->stringPool, engineState->stringPool);
+    cs_hash_table_merge(csound,
+                        current_state->stringPool, engineState->stringPool);
 
     for (count = 0; count < engineState->constantsPool->count; count++) {
      if(csound->oparms->odebug)
@@ -1074,14 +1076,16 @@ int engineState_merge(CSOUND *csound, ENGINE_STATE *engineState)
       current = engineState->instrtxtp[i];
       if(current != NULL){
         if(current->insname == NULL) {
-	  if(csound->oparms->odebug) csound->Message(csound, Str("merging instr %d \n"), i);
+          if(csound->oparms->odebug)
+            csound->Message(csound, Str("merging instr %d \n"), i);
           /* a first attempt at this merge is to make it use
              insert_instrtxt again */
           /* insert instrument in current engine */
           insert_instrtxt(csound,current,i,current_state);
         }
         else {
-	  if(csound->oparms->odebug) csound->Message(csound, Str("merging instr %s \n"), current->insname);
+          if(csound->oparms->odebug)
+            csound->Message(csound, Str("merging instr %s \n"), current->insname);
           /* allocate a named_instr string in the current engine */
           /* FIXME: check the redefinition case for named instrs */
           named_instr_alloc(csound,current->insname,current,-1L,current_state);
@@ -1106,13 +1110,14 @@ int engineState_merge(CSOUND *csound, ENGINE_STATE *engineState)
       int j;
       current = current_state->instrtxtp[i];
       if(current != NULL){
-        if(csound->oparms->odebug) csound->Message(csound, "instr %d \n", i, current);
+        if(csound->oparms->odebug)
+          csound->Message(csound, "instr %d \n", i, current);
         current->nxtinstxt = NULL;
         j = i;
         while(++j < end-1) {
           if(current_state->instrtxtp[j] != NULL){
-	  current->nxtinstxt = current_state->instrtxtp[j];
-	  break;
+          current->nxtinstxt = current_state->instrtxtp[j];
+          break;
           }
         }
       }
@@ -1416,11 +1421,11 @@ PUBLIC int csoundCompileTree(CSOUND *csound, TREE *root)
       *((MYFLT *)(var->memBlock)) = csound->inchnls;
 
     }
-    
+
     if(csound->oparms->realtime) csoundUnlockMutex(csound->init_pass_threadlock);
     /* notify API lock  */
     csoundUnlockMutex(csound->API_lock);
-    
+
     return CSOUND_SUCCESS;
 }
 
@@ -1441,8 +1446,8 @@ PUBLIC int csoundCompileOrc(CSOUND *csound, const char *str)
     }
     else
       return  CSOUND_ERROR;
-    delete_tree(csound, root);    
-   
+    delete_tree(csound, root);
+
     if (UNLIKELY(csound->oparms->odebug))
       debugPrintCsound(csound);
     return retVal;
