b7d780b74630284426ceeeb7b6a2e424946aa3d7
Victor Lazzarini <Victor.Lazzarini@nuim.ie>
2013-05-11 17:59:32 +0100

    fixing string channels

--- Top/threadsafe.c
+++ Top/threadsafe.c
@@ -142,8 +142,9 @@ void csoundSetStringChannel(CSOUND *csound, const char *name, char *string)
       int    size = csoundGetChannelDatasize(csound, name);
       int    *lock = csoundGetChannelLock(csound, (char*) name);
       csoundSpinLock(lock);
-      strncpy((char *) pstring, string, size-1);
-      pstring[size-1] = '\0';
+      if(strlen(string) < size)
+      strcpy((char *) pstring, string);
+      //pstring[size-1] = '\0';
       csoundSpinUnLock(lock);
     }
 }
