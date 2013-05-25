0c2eedee492719487fccff99230223cdb9fa07bf
Michael Gogins <michael.gogins@gmail.com>
2013-05-25 16:05:22 -0400

    Fixing up Android build for MinGW/Csound 6. Had to add -Iinclude. Had to use /c/ndk-modules.

--- android/pluginlibs/libfluidsynth/jni/Android.mk
+++ android/pluginlibs/libfluidsynth/jni/Android.mk
@@ -1,12 +1,13 @@
 LOCAL_PATH := $(call my-dir)
+echo LOCAL_PATH: $LOCAL_PATH
 
 include $(CLEAR_VARS)
 
 CSOUND_SRC_ROOT := ../../../..
-LIBFLUIDSYNTH_SRC_DIR := $(LOCAL_PATH)/../../../../../android/fluidsynth-android/jni/
+LIBFLUIDSYNTH_SRC_DIR := $(NDK_MODULE_PATH)/fluidsynth-android/jni/
 
 LOCAL_MODULE   := fluidsynth 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../../H $(LOCAL_PATH)/../../../.. $(LOCAL_PATH)/../../../ $(LIBFLUIDSYNTH_SRC_DIR)/include $(CSOUND_SRC_ROOT)/Opcodes/fluidOpcodes/
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../../H $(LOCAL_PATH)/../../../../include $(LOCAL_PATH)/../../../.. $(LOCAL_PATH)/../../../ $(LIBFLUIDSYNTH_SRC_DIR)/include $(CSOUND_SRC_ROOT)/Opcodes/fluidOpcodes/
 LOCAL_CFLAGS := -O3 -D__BUILDING_LIBCSOUND -DENABLE_NEW_PARSER -DLINUX -DHAVE_DIRENT_H -DHAVE_FCNTL_H -DHAVE_UNISTD_H -DHAVE_STDINT_H -DHAVE_SYS_TIME_H -DHAVE_SYS_TYPES_H -DHAVE_TERMIOS_H 
 LOCAL_CPPFLAGS :=$(LOCAL_CFLAGS)
 ###
