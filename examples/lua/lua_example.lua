e531b6fdb2296afd7c8c53c0e95fef7b94e1d10e
Michael Gogins <michael.gogins@gmail.com>
2013-06-01 12:42:24 -0400

    Added Start() to lua_example.lua.

--- examples/lua/lua_example.lua
+++ examples/lua/lua_example.lua
@@ -4,15 +4,14 @@ require "luaCsnd6"
 
 csound = luaCsnd6.Csound()
 args = luaCsnd6.CsoundArgVList()
-args_ = {"csound", "-o", "test.wav", "../trapped.csd"}
+args_ = {"csound", "-o", "test.wav", "examples/trapped.csd"}
 for i,v in ipairs(args_) do
     args:Append(v)
 end
 csound:Compile(args.argc(args), args.argv(args))
-csound:startPerformance(nil)
---csound:startPerformance()
+csound:Start()
 pt = luaCsnd6.CsoundPerformanceThread(csound)
-pt:SetScoreOffsetSeconds(21.5)
+-- pt:SetScoreOffsetSeconds(21.5)
 pt:Play()
 luaCsnd6.csoundSleep(10000)
 pt:SetScoreOffsetSeconds(152)
