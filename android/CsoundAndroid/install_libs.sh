454ac6161b77781d856cdb6299ddbaa72c70460e
Michael Gogins <michael.gogins@gmail.com>
2013-06-08 13:16:23 -0400

    Programmatically load sndfile lib before csoundandroid lib.

--- android/CsoundAndroid/install_libs.sh
+++ android/CsoundAndroid/install_libs.sh
@@ -10,6 +10,7 @@ cp -f ../pluginlibs/libfluidsynth/libs/armeabi/libfluidOpcodes.so assets/armeabi
 cp -f ../pluginlibs/libstdutil/libs/armeabi/libstdutil.so assets/armeabi/OPCODE6DIR/
 cp -f ../pluginlibs/luajit-2.0/src/libluajit.so assets/armeabi/OPCODE6DIR/
 cp -f ../pluginlibs/signalflowgraph/libs/armeabi/libsignalflowgraph.so assets/armeabi/OPCODE6DIR/
+cp -f ../pluginlibs/LuaCsound/libs/armeabi/libLuaCsound.so assets/armeabi/OPCODE6DIR/
 
 mkdir assets/armeabi-v7a/
 mkdir assets/armeabi-v7a/OPCODE6DIR/
@@ -17,6 +18,7 @@ cp -f ../pluginlibs/libfluidsynth/libs/armeabi-v7a/libfluidOpcodes.so assets/arm
 cp -f ../pluginlibs/libstdutil/libs/armeabi-v7a/libstdutil.so assets/armeabi-v7a/OPCODE6DIR/
 cp -f ../pluginlibs/luajit-2.0/src/libluajit.so assets/armeabi-v7a/OPCODE6DIR/
 cp -f ../pluginlibs/signalflowgraph/libs/armeabi-v7a/libsignalflowgraph.so assets/armeabi-v7a/OPCODE6DIR/
+cp -f ../pluginlibs/LuaCsound/libs/armeabi-v7a/libLuaCsound.so assets/armeabi-v7a/OPCODE6DIR/
 
 # Also copy other resources used by Csound opcodes.
 
