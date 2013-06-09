396fd04dbabe3ab6b783993f3ed3e47b42cc86f8
Victor Lazzarini <Victor.Lazzarini@nuim.ie>
2013-06-09 10:19:00 +0100

    trying to fix mem issue

--- Opcodes/faustgen.cpp
+++ Opcodes/faustgen.cpp
@@ -1,15 +1,17 @@
 #include "csdl.h"
 #include "faust/llvm-dsp.h"
 
-#define MAXARG 32
+#define MAXARG 40
 
 typedef struct {
   OPDS h;
   MYFLT *outs[MAXARG]; /* outputs */
   STRINGDAT *code;     /* faust code as string */
-  MYFLT *ins[MAXARG]; /* inputs */
-  dsp *engine;  /* faust DSP */
+  MYFLT *ins[VARGMAX]; /* inputs */
+  llvm_dsp *engine;  /* faust DSP */
   llvm_dsp_factory* factory; /* DSP factory */
+  AUXCH memin;
+  AUXCH memout;
 } FAUSTGEN;
 
 /* deinit function
@@ -17,7 +19,7 @@ typedef struct {
 */
 int delete_faustgen(CSOUND *csound, void *p) {
   FAUSTGEN *pp = (FAUSTGEN *) p;
-  deleteDSPInstance((llvm_dsp *)pp->engine);
+  deleteDSPInstance(pp->engine);
   deleteDSPFactory(pp->factory);
   return OK;
 }
@@ -27,6 +29,7 @@ int delete_faustgen(CSOUND *csound, void *p) {
 */
 int init_faustgen(CSOUND *csound, FAUSTGEN *p){
 
+  OPARMS parms;
   char err_msg[256];
   int size;
   int argc = 3;
@@ -48,42 +51,98 @@ int init_faustgen(CSOUND *csound, FAUSTGEN *p){
   if(p->factory == NULL) 
     return csound->InitError(csound, "Faust compilation problem: %s\n", err_msg);
   p->engine = createDSPInstance(p->factory);
-  if(p->engine == NULL) 
+  if(p->engine == NULL) {
+    deleteDSPFactory(p->factory);
     return csound->InitError(csound, "Faust instantiation problem \n");
+  }
   
   /* init engine */
   p->engine->init(csound->GetSr(csound));
 
   if(p->engine->getNumInputs() != p->INCOUNT-1) {
-   deleteDSPInstance((llvm_dsp *) p->engine);
+   deleteDSPInstance(p->engine);
    deleteDSPFactory(p->factory);
    return csound->InitError(csound, "wrong number of input args\n");
   }
   if(p->engine->getNumOutputs() != p->OUTCOUNT){
-    deleteDSPInstance((llvm_dsp *)p->engine);
+    deleteDSPInstance(p->engine);
     deleteDSPFactory(p->factory);
     return csound->InitError(csound, "wrong number of output args\n");
   }
- 
+  /* memory for sampAccurate offsets */
+  csound->GetOParms(csound, &parms);
+  if(parms.sampleAccurate){
+    int size;
+    size = p->engine->getNumInputs()*sizeof(MYFLT *);
+    if(p->memin.auxp == NULL ||
+       p->memin.size < size)
+      csound->AuxAlloc(csound, size, &p->memin);
+    size = p->engine->getNumOutputs()*sizeof(MYFLT *);
+    if(p->memout.auxp == NULL ||
+       p->memout.size < size)
+      csound->AuxAlloc(csound, size, &p->memout);
+  }
+
   csound->RegisterDeinitCallback(csound, p, delete_faustgen);
   return OK;
 }
 
 /* perf-time function
-   FIXME: need to implement cs6 sample-accurate support
    TODO:  k-rate controls
 */
 int perf_faustgen(CSOUND *csound, FAUSTGEN *p){
+  int nsmps = CS_KSMPS, i;
+  uint32_t offset = p->h.insdshead->ksmps_offset;
+  uint32_t early  = p->h.insdshead->ksmps_no_end;
+  MYFLT **in_tmp = (MYFLT **) p->memin.auxp;
+  MYFLT **out_tmp = (MYFLT **) p->memout.auxp;
 
-  int nsmps = CS_KSMPS, i, j;
-  int faustins = p->engine->getNumInputs();   
-  int faustouts = p->engine->getNumOutputs();  
-  MYFLT **ins, **outs;
-  
-  for(i=0; i < faustins; i++) ins[i] = p->ins[i];
-  for(i=0; i < faustouts; i++)  outs[i] = p->outs[i];
-    
-  p->engine->compute(nsmps, ins, outs);
+  if (UNLIKELY(early)) {
+      for (i = 0; i < p->OUTCOUNT; i++)
+        memset(p->outs[i], '\0', nsmps*sizeof(MYFLT));
+      nsmps -= early;
+   }
+  if(UNLIKELY(offset)) {
+    /* offset pointers, save current pos */
+    for (i = 0; i < p->OUTCOUNT; i++){ 
+        memset(p->outs[i], '\0', nsmps*sizeof(MYFLT));
+        out_tmp[i] = p->outs[i];
+        p->outs[i] = &(p->outs[i][offset]);
+  }
+    for (i = 0; i < p->INCOUNT-1; i++){ 
+        in_tmp[i] = p->ins[i];
+        p->ins[i] = &(p->ins[i][offset]);
+  }
+    nsmps -= offset;
+ }
+  p->engine->compute(nsmps, p->ins, p->outs);
+
+  if(UNLIKELY(offset)) {
+    /* restore pos  */
+    for (i = 0; i < p->OUTCOUNT; i++)
+	p->outs[i] = out_tmp[i];
+    for (i = 0; i < p->INCOUNT-1; i++)
+        p->ins[i] = in_tmp[i];
+  }
 
   return OK;
+}
+
+#define S(x)    sizeof(x)
+
+static OENTRY localops[] = {
+  { (char *) "faustgen", S(FAUSTGEN), 0, 5, (char *) "mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm", 
+     (char *)"Sy",(SUBR)init_faustgen, NULL, (SUBR)perf_faustgen}
+};
+
+PUBLIC long csound_opcode_init(CSOUND *csound, OENTRY **ep)
+{
+    IGN(csound);
+    *ep = localops;
+    return (long) sizeof(localops);
+}
+
+PUBLIC int csoundModuleInfo(void)
+{
+    return ((CS_APIVERSION << 16) + (CS_APISUBVER << 8) + (int) sizeof(MYFLT));
 }
