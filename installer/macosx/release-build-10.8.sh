9645d49fbabfa0d80e6bb1b66b480dad0c601d5d
Steven Yi <stevenyi@gmail.com>
2013-06-11 15:48:50 -0400

    fixed installer for libcsnd.6.0.dylib

--- installer/macosx/release-build-10.8.sh
+++ installer/macosx/release-build-10.8.sh
@@ -117,8 +117,8 @@ echo "preparing framework..."
 #cp  csound5-f/examples/csoundapi_tilde/csoundapi-osx.pd $FRAMEWORK32_DIR/$PD_DIR/csoundapi.pd
 #cp  csound5-f/examples/csoundapi_tilde/csapi_demo.csd $FRAMEWORK32_DIR/../Documentation/
 
-#cp  $DIST/lib_csnd.dylib $FRAMEWORK64_DIR/Versions/$CSLIBVERSION/
 #mv  $DIST/CsoundLib64.framework/Resources/Opcodes64/csladspa.dylib  $FRAMEWORK64_DIR/$CSLADSPA_DIR
+cp  $DIST/lib/libcsnd.6.0.dylib $FRAMEWORK64_DIR/Versions/$CSLIBVERSION/
 cp  $DIST/lib/lib_jcsound6.jnilib $FRAMEWORK64_DIR/$JAVA_DIR
 cp  $DIST/lib/csnd6.jar $FRAMEWORK64_DIR/$JAVA_DIR
 cp  $DIST/lib/csoundapi~.pd_darwin $FRAMEWORK64_DIR/$PD_DIR
