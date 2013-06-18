6239b654bfcf3385a2deca54ef523d88aec74b24
Victor Lazzarini <Victor.Lazzarini@nuim.ie>
2013-06-18 08:38:59 +0100

    changed invalue to match new string code

--- H/bus.h
+++ H/bus.h
@@ -178,6 +178,7 @@ int     chnexport_opcode_init(CSOUND *, CHNEXPORT_OPCODE *);
 int     chnparams_opcode_init(CSOUND *, CHNPARAMS_OPCODE *);
 
 int kinval(CSOUND *csound, INVAL *p);
+int kinvalS(CSOUND *csound, INVAL *p);
 int invalset(CSOUND *csound, INVAL *p);
 int invalset_string(CSOUND *csound, INVAL *p);
 int invalset_string_S(CSOUND *csound, INVAL *p);
