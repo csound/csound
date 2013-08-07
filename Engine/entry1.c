706a5ff6f1db92d7ecd2e570195e0b98197128ca
Victor Lazzarini <Victor.Lazzarini@nuim.ie>
2013-08-08 00:02:31 +0100

    fixing ithen

--- Engine/entry1.c
+++ Engine/entry1.c
@@ -895,9 +895,7 @@ OENTRY opcodlst_1[] = {
   { "turnoff2.i",S(TURNOFF2),0,2,     "",     "ikk",  NULL, turnoff2k, NULL     },
   { "turnoff2.r",S(TURNOFF2),0,2,     "",     "ikk",  NULL, turnoff2k, NULL     },
   { "cngoto", S(CGOTO),0,   3,      "",     "Bl",   ingoto, kngoto, NULL     },
-#ifdef VARGA
-  { "cogoto", S(CGOTO),0,   3,      "",     "bl",   iingoto, kingoto, NULL   },
-#endif
+  { "cingoto", S(CGOTO),0,   1,      "",     "Bl",   ingoto, NULL, NULL     },
   { "tempoval", S(GTEMPO),0, 2,  "k", "",      NULL, (SUBR)gettempo, NULL    },
   { "downsamp",S(DOWNSAMP),0,3, "k", "ao",   (SUBR)downset,(SUBR)downsamp        },
   { "upsamp", S(UPSAMP),0,  4,  "a", "k",    NULL,   NULL,   (SUBR)upsamp        },
