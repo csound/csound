c657a77209da07be4d14238c94e975c8c10cc2ea
John ffitch <jpff@codemist.co.uk>
2013-08-07 08:04:31 +0100

    minor improvememts in moogladder and statevar to avoid recalculation

--- OOps/aops.c
+++ OOps/aops.c
@@ -268,18 +268,18 @@ int modkk(CSOUND *csound, AOP *p)
     MYFLT   *r, a, *b;                          \
     uint32_t offset = p->h.insdshead->ksmps_offset;  \
     uint32_t early  = p->h.insdshead->ksmps_no_end;  \
-    uint32_t n, nsmps = CS_KSMPS;      \
-    r = p->r;                                   \
-    a = *p->a;                                  \
-    b = p->b;                                   \
+    uint32_t n, nsmps = CS_KSMPS;                    \
+    r = p->r;                                        \
+    a = *p->a;                                       \
+    b = p->b;                                        \
     if (UNLIKELY(offset)) memset(r, '\0', offset*sizeof(MYFLT)); \
-    if (UNLIKELY(early)) {                      \
-      nsmps -= early;                           \
-      memset(&r[nsmps], '\0', early*sizeof(MYFLT)); \
-    }                                           \
-    for (n=offset; n<nsmps; n++)                \
-      r[n] = a OP b[n];                         \
-    return OK;                                  \
+    if (UNLIKELY(early)) {                           \
+      nsmps -= early;                                \
+      memset(&r[nsmps], '\0', early*sizeof(MYFLT));  \
+    }                                                \
+    for (n=offset; n<nsmps; n++)                     \
+      r[n] = a OP b[n];                              \
+    return OK;                                       \
   }
 
 KA(addka,+)
