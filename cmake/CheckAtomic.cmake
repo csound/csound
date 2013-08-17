50ba18f0e3c69a3b38ec6becaf6783d17e49b544
Steven Yi <stevenyi@gmail.com>
2013-08-17 20:00:54 +0100

    better atomic check

--- cmake/CheckAtomic.cmake
+++ cmake/CheckAtomic.cmake
@@ -2,10 +2,18 @@
 include(CheckCSourceCompiles)
 
 check_c_source_compiles("
+#include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-  int val = 0;
-  __sync_lock_test_and_set(&val, 1); 
+  static volatile unsigned long val = 0;
+  unsigned long u =  __sync_add_and_fetch(&val, 1); 
+
+  if(u - 1 == 0) { 
+    return 0;
+  } 
+ 
+  return 1; 
 }
 
 " HAVE_ATOMIC_BUILTIN)
