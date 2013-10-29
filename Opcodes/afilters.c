dcf303f3ba059f9b01392c7352087e7ec3cff9a7
John ffitch <jpff@codemist.co.uk>
2013-10-29 11:13:53 +0000

    and more arate filters

--- Opcodes/afilters.c
+++ Opcodes/afilters.c
@@ -281,11 +281,81 @@ static int aresonka(CSOUND *csound, RESON *p)
     return OK;
 }
 
+extern int tonset(CSOUND*, TONE*);
+
+static int atonea(CSOUND *csound, TONE *p)
+{
+    MYFLT       *ar, *asig;
+    uint32_t offset = p->h.insdshead->ksmps_offset;
+    uint32_t early  = p->h.insdshead->ksmps_no_end;
+    uint32_t n, nsmps = CS_KSMPS;
+    double      c2 = p->c2, yt1 = p->yt1;
+
+    ar = p->ar;
+    if (UNLIKELY(offset)) memset(ar, '\0', offset*sizeof(MYFLT));
+    if (UNLIKELY(early)) {
+      nsmps -= early;
+      memset(&ar[nsmps], '\0', early*sizeof(MYFLT));
+    }
+    asig = p->asig;
+    for (n=offset; n<nsmps; n++) {
+      double sig = (double)asig[n];
+      double x;
+      if (p->khp[n] != p->prvhp) {
+        double b;
+        p->prvhp = p->khp[n];
+        b = 2.0 - cos((double)(p->khp[n] * csound->tpidsr));
+        p->c2 = c2 = b - sqrt(b * b - 1.0);
+        /*      p->c1 = c1 = 1.0 - c2; */
+      }
+      x = yt1 = c2 * (yt1 + sig);
+      ar[n] = (MYFLT)x;
+      yt1 -= sig;               /* yt1 contains yt1-xt1 */
+    }
+    p->yt1 = yt1;
+    return OK;
+}
+
+static int tonea(CSOUND *csound, TONE *p)
+{
+    MYFLT       *ar, *asig;
+    uint32_t offset = p->h.insdshead->ksmps_offset;
+    uint32_t early  = p->h.insdshead->ksmps_no_end;
+    uint32_t n, nsmps = CS_KSMPS;
+    double      c1 = p->c1, c2 = p->c2;
+    double      yt1 = p->yt1;
+
+    ar = p->ar;
+    asig = p->asig;
+    if (UNLIKELY(offset)) memset(ar, '\0', offset*sizeof(MYFLT));
+    if (UNLIKELY(early)) {
+      nsmps -= early;
+      memset(&ar[nsmps], '\0', early*sizeof(MYFLT));
+    }
+    for (n=offset; n<nsmps; n++) {
+      if (p->khp[n] != (MYFLT)p->prvhp) {
+        double b;
+        p->prvhp = (double)p->khp[n];
+        b = 2.0 - cos((double)(p->prvhp * csound->tpidsr));
+        p->c2 = c2 = b - sqrt(b * b - 1.0);
+        p->c1 = c1 = 1.0 - c2;
+      }
+      yt1 = c1 * (double)(asig[n]) + c2 * yt1;
+      ar[n] = (MYFLT)yt1;
+    }
+    p->yt1 = yt1;
+    return OK;
+}
+
+
 static OENTRY afilts_localops[] =
 {
   { "areson.aa", sizeof(RESON), 0, 5, "a", "aaaoo", (SUBR)rsnset, NULL, (SUBR)aresonaa},
   { "areson.ak", sizeof(RESON), 0, 5, "a", "aakoo", (SUBR)rsnset, NULL, (SUBR)aresonak},
-  { "areson.ka", sizeof(RESON), 0, 5, "a", "akaoo", (SUBR)rsnset, NULL, (SUBR)aresonka}
+  { "areson.ka", sizeof(RESON), 0, 5, "a", "akaoo", (SUBR)rsnset, NULL, (SUBR)aresonka},
+  { "atone.a",  sizeof(TONE),0,  5,   "a", "ako",  (SUBR)tonset, NULL, (SUBR)atonea   },
+  { "tone.a",  sizeof(TONE),0,  5,    "a",  "aao",  (SUBR)tonset, NULL, (SUBR)tonea   }
+
 };
 
 LINKAGE_BUILTIN(afilts_localops)
