9bb357446be8ca12f06de61fb1a79e97398b20d3
veplaini <victor.lazzarini@nuim.ie>
2013-11-13 19:27:14 +0000

    made NaCl build to a library

--- nacl/build.sh
+++ nacl/build.sh
@@ -4,7 +4,6 @@ flex -B -t ../Engine/csound_orc.l > csound_orclex.c
 flex -B ../Engine/csound_pre.lex > csound_prelex.c 
 bison -d -pcsound_orc --report=itemset -o csound_orcparse.c ../Engine/csound_orc.y
 
-
 # Actually build Csound.
 export TOOLCHAIN=pnacl
 make
