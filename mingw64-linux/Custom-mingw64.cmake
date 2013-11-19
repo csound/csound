083c841168d4b4708e8e3c6e2304958eec4ec229
Steven Yi <stevenyi@gmail.com>
2013-11-19 15:33:17 -0500

    added Custom-mingw64.cmake to add settings for mingw64 cross-compile build (i.e. -fpermissive)

---
+++ mingw64-linux/Custom-mingw64.cmake
@@ -0,0 +1,20 @@
+# CUSTOM PROPERTIES TO SET
+
+# GLOBAL
+
+#set(CMAKE_BUILD_TYPE "Debug")
+set(CMAKE_VERBOSE_MAKEFILE ON)
+set(BUILD_STATIC_LIBRARY ON)
+set(USE_OPEN_MP OFF)
+set(TCL_VERSION 8.5)
+#set(PYTHON_INCLUDE_DIRS "/usr/include/python2.7")
+##set(BUILD_CSOUND_AC OFF)
+##set(BUILD_CSOUND_AC_PYTHON_INTERFACE OFF)
+##set(BUILD_CSOUND_AC_LUA_INTERFACE OFF)
+
+#### NOTE the processor type needs setting
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -W -Wall -O3 -mtune=core2 -fpermissive")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -W -Wall -O3 -mtune=core2 -fpermissive")
+## also to test multicore
+#set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wall -W -Wno-missing-field-initializers -Wno-unused-parameter -O3 -mtune=core2 -DJPFF")
+
