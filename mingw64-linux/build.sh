72f0d962a1c1b604034e299edfacaeb39b787b0e
Steven Yi <stevenyi@gmail.com>
2013-11-19 15:33:17 -0500

    added Custom-mingw64.cmake to add settings for mingw64 cross-compile build (i.e. -fpermissive)

--- mingw64-linux/build.sh
+++ mingw64-linux/build.sh
@@ -2,6 +2,6 @@
 
 mkdir csound-mingw64 
 cd csound-mingw64 
-cmake ../.. -DCMAKE_TOOLCHAIN_FILE=../Toolchain-mingw64.cmake -DCMAKE_INSTALL_PREFIX=dist
+cmake ../.. -DCMAKE_TOOLCHAIN_FILE=../Toolchain-mingw64.cmake -DCMAKE_INSTALL_PREFIX=dist -DCUSTOM_CMAKE=../Custom-mingw64.cmake
 make -j6 install
 
