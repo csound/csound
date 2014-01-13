beafdb95030fa21c48cee1c53ac6f3f29f055403
Victor Lazzarini <Victor.Lazzarini@nuim.ie>
2014-01-13 21:12:46 +0000

    fixed the bug in ftalloc() when tables were replaced but the size did not change

--- Engine/fgens.c
+++ Engine/fgens.c
@@ -2283,8 +2283,9 @@ static CS_NOINLINE FUNC *ftalloc(const FGDATA *ff)
                                       "may find this disturbing"), ff->fno);
         }
       }
-      else {                                /* else clear it to zero */
-        memset((void*) ftp->ftable, 0, ff->flen+1);
+      else {     
+                                    /* else clear it to zero */
+        memset((void*) ftp->ftable, 0, sizeof(MYFLT)*(ff->flen+1));
         memset((void*) ftp, 0, sizeof(FUNC)-sizeof(MYFLT*)); /* leaving table! */
       }
     }
