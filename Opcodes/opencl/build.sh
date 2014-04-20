3a304a81c734fb3f1c3544fe4376596a47ae8cf0
veplaini <victor.lazzarini@nuim.ie>
2014-04-20 11:53:45 +0100

    closing issue 311

--- Opcodes/opencl/build.sh
+++ Opcodes/opencl/build.sh
@@ -1,1 +1,1 @@
-gcc -dynamiclib -o libclop1.dylib -DUSE_DOUBLE -D_FORTIFY_SOURCE=0 cladsynth.c -I../../include -framework OpenCL
+gcc -O3 -dynamiclib -o libclop1.dylib -DUSE_DOUBLE -D_FORTIFY_SOURCE=0 cladsynth.c -I../../include -framework OpenCL
