237579d77e5cdf356be376c6c761c7679c9ccfd2
Steven Yi <stevenyi@gmail.com>
2014-05-05 13:02:31 +0100

    modified build script to name 10.9 package as universal

--- installer/macosx/release-build-10.8.sh
+++ installer/macosx/release-build-10.8.sh
@@ -2,9 +2,9 @@
 
 export MANUAL_DIR=`pwd`/../../../manual
 export CS_VERSION="6.03.0"
-export PACKAGE_NAME=csound${CS_VERSION}-OSX10.9-x86_64.pkg
+export PACKAGE_NAME=csound${CS_VERSION}-OSX10.9-universal.pkg
 export DMG_DIR="Csound ${CS_VERSION}"
-export DMG_NAME="csound${CS_VERSION}-OSX10.9-x86_64.dmg"
+export DMG_NAME="csound${CS_VERSION}-OSX10.9-universal.dmg"
 # If arg2 passed in, will cd into that dir and rebuild, otherwise
 # will clone from repo and do a fresh build
 
