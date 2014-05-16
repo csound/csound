91f047c6cf7b4f9762bc051ee3e2d7bf5800894b
John ffitch <jpff@codemist.co.uk>
2014-05-16 10:06:42 +0100

    More coverity buffer overflow fixes

--- util/lpanal.c
+++ util/lpanal.c
@@ -438,7 +438,7 @@ static int lpanal(CSOUND *csound, int argc, char **argv)
                         //strlcat(tp,s,(LPBUFSIZ - sizeof(LPHEADER) + 4));
                         strncat(tp,s,
                                 (LPBUFSIZ - sizeof(LPHEADER) + 3-strlen(tp)));
-                        tp[LPBUFSIZ - sizeof(LPHEADER) + 3] = '\0';
+                        tp[LPBUFSIZ - 1] = '\0';
                         tp += strlen(tp);
                         break;
         case 'P':       FIND(Str("no low frequency"))
