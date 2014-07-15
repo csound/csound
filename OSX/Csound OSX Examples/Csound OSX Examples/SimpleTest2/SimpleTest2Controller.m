7e05962be48678cf3f89aed77010ea260719fdae
Aurelius Prochazka <aure@aure.com>
2014-07-14 17:46:49 -0700

    Creating more examples for the Csound OSX Examples project, specifically the Simple Test 2 and Button Test from the iOS Examples project.  Neither are functional yet.

--- OSX/Csound OSX Examples/Csound OSX Examples/SimpleTest2/SimpleTest2Controller.m
+++ OSX/Csound OSX Examples/Csound OSX Examples/SimpleTest2/SimpleTest2Controller.m
@@ -7,7 +7,48 @@
 //
 
 #import "SimpleTest2Controller.h"
+#import "CsoundUI.h"
+
+@interface SimpleTest2Controller() {
+    CsoundObj* csound;
+}
+@end
 
 @implementation SimpleTest2Controller
+
+- (IBAction)toggleOnOff:(id)sender {
+    
+	if([self.startStopButton.title isEqualToString:@"Start"]) {
+        
+        NSString *tempFile = [[NSBundle mainBundle] pathForResource:@"test2" ofType:@"csd"];
+        NSLog(@"FILE PATH: %@", tempFile);
+        
+        csound = [[CsoundObj alloc] init];
+        [csound addCompletionListener:self];
+        
+        CsoundUI *csoundUI = [[CsoundUI alloc] initWithCsoundObj:csound];
+        [csoundUI addSlider:_rateSlider     forChannelName:@"noteRate"];
+        [csoundUI addSlider:_durationSlider forChannelName:@"duration"];
+        [csoundUI addSlider:_attackSlider   forChannelName:@"attack"];
+        [csoundUI addSlider:_decaySlider    forChannelName:@"decay"];
+        [csoundUI addSlider:_sustainSlider  forChannelName:@"sustain"];
+        [csoundUI addSlider:_releaseSlider  forChannelName:@"release"];
+        
+        [csound startCsound:tempFile];
+        
+	} else {
+        [csound stopCsound];
+    }
+}
+
+#pragma mark CsoundObjCompletionListener
+
+-(void)csoundObjDidStart:(CsoundObj *)csoundObj {
+    self.startStopButton.title = @"Stop";
+}
+
+-(void)csoundObjComplete:(CsoundObj *)csoundObj {
+	self.startStopButton.title = @"Start";
+}
 
 @end
