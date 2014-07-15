ac1988bfca7631b7093f56c5e852bc9a685b7058
Aurelius Prochazka <aure@aure.com>
2014-07-14 21:47:53 -0700

    Further work making the OSX Examples parallel to the iOS examples.

---
+++ OSX/Csound OSX Examples/Csound OSX Examples/SimpleTest2WindowController.h
@@ -0,0 +1,14 @@
+//
+//  SimpleTest2WindowController.h
+//  Csound OSX Examples
+//
+//  Created by Aurelius Prochazka on 7/14/14.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+#import "CsoundObj.h"
+
+@interface SimpleTest2WindowController : NSWindowController<CsoundObjCompletionListener>
+
+@end
--- OSX/Csound OSX Examples/Csound OSX Examples/SimpleTest2WindowController.m
+++ OSX/Csound OSX Examples/Csound OSX Examples/SimpleTest2WindowController.m
@@ -1,20 +1,27 @@
 //
-//  SimpleTest2Controller.m
+//  SimpleTest2WindowController.m
 //  Csound OSX Examples
 //
 //  Created by Aurelius Prochazka on 7/14/14.
 //
 //
 
-#import "SimpleTest2Controller.h"
+#import "SimpleTest2WindowController.h"
 #import "CsoundUI.h"
 
-@interface SimpleTest2Controller() {
+@interface SimpleTest2WindowController() {
     CsoundObj* csound;
 }
+@property (strong) IBOutlet NSButton *startStopButton;
+@property (strong) IBOutlet NSSlider *rateSlider;
+@property (strong) IBOutlet NSSlider *durationSlider;
+@property (strong) IBOutlet NSSlider *attackSlider;
+@property (strong) IBOutlet NSSlider *decaySlider;
+@property (strong) IBOutlet NSSlider *sustainSlider;
+@property (strong) IBOutlet NSSlider *releaseSlider;
 @end
 
-@implementation SimpleTest2Controller
+@implementation SimpleTest2WindowController
 
 - (IBAction)toggleOnOff:(id)sender {
     
