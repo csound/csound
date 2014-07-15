4210111d1d0d6f2c2e6fe8a4265cf4ea5cb1e2b2
Aurelius Prochazka <aure@aure.com>
2014-07-14 17:46:49 -0700

    Creating more examples for the Csound OSX Examples project, specifically the Simple Test 2 and Button Test from the iOS Examples project.  Neither are functional yet.

--- OSX/Csound OSX Examples/Csound OSX Examples/SimpleTest2/SimpleTest2Controller.h
+++ OSX/Csound OSX Examples/Csound OSX Examples/SimpleTest2/SimpleTest2Controller.h
@@ -7,7 +7,16 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "CsoundObj.h"
 
-@interface SimpleTest2Controller : NSObject
+@interface SimpleTest2Controller : NSObject <CsoundObjCompletionListener>
+
+@property (weak) IBOutlet NSButton *startStopButton;
+@property (weak) IBOutlet NSSlider *rateSlider;
+@property (weak) IBOutlet NSSlider *durationSlider;
+@property (weak) IBOutlet NSSlider *attackSlider;
+@property (weak) IBOutlet NSSlider *decaySlider;
+@property (weak) IBOutlet NSSlider *sustainSlider;
+@property (weak) IBOutlet NSSlider *releaseSlider;
 
 @end
