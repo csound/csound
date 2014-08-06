ad5f0f669372cdadf2ac73bce23a430489e28a38
Aurelius Prochazka <aure@aure.com>
2014-08-05 23:02:40 -0700

    Started the Record Test example.

---
+++ OSX/Csound OSX Examples/Csound OSX Examples/RecordTest/RecordTestWindowController.h
@@ -0,0 +1,14 @@
+//
+//  RecordTestWindowController.h
+//  Csound OSX Examples
+//
+//  Created by Aurelius Prochazka on 7/27/14.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+#import "BaseCsoundWindowController.h"
+
+@interface RecordTestWindowController : BaseCsoundWindowController
+
+@end
