477b3c889a9533fa8dfa9de4dc7c448e1162d3b2
Aurelius Prochazka <aure@aure.com>
2014-08-16 00:38:39 -0700

    CsoundBindings for Csound for OSX
    Removed most occurrences of CsoundValueCacheable and replaced with CsoundBinding to match the iOS project.

--- OSX/Csound OSX Examples/csound-OSX/classes/bindings/ui/CsoundSliderBinding.h
+++ OSX/Csound OSX Examples/csound-OSX/classes/bindings/ui/CsoundSliderBinding.h
@@ -1,15 +1,15 @@
-/* 
+/*
  
- CachedButton.h:
+ CsoundSliderBinding.h:
  
- Copyright (C) 2011 Steven Yi
+ Copyright (C) 2014 Steven Yi, Aurelius Prochazka
  
  This file is part of Csound for iOS.
  
  The Csound for iOS Library is free software; you can redistribute it
  and/or modify it under the terms of the GNU Lesser General Public
  License as published by the Free Software Foundation; either
- version 2.1 of the License, or (at your option) any later version.   
+ version 2.1 of the License, or (at your option) any later version.
  
  Csound is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -22,18 +22,12 @@
  02111-1307 USA
  
  */
+
 #import <Foundation/Foundation.h>
-#import "BaseValueCacheable.h"
 #import "CsoundObj.h"
 
-@interface CachedButton : BaseValueCacheable {
-    double cachedValue;
-    double *channelPtr;
-}
+@interface CsoundSliderBinding : NSObject<CsoundBinding>
 
-@property (nonatomic, strong) NSString *channelName;
-@property (nonatomic, strong) NSButton *button;
-
--(CachedButton*)init:(NSButton *)button channelName:(NSString *)channelName;
+-(instancetype)initSlider:(NSSlider *)slider channelName:(NSString *)channelName;
 
 @end
