f7788a1b06439c1fcfebb85d7cca1829f150ccf4
John ffitch <jpff@codemist.co.uk>
2014-10-18 15:26:57 +0100

    ore controlable vactrol

--- Opcodes/buchla.c
+++ Opcodes/buchla.c
@@ -33,9 +33,7 @@ typedef struct {
       MYFLT       *ain1, *ain2, *ain3, *ain4, *in5, *in6;
   // Internal
       MYFLT       so, sx, sd, xo;
-      MYFLT       c1, c2, c3;
-      // vactro model
-      MYFLT       s1p;
+      double      f;
 } BUCHLA;
 
 #define clip(a,b,c) (a<b ? b : a>c ? c : a)
@@ -43,14 +41,15 @@ typedef struct {
 int poly_LPG_init(CSOUND* csound, BUCHLA *p)
 {
     p->so = p->sx = p->sd = p->xo = 0.0;
-    p->c1 = (1e-09);
-    p->c2 = (2.2e-10);
+#define C1 (1e-09)
+#define C2 (2.2e-10)
+    p->f = 0.5/csound->GetSr(csound);
     return OK;
 }
 
 int poly_LPG_perf(CSOUND* csound, BUCHLA *p)
 {
-    double c3, rf, r3, max_res, a, f, a1, a2, b1, b2, b3, b4;
+    double c3, rf, r3, max_res, a, f=p->f, a1, a2, b1, b2, b3, b4;
     double Dmas, yx, yo, yd, tanh_xo, Dx, Do;
     MYFLT *x, *out1, *out2, *out3;
     uint32_t offset = p->h.insdshead->ksmps_offset;
@@ -63,11 +62,11 @@ int poly_LPG_perf(CSOUND* csound, BUCHLA *p)
       c3 = 0.0;
 
     //r1 = 1e3;
-    rf = *p->ain2;               /* does this need to be audio? */
+    rf = *p->ain2;               /* from a vactrol operation */
     //rf = 30e3;
     r3 = *p->ain3;               /* does this need to be audio? */
 
-    max_res = 1.0*(2.0 *p->c1*r3+(p->c2+c3)*(r3+rf))/(c3*r3);
+    max_res = 1.0*(2.0*C1*r3+(C2+c3)*(r3+rf))/(c3*r3);
     //a = in4* max_res;
     x = p->ain1;
     out1 = p->out1;
@@ -77,16 +76,14 @@ int poly_LPG_perf(CSOUND* csound, BUCHLA *p)
     f = 0.5/csound->GetSr(csound);
     //f = 2*pi * (in2+1e-3)*0.5/samplerate;
 
-    a1 = 1.0/(p->c1*rf);
-    a2 = -(1/rf+1/r3)/p->c1;
-    b1 = 1.0/(rf*p->c2);
-    b2 = -2.0/(rf*p->c2);
-    b3 = 1.0/(rf*p->c2);
-    b4 = c3/p->c2;
+    a1 =  1.0/(C1*rf);
+    a2 = -(1/rf+1/r3)/C1;
+    b1 =  1.0/(rf*C2);
+    b2 = -2.0/(rf*C2);
+    b3 =  1.0/(rf*C2);
+    b4 =  c3/C2;
 
     tanh_xo= tanh(p->xo);
-    //b2 = (a*c3/c1 -1 - 1/(1+r1/rf))/(rf*(c2+c3));\t\t\t//\tExtra Feedback resistor
-    //b3 =(1/(1+r1/rf)- (1+rf/r3)*a*c3/c1)/(rf*(c2+c3));\t//\tExtra Feedback resistor
 
     Dx =1.0/(1.0-b2*f);
     Do =1.0/(1.0-a2*f);
@@ -108,24 +105,28 @@ int poly_LPG_perf(CSOUND* csound, BUCHLA *p)
       for (n=offset; n<nsmps; n++) {
         a = clip(p->ain4[n],0.0,max_res);
         Dmas = 1.0/(1.0-Dx*(f*f*b3*Do*a1 + b4*f*a*(1.0-txo2)*Do*a1 - b4));
-        yx =(p->sx + f*b1*x[n] + f*b3*Do*p->so + f*b4*(p->sd+(1.0/f)*a*(tanh_xo - p->xo*(1.0-txo2))) +
+        yx =(p->sx + f*b1*x[n] + f*b3*Do*p->so +
+            f*b4*(p->sd+(1.0/f)*a*(tanh_xo - p->xo*(1.0-txo2))) +
              b4*a*(1.0-txo2)*Do*p->so)*Dx*Dmas;
         yo =(p->so+f*a2*yx)*Do;
-        yd = (p->sd+(1/f)*a*(tanh_xo - p->xo*(1.0-txo2))) + (1.0/f)*(a*((1.0-txo2))*yo - yx);
+        yd = (p->sd+(1/f)*a*(tanh_xo - p->xo*(1.0-txo2))) +
+             (1.0/f)*(a*((1.0-txo2))*yo - yx);
         p->sx += 2.0*f*(b1*x[n] + b2*yx + b3*yo +b4*yd);
         p->so += 2.0*f*(a1*yx + a2*yo);
-        p->sd = -(p->sd+(2.0/f)*a*(tanh_xo - p->xo*(1.0-txo2))) - (2.0/f)*(a*(1.0-txo2)*yo - yx);
+        p->sd = -(p->sd+(2.0/f)*a*(tanh_xo - p->xo*(1.0-txo2))) -
+                 (2.0/f)*(a*(1.0-txo2)*yo - yx);
         p->xo = yo;
-        out1[n] = yo;
-        out2[n] = yx;
-        out3[n] = yd;
+        out1[n] = (MYFLT)yo;
+        out2[n] = (MYFLT)yx;
+        out3[n] = (MYFLT)yd;
       }
     }
     else /* if (in6 < 0.5) */ {
       for (n=offset; n<nsmps; n++) {
         a = clip(p->ain4[n],0.0,max_res);
         Dmas = 1.0/(1.0-Dx*(f*f*b3*Do*a1 + b4*f*a*Do*a1 - b4));
-        yx = (p->sx + f*b1*x[n] + f*b3*Do*p->so + f*b4*p->sd + b4*a*Do*p->so)*Dx*Dmas;
+        yx = (p->sx + f*b1*x[n] + f*b3*Do*p->so + 
+              f*b4*p->sd + b4*a*Do*p->so)*Dx*Dmas;
         yo = (p->so+f*a1*yx)*Do;
         yd = p->sd + (1.0/f)*(a*yo-yx) ;
         p->sx += 2.0*f*(b1*x[n] + b2*yx + b3*yo +b4*yd);
@@ -143,14 +144,20 @@ typedef struct {
       OPDS        h;
       MYFLT       *out;
       MYFLT       *inp;
+      MYFLT       *up;
+      MYFLT       *down;
       double      s1;
       double      a_base;
+      double      t_up;
+      double      t_down;
 } VACTROL;
 
 int vactrol_init(CSOUND *csound, VACTROL* p)
 {
-    p->s1 = 00;
+    p->s1 = 0;
     p->a_base = 1000.0*M_PI/(csound->GetSr(csound));
+    p->t_down = *p->down<FL(0.0) ? 3.0e3 : (double)*p->down; 
+    p->t_up   = *p->up<FL(0.0) ? 20.0 : (double)*p->up; 
     return OK;
 }
 
@@ -158,10 +165,8 @@ int vactrol_perf(CSOUND *csound, VACTROL* p)
 {
     double s1 = p->s1;
     double a_base = p->a_base; 
-    //double T_DOWN = 3e3; // Fall time
-#define T_DOWN (3.0e3)
-    //double T_UP = 20.0; // Rise time
-#define T_UP (20.0)
+    double T_DOWN = p->t_down; // Fall time
+    double T_UP   = p->t_up; // Rise time
     uint32_t offset = p->h.insdshead->ksmps_offset;
     uint32_t early  = p->h.insdshead->ksmps_no_end;
     uint32_t n, nsmps = CS_KSMPS;
@@ -204,8 +209,10 @@ int vactrol_perf(CSOUND *csound, VACTROL* p)
 #define S       sizeof
 
 static OENTRY buchla_localops[] = {
-  { "buchla", S(BUCHLA), 0, 5, "aaa", "aaaaPP", (SUBR)poly_LPG_init, NULL, (SUBR)poly_LPG_perf },
-  { "vactrol", S(VACTROL), 0, 5, "a", "a", (SUBR)vactrol_init, NULL, (SUBR)vactrol_perf }
+  { "buchla", S(BUCHLA), 0, 5, "aaa", "aaaaPP",
+                                 (SUBR)poly_LPG_init, NULL, (SUBR)poly_LPG_perf },
+  { "vactrol", S(VACTROL), 0, 5, "a", "ajj",
+                                 (SUBR)vactrol_init, NULL, (SUBR)vactrol_perf }
 };
 
 LINKAGE_BUILTIN(buchla_localops)
