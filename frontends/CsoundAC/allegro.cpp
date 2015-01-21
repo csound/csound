8f9ef0cd56d9b211b95733877b9bb889020b4960
jpff <jpff@codemist.co.uk>
2015-01-21 16:35:27 +0000

    correct strlcpy->strncpy

--- frontends/CsoundAC/allegro.cpp
+++ frontends/CsoundAC/allegro.cpp
@@ -56,8 +56,8 @@ void Alg_atoms::expand()
     maxlen += (maxlen >> 2); // add 25%
     Alg_attribute *new_atoms = new Alg_attribute[maxlen];
     // now do copy
-    // **** Suggesred fix -- Pff
-    if (beats) memcpy(new_atoms, atoms, len * sizeof(Alg_attribute));
+    // **** Suggested fix -- JPff
+    if (atoms) memcpy(new_atoms, atoms, len * sizeof(Alg_attribute));
     if (atoms) delete[] atoms;
     atoms = new_atoms;
 }
