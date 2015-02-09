ca3f9c325458f1d8986d2eb554c34be8e100a468
Aurelius Prochazka <aure@aure.com>
2015-02-08 22:45:01 -0800

    A lot of unused variables removed and tightening up of code

--- iOS/Csound iOS Examples/Csound iOS Examples/ViewControllers/Waveview/Waveview.m
+++ iOS/Csound iOS Examples/Csound iOS Examples/ViewControllers/Waveview/Waveview.m
@@ -1,4 +1,4 @@
-/* 
+/*
  
  Waveview.m:
  
@@ -9,7 +9,7 @@
  The Csound for iOS Library is free software; you can redistribute it
  and/or modify it under the terms of the GNU Lesser General Public
  License as published by the Free Software Foundation; either
- version 2.1 of the License, or (at your option) any later version.   
+ version 2.1 of the License, or (at your option) any later version.
  
  Csound is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -27,117 +27,81 @@
 #import "CsoundObj.h"
 
 @implementation Waveview
-
-- (id)initWithFrame:(CGRect)frame
 {
-    self = [super initWithFrame:frame];
-    if (self) {
-    
-    }
-    return self;
+    BOOL tableLoaded;
+    CGFloat lastY;
+    CsoundObj *csObj;
+    MYFLT *table;
+    int tableLength;
+    MYFLT *displayData;
 }
-
-
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-
 
 - (void)drawRect:(CGRect)rect
 {
-
-	CGContextRef context = UIGraphicsGetCurrentContext();
-//	CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB()
-	CGContextSetRGBFillColor(context, 0, 0, 0, 1);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextSetRGBFillColor(context, 0, 0, 0, 1);
     CGContextFillRect(context, rect);
-	
-	if (tableLoaded) {
-		
-		int width = self.frame.size.width;
-		
-		CGContextSetRGBStrokeColor(context, 255, 255, 255, 1);
-		CGContextSetRGBFillColor(context, 255, 255, 255, 1);
-		CGMutablePathRef fill_path = CGPathCreateMutable();
-		CGFloat x = 0;
-		CGFloat y = displayData[0];
-		
-		CGPathMoveToPoint(fill_path, &CGAffineTransformIdentity, x, y);
-		
-		for(int i = 1; i < width; i++) {
-			CGPathAddLineToPoint(fill_path, &CGAffineTransformIdentity, i, displayData[i]);
-			//NSLog(@"%d: %f", index, val);
-										  
-		}
-	
-		CGContextAddPath(context, fill_path);	
-		CGContextSetAllowsAntialiasing(context, YES);
-		CGContextDrawPath(context, kCGPathStroke);
-		CGPathRelease(fill_path);
-	}
+    
+    if (tableLoaded) {
+        int width = self.frame.size.width;
+        
+        CGContextSetRGBStrokeColor(context, 255, 255, 255, 1);
+        CGContextSetRGBFillColor(context, 255, 255, 255, 1);
+        CGMutablePathRef fill_path = CGPathCreateMutable();
+        CGFloat x = 0;
+        CGFloat y = displayData[0];
+        
+        CGPathMoveToPoint(fill_path, &CGAffineTransformIdentity, x, y);
+        
+        for(int i = 1; i < width; i++) {
+            CGPathAddLineToPoint(fill_path, &CGAffineTransformIdentity, i, displayData[i]);
+        }
+        CGContextAddPath(context, fill_path);
+        CGContextSetAllowsAntialiasing(context, YES);
+        CGContextDrawPath(context, kCGPathStroke);
+        CGPathRelease(fill_path);
+    }
 }
 
 - (void)setup:(CsoundObj *)csoundObj
 {
-	tableLoaded = NO;
-	csObj = csoundObj;
-
-	
-	
-}
-
-- (void)updateValuesToCsound;
-{
-	
+    tableLoaded = NO;
+    csObj = csoundObj;
 }
 
 - (void)updataDisplayData
 {
-	int width = self.frame.size.width;
-	int height = self.frame.size.height;
-	int middle = height / 2;
+    float yScalingFactor = 0.8;
 
-	@autoreleasepool {
-
-
-		displayData = malloc(sizeof(MYFLT) * width);
-		
-			
-		for(int i = 0; i < width; i++) {
-			float percent = i / (float)(width);
-			int index = (int)(percent * tableLength);
-			displayData[i] = (table[index] * middle) + middle;
-			
-			//NSLog(@"%d: %f", index, val);
-			
-		}
-		
-		
-		[self performSelectorOnMainThread:@selector(setNeedsDisplay) withObject:nil waitUntilDone:NO];
-	
-	}
-
+    int width = self.frame.size.width;
+    int height = self.frame.size.height;
+    int middle = height / 2;
+    
+    displayData = malloc(sizeof(MYFLT) * width);
+    
+    for(int i = 0; i < width; i++) {
+        float percent = i / (float)(width);
+        int index = (int)(percent * tableLength);
+        displayData[i] = (table[index] * middle) + middle;
+    }
+    
+    [self performSelectorOnMainThread:@selector(setNeedsDisplay)
+                           withObject:nil
+                        waitUntilDone:NO];
 }
 
 - (void)updateValuesFromCsound
 {
-	if (!tableLoaded) {
-	
-		@autoreleasepool {
-
-			CSOUND *cs = [csObj getCsound];
-			sr = csoundGetSr(cs);
-			ksmps = csoundGetKsmps(cs);
-			
-					
-				if ((tableLength = csoundTableLength(cs, 1)) > 0) {
-					
-					table = malloc(tableLength * sizeof(MYFLT));
-					csoundGetTable(cs, &table, 1);
-					tableLoaded = YES;
-					[self performSelectorInBackground:@selector(updataDisplayData) withObject:nil];
-				}
-
-		}
-	}
+    if (!tableLoaded) {
+        CSOUND *cs = [csObj getCsound];
+        
+        if ((tableLength = csoundTableLength(cs, 1)) > 0) {
+            table = malloc(tableLength * sizeof(MYFLT));
+            csoundGetTable(cs, &table, 1);
+            tableLoaded = YES;
+            [self performSelectorInBackground:@selector(updataDisplayData) withObject:nil];
+        }
+    }
 }
- 
+
 @end
