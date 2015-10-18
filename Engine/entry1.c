3bf5977a6af85b595954200a26ac82c7802ba18a
jpff <jpff@codemist.co.uk>
2015-10-18 14:55:31 +0100

    tweak to ?: with strings

--- Engine/entry1.c
+++ Engine/entry1.c
@@ -194,7 +194,7 @@ OENTRY opcodlst_1[] = {
   { ":cond.i",     S(CONVAL),0,  1,      "i",    "bii",  conval                  },
   { ":cond.k",     S(CONVAL),0,  2,      "k",    "Bkk",  NULL,   conval          },
   { ":cond.a",     S(CONVAL),0,  4,      "a",    "Bxx",  NULL,   NULL,   aconval },
-  { ":cond.S",     S(CONVAL),0,  2,      "S",    "BSS",  NULL,   conval       },
+  { ":cond.S",     S(CONVAL),0,  3,      "S",    "BSS",  conval, conval       },
   { "##add.ii",  S(AOP),0,    1,      "i",    "ii",   addkk                   },
   { "##sub.ii",  S(AOP),0,    1,      "i",    "ii",   subkk                   },
   { "##mul.ii",  S(AOP),0,    1,      "i",    "ii",   mulkk                   },
