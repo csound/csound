86413cc7154d85a497b4375f6b844bafc8e67ebe
jpff <jpff@codemist.co.uk>
2015-10-21 13:44:17 +0100

    adjustment to bison code

--- Engine/new_orc_parser.c
+++ Engine/new_orc_parser.c
@@ -169,8 +169,8 @@ TREE *csoundParseOrc(CSOUND *csound, const char *str)
         corfile_putc('\0', csound->orchstr);
         corfile_putc('\0', csound->orchstr);
       }
-      csound->DebugMsg(csound, "Calling preprocess on >>%s<<\n",
-              corfile_body(csound->orchstr));
+      /* csound->DebugMsg(csound, "Calling preprocess on >>%s<<\n", */
+      /*         corfile_body(csound->orchstr)); */
       //csound->DebugMsg(csound,"FILE: %s \n", csound->orchstr->body);
       //    csound_print_preextra(&qq);
       cs_init_math_constants_macros(csound, &qq);
@@ -182,8 +182,8 @@ TREE *csoundParseOrc(CSOUND *csound, const char *str)
         csound->LongJmp(csound, 1);
       }
       csound_prelex_destroy(qq.yyscanner);
-      csound->DebugMsg(csound, "yielding >>%s<<\n",
-                       corfile_body(csound->expanded_orc));
+      /* csound->DebugMsg(csound, "yielding >>%s<<\n", */
+      /*                  corfile_body(csound->expanded_orc)); */
       corfile_rm(&csound->orchstr);
 
     }
