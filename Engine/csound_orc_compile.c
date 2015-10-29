b95e9186c18418108f436bf7e12eae5ce1833f8d
jpff <jpff@codemist.co.uk>
2015-10-29 16:19:22 +0000

    add setjmp to csoundcomileorc

--- Engine/csound_orc_compile.c
+++ Engine/csound_orc_compile.c
@@ -1747,6 +1747,8 @@ extern void sanitize(CSOUND *csound);
 PUBLIC int csoundCompileOrc(CSOUND *csound, const char *str)
 {
     int retVal=1;
+    if ((retVal=setjmp(csound->exitjmp))) return retVal;
+    retVal = 1;
     TREE *root = csoundParseOrc(csound, str);
     if (LIKELY(root != NULL)) {
      retVal = csoundCompileTree(csound, root);
