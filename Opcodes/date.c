5489d8a3a96075bee5a7975f478c72e0f151a8bd
jpff <jpff@codemist.co.uk>
2015-11-12 14:35:00 +0000

    revised date opcode

--- Opcodes/date.c
+++ Opcodes/date.c
@@ -52,11 +52,19 @@ static int datemyfltset(CSOUND *csound, DATEMYFLT *p)
     struct timespec tp;
     clock_gettime(CLOCK_REALTIME, &tp);
     *p->time_ = (MYFLT) (tp.tv_sec-base);
-    *p->time_ += (MYFLT)(tp.tv_nsec*1.0e-9);
+    *p->time_ += (MYFLT)(tp.tv_nsec)*1.0e-9;
     if (p->OUTOCOUNT==2) *p->nano =(MYFLT)tp.tv_nsec;
 #else
+  #ifdef __MACH
+    struct timeval tp;
+    int rv = gettimeofday(&tp, NULL);
+    *p->time_  = (MYFLT)(tp.tv_sec-base);
+    *p->time_ += (MYFLT)(tp.tv_usec)*1.0e-6;
+    if (p->OUTOCOUNT==2) *p->nano =(MYFLT)(tp.tv_usec * 1000);
+  #else 
     *p->time_ = (MYFLT) (time(NULL)-base);
     if (p->OUTOCOUNT==2) *p->nano = FL(0.0);
+  #endif
 #endif
     return OK;
 }
