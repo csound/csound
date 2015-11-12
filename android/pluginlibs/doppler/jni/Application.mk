6770316cd9fd6e9c55f9730910a0a6c09a671c20
gogins <michael.gogins@gmail.com>
2015-11-11 19:59:44 -0500

    Made build, compile, and target SDKs consistent at the lowest common denominator for all Android build files. Introduced csoundCompileCsdText to the API but not as a member of the CSOUND struct.

--- android/pluginlibs/doppler/jni/Application.mk
+++ android/pluginlibs/doppler/jni/Application.mk
@@ -1,5 +1,6 @@
-APP_CPPFLAGS += -fexceptions -frtti
 APP_ABI := armeabi armeabi-v7a
+APP_CPPFLAGS += -fexceptions -frtti
 APP_OPTIM := release
+APP_PLATFORM := 19
 APP_STL := gnustl_shared
-#NDK_TOOLCHAIN_VERSION := 4.8
+NDK_TOOLCHAIN_VERSION := 4.8
