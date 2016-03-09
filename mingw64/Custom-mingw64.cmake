49bba4ca6372d504b849a1a3e78cff9ffdf31bc4
gogins <michael.gogins@gmail.com>
2016-03-09 17:41:09 +0200

    Find modules for mingw64 build and associated changes.

--- mingw64/Custom-mingw64.cmake
+++ mingw64/Custom-mingw64.cmake
@@ -1,30 +1,8 @@
-# CUSTOM PROPERTIES TO SET
-
-# GLOBAL
-
-set(CMAKE_BUILD_TYPE "RelWithDebInfo")
-set(CMAKE_VERBOSE_MAKEFILE OFF)
+set(BUILD_PD_CLASS OFF)
 set(BUILD_STATIC_LIBRARY ON)
-set(USE_OPEN_MP OFF)
-set(TCL_VERSION 8.5)
-
+set(BUILD_TESTS OFF)
+set(CMAKE_BUILD_TYPE "RelWithDebInfo")
+set(CMAKE_VERBOSE_MAKEFILE ON)
 set(NEED_PORTTIME OFF)
-
-#set(PYTHON_INCLUDE_DIRS "/usr/include/python2.7")
-##set(BUILD_CSOUND_AC OFF)
-set(BUILD_CSOUND_AC_PYTHON_INTERFACE OFF)
-set(BUILD_PYTHON_OPCODES OFF)
-##set(BUILD_CSOUND_AC_LUA_INTERFACE OFF)
-
-
-#GET_FILENAME_COMPONENT(MINGW_DEPS_DIR "${CMAKE_CURRENT_BINARY_DIR}/../mingw64" ABSOLUTE)
-#list(APPEND CMAKE_SYSTEM_LIBRARY_PATH "c:/cygwin64/lib")
-#list(APPEND CMAKE_SYSTEM_INCLUDE_PATH "c:/cygwin64/usr/include")
-#set(CMAKE_SYSTEM_INCLUDE_PATH "${CMAKE_SYSTEM_INCLUDE_PATH};$MINGW_DEPS_DIR/usr/local/include")
-
-#### NOTE the processor type needs setting
-#set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -W -Wall -O3 -mtune=core2 -I${MINGW_DEPS_DIR}/usr/local/include --sysroot=/usr/x86_64-w64-mingw32")
-#set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -W -Wall -O3 -mtune=core2 -fpermissive")
-## also to test multicore
-#set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wall -W -Wno-missing-field-initializers -Wno-unused-parameter -O3 -mtune=core2 -DJPFF")
-
+set(TCL_VERSION 8.5)
+set(USE_OPEN_MP OFF)
