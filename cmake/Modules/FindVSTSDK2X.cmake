89582153cf7357269eead042406fe94c0be3989b
gogins <michael.gogins@gmail.com>
2016-03-12 21:57:01 +0200

    Some fixes to build system for msys64 build.

--- cmake/Modules/FindVSTSDK2X.cmake
+++ cmake/Modules/FindVSTSDK2X.cmake
@@ -4,9 +4,9 @@
 #  VSTSDK2X_DIR - The VST SDK root directory.
 #  VSTSDK2X_INCLUDE_DIRS - The VST SDK include directories.
 
-find_path(VSTSDK2X_INCLUDE_DIR vstsdk2.4/pluginterfaces/vst2.x/aeffect.h)
+find_path(VSTSDK2X_INCLUDE_DIR pluginterfaces/vst2.x/aeffect.h PATH_SUFFIXES vstsdk2.4)
 
-set(VSTSDK2X_DIR ${VSTSDK2X_INCLUDE_DIR}/vstsdk2.4/)
+set(VSTSDK2X_DIR ${VSTSDK2X_INCLUDE_DIR})
 set(VSTSDK2X_INCLUDE_DIRS ${VSTSDK2X_INCLUDE_DIR} )
 
 include(FindPackageHandleStandardArgs)
