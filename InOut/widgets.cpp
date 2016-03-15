3ad43425ade1489e81b35ae24d3eedbaf2e7fcef
jpff <jpff@codemist.co.uk>
2016-03-15 15:59:54 +0000

    release notes

--- InOut/widgets.cpp
+++ InOut/widgets.cpp
@@ -120,7 +120,7 @@ extern "C" {
         while (p->eventQueue != NULL) {
           rtEvt_t *ep = p->eventQueue;
           p->eventQueue = ep->nxt;
-	  ep->evt->pinstance = NULL;
+          ep->evt->pinstance = NULL;
           csound->UnlockMutex(p->mutex_);
           csound->insert_score_event_at_sample(csound, &(ep->evt),
                                      csound->GetCurrentTimeSamples(csound));
@@ -168,7 +168,7 @@ extern "C" {
         /* Create the new event */
         evt = (rtEvt_t*) malloc(sizeof(rtEvt_t));
         evt->nxt = NULL;
-	evt->pinstance = NULL;
+        evt->pinstance = NULL;
         evt->evt.strarg = NULL; evt->evt.scnt = 0;
         evt->evt.opcod = (char) *args[0];
         if (evt->evt.opcod == '\0')
@@ -207,9 +207,9 @@ extern "C" {
             e.p[i] = *args[i];
           if (e.p[2] < MYFLT(0.0))
             e.p[2] = MYFLT(0.0);
-	  e.pinstance = NULL;
+          e.pinstance = NULL;
           csound->insert_score_event_at_sample(csound, &e,
-	                          csound->GetCurrentTimeSamples(csound));
+                                  csound->GetCurrentTimeSamples(csound));
         }
   }
 }
