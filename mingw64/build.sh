1dfef631fa9a94dafc8bfdf61c6de30113b13860
gogins <michael.gogins@gmail.com>
2016-03-26 09:30:43 +0300

    Ensure plain build.sh for mingw64 actually works out of the box. Conditional code for Python and VST features in the Windows x64 installer. Better fix for RAWWAVE_PATH in stkops.dll.

--- mingw64/build.sh
+++ mingw64/build.sh
@@ -1,7 +1,40 @@
 #!/bin/bash
-
+echo "Overrides for Csound's CMake build system."
+echo
+echo "Building Csound..."
 mkdir csound-mingw64
 cd csound-mingw64
-cmake ../.. -DCMAKE_TOOLCHAIN_FILE=../Toolchain-mingw64.cmake -DCMAKE_INSTALL_PREFIX=dist -DCUSTOM_CMAKE=../Custom-mingw64.cmake -DUSE_GETTEXT=0 -DSWIG_DIR=C:\msys2\mingw64\share\swig\3.0.6 -G "MSYS Makefiles" $@
-make -j6 install
+pwd
+rm -rf dist
+cmake ../.. -G "MSYS Makefiles" \
+-DUSE_GETTEXT=0 \
+-DBUILD_CSOUNDVST=0 \
+-DBUILD_PD_CLASS=0 \
+-DBUILD_STATIC_LIBRARY=1 \
+-DBUILD_TESTS=0 \
+-DBUILD_VST4CS_OPCODES=0 \
+-DCMAKE_BUILD_TYPE=RelWithDebInfo \
+-DCMAKE_INSTALL_PREFIX=dist \
+-DCMAKE_VERBOSE_MAKEFILE=0 \
+-DMUSICXML_LIBRARY=D:/msys64/mingw64/bin/libmusicxml2.dll \
+-DNEED_PORTTIME=0 \
+-DTCL_VERSION=8.5 \
+-DUSE_CURL=0 \
+-DUSE_OPEN_MP=0 \
+-DSWIG_DIR=C:\msys2\mingw64\share\swig\3.0.6
+if [ $? -ne 0 ]; then
+    echo "Failed to run CMake."
+    exit
+fi
+make -j6 $@
+if [ $? -ne 0 ]; then
+    echo "Failed to run make."
+    exit
+fi
+make install
+make -j6 $@
+if [ $? -ne 0 ]; then
+    echo "Failed to run make install."
+    exit
+fi
 
