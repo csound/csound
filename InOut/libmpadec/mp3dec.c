5ba598fb3674de05312b3e23e2f74d46dfa1a2c4
veplaini <victor.lazzarini@nuim.ie>
2016-04-15 08:26:21 +0100

    mp3scal modifications

--- InOut/libmpadec/mp3dec.c
+++ InOut/libmpadec/mp3dec.c
@@ -65,7 +65,7 @@ int mp3dec_init_file(mp3dec_t mp3dec, int fd, int64_t length, int nogap)
       if (tmp >= 0) {
         mp3->stream_size = tmp;
         tmp = lseek(fd, mp3->stream_offset, SEEK_SET);
-        if (tmp<0) fprintf(stderr, "seek failure im mp3\n");
+        // if (tmp<0) fprintf(stderr, "seek failure im mp3\n");
       } else mp3->flags &= ~MP3DEC_FLAG_SEEKABLE;
     }
     if (mp3->stream_size > mp3->stream_offset) {
@@ -79,8 +79,8 @@ int mp3dec_init_file(mp3dec_t mp3dec, int fd, int64_t length, int nogap)
       if (hdr[0] == 'I' && hdr[1] == 'D' && hdr[2] == '3') {
         /* A*2^21+B*2^14+C*2^7+D=A*2097152+B*16384+C*128+D*/
         mp3->stream_offset = hdr[6]*2097152+hdr[7]*16384+hdr[8]*128+hdr[9] + 10;
-        fprintf(stderr, "==== found ID3 tag, skipping %lld bytes ==== \n",
-                mp3->stream_offset);
+        // fprintf(stderr, "==== found ID3 tag, skipping %lld bytes ==== \n",
+	//     mp3->stream_offset);
       }
       (void) lseek(fd, mp3->stream_offset, SEEK_SET);
     }
@@ -131,7 +131,7 @@ int mp3dec_init_file(mp3dec_t mp3dec, int fd, int64_t length, int nogap)
                         NULL, 0, &mp3->in_buffer_offset, NULL);
       mp3->in_buffer_used -= mp3->in_buffer_offset;
       if (r != MPADEC_RETCODE_OK) {
-        //this is a fix for ID3 tag at the start of a file
+        // this is a fix for ID3 tag at the start of a file
         while(r == 7) { /* NO SYNC, read more data */
           int32_t n = sizeof(mp3->in_buffer);
           if (mp3->stream_size && (n > mp3->stream_size))
