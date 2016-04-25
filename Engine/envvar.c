5ae0483a6986265883e46d0a72bf1e8e6237e846
jpff <jpff@codemist.co.uk>
2016-04-25 20:21:48 +0100

    warning in xadsr too short

--- Engine/envvar.c
+++ Engine/envvar.c
@@ -1011,19 +1011,22 @@ void *csoundFileOpenWithType(CSOUND *csound, void *fd, int type,
     if (env == NULL) {
       fullName = (char*) name;
 #if defined(WIN32)
-      /* To handle Widows errors in file name characters. */
-      size_t sz = MultiByteToWideChar(CP_UTF8, 0, name, -1, NULL, 0);
-      wchar_t *wfname = alloca(sz);
-      wchar_t *wmode = 0;
-      MultiByteToWideChar(CP_UTF8, 0, name, -1, wfname, sz);
-      sz = MultiByteToWideChar(CP_UTF8, 0, param, -1, NULL, 0);
-      wmode = alloca(sz);
-      MultiByteToWideChar(CP_UTF8, 0, param, -1, wmode, sz);
-      if (type == CSFILE_STD) {
-        tmp_f = _wfopen(wfname, wmode);
-        if (tmp_f == NULL) {
-          perror(Str("csound->FileOpen2 failed:"));
-          goto err_return;
+      {
+        /* To handle Widows errors in file name characters. */
+        size_t sz = MultiByteToWideChar(CP_UTF8, 0, name, -1, NULL, 0);
+        wchar_t *wfname = alloca(sz);
+
+        wchar_t *wmode = 0;
+        MultiByteToWideChar(CP_UTF8, 0, name, -1, wfname, sz);
+        sz = MultiByteToWideChar(CP_UTF8, 0, param, -1, NULL, 0);
+        wmode = alloca(sz);
+        MultiByteToWideChar(CP_UTF8, 0, param, -1, wmode, sz);
+        if (type == CSFILE_STD) {
+          tmp_f = _wfopen(wfname, wmode);
+          if (tmp_f == NULL) {
+            perror(Str("csound->FileOpen2 failed:"));
+            goto err_return;
+          }
         }
       }
 #else
