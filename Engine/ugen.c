73e74d0e59956047da22ed936b6f42809de3f854
Steven Yi <stevenyi@gmail.com>
2016-08-12 17:16:36 -0400

    added stubbed out Ugen API functions

--- Engine/ugen.c
+++ Engine/ugen.c
@@ -16,51 +16,54 @@
  * - context: required for things like hold, releasing, etc.
  * */
 
-#include "csoundCore.h"
-
-typedef struct {
-  CSOUND* csound;
-  INSDS* insds;
-  OENTRY* oentry;
-  void* data;
-  int (*init)(void* data);
-  int (*perform)(void* data);
-  int (*destroy)(void* data);
-} UGEN;
-
-typedef struct {
-  INSDS* insds;
-} UGEN_CONTEXT;
-
-typedef struct {
-  CSOUND* csound;
-} UGEN_FACTORY;
+#include "ugen.h"
 
 /** Creates a UGEN_FACTORY, used to list available UGENs (Csound Opcodes),
  * as well as create instances of UGENs. User should configure the CSOUND
  * instance for sr and ksmps before creating a factory. */ 
-PUBLIC UGEN_FACTORY* ugen_factory_new(CSOUND* csound);
+PUBLIC UGEN_FACTORY* ugen_factory_new(CSOUND* csound) {
+  UGEN_FACTORY* factory = csound->Calloc(csound, sizeof(UGEN_FACTORY));
+  return factory;
+}
 
 /* Delete a UGEN_FACTORY */
-PUBLIC bool ugen_factory_delete(CSOUND* csound);
+PUBLIC bool ugen_factory_delete(CSOUND* csound) {
+  return false;
+}
 
-PUBLIC UGEN_CONTEXT* ugen_context_new(UGEN_FACTORY* factory);
+PUBLIC UGEN_CONTEXT* ugen_context_new(UGEN_FACTORY* factory) {
+  return NULL;
+}
 
-PUBLIC UGEN_CONTEXT* ugen_context_delete(UGEN_FACTORY* factory);
+PUBLIC UGEN_CONTEXT* ugen_context_delete(UGEN_FACTORY* factory) {
+  return NULL;
+}
 
 /** Create a new UGEN, using the given UGEN_FACTORY and OENTRY */
-PUBLIC UGEN* ugen_new(UGEN_FACTORY* factory, OENTRY* oentry);
+PUBLIC UGEN* ugen_new(UGEN_FACTORY* factory, OENTRY* oentry) {
+  return NULL;
+}
 
 
-PUBLIC bool ugen_set_output(UGEN* ugen, int index, void* arg);
+PUBLIC bool ugen_set_output(UGEN* ugen, int index, void* arg) {
+  return false;
+}
 
-PUBLIC bool ugen_set_input(UGEN* ugen, int index, void* arg);
+PUBLIC bool ugen_set_input(UGEN* ugen, int index, void* arg) {
+  return false;
+}
 
-PUBLIC int ugen_init(UGEN* ugen);
+PUBLIC int ugen_init(UGEN* ugen) {
+  return 0;
+}
 
-PUBLIC int ugen_perform(UGEN* ugen);
+PUBLIC int ugen_perform(UGEN* ugen) {
+  return 0;
+}
 
-PUBLIC bool ugen_delete(UGEN* ugen);
+PUBLIC bool ugen_delete(UGEN* ugen) {
+  return true;
+}
 
 
 
