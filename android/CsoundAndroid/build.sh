33b6655628ae269a8e3c750b4fd74f7654195f11
Michael Gogins <michael.gogins@gmail.com>
2016-09-03 12:07:16 -0400

    Changes to enable building Csound for Android on Linux.

--- android/CsoundAndroid/build.sh
+++ android/CsoundAndroid/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-flex -B -t ../../Engine/csound_orc.l > jni/csound_orclex.c 
+flex -B -t ../../Engine/csound_orc.lex > jni/csound_orclex.c 
 flex -B ../../Engine/csound_pre.lex > jni/csound_prelex.c 
 bison -d -pcsound_orc --report=itemset -o jni/csound_orcparse.c ../../Engine/csound_orc.y
 
