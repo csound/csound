50207aad8a4ab30bfd414343dec08b15835884d5
Michael Gogins <michael.gogins@gmail.com>
2016-09-05 16:39:41 -0400

    Undoing prior 2 commits with directory deletes, re-committing restored directories and needed changes.

--- android/pluginlibs/LuaCsound/jni/Application.mk
+++ android/pluginlibs/LuaCsound/jni/Application.mk
@@ -1,6 +1,8 @@
 APP_ABI := armeabi armeabi-v7a
-APP_CPPFLAGS += -fexceptions -frtti
+APP_CPPFLAGS += -fexceptions -frtti -std=c++11
 APP_OPTIM := release
 APP_PLATFORM := 19
-APP_STL := c++_shared
-NDK_TOOLCHAIN_VERSION := clang
+# The following two lines are required because luajit does not currently build 
+# for Android arm with clang.
+APP_STL := gnustl_static
+NDK_TOOLCHAIN_VERSION := 4.9
