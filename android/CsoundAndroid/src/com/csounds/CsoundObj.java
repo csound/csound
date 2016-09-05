59e10179e53d637c84a8d78c1f569da80e8bead5
Michael Gogins <michael.gogins@gmail.com>
2016-09-05 11:00:16 -0400

    Revert "Changes to enable building Csound for Android on Linux."
    This reverts commit df36b33e2e6fd918e19164c2cb8d04123ab97223.

--- android/CsoundAndroid/src/com/csounds/CsoundObj.java
+++ android/CsoundAndroid/src/com/csounds/CsoundObj.java
@@ -196,14 +196,7 @@ public class CsoundObj {
 		sendScore(score);
 	}
 
-    public void compileCsdText(String csd_text) {
-        csound.CompileCsdText(csd_text);
-    }
-    public void updateOrchestra(String orchestraString) {
-        csound.CompileOrc(orchestraString);
-    }
-
-	public void compileOrc(String orchestraString) {
+	public void updateOrchestra(String orchestraString) {
 		csound.CompileOrc(orchestraString);
 	}
 
@@ -279,11 +272,6 @@ public class CsoundObj {
 		return retVal;
 	}
 
-    @JavascriptInterface
-    public int SetOption(String option) {
-        return csound.SetOption(option);
-    }
-
 	/* Render Methods */
 
 	private void runCsoundOpenSL(File f) {
@@ -303,12 +291,7 @@ public class CsoundObj {
 			callbacks.SetMessageCallback();
 		}
 		if(!isAsync) this.pause();
-		if(f.getAbsolutePath().toLowerCase().endsWith(".csd")) {
-			retVal = csound.Compile(f.getAbsolutePath());
-		} else {
-            csound.SetOption("-odac");
-			retVal = csound.Start();
-		}
+		retVal = csound.Compile(f.getAbsolutePath());
 		Log.d("CsoundObj", "Return Value2: " + retVal);
 		if (retVal == 0) {
 			for (int i = 0; i < bindings.size(); i++) {
