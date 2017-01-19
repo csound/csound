83fb28b45266a1ab0508d1b23c4d2ba2f1d3d181
Michael Gogins <michael.gogins@gmail.com>
2017-01-19 17:35:50 -0500

    Clarifying API behavior, getting Lisp example to run.

--- include/csound.h
+++ include/csound.h
@@ -662,11 +662,14 @@ extern "C" {
     PUBLIC int csoundCompileArgs(CSOUND *, int argc, const char **argv);
 
     /**
-     * Prepares Csound for performance after compilation
-     * using one or more of the above functions.
-     * NB: this is called internally by csoundCompile(), therefore
-     * it is only required if performance is started without
-     * a call to that function
+     * Prepares Csound for performance. Normally called after compiling 
+     * a csd file or an orc file, in which case score preprocessing is 
+     * performed and performance terminates when the score terminates.
+     *
+     * However, if called before compiling a csd file or an orc file, 
+     * score preprocessing is not performed and "i" statements are dispatched 
+     * as real-time events, the <CsOptions> tag is ignored, and performance 
+     * continues indefinitely or until ended using the API.
      */
     PUBLIC int csoundStart(CSOUND *csound);
 
@@ -689,41 +692,64 @@ extern "C" {
     PUBLIC int csoundCompile(CSOUND *, int argc, const char **argv);
 
     /**
-     * Compiles a Csound input file (CSD, .csd file)
-     * which includes command-line arguments,
-     * but does not perform the file. Returns a non-zero error code on failure.
-     * In this (host-driven) mode, the sequence of calls should be as follows:
-     * /code
-     *       csoundCompileCsd(csound, str);
-     *       while (!csoundPerformBuffer(csound));
-     *       csoundCleanup(csound);
-     *       csoundReset(csound);
-     * /endcode
-     * NB: this function can be called during performance to
+     * Compiles a Csound input file (CSD, .csd file), but does not perform it.  
+     * Returns a non-zero error code on failure. 
+     *
+     * If csoundStart is called before csoundCompileCsd, the <CsOptions> 
+     * element is ignored (but csoundSetOption can be called any number of 
+     * times), the <CsScore> element is not pre-processed, but dispatched as 
+     * real-time events; and performance continues indefinitely, or until 
+     * ended by calling csoundStop or some other logic. In this "real-time" 
+     * mode, the sequence of calls should be:
+     *
+     * \code
+     *
+     * csoundSetOption("-an_option");
+     * csoundSetOption("-another_option");
+     * csoundStart(csound);
+     * csoundCompileCsd(csound, csd_filename);
+     * while (1) {
+     *    csoundPerformBuffer(csound);
+     *    // Something to break out of the loop 
+     *    // when finished here... 
+     * }
+     * csoundCleanup(csound);
+     * csoundReset(csound);
+     *
+     * \endcode
+     *
+     * NB: this function can be called repeatedly during performance to
      * replace or add new instruments and events.
-     * On a first call and if called before csoundStart(), this function
-     * behaves similarly to csoundCompile()
+     *
+     * But if csoundCompileCsd is called before csoundStart, the <CsOptions> 
+     * element is used, the <CsScore> section is pre-processed and dispatched 
+     * normally, and performance terminates when the score terminates, or 
+     * csoundStop is called. In this "non-real-time" mode (which can still 
+     * output real-time audio and handle real-time events), the sequence of 
+     * calls should be:
+     * 
+     * \code
+     *  
+     * csoundCompileCsd(csound, csd_filename);
+     * csoundStart(csound);
+     * while (1) {
+     *    int finished = csoundPerformBuffer(csound);
+     *    if (finished) break;
+     * }
+     * csoundCleanup(csound);
+     * csoundReset(csound);
+     *
+     * \endcode    
      *
      */
-
-    PUBLIC int csoundCompileCsd(CSOUND *csound, const char *str);
+    PUBLIC int csoundCompileCsd(CSOUND *csound, const char *csd_filename);
 
     /**
-     * Compiles a Csound input file contained in a string of text,
-     * which includes command-line arguments, orchestra, score, etc.,
-     * but does not perform the file. Returns a non-zero error code on failure.
-     * In this (host-driven) mode, the sequence of calls should be as follows:
-     * /code
-     *       csoundCompileCsdText(csound, csd_text);
-     *       while (!csoundPerformBuffer(csound));
-     *       csoundCleanup(csound);
-     *       csoundReset(csound);
-     * /endcode
-     * NB: A temporary file is created, the csd_text is written to the temporary
-     * file, and csoundCompileCsd is called with the name of the temporary file,
-     * which is deleted after compilation. Behavior may vary by platform.
+     * Behaves the same way as csoundCompileCsd, except that the content 
+     * of the CSD is read from the csd_text string rather than from a file.
+     * This is convenient when it is desirable to package the csd as part of 
+     * an application or a multi-language piece.
      */
-
     PUBLIC int csoundCompileCsdText(CSOUND *csound, const char *csd_text);
 
     /**
