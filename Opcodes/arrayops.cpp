71ef2536ba4c87c422d67447b55bcd9bcf2d6f32
veplaini <victor.lazzarini@nuim.ie>
2017-01-28 22:11:45 +0000

    array operators

---
+++ Opcodes/arrayops.cpp
@@ -0,0 +1,73 @@
+/*
+  arrayops.c: array operators
+
+  Copyright (C) 2017 Victor Lazzarini
+  This file is part of Csound.
+
+  The Csound Library is free software; you can redistribute it
+  and/or modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  Csound is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with Csound; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+  02111-1307 USA
+*/
+#include <plugin.h>
+#include <algorithm>
+#include <cmath>
+
+/** i-time, k-rate operator
+    kout[] op kin[]
+ */
+template<double (*op)(double)>
+struct ArrayOp : csnd::Plugin<1, 1> {
+  int init() {
+    csnd::Vector<MYFLT> &out =  outargs.vector_data<MYFLT>(0);
+    csnd::Vector<MYFLT> &in =  inargs.vector_data<MYFLT>(0);
+    out.init(csound,in.len());
+    std::transform(in.begin(), in.end(), out.begin(),
+		   [](MYFLT f) { return op(f); });
+    return OK;
+  }
+  
+  int kperf() {    
+    csnd::Vector<MYFLT> &out = outargs.vector_data<MYFLT>(0);
+    csnd::Vector<MYFLT> &in =  inargs.vector_data<MYFLT>(0);
+    std::transform(in.begin(), in.end(), out.begin(),
+		   [](MYFLT f) { return op(f); });
+     
+    return OK;
+  }
+};
+
+
+/** Module creation, initalisation and destruction
+ */
+extern "C" {
+PUBLIC int csoundModuleInit(CSOUND *csound) {
+  csnd::plugin<ArrayOp<std::sqrt>>(csound, "sqrt", "i[]", "i[]", csnd::thread::i);
+  csnd::plugin<ArrayOp<std::sqrt>>(csound, "sqrt", "k[]", "k[]", csnd::thread::ik);
+  csnd::plugin<ArrayOp<std::cos>>(csound, "cos", "i[]", "i[]", csnd::thread::i);
+  csnd::plugin<ArrayOp<std::cos>>(csound, "cos", "k[]", "k[]", csnd::thread::ik);
+  csnd::plugin<ArrayOp<std::sin>>(csound, "sin", "i[]", "i[]", csnd::thread::i);
+  csnd::plugin<ArrayOp<std::sin>>(csound, "sin", "k[]", "k[]", csnd::thread::ik);
+  csnd::plugin<ArrayOp<std::tan>>(csound, "tan", "i[]", "i[]", csnd::thread::i);
+  csnd::plugin<ArrayOp<std::tan>>(csound, "tan", "k[]", "k[]", csnd::thread::ik);
+  csnd::plugin<ArrayOp<std::acos>>(csound, "cosinv", "i[]", "i[]", csnd::thread::i);
+  csnd::plugin<ArrayOp<std::acos>>(csound, "cosinv", "k[]", "k[]", csnd::thread::ik);
+  csnd::plugin<ArrayOp<std::asin>>(csound, "sininv", "i[]", "i[]", csnd::thread::i);
+  csnd::plugin<ArrayOp<std::asin>>(csound, "sininv", "k[]", "k[]", csnd::thread::ik);
+  csnd::plugin<ArrayOp<std::atan>>(csound, "taninv", "i[]", "i[]", csnd::thread::i);
+  csnd::plugin<ArrayOp<std::atan>>(csound, "taninv", "k[]", "k[]", csnd::thread::ik);
+  return 0;
+}
+PUBLIC int csoundModuleCreate(CSOUND *csound) { return 0; }
+PUBLIC int csoundModuleDestroy(CSOUND *csound) { return 0; }
+}
