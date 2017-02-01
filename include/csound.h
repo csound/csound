06c498ff8227e153497a81a18f3a9d7cc3b0fa40
jpff <jpff@codemist.co.uk>
2017-02-01 17:23:03 +0000

    srrngs, tabs

--- include/csound.h
+++ include/csound.h
@@ -662,13 +662,13 @@ extern "C" {
     PUBLIC int csoundCompileArgs(CSOUND *, int argc, const char **argv);
 
     /**
-     * Prepares Csound for performance. Normally called after compiling 
-     * a csd file or an orc file, in which case score preprocessing is 
+     * Prepares Csound for performance. Normally called after compiling
+     * a csd file or an orc file, in which case score preprocessing is
      * performed and performance terminates when the score terminates.
      *
-     * However, if called before compiling a csd file or an orc file, 
-     * score preprocessing is not performed and "i" statements are dispatched 
-     * as real-time events, the <CsOptions> tag is ignored, and performance 
+     * However, if called before compiling a csd file or an orc file,
+     * score preprocessing is not performed and "i" statements are dispatched
+     * as real-time events, the <CsOptions> tag is ignored, and performance
      * continues indefinitely or until ended using the API.
      */
     PUBLIC int csoundStart(CSOUND *csound);
@@ -692,14 +692,14 @@ extern "C" {
     PUBLIC int csoundCompile(CSOUND *, int argc, const char **argv);
 
     /**
-     * Compiles a Csound input file (CSD, .csd file), but does not perform it.  
-     * Returns a non-zero error code on failure. 
+     * Compiles a Csound input file (CSD, .csd file), but does not perform it.
+     * Returns a non-zero error code on failure.
      *
-     * If csoundStart is called before csoundCompileCsd, the <CsOptions> 
-     * element is ignored (but csoundSetOption can be called any number of 
-     * times), the <CsScore> element is not pre-processed, but dispatched as 
-     * real-time events; and performance continues indefinitely, or until 
-     * ended by calling csoundStop or some other logic. In this "real-time" 
+     * If csoundStart is called before csoundCompileCsd, the <CsOptions>
+     * element is ignored (but csoundSetOption can be called any number of
+     * times), the <CsScore> element is not pre-processed, but dispatched as
+     * real-time events; and performance continues indefinitely, or until
+     * ended by calling csoundStop or some other logic. In this "real-time"
      * mode, the sequence of calls should be:
      *
      * \code
@@ -710,8 +710,8 @@ extern "C" {
      * csoundCompileCsd(csound, csd_filename);
      * while (1) {
      *    csoundPerformBuffer(csound);
-     *    // Something to break out of the loop 
-     *    // when finished here... 
+     *    // Something to break out of the loop
+     *    // when finished here...
      * }
      * csoundCleanup(csound);
      * csoundReset(csound);
@@ -721,15 +721,15 @@ extern "C" {
      * NB: this function can be called repeatedly during performance to
      * replace or add new instruments and events.
      *
-     * But if csoundCompileCsd is called before csoundStart, the <CsOptions> 
-     * element is used, the <CsScore> section is pre-processed and dispatched 
-     * normally, and performance terminates when the score terminates, or 
-     * csoundStop is called. In this "non-real-time" mode (which can still 
-     * output real-time audio and handle real-time events), the sequence of 
+     * But if csoundCompileCsd is called before csoundStart, the <CsOptions>
+     * element is used, the <CsScore> section is pre-processed and dispatched
+     * normally, and performance terminates when the score terminates, or
+     * csoundStop is called. In this "non-real-time" mode (which can still
+     * output real-time audio and handle real-time events), the sequence of
      * calls should be:
-     * 
+     *
      * \code
-     *  
+     *
      * csoundCompileCsd(csound, csd_filename);
      * csoundStart(csound);
      * while (1) {
@@ -739,15 +739,15 @@ extern "C" {
      * csoundCleanup(csound);
      * csoundReset(csound);
      *
-     * \endcode    
+     * \endcode
      *
      */
     PUBLIC int csoundCompileCsd(CSOUND *csound, const char *csd_filename);
 
     /**
-     * Behaves the same way as csoundCompileCsd, except that the content 
+     * Behaves the same way as csoundCompileCsd, except that the content
      * of the CSD is read from the csd_text string rather than from a file.
-     * This is convenient when it is desirable to package the csd as part of 
+     * This is convenient when it is desirable to package the csd as part of
      * an application or a multi-language piece.
      */
     PUBLIC int csoundCompileCsdText(CSOUND *csound, const char *csd_text);
@@ -2085,7 +2085,7 @@ extern "C" {
 #ifndef SWIG
 
 #include <libkern/OSAtomic.h>
-  
+
 #define csoundSpinLock(spinlock)                \
     {                                           \
        OSSpinLockLock(spinlock);                \
