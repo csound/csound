57005b418999c5500a76a4fc3eb109214235ece6
veplaini <victor.lazzarini@nuim.ie>
2017-02-18 17:57:05 +0000

    license etc

--- include/modload.h
+++ include/modload.h
@@ -1,3 +1,28 @@
+/*
+  modload.h
+  CPOF Csound Plugin Opcode Framework
+  module load C functions and entry point
+
+  (c) Victor Lazzarini, 2017
+
+  This file is part of Csound.
+
+  The Csound Library is free software; you can redistribute it
+  and/or modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  Csound is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with Csound; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+  02111-1307 USA
+
+*/
 #ifndef __MODLOAD__H
 #define __MODLOAD__H
 
