fed6d96b1ba7dc0ffbee866bd5e016598e4059d0
veplaini <victor.lazzarini@nuim.ie>
2017-03-19 00:19:43 +0000

    tvconv partitioned

--- Opcodes/pvsops.cpp
+++ Opcodes/pvsops.cpp
@@ -62,26 +62,55 @@ struct PVTrace : csnd::FPlugin<1, 2> {
   }
 };
 
-struct TVConv : csnd::Plugin<1,3> {
+struct TVConv : csnd::Plugin<1,4> {
   static constexpr char const *otypes = "a";
-  static constexpr char const *itypes = "aai";
+  static constexpr char const *itypes = "aaii";
   csnd::AuxMem<MYFLT> ir;
   csnd::AuxMem<MYFLT> in;
   csnd::AuxMem<MYFLT> out;
   csnd::AuxMem<MYFLT> saved;
   uint32_t n; 
-  uint32_t ns;
+  uint32_t fils;
+  uint32_t pars;
+  uint32_t ffts;
+  uint32_t nparts;
+  uint32_t pp;
   csnd::fftp fwd,inv;
+  typedef std::complex<MYFLT> cmplx;
+
+  uint32_t rpow2(uint32_t n) {
+  uint32_t v = 2;
+  while (v <= n)
+    v <<= 1;
+  if((n - (v >> 1)) < (v - n))  
+   return v >> 1;
+  else return v;
+  } 
+
+  cmplx *to_cmplx(MYFLT *f) {
+    return reinterpret_cast<cmplx*>(f);
+  }
+
+  cmplx real_prod(cmplx &a, cmplx &b) {
+    return cmplx(a.real()*b.real(),	 
+		 a.imag()*b.imag());
+  }
 
   int init() {
-    ns = inargs[2];
-    fwd = csound->fft_setup(2*ns,FFT_FWD);
-    inv = csound->fft_setup(2*ns,FFT_INV);
-    ir.allocate(csound, 2*ns);
-    in.allocate(csound, 2*ns);
-    out.allocate(csound, 2*ns);
-    saved.allocate(csound, ns);
+    fils = rpow2(inargs[2]);
+    pars = rpow2(inargs[3]);
+    if(pars > fils) std::swap(pars,fils);
+    ffts = pars*2;
+    fwd = csound->fft_setup(ffts,FFT_FWD);
+    inv = csound->fft_setup(ffts,FFT_INV);
+    ir.allocate(csound, 2*fils);
+    in.allocate(csound, 2*fils);
+    out.allocate(csound, 2*pars);
+    saved.allocate(csound, pars);
     n = 0;
+    pp = 0;
+    nparts = fils/pars;
+    fils *= 2;
     return OK;
   }
 
@@ -94,24 +123,29 @@ struct TVConv : csnd::Plugin<1,3> {
     auto inp = insig.begin();
 
     for(auto &s: outsig) {
-      ir[n] = *irp++;
-      in[n] = *inp++;
+      ir[n+pp] = *irp++;
+      in[n+pp] = *inp++;
+      in[n+pp+pars] = ir[n+pp+pars] = 0.;
       s  = out[n] + saved[n];
-      saved[n] = out[n + ns];
-       if(++n == ns) {
-	 std::complex<MYFLT> *ins,*irs;
-	 std::complex<MYFLT> *ous =
-	  reinterpret_cast<std::complex<MYFLT>*>(out.data());
-	 std::fill(in.begin()+ns,in.end(),0.); 
-	 std::fill(ir.begin()+ns,ir.end(),0.);
+      saved[n] = out[n + pars];
+       if(++n == pars) {
+	 cmplx *ins,*irs, *ous = to_cmplx(out.data());
+	 std::fill(out.begin(), out.end(), 0.);
 	 // FFT
-         irs = csound->rfft(fwd, ir.data());
-	 ins = csound->rfft(fwd, in.data());
-         // Mult
-	 for(uint i=1; i < ns; i++)
-	   ous[i] =  ins[i] * irs[i];    
-	 ous[0].real(irs[0].real()*ins[0].real());	 
-	 ous[0].imag(irs[0].imag()*ins[0].imag());
+         csound->rfft(fwd, ir.data() + pp);
+	 csound->rfft(fwd, in.data() + pp);
+	 pp += ffts;
+	 if(pp == fils) pp = 0;
+	 // spectral delay line
+         for(uint32_t k = 0, kp = pp; k < nparts; k++, kp+=ffts) {
+	   if(kp == fils) kp = 0;
+	   ins = to_cmplx(in.data() + kp);
+	   irs = to_cmplx(ir.data() + (nparts - k - 1)*ffts);
+	   // spectral product
+           for(uint i=1; i < pars; i++)
+	     ous[i] +=  ins[i] * irs[i];
+	   ous[0] += real_prod(ins[0],irs[0]);
+	 }
 	 // IFFT
 	 csound->rfft(inv, out.data());
          n = 0;
