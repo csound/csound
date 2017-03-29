fee54c57ad4895780fa652a0c0914f45f71239da
Stephen Kyne <xskyne@gmail.com>
2017-03-29 18:42:24 +0100

    Changed impl of float to int rounding for MSVC. Now 3 times faster which puts it on par with linux/osx platforms. Remove some redundant uses of the rounding macros

--- Opcodes/signalflowgraph.cpp
+++ Opcodes/signalflowgraph.cpp
@@ -962,7 +962,7 @@ struct Inletv : public OpcodeBase<Inletv> {
             warn(csound, "myFltsPerArrayElement: %d\n", myFltsPerArrayElement);
             arraySize = myFltsPerArrayElement;
             for(size_t dimension = 0; dimension < vsignal->dimensions; ++dimension) {
-                arraySize *= MYFLT2LRND(vsignal->sizes[dimension]);
+                arraySize *= vsignal->sizes[dimension];
             }
             warn(csound, "arraySize: %d\n", arraySize);
             warn(csound, "sourceOutlets: 0x%x\n", sourceOutlets);
