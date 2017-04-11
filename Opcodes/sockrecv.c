9dd10ea83e7609e0d1d4ffb54106cbeb793796d5
veplaini <victor.lazzarini@nuim.ie>
2017-04-11 22:31:02 +0100

    trying to set non-blocking mode on windows

--- Opcodes/sockrecv.c
+++ Opcodes/sockrecv.c
@@ -532,6 +532,10 @@ static int init_raw_osc(CSOUND *csound, RAWOSC *p)
 #ifndef WIN32
     if (UNLIKELY(fcntl(p->sock, F_SETFL, O_NONBLOCK)<0))
       return csound->InitError(csound, Str("Cannot set nonblock"));
+#else
+    int nMode = 1; // 1: NON-BLOCKING
+    if (ioctlsocket (objSocket, FIONBIO, &nMode) == SOCKET_ERROR)
+       return csound->InitError(csound, Str("Cannot set nonblock"));
 #endif
     if (UNLIKELY(p->sock < 0)) {
       return csound->InitError
