7a8550387c8ec0c2e67d633fd9526a1f8fae14f3
Steven Yi <stevenyi@gmail.com>
2017-04-15 14:25:16 -0400

    added k35_lpf and k35_hpf filters

--- Opcodes/wpfilters.c
+++ Opcodes/wpfilters.c
@@ -540,6 +540,269 @@ static int diode_ladder_perf(CSOUND* csound,
 }
 
 
+static int k35_lpf_init(CSOUND* csound, K35_LPF* p) {
+
+	if (*p->skip == 0.0) {
+		p->z1 = 0.0;
+		p->z2 = 0.0;
+		p->z3 = 0.0;
+		p->last_cut = -1.0;
+		p->last_q = -1.0;
+		p->g = 0.0;
+		p->G = 0.0;
+		p->S35 = 0.0;
+		p->alpha = 0.0;
+		p->lpf2_beta = 0.0;
+		p->hpf1_beta = 0.0;
+	}
+
+	return OK;
+}
+
+
+static int k35_lpf_perf(CSOUND* csound, K35_LPF* p) {
+
+	double z1 = p->z1;
+	double z2 = p->z2;
+	double z3 = p->z3;
+	double last_cut = p->last_cut;
+	double last_q = p->last_q;
+	double g = p->g;
+	double G = p->G;
+	double K = p->K;
+	double S35 = p->S35;
+	double alpha = p->alpha;
+	double lpf2_beta = p->lpf2_beta;
+	double hpf1_beta = p->hpf1_beta;
+
+	uint32_t offset = p->h.insdshead->ksmps_offset;
+	uint32_t early = p->h.insdshead->ksmps_no_end;
+	uint32_t n, nsmps = CS_KSMPS;
+
+	double T = csound->onedsr;
+	double Tdiv2 = T / 2.0;
+	double two_div_T = 2.0 / T;
+
+	int cutoff_arate = IS_ASIG_ARG(p->cutoff);
+	int q_arate = IS_ASIG_ARG(p->q);
+
+	MYFLT cutoff = cutoff_arate ? 0.0 : *p->cutoff;
+	MYFLT q = q_arate ? 0.0 : *p->q;
+
+	int nonlinear = MYFLT2LONG(*p->nonlinear);
+	double saturation = *p->saturation;
+
+	for (n = offset; n < nsmps; n++) {
+		MYFLT in = p->in[n];
+
+		if (cutoff_arate) {
+			cutoff = p->cutoff[n];
+		}
+		if (q_arate) {
+			q = p->q[n];
+			// clamp from 1.0 to 10.0
+			q = (q > 10.0) ? 10.0 : (q < 1.0) ? 1.0 : q;
+		}
+
+		if (cutoff != last_cut) {
+			double wd = TWOPI * cutoff;
+			double wa = two_div_T * tan(wd * Tdiv2);
+			g = wa * Tdiv2;
+			G = g / (1.0 + g);
+		}
+
+		if (q != last_q) {
+			K = 0.01 + ((2.0 - 0.01) * (q / 10.0));
+		}
+
+		if ((cutoff != last_cut) || (q != last_q)) {
+			lpf2_beta = (K - (K * G)) / (1.0 + g);
+			hpf1_beta = -1.0 / (1.0 + g);
+			alpha = 1.0 / (1.0 - (K * G) + (K * G * G));
+		}
+
+
+		last_cut = cutoff;
+		last_q = q;
+
+		// LPF1
+		double v1 = (in - z1) * G;
+		double lp1 = v1 + z1;
+		z1 = lp1 + v1;
+
+		double u = alpha * (lp1 + S35);
+
+		if (nonlinear) {
+			u = tanh(u * saturation);
+		}
+
+		// LPF2
+		double v2 = (u - z2) * G;
+		double lp2 = v2 + z2;
+		z2 = lp2 + v2;
+		double y = K * lp2;
+
+		// HPF1
+
+		double v3 = (y - z3) * G;
+		double lp3 = v3 + z3;
+		z3 = lp3 + v3;
+		double hp1 = y - lp3;
+
+		S35 = (lpf2_beta * z2) + (hpf1_beta * z3);
+		double out = (K > 0) ? (y / K) : y;
+
+		p->out[n] = out;
+	}
+
+	p->z1 = z1;
+	p->z2 = z2;
+	p->z3 = z3;
+	p->last_cut = last_cut;
+	p->last_q = last_q;
+	p->g = g;
+	p->G = G;
+	p->K = K;
+	p->S35 = S35;
+	p->alpha = alpha;
+	p->lpf2_beta = lpf2_beta;
+	p->hpf1_beta = hpf1_beta;
+
+	return OK;
+}
+
+
+static int k35_hpf_init(CSOUND* csound, K35_HPF* p) {
+
+	if (*p->skip == 0.0) {
+		p->z1 = 0.0;
+		p->z2 = 0.0;
+		p->z3 = 0.0;
+		p->last_cut = -1.0;
+		p->last_q = -1.0;
+		p->g = 0.0;
+		p->G = 0.0;
+		p->S35 = 0.0;
+		p->alpha = 0.0;
+		p->hpf2_beta = 0.0;
+		p->lpf1_beta = 0.0;
+	}
+
+	return OK;
+}
+
+
+static int k35_hpf_perf(CSOUND* csound, K35_HPF* p) {
+
+	double z1 = p->z1;
+	double z2 = p->z2;
+	double z3 = p->z3;
+	double last_cut = p->last_cut;
+	double last_q = p->last_q;
+	double g = p->g;
+	double G = p->G;
+	double K = p->K;
+	double S35 = p->S35;
+	double alpha = p->alpha;
+	double hpf2_beta = p->hpf2_beta;
+	double lpf1_beta = p->lpf1_beta;
+
+	uint32_t offset = p->h.insdshead->ksmps_offset;
+	uint32_t early = p->h.insdshead->ksmps_no_end;
+	uint32_t n, nsmps = CS_KSMPS;
+
+	double T = csound->onedsr;
+	double Tdiv2 = T / 2.0;
+	double two_div_T = 2.0 / T;
+
+	int cutoff_arate = IS_ASIG_ARG(p->cutoff);
+	int q_arate = IS_ASIG_ARG(p->q);
+
+	MYFLT cutoff = cutoff_arate ? 0.0 : *p->cutoff;
+	MYFLT q = q_arate ? 0.0 : *p->q;
+
+	int nonlinear = MYFLT2LONG(*p->nonlinear);
+	double saturation = *p->saturation;
+
+	for (n = offset; n < nsmps; n++) {
+		MYFLT in = p->in[n];
+
+		if (cutoff_arate) {
+			cutoff = p->cutoff[n];
+		}
+		if (q_arate) {
+			q = p->q[n];
+			// clamp from 1.0 to 10.0
+			q = (q > 10.0) ? 10.0 : (q < 1.0) ? 1.0 : q;
+		}
+
+		if (cutoff != last_cut) {
+			double wd = TWOPI * cutoff;
+			double wa = two_div_T * tan(wd * Tdiv2);
+			g = wa * Tdiv2;
+			G = g / (1.0 + g);
+		}
+
+		if (q != last_q) {
+			K = 0.01 + ((2.0 - 0.01) * (q / 10.0));
+		}
+
+		if ((cutoff != last_cut) || (q != last_q)) {
+			hpf2_beta = -G / (1.0 + g); 
+			lpf1_beta = 1.0 / (1.0 + g);
+			alpha = 1.0 / (1.0 - (K * G) + (K * G * G));
+		}
+
+
+		last_cut = cutoff;
+		last_q = q;
+
+		// HPF1
+		double v1 = (in - z1) * G;
+		double lp1 = v1 + z1;
+		z1 = lp1 + v1;
+		double y1 = in - lp1;
+
+		double u = alpha * (y1 + S35);
+		double y = K * u;
+
+		if (nonlinear) {
+			y = tanh(y * saturation);
+		}
+
+		// HPF2
+		double v2 = (y - z2) * G;
+		double lp2 = v2 + z2;
+		z2 = lp2 + v2;
+		double hp2 = y - lp2;
+
+		// LPF1
+		double v3 = (hp2 - z3) * G;
+		double lp3 = v3 + z3;
+		z3 = lp3 + v3;
+
+		S35 = (hpf2_beta * z2) + (lpf1_beta * z3);
+		double out = (K > 0) ? (y / K) : y;
+
+		p->out[n] = out;
+	}
+
+	p->z1 = z1;
+	p->z2 = z2;
+	p->z3 = z3;
+	p->last_cut = last_cut;
+	p->last_q = last_q;
+	p->g = g;
+	p->G = G;
+	p->K = K;
+	p->S35 = S35;
+	p->alpha = alpha;
+	p->hpf2_beta = hpf2_beta;
+	p->lpf1_beta = lpf1_beta;
+
+	return OK;
+}
+
 
 static OENTRY wpfilters_localops[] =
 {
@@ -547,6 +810,8 @@ static OENTRY wpfilters_localops[] =
   { "zdf_2pole", sizeof(ZDF_2POLE), 0,5,"aaa","axxo",(SUBR)zdf_2pole_init,NULL,(SUBR)zdf_2pole_perf},
   { "zdf_ladder", sizeof(ZDF_LADDER), 0,5,"a","axxo",(SUBR)zdf_ladder_init,NULL,(SUBR)zdf_ladder_perf},
   { "diode_ladder", sizeof(DIODE_LADDER), 0,5,"a","axxOPo",(SUBR)diode_ladder_init,NULL,(SUBR)diode_ladder_perf},
+  { "k35_lpf", sizeof(K35_LPF), 0,5,"a","axxOPo",(SUBR)k35_lpf_init,NULL,(SUBR)k35_lpf_perf},
+  { "k35_hpf", sizeof(K35_LPF), 0,5,"a","axxOPo",(SUBR)k35_hpf_init,NULL,(SUBR)k35_hpf_perf},
 };
 
 LINKAGE_BUILTIN(wpfilters_localops)
