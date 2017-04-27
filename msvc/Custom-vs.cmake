e09f90f68b625c22fe9581cdb91e5f88914627a3
Stephen Kyne <skyne@avaya.com>
2017-04-27 17:30:11 +0100

    MSVC: Disabled warnings in build

--- msvc/Custom-vs.cmake
+++ msvc/Custom-vs.cmake
@@ -1,38 +1,25 @@
 # CUSTOM PROPERTIES TO SET
-
 # GLOBAL
 
-#set(CMAKE_BUILD_TYPE "Debug")
 set(CMAKE_VERBOSE_MAKEFILE ON)
 set(BUILD_STATIC_LIBRARY ON)
 set(USE_OPEN_MP OFF)
 set(TCL_VERSION 8.5)
-#set(PYTHON_INCLUDE_DIRS "/usr/include/python2.7")
-##set(BUILD_CSOUND_AC OFF)
-##set(BUILD_CSOUND_AC_PYTHON_INTERFACE OFF)
-##set(BUILD_CSOUND_AC_LUA_INTERFACE OFF)
 
-#### NOTE the processor type needs setting
-#set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -W -Wall -mtune=core2")
-## also to test multicore
-#set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wall -W -Wno-missing-field-initializers -Wno-unused-parameter -mtune=core2 -DJPFF")
-
+# Disable the following warnings in msvc
+# C4244 loss of data in conversion
+# C4267 loss of data in conversion
+# C4005 macro refinitions
+# C4996 unsafe functions
+set(CMAKE_C_FLAGS "${CMAKE_CX_FLAGS} /wd4244 /wd4267 /wd4005 /wd4996")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /wd4244 /wd4267 /wd4005 /wd4996")
 
 set(BUILD_PD_CLASS OFF)
 
-#list(APPEND CMAKE_SYSTEM_INCLUDE_PATH 
-#  "c:/Program Files/Mega-Nerd/libsndfile/include")
-#list(APPEND CMAKE_SYSTEM_LIBRARY_PATH
-#  "c:/Program Files/Mega-Nerd/libsndfile/lib")
-
-
+# FIXME this is supposedly a bad thing, figure out something else maybe
 list(APPEND CMAKE_SYSTEM_INCLUDE_PATH 
   "${CMAKE_CURRENT_BINARY_DIR}/../deps/include")
 list(APPEND CMAKE_SYSTEM_LIBRARY_PATH
   "${CMAKE_CURRENT_BINARY_DIR}/../deps/lib")
 
-#list(APPEND CMAKE_SYSTEM_LIBRARY_PATH
-#    "c:/Python25/libs")
-#list(APPEND CMAKE_SYSTEM_INCLUDE_PATH
-#    "c:/Python25/include")
 
