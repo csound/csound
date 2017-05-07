8e6bc3697b4dea5f1baa30f7f157cac60b65cedc
jpff <jpff@codemist.co.uk>
2017-05-07 14:55:58 +0100

    experimental adding of backtrace to signals

--- Top/csound.c
+++ Top/csound.c
@@ -946,8 +946,32 @@ static void destroy_all_instances(void)
 #if defined(ANDROID) || (!defined(LINUX) && !defined(SGI) && \
                          !defined(__HAIKU__) && !defined(__BEOS__) && \
                          !defined(__MACH__) && !defined(__EMSCRIPTEN__))
+#include <execinfo.h>
 static char *signal_to_string(int sig)
 {
+    {
+      int j, nptrs;
+#define SIZE 100
+      void *buffer[100];
+      char **strings;
+
+      nptrs = backtrace(buffer, SIZE);
+      printf("backtrace() returned %d addresses\n", nptrs);
+
+      /* The call backtrace_symbols_fd(buffer, nptrs, STDOUT_FILENO)
+         would produce similar output to the following: */
+
+      strings = backtrace_symbols(buffer, nptrs);
+      if (strings == NULL) {
+        perror("backtrace_symbols");
+        exit(EXIT_FAILURE);
+      }
+
+      for (j = 0; j < nptrs; j++)
+        printf("%s\n", strings[j]);
+      
+      free(strings);
+    }
     switch(sig) {
 #ifdef SIGHUP
     case SIGHUP:
