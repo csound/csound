45d6e97a9a3614d013c5f08aef76cda8ce8f2e44
veplaini <victor.lazzarini@nuim.ie>
2017-06-08 21:56:40 +0100

    wasm example collection

---
+++ emscripten/wasm/update_scripts.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+cp ../dist-wasm/libcsound.wasm .
+cp ../dist-wasm/libcsound.js ./js/libcsound.js
+cp ../dist-wasm/libcsound.js.map ./js/libcsound.js.map
+cp ../dist-wasm/CsoundObj.js ./js/CsoundObj.js
