c7086644791633af7ecaafb6c6119fb8ecd825ab
veplaini <victor.lazzarini@nuim.ie>
2017-06-10 16:22:33 +0100

    more examples

--- emscripten/wasm/update_scripts.sh
+++ emscripten/wasm/update_scripts.sh
@@ -3,3 +3,4 @@ cp ../dist-wasm/libcsound.wasm .
 cp ../dist-wasm/libcsound.js ./js/libcsound.js
 cp ../dist-wasm/libcsound.js.map ./js/libcsound.js.map
 cp ../dist-wasm/CsoundObj.js ./js/CsoundObj.js
+cp ../src/csound.js ./js/csound.js
