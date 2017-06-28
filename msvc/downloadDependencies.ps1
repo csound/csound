1f65cf2e6d31b04f2bda88153133cd5c837a9621
Stephen Kyne <xskyne@gmail.com>
2017-06-28 12:11:56 +0100

    MSVC: Checking out correct branch for fluidsynth

--- msvc/downloadDependencies.ps1
+++ msvc/downloadDependencies.ps1
@@ -250,8 +250,7 @@ if (Test-Path "fluidsynth")
 else 
 {
     #Switch to offical branch when PR is merged in
-    git clone --depth=1 "https://github.com/stekyne/fluidsynth.git"
-    git checkout master
+    git clone --depth=1 -b master "https://github.com/stekyne/fluidsynth.git"
 }
 
 rm -Path fluidsynthbuild -Force -Recurse -ErrorAction SilentlyContinue
