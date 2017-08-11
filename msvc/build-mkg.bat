952a7c75c7902e51ec86304a7888494d7be1098d
gogins <michael.gogins@gmail.com>
2017-08-11 14:18:38 -0400

    Removed 'mkg' scripts from Git.

--- msvc/build-mkg.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-rem powershell -ExecutionPolicy ByPass -File downloadDependencies.ps1
-powershell -ExecutionPolicy ByPass -File generateProject-mkg.ps1
-cmake --build csound-vs --config RelWithDebInfo
