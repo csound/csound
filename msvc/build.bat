716a0a090b7bb4e40b586d3972d133292868175e
gogins <michael.gogins@gmail.com>
2017-08-14 15:36:47 -0400

    Updates for configurable MSVC build.

--- msvc/build.bat
+++ msvc/build.bat
@@ -1,4 +1,5 @@
 @echo off
-powershell -ExecutionPolicy ByPass -File downloadDependencies.ps1
-powershell -ExecutionPolicy ByPass -File generateProject.ps1
+echo "usage: build [cmake_generator cmake_toolset]"
+powershell -ExecutionPolicy ByPass -File downloadDependencies.ps1 %1 %2
+powershell -ExecutionPolicy ByPass -File generateProject.ps1 %1 %2
 cmake --build csound-vs --config Release
