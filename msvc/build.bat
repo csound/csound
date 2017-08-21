1063a17d3ec88737690c324c6d96f13ed3fbcb9e
gogins <michael.gogins@gmail.com>
2017-08-21 16:18:27 -0400

    Get AppVeyor to download and build HTML5-enabled CsoundQt (#793).

--- msvc/build.bat
+++ msvc/build.bat
@@ -1,5 +1,4 @@
 @echo off
-echo "usage: build [cmake_generator cmake_toolset]"
 powershell -ExecutionPolicy ByPass -File downloadDependencies.ps1 -vsGenerator "Visual Studio 15 2017 Win64" -vsToolset "v141"
 powershell -ExecutionPolicy ByPass -File generateProject.ps1 -vsGenerator "Visual Studio 15 2017 Win64" -vsToolset "v141"
 cmake --build csound-vs --config Release
