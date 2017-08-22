c4aaba0688ab2e26de655c29f2a9e70413a7d96c
Michael Gogins <michael.gogins@gmail.com>
2017-08-22 17:25:41 -0400

    float-version.h.in is no good for AppVeyor build of CsoundQt (#793).

--- msvc/build_csoundqt.bat
+++ msvc/build_csoundqt.bat
@@ -2,11 +2,11 @@ cd staging\CsoundQt
 qmake.exe qcs.pro ^
     CONFIG+=html_webengine ^
     CONFIG+=perfThread_build ^
-    USE_DOUBLE=1 ^
+    DEFINES+="USE_DOUBLE=1" ^
     CSOUND_API_INCLUDE_DIR=%~dp0/"../include" ^
     CSOUND_INTERFACES_INCLUDE_DIR=%~dp0/"../interfaces" ^
     CSOUND_LIBRARY_DIR=%~dp0csound-vs\\Release ^
     LIBS+=%~dp0/deps/lib/libsndfile-1.lib ^
     INCLUDEPATH+="%~dp0deps\include"
-nmake.exe  
+nmake.exe   
 cd ..\..
