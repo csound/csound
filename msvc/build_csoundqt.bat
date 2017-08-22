553cb403e6c16791688e2a1591d697b3536242aa
Michael Gogins <michael.gogins@gmail.com>
2017-08-22 19:07:30 -0400

    Adding explicit 'QT+=xml' (#793).

--- msvc/build_csoundqt.bat
+++ msvc/build_csoundqt.bat
@@ -2,6 +2,7 @@ cd staging\CsoundQt
 qmake.exe qcs.pro ^
     CONFIG+=html_webengine ^
     CONFIG+=perfThread_build ^
+    QT+=xml ^
     DEFINES+="USE_DOUBLE=1" ^
     CSOUND_API_INCLUDE_DIR=%~dp0/"../include" ^
     CSOUND_INTERFACES_INCLUDE_DIR=%~dp0/"../interfaces" ^
