681c6cae92d8ef7c0aeeee2b7eb0a0f6ecd21a3d
jpff <jpff@codemist.co.uk>
2017-08-22 15:05:45 +0100

    layout

--- Opcodes/sockrecv.c
+++ Opcodes/sockrecv.c
@@ -569,7 +569,7 @@ static int init_raw_osc(CSOUND *csound, RAWOSC *p)
 
   csound->RegisterDeinitCallback(csound, (void *) p, destroy_raw_osc);
   tabensure(csound, p->sout,2);
-  
+
   return OK;
 }
 
