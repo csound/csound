e92c8a438ff32d87b6858da70b329ad1b0d6a4e4
Michael Gogins <michael.gogins@gmail.com>
2017-08-23 10:22:58 -0400

    Adding explicit csound64.lib (#793).

--- msvc/build_csoundqt.bat
+++ msvc/build_csoundqt.bat
@@ -7,6 +7,7 @@ qmake.exe qcs.pro ^
     CSOUND_API_INCLUDE_DIR=%~dp0/"../include" ^
     CSOUND_INTERFACES_INCLUDE_DIR=%~dp0/"../interfaces" ^
     CSOUND_LIBRARY_DIR=%~dp0csound-vs/Release ^
+    DEFAULT_CSOUND_LIBS=csound64.lib ^
     CSOUND_LIBRARY=csound64.lib ^
     LIBS+=%~dp0/deps/lib/libsndfile-1.lib ^
     INCLUDEPATH+="%~dp0deps\include"
