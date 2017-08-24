f51699ca5966e8285d56063de6108509287da677
Michael Gogins <michael.gogins@gmail.com>
2017-08-23 20:13:28 -0400

    Trying to diagnose CsoundQt build failure on AppVeyor (#793).

--- msvc/build_csoundqt.bat
+++ msvc/build_csoundqt.bat
@@ -8,7 +8,7 @@ qmake.exe qcs.pro ^
     DEFINES+="USE_DOUBLE=1" ^
     CSOUND_API_INCLUDE_DIR=%~dp0/"../include" ^
     CSOUND_INTERFACES_INCLUDE_DIR=%~dp0/"../interfaces" ^
-    CSOUND_LIBRARY_DIR=%~dp0csound-vs/\Release ^
+    CSOUND_LIBRARY_DIR=%APPVEYOR_BUILD_FOLDER%\msvc\csound-vs\Release\ ^
     DEFAULT_CSOUND_LIBS=csound64.lib ^
     CSOUND_LIBRARY=csound64.lib ^
     LIBS+=%~dp0/deps/lib/libsndfile-1.lib ^
