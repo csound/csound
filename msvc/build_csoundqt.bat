51ec8f5b85621e9c2afe7d992184bbe17fb9c46f
Michael Gogins <michael.gogins@gmail.com>
2017-08-24 16:45:01 -0400

    HTML5 branch for AppVeyor build of CsoundQt (#793).

--- msvc/build_csoundqt.bat
+++ msvc/build_csoundqt.bat
@@ -8,12 +8,12 @@ qmake.exe qcs.pro ^
     DEFINES+="USE_DOUBLE=1" ^
     CSOUND_API_INCLUDE_DIR="%APPVEYOR_BUILD_FOLDER%\include" ^
     CSOUND_INTERFACES_INCLUDE_DIR="%APPVEYOR_BUILD_FOLDER%\interfaces" ^
-    CSOUND_LIBRARY_DIR=%APPVEYOR_BUILD_FOLDER%\msvc\csound-vs\Release\ ^
+    CSOUND_LIBRARY_DIR=%APPVEYOR_BUILD_FOLDER%\msvc\csound-vs\Release" ^
     DEFAULT_CSOUND_LIBS=csound64.lib ^
     CSOUND_LIBRARY=csound64.lib ^
     LIBS+="%VCPKGDir%\installed\x64-windows\lib\libsndfile-1.lib" ^
     INCLUDEPATH+="%~dp0deps\include" ^
-    INCLUDEPATH+="%VCPKGDir%\installed\x64-windows\include\"
+    INCLUDEPATH+="%VCPKGDir%\installed\x64-windows\include"
 nmake.exe
 move "bin\CsoundQt*.*" "../../csound-vs/Release/"
 cd ..\..
