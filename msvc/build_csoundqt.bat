c7794d9841bf870ae36ec99c1602c7e1f5a87f4c
Michael Gogins <michael.gogins@gmail.com>
2017-08-25 15:34:11 -0400

    Improved installer script for AppVeyor build with some optional targets (#793).

--- msvc/build_csoundqt.bat
+++ msvc/build_csoundqt.bat
@@ -1,4 +1,7 @@
 cd staging\CsoundQt
+echo "INCLUDE:" %INCLUDE%
+set INCLUDE=%INCLUDE%;"%VCPKGDir%\installed\x64-windows\include"
+echo "INCLUDE:" %INCLUDE%
 echo "CSOUND_LIBRARY_DIR:" %~dp0csound-vs\Release
 dir %~dp0csound-vs\Release
 qmake.exe qcs.pro ^
@@ -7,7 +10,7 @@ qmake.exe qcs.pro ^
     QT+=xml ^
     DEFINES+="USE_DOUBLE=1" ^
     CSOUND_API_INCLUDE_DIR="%APPVEYOR_BUILD_FOLDER%\\include" ^
-    CSOUND_INTERFACES_INCLUDE_DIR="%APPVEYOR_BUILD_FOLDER%\interfaces" ^
+    CSOUND_INTERFACES_INCLUDE_DIR="%APPVEYOR_BUILD_FOLDER%\\interfaces" ^
     CSOUND_LIBRARY_DIR=%APPVEYOR_BUILD_FOLDER%\\msvc\\csound-vs\\Release" ^
     DEFAULT_CSOUND_LIBS=csound64.lib ^
     CSOUND_LIBRARY=csound64.lib ^
