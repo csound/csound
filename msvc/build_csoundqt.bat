294d3f495b189d9327a93bf5f45f9c8c5b1b2c16
Michael Gogins <michael.gogins@gmail.com>
2017-08-26 13:33:48 -0400

    Support for MSVS 2017 for AppVeyor build.

--- msvc/build_csoundqt.bat
+++ msvc/build_csoundqt.bat
@@ -1,20 +1,22 @@
 echo TODO: This is a mess. The CsoundQt build system needs some work and then this should be brought into appveyor.yaml.
 cd staging\CsoundQt
 echo "INCLUDE:" %INCLUDE%
-set INCLUDE=%INCLUDE%%VCPKGDir%\installed\x64-windows\include;
+set INCLUDE=%INCLUDE%%VCPKGDir%\\installed\\x64-windows\\include;
 echo "INCLUDE:" %INCLUDE%
 echo "CSOUND_LIBRARY_DIR:" %~dp0csound-vs\Release
 dir %~dp0csound-vs\Release
 qmake.exe qcs.pro ^
     CONFIG+=html_webengine ^
-    CONFIG+=perfThread_build ^
+    CONFIG+=thread ^
+    CONFIG+=csound6 ^
+    CONFIG+=build64 ^
     CONFIG+=QCS_QT59 ^
     QT+=xml ^
     DEFINES+="USE_DOUBLE=1" ^
-    DEFINES+="USE_QT_GT_59=1" ^
+    DEFINES+="USE_QT_GT_58=1" ^
     CSOUND_API_INCLUDE_DIR="%APPVEYOR_BUILD_FOLDER%\\include" ^
     CSOUND_INTERFACES_INCLUDE_DIR="%APPVEYOR_BUILD_FOLDER%\\interfaces" ^
-    CSOUND_LIBRARY_DIR=%APPVEYOR_BUILD_FOLDER%\\msvc\\csound-vs\\Release" ^
+    CSOUND_LIBRARY_DIR="%APPVEYOR_BUILD_FOLDER%\\msvc\\csound-vs\\Release" ^
     DEFAULT_CSOUND_LIBS=csound64.lib ^
     CSOUND_LIBRARY=csound64.lib ^
     -after ^
