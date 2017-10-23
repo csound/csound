261915c4e12ba47b414e122672df998135f1cee7
jpff <jpff@codemist.co.uk>
2017-10-23 18:38:31 +0100

    anoter typo

--- util1/csd_util/csb64enc.c
+++ util1/csd_util/csb64enc.c
@@ -58,7 +58,7 @@ int main(int argc, char **argv)
           exit(-1);
         }
         i++;
-        stle = atoi(argv[i]);
+        style = atoi(argv[i]);
       }
       else {                                    /* input file name */
         if (*(argv[i]) == '-') {
