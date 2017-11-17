aa4662ef93506e6b9e5d54e6f9e4d4c26dbe004a
Steven Yi <stevenyi@gmail.com>
2017-11-17 18:07:17 -0500

    fix csoundCondWait and csoundCondSignal for MSVC

--- Top/threads.c
+++ Top/threads.c
@@ -802,11 +802,14 @@ PUBLIC void* csoundCreateCondVar()
 }
 
 PUBLIC void csoundCondWait(void* condVar, void* mutex) {
-    SleepConditionVariableCS(&condVar, &mutex, INFINITE);
+    CONDITION_VARIABLE* cv = (CONDITION_VARIABLE*)condVar;
+    CRITICAL_SECTION* cs = (CRITICAL_SECTION*)mutex;
+    SleepConditionVariableCS(cv, cs, INFINITE);
 }
 
 PUBLIC void csoundCondSignal(void* condVar) {
-    WakeConditionVariable(&condVar);
+    CONDITION_VARIABLE* cv = (CONDITION_VARIABLE*)condVar;
+    WakeConditionVariable(cv);
 }
 
 /* ------------------------------------------------------------------------ */
