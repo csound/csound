480a2be31b42b8edebaf508c1f9fcd0f69ad0f40
gogins <michael.gogins@gmail.com>
2017-11-20 16:39:54 -0800

    RelWithDebInfo.

--- msvc/build-csoundqt.bat
+++ msvc/build-csoundqt.bat
@@ -11,12 +11,12 @@ qmake.exe qcs.pro ^
     DEFINES+="USE_QT_GT_58=1" ^
     CSOUND_API_INCLUDE_DIR="%APPVEYOR_BUILD_FOLDER%\\include" ^
     CSOUND_INTERFACES_INCLUDE_DIR="%APPVEYOR_BUILD_FOLDER%\\interfaces" ^
-    CSOUND_LIBRARY_DIR="%APPVEYOR_BUILD_FOLDER%\\msvc\\csound-vs\\Release" ^
+    CSOUND_LIBRARY_DIR="%APPVEYOR_BUILD_FOLDER%\\msvc\\csound-vs\\RelWithDebInfo" ^
     DEFAULT_CSOUND_LIBS=csound64.lib ^
     CSOUND_LIBRARY=csound64.lib ^
-    LCSOUND=%APPVEYOR_BUILD_FOLDER%\\msvc\\csound-vs\\Release\\csound64.lib ^
+    LCSOUND=%APPVEYOR_BUILD_FOLDER%\\msvc\\csound-vs\\RelWithDebInfo\\csound64.lib ^
     LIBS+="%VCPKGDir%\\installed\\x64-windows\\lib\\libsndfile-1.lib" ^
-    LIBS+="%APPVEYOR_BUILD_FOLDER%\\msvc\\csound-vs\\Release\\csound64.lib" ^
+    LIBS+="%APPVEYOR_BUILD_FOLDER%\\msvc\\csound-vs\\RelWithDebInfo\\csound64.lib" ^
     LIBS+="Ole32.lib" ^
     -after ^
     CSOUND_INTERFACES_INCLUDE_DIR+="%~dp0deps\\include" ^
@@ -24,5 +24,5 @@ qmake.exe qcs.pro ^
     CSOUND_INTERFACES_INCLUDE_DIR+="%VCPKGDir%\\installed\\x64-windows\\include" ^
     INCLUDEPATH+="%VCPKGDir%\\installed\\x64-windows\\include"
 nmake.exe
-move "bin\\CsoundQt*.*" "..\\..\\csound-vs\\Release\\"
+move "bin\\CsoundQt*.*" "..\\..\\csound-vs\\RelWithDebInfo\\"
 cd ..\..
