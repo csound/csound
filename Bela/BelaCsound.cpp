66d0547f1e6ea28e99a8e371ac6de79fd5ae8824
veplaini <victor.lazzarini@nuim.ie>
2017-12-19 12:50:36 +0000

    analogue channels numbering

--- Bela/BelaCsound.cpp
+++ Bela/BelaCsound.cpp
@@ -41,7 +41,7 @@ bool setup(BelaContext *context, void *userData)
   /* set up the channels */
   for(int i; i < context->analogInChannels; i++) {
     gCsData.channel[i].data.resize(context->analogFrames);
-    gCsData.channel[i].name << "analogue" << i;
+    gCsData.channel[i].name << "analogue" << i+1;
   }
   
   if(gCsData->res != 0) return false;
