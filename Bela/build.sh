4596ae60a9fed7bd61ca8d8778a01954c1284852
veplaini <victor.lazzarini@nuim.ie>
2018-01-08 14:46:23 +0000

    build.sh updated

--- Bela/build.sh
+++ Bela/build.sh
@@ -2,9 +2,9 @@
 # Build command for belacsound
 c++ main.cpp -o belacsound -std=c++11 -I/usr/xenomai/include -D_GNU_SOURCE \
     -D_REENTRANT -D__XENO__ -I/usr/xenomai/include/posix -I/usr/local/lib \
-    -I/root/Bela/include -L/usr/local/lib -lcsound -lsndfile -L/root/Bela/lib \
-    -lbela -lbelaextra -Wl,@/usr/xenomai/lib/posix.wrappers -L/usr/xenomai/lib \
-    -lpthread_rt -lxenomai -lpthread -lrt -lNE10 
+    -I/root/Bela/include -L/usr/local/lib `/usr/xenomai/bin/xeno-config --skin=posix --ldflags` \
+    -lcsound -lsndfile -L/root/Bela/lib \
+    -lbela -lbelaextra -lNE10 
 
 
 
