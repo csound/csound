f8772ed22e7ef68e2ffb3be66c7d56ea30e04aa7
veplaini <victor.lazzarini@nuim.ie>
2018-01-08 15:58:58 +0000

    build options

--- Bela/build.sh
+++ Bela/build.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 # Build command for belacsound
-c++ main.cpp -o belacsound -std=c++11 -I/usr/xenomai/include -D_GNU_SOURCE \
-    -D_REENTRANT -D__XENO__ -I/usr/xenomai/include/posix -I/usr/local/lib \
+c++ main.cpp -o belacsound -std=c++11 -fno-pie -no-pie `/usr/xenomai/bin/xeno-config --skin=posix --cflags` -I/usr/local/lib \
     -I/root/Bela/include -L/usr/local/lib `/usr/xenomai/bin/xeno-config --skin=posix --ldflags` \
     -lcsound -lsndfile -L/root/Bela/lib \
     -lbela -lbelaextra -lNE10 
