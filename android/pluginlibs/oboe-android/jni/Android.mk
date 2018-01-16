5294707f99a804afaf1cedcb1b8c0bd88395227d
veplaini <victor.lazzarini@nuim.ie>
2018-01-16 22:40:44 +0000

    fixing makefile

--- android/pluginlibs/oboe-android/jni/Android.mk
+++ android/pluginlibs/oboe-android/jni/Android.mk
@@ -6,25 +6,25 @@ CSOUND_SRC_ROOT := ../../../..
 
 LOCAL_MODULE   := liboboe
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../oboe/include $(LOCAL_PATH)/../../oboe/src
+LOCAL_C_INCLUDES := $(NDK_MODULE_PATH)/oboe/include $(NDK_MODULE_PATH)/oboe/src
 
-LOCAL_SRC_FILES := ../../oboe/src/aaudio/AAudioLoader.cpp \
-        ../../oboe/src/aaudio/AudioStreamAAudio.cpp \
-        ../../oboe/src/common/LatencyTuner.cpp \
-        ../../oboe/src/common/AudioStream.cpp \
-        ../../oboe/src/common/AudioStreamBuilder.cpp \
-        ../../oboe/src/common/Utilities.cpp \
-        ../../oboe/src/fifo/FifoBuffer.cpp \
-        ../../oboe/src/fifo/FifoController.cpp \
-        ../../oboe/src/fifo/FifoControllerBase.cpp \
-        ../../oboe/src/fifo/FifoControllerIndirect.cpp \
-        ../../oboe/src/opensles/AudioInputStreamOpenSLES.cpp \
-        ../../oboe/src/opensles/AudioOutputStreamOpenSLES.cpp \
-        ../../oboe/src/opensles/AudioStreamBuffered.cpp \
-        ../../oboe/src/opensles/AudioStreamOpenSLES.cpp \
-        ../../oboe/src/opensles/EngineOpenSLES.cpp \
-        ../../oboe/src/opensles/OpenSLESUtilities.cpp \
-        ../../oboe/src/opensles/OutputMixerOpenSLES.cpp
+LOCAL_SRC_FILES := $(NDK_MODULE_PATH)/oboe/src/aaudio/AAudioLoader.cpp \
+        $(NDK_MODULE_PATH)/oboe/src/aaudio/AudioStreamAAudio.cpp \
+        $(NDK_MODULE_PATH)/oboe/src/common/LatencyTuner.cpp \
+        $(NDK_MODULE_PATH)/oboe/src/common/AudioStream.cpp \
+        $(NDK_MODULE_PATH)/oboe/src/common/AudioStreamBuilder.cpp \
+        $(NDK_MODULE_PATH)/oboe/src/common/Utilities.cpp \
+        $(NDK_MODULE_PATH)/oboe/src/fifo/FifoBuffer.cpp \
+        $(NDK_MODULE_PATH)/oboe/src/fifo/FifoController.cpp \
+        $(NDK_MODULE_PATH)/oboe/src/fifo/FifoControllerBase.cpp \
+        $(NDK_MODULE_PATH)/oboe/src/fifo/FifoControllerIndirect.cpp \
+        $(NDK_MODULE_PATH)/oboe/src/opensles/AudioInputStreamOpenSLES.cpp \
+        $(NDK_MODULE_PATH)/oboe/src/opensles/AudioOutputStreamOpenSLES.cpp \
+        $(NDK_MODULE_PATH)/oboe/src/opensles/AudioStreamBuffered.cpp \
+        $(NDK_MODULE_PATH)/oboe/src/opensles/AudioStreamOpenSLES.cpp \
+        $(NDK_MODULE_PATH)/oboe/src/opensles/EngineOpenSLES.cpp \
+        $(NDK_MODULE_PATH)/oboe/src/opensles/OpenSLESUtilities.cpp \
+        $(NDK_MODULE_PATH)/oboe/src/opensles/OutputMixerOpenSLES.cpp
         
 LOCAL_LDLIBS += -llog -lOpenSLES 
 
