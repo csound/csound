176de02dc3852ad5a3ecdcc1532fd34875ce607d
Steven Yi <stevenyi@gmail.com>
2018-01-19 17:32:00 -0500

    fixed typo

--- emscripten/update_example_libs_from_dist_wasm.sh
+++ emscripten/update_example_libs_from_dist_wasm.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 mkdir examples-wasm
-cp -r examples-src/* example-wasm/
+cp -r examples-src/* examples-wasm/
 cp dist-wasm/* examples-wasm/javascripts
 mv examples-wasm/javascripts/libcsound.wasm examples-wasm
 
