a0984eb08e7f23e1012dc88e24b87f1ff68d4bfe
veplaini <victor.lazzarini@nuim.ie>
2018-01-19 16:51:25 +0000

    moving other things out

--- frontends/nwjs/make_csound_node_windows_x64.cmd
+++
@@ -1,5 +0,0 @@
-set CSOUND_HOME=D:\msys64\home\restore\csound
-set PYTHON=C:\Program_Files\Anaconda2\python.exe
-npm config set msvs_version 2015 --global
-npm configure --version=0.23.5
-nw-gyp rebuild --target=0.23.5 --arch=x64 --msvs_version=2015 --platform_toolset=v140_xp
