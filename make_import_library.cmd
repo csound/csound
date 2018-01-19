e87dd45546c0c2cefb9d65492859f21999966f92
veplaini <victor.lazzarini@nuim.ie>
2018-01-19 10:29:36 +0000

    tidying up and moving things

--- make_import_library.cmd
+++
@@ -1,6 +0,0 @@
-@echo CREATING MICROSOFT IMPORT LIBRARY FOR CSOUND64.DLL
-dumpbin /exports %CSOUND_HOME%\csound64.dll > exports.txt
-@echo LIBRARY CSOUND64 >> %CSOUND_HOME%\csound64.def
-@echo EXPORTS >> %CSOUND_HOME%\csound64.def
-FOR /F "skip=19 tokens=4" %%l in (exports.txt) do @echo %%l >> %CSOUND_HOME%\csound64.def
-lib /def:%CSOUND_HOME%\csound64.def /out:%CSOUND_HOME%\csound64.lib /machine:x86
