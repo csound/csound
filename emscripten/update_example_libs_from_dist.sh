446da7cc7987ae94614ee5288c03c6b034d2303b
veplaini <victor.lazzarini@nuim.ie>
2018-01-20 10:39:31 +0000

    updated scripts to test for dirs

--- emscripten/update_example_libs_from_dist.sh
+++ emscripten/update_example_libs_from_dist.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-mkdir examples
+if [ ! -d examples ]
+ then
+     mkdir examples
+fi
 cp -r examples-src/* examples/
 cp dist/* examples/javascripts
 mv examples/javascripts/libcsound.js.mem examples
