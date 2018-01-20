35b037c34a4b7f3852703bed1b89c29b7c118f06
veplaini <victor.lazzarini@nuim.ie>
2018-01-20 10:39:31 +0000

    updated scripts to test for dirs

--- emscripten/update_example_libs_from_dist_wasm.sh
+++ emscripten/update_example_libs_from_dist_wasm.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-mkdir examples-wasm
+if [ ! -d examples-wasm ]
+ then
+   mkdir examples-wasm
+fi
 cp -r examples-src/* examples-wasm/
 cp dist-wasm/* examples-wasm/javascripts
 mv examples-wasm/javascripts/libcsound.wasm examples-wasm
