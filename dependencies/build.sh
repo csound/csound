9353169c78b4cd200dddb08f97c1125ff00dceac
veplaini <victor.lazzarini@nuim.ie>
2018-02-01 10:00:34 +0000

    updating OSX dependencies build to install locally

--- dependencies/build.sh
+++ dependencies/build.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
+# change this if installation needs to go elsewhere
+PREFIX=$HOME
 cd build
-
 C_FLAGS="-arch i386 -arch x86_64 -mmacosx-version-min=10.4"
 
 # PORTAUDIO
 if [ ! -f portaudio/.complete ]; then
 cd portaudio
-./configure --enable-mac-universal 
+./configure --enable-mac-universal --prefix=$PREFIX
 make
-sudo make install
+make install
 touch .complete
 cd ..
 fi
@@ -22,22 +23,21 @@ rm -rf portmidi-cmake
 mkdir portmidi-cmake
 cp ../patches/portmidi-CMakeLists.txt portmidi-svn/CmakeLists.txt
 cd portmidi-cmake
-cmake ../portmidi-svn
+cmake ../portmidi-svn -DCMAKE_INSTALL_PREFIX=$PREFIX
 make
-sudo make install
+make install
 touch .complete
 cd ..
 
 fi
 
 # OGG
-
 if [ ! -f libogg-1.3.0/.complete ]; then
 
 cd libogg-1.3.0
-./configure CFLAGS="${C_FLAGS}" --disable-dependency-tracking 
+./configure CFLAGS="${C_FLAGS}" --disable-dependency-tracking --prefix=$PREFIX
 make 
-sudo make install
+make install
 touch .complete
 cd ..
 fi
@@ -46,9 +46,9 @@ fi
 if [ ! -f libvorbis-1.3.3/.complete ]; then
 
 cd libvorbis-1.3.3
-./configure CFLAGS="${C_FLAGS}" --disable-dependency-tracking 
+./configure CFLAGS="${C_FLAGS}" --disable-dependency-tracking --prefix=$PREFIX
 make 
-sudo make install
+make install
 touch .complete
 cd ..
 fi
@@ -59,21 +59,21 @@ fi
 if [ ! -f flac-1.2.1-i386/.complete ]; then
 
 cd flac-1.2.1
-./configure  --disable-dependency-tracking --disable-cpplibs --disable-asm-optimizations --enable-sse
+./configure  --disable-dependency-tracking --disable-cpplibs --disable-asm-optimizations --enable-sse --prefix=$PREFIX
 make 
-sudo make install
+make install
 
 cd ../flac-1.2.1-i386
-./configure CC="gcc -m32" --disable-dependency-tracking --disable-cpplibs --disable-asm-optimizations --enable-sse
+./configure CC="gcc -m32" --disable-dependency-tracking --disable-cpplibs --disable-asm-optimizations --enable-sse --prefix=$PREFIX 
 make
 
-cp /usr/local/lib/libFLAC.a libflac-x86_64.a
-cp /usr/local/lib/libFLAC.8.2.0.dylib libflac-x86_64.dylib 
+cp $PREFIX/lib/libFLAC.a libflac-x86_64.a
+cp $PREFIX/libFLAC.8.2.0.dylib libflac-x86_64.dylib 
 
-sudo rm /usr/local/lib/libFLAC.a
-sudo rm /usr/local/lib/libFLAC.8.2.0.dylib
-sudo lipo -create src/libFLAC/.libs/libflac.a libflac-x86_64.a -output /usr/local/lib/libFLAC.a
-sudo lipo -create src/libFLAC/.libs/libflac.8.2.0.dylib libflac-x86_64.dylib -output /usr/local/lib/libFLAC.8.2.0.dylib
+rm $PREFIX/lib/libFLAC.a
+rm $PREFIX/lib/libFLAC.8.2.0.dylib
+lipo -create src/libFLAC/.libs/libflac.a libflac-x86_64.a -output $PREFIX/lib/libFLAC.a
+lipo -create src/libFLAC/.libs/libflac.8.2.0.dylib libflac-x86_64.dylib -output $PREFIX/lib/libFLAC.8.2.0.dylib
 
 touch .complete
 cd ..
@@ -82,9 +82,9 @@ fi
 # LIBSNDFILE
 if [ ! -f libsndfile-1.0.25/.complete ]; then
 cd libsndfile-1.0.25
-./configure CFLAGS="$C_FLAGS" --disable-dependency-tracking  --disable-sqlite
+./configure CFLAGS="$C_FLAGS" --disable-dependency-tracking  --disable-sqlite --prefix=$PREFIX 
 make || true
-sudo make install
+make install
 touch .complete
 cd ..
 fi
@@ -93,18 +93,18 @@ fi
 if [ ! -f wiiuse_v0.13/.complete ]; then
 cd wiiuse_v0.13
 xcodebuild -project wiiuse.xcodeproj -sdk macosx
-sudo cp build/Release/libwiiuse.dylib /usr/local/lib
-sudo cp wiiuse_v0.12/src/wiiuse.h /usr/local/include
+cp build/Release/libwiiuse.dylib $PREFIX/lib
+cp wiiuse_v0.12/src/wiiuse.h $PREFIX/include
 touch .complete
 cd ..
 fi
 
 # FLTK 
-if [ ! -f fltk-1.3.2/.complete ]; then
-cd fltk-1.3.2
-./configure  --with-archflags="-arch i386 -arch x86_64" --enable-shared
+if [ ! -f fltk-1.3.4/.complete ]; then
+cd fltk-1.3.4
+./configure  --with-archflags="-arch i386 -arch x86_64" --enable-shared --prefix=$PREFIX
 make 
-sudo make install
+make install
 touch .complete
 cd ..
 fi
@@ -112,9 +112,9 @@ fi
 # LIBLO 
 if [ ! -f liblo-0.28/.complete ]; then
 cd liblo-0.28 
-./configure CFLAGS="$C_FLAGS" --disable-dependency-tracking 
+./configure CFLAGS="$C_FLAGS" --disable-dependency-tracking --prefix=$PREFIX
 make 
-sudo make install
+make install
 touch .complete
 cd ..
 fi
@@ -122,9 +122,19 @@ fi
 # LIBPNG
 if [ ! -f libpng-1.6.6/.complete ]; then
 cd libpng-1.6.6
-./configure CFLAGS="$C_FLAGS" --disable-dependency-tracking 
+./configure CFLAGS="$C_FLAGS" --disable-dependency-tracking --prefix=$PREFIX
 make 
-sudo make install
+make install
+touch .complete
+cd ..
+fi
+
+# FLUIDSYNTH
+if [ ! -f fluidsynth-1.0.9/.complete ]; then
+cd fluidsynth-1.0.9
+./configure CFLAGS="$C_FLAGS" --prefix=$PREFIX
+make 
+make install
 touch .complete
 cd ..
 fi
