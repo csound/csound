12929a9939adccb2b3ef0b4586ae8d0827b3a08e
veplaini <victor.lazzarini@nuim.ie>
2018-02-01 11:31:10 +0000

    updating dependency scripts

--- dependencies/download.sh
+++ dependencies/download.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-flac_src="http://sourceforge.net/projects/flac/files/flac-src/flac-1.2.1-src/flac-1.2.1.tar.gz"
+flac_src="https://ftp.osuosl.org/pub/xiph/releases/flac/flac-1.3.2.tar.xz"
 ogg_src="http://downloads.xiph.org/releases/ogg/libogg-1.3.0.tar.gz"
 vorbis_src="http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.3.tar.gz"
-libsndfile_src="http://www.mega-nerd.com/libsndfile/files/libsndfile-1.0.25.tar.gz"
+libsndfile_src="http://www.mega-nerd.com/libsndfile/files/libsndfile-1.0.28.tar.gz"
 liblo_src="http://sourceforge.net/projects/liblo/files/liblo/0.28/liblo-0.28.tar.gz"
 #wiiuse_src="http://sourceforge.net/projects/wiiuse/files/wiiuse/v0.12/wiiuse_v0.12_src.tar.gz"
 wiiuse_src="http://codemist.co.uk/jpff/wiiuse_v0.13a.zip"
@@ -17,7 +17,7 @@ fltk_src="http://fltk.org/pub/fltk/1.3.4/fltk-1.3.4-source.tar.gz"
 png_src="http://sourceforge.net/projects/libpng/files/libpng16/older-releases/1.6.6/libpng-1.6.6.tar.gz"
 fluidsynth_src="http://sourceforge.net/projects/fluidsynth/files/older%20releases/fluidsynth-1.0.9.tar.gz"
 luajit_src="http://luajit.org/download/LuaJIT-2.0.2.tar.gz"
-packages=($portaudio_src $portmidi_src $flac_src $ogg_src $vorbis_src $libsndfile_src $liblo_src $wiiuse_src $fltk_src $png_src $boost_src $fluidsynth_src $luajit_src)
+packages=($portaudio_src $portmidi_src $flac_src $ogg_src $vorbis_src $libsndfile_src $liblo_src $wiiuse_src $fltk_src $png_src $fluidsynth_src)
 
 
 #prepare
@@ -43,4 +43,4 @@ done
 
 svn co https://svn.code.sf.net/p/portmedia/code/portmidi/trunk portmidi-svn
 
-cp -r flac-1.2.1 flac-1.2.1-i386
+cp -r flac-1.3.2 flac-1.3.2-i386
