b530c75a3c66d2565502d7dfa2d4ee88efe56538
veplaini <victor.lazzarini@nuim.ie>
2018-02-01 10:00:34 +0000

    updating OSX dependencies build to install locally

--- dependencies/download.sh
+++ dependencies/download.sh
@@ -8,11 +8,11 @@ liblo_src="http://sourceforge.net/projects/liblo/files/liblo/0.28/liblo-0.28.tar
 #wiiuse_src="http://sourceforge.net/projects/wiiuse/files/wiiuse/v0.12/wiiuse_v0.12_src.tar.gz"
 wiiuse_src="http://codemist.co.uk/jpff/wiiuse_v0.13a.zip"
 #portaudio_src="http://www.portaudio.com/archives/pa_stable_v19_20111121.tgz"
-portaudio_src="http://portaudio.com/archives/pa_snapshot.tgz"
+portaudio_src="http://www.portaudio.com/archives/pa_snapshot.tgz"
 #portmidi_src="http://sourceforge.net/projects/portmedia/files/portmidi/217/portmidi-src-217.zip"
 boost_src="http://sourceforge.net/projects/boost/files/boost/1.52.0/boost_1_52_0.tar.gz"
 #fltk_src="http://ftp.easysw.com/pub/fltk/1.3.1/fltk-1.3.1-source.tar.gz"
-fltk_src="http://fltk.org/pub/fltk/1.3.2/fltk-1.3.2-source.tar.gz"
+fltk_src="http://fltk.org/pub/fltk/1.3.4/fltk-1.3.4-source.tar.gz"
 #png_src="ftp://ftp.simplesystems.org/pub/libpng/png/src/libpng-1.5.13.tar.gz"
 png_src="http://sourceforge.net/projects/libpng/files/libpng16/older-releases/1.6.6/libpng-1.6.6.tar.gz"
 fluidsynth_src="http://sourceforge.net/projects/fluidsynth/files/older%20releases/fluidsynth-1.0.9.tar.gz"
@@ -29,7 +29,7 @@ mkdir build
 cd cache
 for i in ${packages[*]}; do
   echo $i
-  wget -nc $i 
+  wget -nc --no-check-certificate $i 
 done
 
 cd ../build
