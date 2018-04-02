656f1b273d77806204f8e9543f8ca84b041ed3ee
jpff <jpff@codemist.co.uk>
2018-04-02 18:02:05 +0100

    release notes

--- Opcodes/pvlock.c
+++ Opcodes/pvlock.c
@@ -575,7 +575,8 @@ static int32_t sinit3(CSOUND *csound, DATASPACE *p)
     fd  = csound->FileOpen2(csound, &(p->sf), CSFILE_SND_R, name, &sfinfo,
                             "SFDIR;SSDIR", CSFTYPE_UNKNOWN_AUDIO, 0);
     if (p->sf == NULL)
-      return csound->InitError(csound, Str("filescal: failed to open file %s\n"), name);
+      return csound->InitError(csound,
+                               Str("filescal: failed to open file %s\n"), name);
     if (sfinfo.samplerate != CS_ESR)
       p->resamp = sfinfo.samplerate/CS_ESR;
     else
