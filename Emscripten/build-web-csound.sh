659e848b74860098a28537468823cf70117a8899
Steven Yi <stevenyi@gmail.com>
2018-04-21 17:12:58 -0400

    make work for both ScriptProcessor and AudioWorklet on Desktop Chrome and FF and Android Chrome and FF; add additional post-process of js to get it to work with CsoundObjNew.js

--- Emscripten/build-web-csound.sh
+++ Emscripten/build-web-csound.sh
@@ -26,13 +26,18 @@ emcc -s LINKABLE=1 -s ASSERTIONS=0 ../src/CsoundObj.c -I../../include -Iinclude 
 
 # Keep exports in alphabetical order please, to correlate with CsoundObj.js.
 
-## First build for WASM/ScriptProcessorNode (async compilation = 1)
-emcc -v -O2 -g4 -DINIT_STATIC_MODULES=0 -s WASM=1 -s ASSERTIONS=0 -s "BINARYEN_METHOD='native-wasm'" -s LINKABLE=1 -s RESERVED_FUNCTION_POINTERS=1 -s ALLOW_MEMORY_GROWTH=1 -s NO_EXIT_RUNTIME=0 -s BINARYEN_ASYNC_COMPILATION=1 -s MODULARIZE=1 -s EXPORT_NAME=\"'libcsound'\" -s EXTRA_EXPORTED_RUNTIME_METHODS='["ccall", "cwrap"]' CsoundObj.bc FileList.bc libcsound.a ../deps/libsndfile-1.0.25/libsndfile-wasm.a -o libcsound.js
 
-## Second build for WASM/AudioWorklet (async compilation = 0)
+## First build for WASM/ScriptProcessorNode (async compilation = 1, assertions = 1)
+emcc -v -O2 -g4 -DINIT_STATIC_MODULES=0 -s WASM=1 -s ASSERTIONS=1 -s "BINARYEN_METHOD='native-wasm'" -s LINKABLE=1 -s RESERVED_FUNCTION_POINTERS=1 -s ALLOW_MEMORY_GROWTH=1 -s NO_EXIT_RUNTIME=0 -s BINARYEN_ASYNC_COMPILATION=1 -s MODULARIZE=1 -s EXPORT_NAME=\"'libcsound'\" -s EXTRA_EXPORTED_RUNTIME_METHODS='["ccall", "cwrap"]' CsoundObj.bc FileList.bc libcsound.a ../deps/libsndfile-1.0.25/libsndfile-wasm.a -o libcsound.js
+ 
+## Second build for WASM/AudioWorklet (async compilation = 0, assertions = 0)
 emcc -v -O2 -g4 -DINIT_STATIC_MODULES=0 -s WASM=1 -s ASSERTIONS=0 -s "BINARYEN_METHOD='native-wasm'" -s LINKABLE=1 -s RESERVED_FUNCTION_POINTERS=1 -s ALLOW_MEMORY_GROWTH=1 -s NO_EXIT_RUNTIME=0 -s BINARYEN_ASYNC_COMPILATION=0 -s MODULARIZE=1 -s EXPORT_NAME=\"'libcsound'\" -s EXTRA_EXPORTED_RUNTIME_METHODS='["ccall", "cwrap"]' CsoundObj.bc FileList.bc libcsound.a ../deps/libsndfile-1.0.25/libsndfile-wasm.a -o libcsound-worklet.js
 
+
 sh ../convert.sh
+
+echo "AudioWorkletGlobalScope.libcsound = libcsound" >> libcsound.js
+echo "AudioWorkletGlobalScope.libcsound = libcsound" >> libcsound-worklet.js
 
 cd ..
 rm -rf dist-web-csound
