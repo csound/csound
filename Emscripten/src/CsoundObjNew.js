1d689a79989b3548b879152c535ec2d50f02fef2
Steven Yi <stevenyi@gmail.com>
2018-04-26 11:05:24 -0400

    updated to load libcsound-worklet.wasm.js instead of base64 version

--- Emscripten/src/CsoundObjNew.js
+++ Emscripten/src/CsoundObjNew.js
@@ -136,7 +136,7 @@ if(typeof AudioWorkletNode !== 'undefined' &&
     static importScripts(script_base='./') {
       let actx = CSOUND_AUDIO_CONTEXT;
       return new Promise( (resolve) => {
-        actx.audioWorklet.addModule(script_base + 'libcsound-worklet.base64.js').then(() => {
+        actx.audioWorklet.addModule(script_base + 'libcsound-worklet.wasm.js').then(() => {
         actx.audioWorklet.addModule(script_base + 'libcsound-worklet.js').then(() => {
         actx.audioWorklet.addModule(script_base + 'CsoundProcessor.js').then(() => {
           resolve(); 
