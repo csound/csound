28ee58b22585a965ec0be83933cab84a8ebb7c71
veplaini <victor.lazzarini@nuim.ie>
2018-05-01 17:45:03 +0100

    tidy up of directory

--- Emscripten/wasm/update_scripts.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-cp ../dist-wasm/libcsound.wasm .
-cp ../dist-wasm/libcsound.js ./js/libcsound.js
-cp ../dist-wasm/libcsound.js.map ./js/libcsound.js.map
-cp ../src/CsoundObj.js ./js/CsoundObj.js
-cp ../src/csound.js ./js/csound.js
