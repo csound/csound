6e4d5e82db31973b5f87baf93a53dbbb3bd3c71c
Steven Yi <stevenyi@gmail.com>
2018-05-02 10:51:03 -0400

    updated to use separate CsoundNode and CsoundScriptProcessorNode; nodes should be usable on their own outside of CsoundObj

--- Emscripten/src/CsoundProcessor.js
+++ Emscripten/src/CsoundProcessor.js
@@ -19,33 +19,33 @@ let FS = WAM["FS"];
 // Get cwrap-ed functions
 const Csound = {
 
-    new: WAM.cwrap('CsoundObj_new', ['number'], null),
-    compileCSD: WAM.cwrap('CsoundObj_compileCSD', ['number'], ['number', 'string']),
-    evaluateCode: WAM.cwrap('CsoundObj_evaluateCode', ['number'], ['number', 'string']),
-    readScore: WAM.cwrap('CsoundObj_readScore', ['number'], ['number', 'string']),
+  new: WAM.cwrap('CsoundObj_new', ['number'], null),
+  compileCSD: WAM.cwrap('CsoundObj_compileCSD', ['number'], ['number', 'string']),
+  evaluateCode: WAM.cwrap('CsoundObj_evaluateCode', ['number'], ['number', 'string']),
+  readScore: WAM.cwrap('CsoundObj_readScore', ['number'], ['number', 'string']),
 
-    reset: WAM.cwrap('CsoundObj_reset', null, ['number']),
-    getOutputBuffer: WAM.cwrap('CsoundObj_getOutputBuffer', ['number'], ['number']),
-    getInputBuffer: WAM.cwrap('CsoundObj_getInputBuffer', ['number'], ['number']),
-    getControlChannel: WAM.cwrap('CsoundObj_getControlChannel', ['number'], ['number', 'string']),
-    setControlChannel: WAM.cwrap('CsoundObj_setControlChannel', null, ['number', 'string', 'number']),
-    setStringChannel: WAM.cwrap('CsoundObj_setStringChannel', null, ['number', 'string', 'string']),
-    getKsmps: WAM.cwrap('CsoundObj_getKsmps', ['number'], ['number']),
-    performKsmps: WAM.cwrap('CsoundObj_performKsmps', ['number'], ['number']),
+  reset: WAM.cwrap('CsoundObj_reset', null, ['number']),
+  getOutputBuffer: WAM.cwrap('CsoundObj_getOutputBuffer', ['number'], ['number']),
+  getInputBuffer: WAM.cwrap('CsoundObj_getInputBuffer', ['number'], ['number']),
+  getControlChannel: WAM.cwrap('CsoundObj_getControlChannel', ['number'], ['number', 'string']),
+  setControlChannel: WAM.cwrap('CsoundObj_setControlChannel', null, ['number', 'string', 'number']),
+  setStringChannel: WAM.cwrap('CsoundObj_setStringChannel', null, ['number', 'string', 'string']),
+  getKsmps: WAM.cwrap('CsoundObj_getKsmps', ['number'], ['number']),
+  performKsmps: WAM.cwrap('CsoundObj_performKsmps', ['number'], ['number']),
 
-    render: WAM.cwrap('CsoundObj_render', null, ['number']),
-    getInputChannelCount: WAM.cwrap('CsoundObj_getInputChannelCount', ['number'], ['number']),
-    getOutputChannelCount: WAM.cwrap('CsoundObj_getOutputChannelCount', ['number'], ['number']),
-    getTableLength: WAM.cwrap('CsoundObj_getTableLength', ['number'], ['number', 'number']),
-    getTable: WAM.cwrap('CsoundObj_getTable', ['number'], ['number', 'number']),
-    getZerodBFS: WAM.cwrap('CsoundObj_getZerodBFS', ['number'], ['number']),
-    compileOrc: WAM.cwrap('CsoundObj_compileOrc', 'number', ['number', 'string']),
-    setOption: WAM.cwrap('CsoundObj_setOption', null, ['number', 'string']),
-    prepareRT: WAM.cwrap('CsoundObj_prepareRT', null, ['number']),
-    getScoreTime: WAM.cwrap('CsoundObj_getScoreTime', null, ['number']),
-    setTable: WAM.cwrap('CsoundObj_setTable', null, ['number', 'number', 'number', 'number']),
-    play: WAM.cwrap('CsoundObj_play', null, ['number']),
-    pause: WAM.cwrap('CsoundObj_pause', null, ['number']),
+  render: WAM.cwrap('CsoundObj_render', null, ['number']),
+  getInputChannelCount: WAM.cwrap('CsoundObj_getInputChannelCount', ['number'], ['number']),
+  getOutputChannelCount: WAM.cwrap('CsoundObj_getOutputChannelCount', ['number'], ['number']),
+  getTableLength: WAM.cwrap('CsoundObj_getTableLength', ['number'], ['number', 'number']),
+  getTable: WAM.cwrap('CsoundObj_getTable', ['number'], ['number', 'number']),
+  getZerodBFS: WAM.cwrap('CsoundObj_getZerodBFS', ['number'], ['number']),
+  compileOrc: WAM.cwrap('CsoundObj_compileOrc', 'number', ['number', 'string']),
+  setOption: WAM.cwrap('CsoundObj_setOption', null, ['number', 'string']),
+  prepareRT: WAM.cwrap('CsoundObj_prepareRT', null, ['number']),
+  getScoreTime: WAM.cwrap('CsoundObj_getScoreTime', null, ['number']),
+  setTable: WAM.cwrap('CsoundObj_setTable', null, ['number', 'number', 'number', 'number']),
+  play: WAM.cwrap('CsoundObj_play', null, ['number']),
+  pause: WAM.cwrap('CsoundObj_pause', null, ['number']),
 
 }
 //var _openAudioOut = WAM.cwrap('CsoundObj_openAudioOut', null, ['number']);
@@ -55,168 +55,168 @@ const Csound = {
 
 class CsoundProcessor extends AudioWorkletProcessor {
 
-    static get parameterDescriptors() {
-	return [];
+  static get parameterDescriptors() {
+    return [];
+  }
+
+  constructor(options) {
+    super(options);
+    let p = this.port;
+    WAM["print"] = (t) => {
+      p.postMessage(["log", t]);
+    };
+    WAM["printErr"] = (t) => {
+      p.postMessage(["log", t]);
+    };
+
+    let csObj = Csound.new();
+    this.csObj = csObj;
+    // engine status
+    this.status = 0;
+    this.running = false;
+    this.started = false;
+
+    Csound.setOption(this.csObj, "-odac");
+    Csound.setOption(this.csObj, "-+rtaudio=null");
+
+    this.port.onmessage = this.handleMessage.bind(this);
+    this.port.start();
+  }
+
+
+  process(inputs, outputs, parameters) {
+    if (this.csoundOutputBuffer == null ||
+      this.running == false) {
+      return true;
     }
 
-    constructor(options) {
-	super(options);
-	let p = this.port;
-	WAM["print"] = (t) => {
-	    p.postMessage(["log", t]);
-	};
-	WAM["printErr"] = (t) => {
-	    p.postMessage(["log", t]);
-	};
+    let input = inputs[0];
+    let output = outputs[0];
 
-	let csObj = Csound.new();
-	this.csObj = csObj;
-	// engine status
-	this.status = 0;
-	this.running = false;
-	this.started = false;
-	
-	Csound.setOption(this.csObj, "-odac");
-	Csound.setOption(this.csObj, "-+rtaudio=null");
+    let bufferLen = output[0].length;
 
-	this.port.onmessage = this.handleMessage.bind(this);
-	this.port.start();
+    let csOut = this.csoundOutputBuffer;
+    let csIn = this.csoundInputBuffer;
+    let ksmps = this.ksmps;
+    let zerodBFS = this.zerodBFS;
+
+    let cnt = this.cnt;
+    let nchnls = this.nchnls;
+    let status = this.status;
+
+    for (let i = 0; i < bufferLen; i++, cnt++) {
+      if(cnt == ksmps && status == 0) {
+        // if we need more samples from Csound
+        status = Csound.performKsmps(this.csObj);
+        cnt = 0;
+      }
+
+      for (let channel = 0; channel < input.length; channel++) {
+        let inputChannel = input[channel];
+        csIn[cnt*nchnls + channel] = inputChannel[i] * zerodBFS;
+      }
+      for (let channel = 0; channel < output.length; channel++) {
+        let outputChannel = output[channel];
+        if(status == 0)
+          outputChannel[i] = csOut[cnt*nchnls + channel] / zerodBFS;
+        else
+          outputChannel[i] = 0;
+      } 
     }
 
+    this.cnt = cnt;
+    this.status = status;
 
-    process(inputs, outputs, parameters) {
-	if (this.csoundOutputBuffer == null ||
-	    this.running == false) {
-	    return true;
-	}
+    return true;
+  }
 
-	let input = inputs[0];
-	let output = outputs[0];
+  start() {
+    if(this.started == false) {
+      let csObj = this.csObj;
+      let ksmps = Csound.getKsmps(csObj);
+      this.ksmps = ksmps;
+      this.cnt = ksmps;
+      let outputChannelCount = 2;
+      let inputChannelCount = 1;
 
-	let bufferLen = output[0].length;
+      //hardcode for now, but has to match Node's input/output settings
+      this.nchnls = 2;
+      this.nchnls_i = 1;
 
-	let csOut = this.csoundOutputBuffer;
-	let csIn = this.csoundInputBuffer;
-	let ksmps = this.ksmps;
-	let zerodBFS = this.zerodBFS;
+      Csound.prepareRT(csObj);
+      Csound.play(csObj);
 
-	let cnt = this.cnt;
-	let nchnls = this.nchnls;
-	let status = this.status;
-
-	for (let i = 0; i < bufferLen; i++, cnt++) {
-	    if(cnt == ksmps && status == 0) {
-		// if we need more samples from Csound
-		status = Csound.performKsmps(this.csObj);
-		cnt = 0;
-	    }
-
-	    for (let channel = 0; channel < input.length; channel++) {
-		let inputChannel = input[channel];
-                csIn[cnt*nchnls + channel] = inputChannel[i] * zerodBFS;
-	    }
-	    for (let channel = 0; channel < output.length; channel++) {
-		let outputChannel = output[channel];
-		if(status == 0)
-		    outputChannel[i] = csOut[cnt*nchnls + channel] / zerodBFS;
-		else
-		    outputChannel[i] = 0;
-	    } 
-	}
-
-	this.cnt = cnt;
-	this.status = status;
-
-	return true;
+      let outputPointer = Csound.getOutputBuffer(csObj);
+      this.csoundOutputBuffer = new Float32Array(WAM.HEAP8.buffer, outputPointer, ksmps * outputChannelCount);
+      let inputPointer = Csound.getInputBuffer(csObj);
+      this.csoundInputBuffer = new Float32Array(WAM.HEAP8.buffer, inputPointer, ksmps * inputChannelCount);
+      this.zerodBFS = Csound.getZerodBFS(csObj);
+      this.started = true;
     }
+    this.running = true;
+  }
 
-    start() {
-	if(this.started == false) {
-	let csObj = this.csObj;
-	let ksmps = Csound.getKsmps(csObj);
-	this.ksmps = ksmps;
-	this.cnt = ksmps;
-	let outputChannelCount = 2;
-	let inputChannelCount = 1;
+  compileOrc(orcString) {
+    Csound.compileOrc(this.csObj, orcString);
+  }
 
-	//hardcode for now, but has to match Node's input/output settings
-	this.nchnls = 2;
-	this.nchnls_i = 1;
+  handleMessage(event) {
+    let data = event.data;
 
-	Csound.prepareRT(csObj);
-	Csound.play(csObj);
-	
-	let outputPointer = Csound.getOutputBuffer(csObj);
-	this.csoundOutputBuffer = new Float32Array(WAM.HEAP8.buffer, outputPointer, ksmps * outputChannelCount);
-	let inputPointer = Csound.getInputBuffer(csObj);
-	this.csoundInputBuffer = new Float32Array(WAM.HEAP8.buffer, inputPointer, ksmps * inputChannelCount);
-	this.zerodBFS = Csound.getZerodBFS(csObj);
-	this.started = true;
-	}
-	this.running = true;
+    switch (data[0]) {
+      case "compileCSD":
+        this.status = Csound.compileCSD(this.csObj, data[1]);
+        break;
+      case "compileOrc":
+        Csound.compileOrc(this.csObj, data[1]);
+        break;
+      case "evalCode":
+        Csound.evaluateCode(this.csObj, data[1]);
+        break;
+      case "readScore":
+        Csound.readScore(this.csObj, data[1]);
+        break;
+      case "setControlChannel":
+        Csound.setControlChannel(this.csObj,
+          data[1], data[2]);
+        break;
+      case "setStringChannel":
+        Csound.setStringChannel(this.csObj,
+          data[1], data[2]);
+        break;
+      case "start":
+        this.start();
+        break;
+      case "stop":
+        this.running = false;
+        break;
+      case "play":
+        this.start();
+        break;
+      case "setOption":
+        Csound.setOption(this.csObj, data[1]);
+        break;
+      case "reset":
+        Csound.reset(this.csObj);
+        Csound.setOption(this.csObj, "-odac");
+        Csound.setOption(this.csObj, "-+rtaudio=null");
+        this.csoundOutputBuffer = null; 
+        this.ksmps = null; 
+        this.zerodBFS = null; 
+        break;
+      case "writeToFS":
+        let name = data[1];
+        let blobData = data[2];
+        let buf = new Uint8Array(blobData)
+        let stream = FS.open(name, 'w+');
+        FS.write(stream, buf, 0, buf.length, 0);
+        FS.close(stream);
+
+        break
+        default:
+        console.log('[CsoundAudioProcessor] Invalid Message: "' + event.data);
     }
-
-    compileOrc(orcString) {
-	Csound.compileOrc(this.csObj, orcString);
-    }
-
-    handleMessage(event) {
-	let data = event.data;
-
-	switch (data[0]) {
-	case "compileCSD":
-            this.status = Csound.compileCSD(this.csObj, data[1]);
-            break;
-	case "compileOrc":
-            Csound.compileOrc(this.csObj, data[1]);
-            break;
-	case "evalCode":
-            Csound.evaluateCode(this.csObj, data[1]);
-            break;
-	case "readScore":
-            Csound.readScore(this.csObj, data[1]);
-            break;
-	case "setControlChannel":
-            Csound.setControlChannel(this.csObj,
-				     data[1], data[2]);
-            break;
-	case "setStringChannel":
-            Csound.setStringChannel(this.csObj,
-				    data[1], data[2]);
-            break;
-	case "start":
-            this.start();
-            break;
-	case "stop":
-	    this.running = false;
-	    break;
-	case "play":
-	    this.start();
-	    break;
-	case "setOption":
-            Csound.setOption(this.csObj, data[1]);
-            break;
-	case "reset":
-            Csound.reset(this.csObj);
-            Csound.setOption(this.csObj, "-odac");
-            Csound.setOption(this.csObj, "-+rtaudio=null");
-            this.csoundOutputBuffer = null; 
-            this.ksmps = null; 
-            this.zerodBFS = null; 
-            break;
-	case "writeToFS":
-            let name = data[1];
-            let blobData = data[2];
-            let buf = new Uint8Array(blobData)
-            let stream = FS.open(name, 'w+');
-            FS.write(stream, buf, 0, buf.length, 0);
-            FS.close(stream);
-
-            break
-	default:
-            console.log('[CsoundAudioProcessor] Invalid Message: "' + event.data);
-	}
-    }
+  }
 }
 
 registerProcessor('Csound', CsoundProcessor);
