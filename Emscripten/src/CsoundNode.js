b2eb314f06d71d3d5d1d326e38905604a28f2414
veplaini <victor.lazzarini@nuim.ie>
2018-05-05 10:19:49 +0100

    annotations

--- Emscripten/src/CsoundNode.js
+++ Emscripten/src/CsoundNode.js
@@ -15,139 +15,214 @@
 
 // Setup a single global AudioContext object if not already defined
 var CSOUND_AUDIO_CONTEXT = CSOUND_AUDIO_CONTEXT || 
-  (function() {
+    (function() {
 
-    try {
-        var AudioContext = window.AudioContext || window.webkitAudioContext;
-        return new AudioContext();      
-    }
-    catch(error) {
+        try {
+            var AudioContext = window.AudioContext || window.webkitAudioContext;
+            return new AudioContext();      
+        }
+        catch(error) {
 
-        console.log('Web Audio API is not supported in this browser');
-    }
-    return null;
-  }());
+            console.log('Web Audio API is not supported in this browser');
+        }
+        return null;
+    }());
 
 
-/* CsoundNode class to use for AudioWorklet */
+/** This ES6 Class defines a Custom Node as an AudioWorkletNode 
+ *  that holds a Csound engine.
+ */
 class CsoundNode extends AudioWorkletNode {
 
-  constructor(context, options) {
-    options = options || {};
-    options.numberOfInputs  = 1;
-    options.numberOfOutputs = 2;
-    options.channelCount = 2;
-    options.sampleRate = CSOUND_AUDIO_CONTEXT.sampleRate;  
+    /** @constructor
+     *
+     * @param {AudioContext} context AudioContext in which this node will run.
+     * @param {object} options Configuration options, holding numberOfInputs,
+     *   numberOfOutputs.
+     */
+    constructor(context, options) {
+        options = options || {};
+        options.numberOfInputs  = 1;
+        options.numberOfOutputs = 2;
+        options.channelCount = 2;
+        options.sampleRate = context.sampleRate;  
 
-    super(context, 'Csound', options);
+        super(context, 'Csound', options);
 
-    this.msgCallback = (msg) => { console.log(msg); }
+        this.msgCallback = (msg) => { console.log(msg); }
 
-    this.port.start();
-    this.port.onmessage = (event) => {
-      let data = event.data;
-      switch(data[0]) {
-        case "log":
-          this.msgCallback(data[1]);
-          break;
-          default:
-          console.log('[CsoundNode] Invalid Message: "' + event.data);
-      }
+        this.port.start();
+        this.port.onmessage = (event) => {
+            let data = event.data;
+            switch(data[0]) {
+            case "log":
+                this.msgCallback(data[1]);
+                break;
+            default:
+                console.log('[CsoundNode] Invalid Message: "' + event.data);
+            }
 
-    };
-  }
+        };
+    }
+    
+    /** Writes data to a file in the WASM filesystem for
+     *  use with csound.
+     *
+     * @param {string} filePath A string containing the path to write to.
+     * @param {blob}   blobData The data to write to file.
+     */  
+    writeToFS(filePath, blobData) {
+        this.port.postMessage(["writeToFS", filePath, blobData]);
+    }
 
-  writeToFS(filePath, blobData) {
-    this.port.postMessage(["writeToFS", filePath, blobData]);
-  }
+    /** Compiles a CSD, which may be given as a filename in the
+     *  WASM filesystem or a string containing the code
+     *
+     * @param {string} csd A string containing the CSD filename or the CSD code.
+     */
+    compileCSD(filePath) {
+        this.port.postMessage(["compileCSD", filePath]);
+    }
 
-  compileCSD(filePath) {
-    this.port.postMessage(["compileCSD", filePath]);
-  }
+    /** Compiles Csound orchestra code.
+     *
+     * @param {string} orcString A string containing the orchestra code.
+     */    
+    compileOrc(orcString) {
+        this.port.postMessage(["compileOrc", orcString]);
+    }
 
-  compileOrc(orcString) {
-    this.port.postMessage(["compileOrc", orcString]);
-  }
+    /** Sets a Csound engine option (flag)
+     *  
+     *
+     * @param {string} option The Csound engine option to set. This should
+     * not contain any whitespace.
+     */
+    setOption(option) {
+        this.port.postMessage(["setOption", option]);    
+    }
 
-  setOption(option) {
-    this.port.postMessage(["setOption", option]);    
-  }
+    render(filePath) {
+    }
 
-  render(filePath) {
-  }
+    /** Evaluates Csound orchestra code.
+     *
+     * @param {string} codeString A string containing the orchestra code.
+     */   
+    evaluateCode(codeString) {
+        this.port.postMessage(["evalCode", codeString]);
+    }
 
-  evaluateCode(codeString) {
-    this.port.postMessage(["evalCode", codeString]);
-  }
+    /** Reads a numeric score string.
+     *
+     * @param {string} scoreString A string containing a numeric score.
+     */    
+    readScore(scoreString) {
+        this.port.postMessage(["readScore", scoreString]);
+    }
 
-  readScore(scoreString) {
-    this.port.postMessage(["readScore", scoreString]);
-  }
+    /** Sets the value of a control channel in the software bus
+     *
+     * @param {string} channelName A string containing the channel name.
+     * @param {number} value The value to be set.
+     */ 
+    setControlChannel(channelName, value) {
+        this.port.postMessage(["setControlChannel",
+                               channelName, value]);
+    }
 
-  setControlChannel(channelName, value) {
-    this.port.postMessage(["setControlChannel",
-      channelName, value]);
-  }
+    /** Sets the value of a string channel in the software bus
+     *
+     * @param {string} channelName A string containing the channel name.
+     * @param {string} stringValue The string to be set.
+     */ 
+    setStringChannel(channelName, value) {
+        this.port.postMessage(["setStringChannel",
+                               channelName, value]);
+    }
 
-  setStringChannel(channelName, value) {
-    this.port.postMessage(["setStringChannel",
-      channelName, value]);
-  }
+    /** Starts processing in this node
+     */
+    start() {
+        this.port.postMessage(["start"]);
+    }
 
-  start() {
-    this.port.postMessage(["start"]);
-  }
+    /** Resets the Csound engine.
+     */
+    reset() {
+        this.port.postMessage(["reset"]);
+    }
 
-  reset() {
-    this.port.postMessage(["reset"]);
-  }
+    destroy() {
+    }
 
-  destroy() {
-  }
+    /** Starts performance, same as start()
+     */
+    play() {
+        this.port.postMessage(["play"]);
+    }
+    
+    /** Stops (pauses) performance
+     */
+    stop() {
+        this.port.postMessage(["stop"]);
+    }
 
-  openAudioOut() {
-  }
+    /** Sets a callback to process Csound console messages.
+     *
+     * @param {function} msgCallback A callback to process messages 
+     * with signature function(message), where message is a string
+     * from Csound.
+     */ 
+    setMessageCallback(msgCallback) {
+        this.msgCallback = msgCallback;
+    }
 
-  closeAudioOut() {
-  }
-
-  play() {
-    this.port.postMessage(["play"]);
-  }
-
-  stop() {
-    this.port.postMessage(["stop"]);
-  }
-
-  setMessageCallback(msgCallback) {
-    this.msgCallback = msgCallback;
-  }
-  
-  midiMessage(byte1, byte2, byte3) {
-    this.port.postMessage(["midiMessage", byte1, byte2, byte3]);
-  }
+    /** Sends a MIDI channel message to Csound
+     *
+     * @param {number} byte1 MIDI status byte
+     * @param {number} byte2 MIDI data byte 1
+     * @param {number} byte1 MIDI data byte 2
+     *
+     */
+    midiMessage(byte1, byte2, byte3) {
+        this.port.postMessage(["midiMessage", byte1, byte2, byte3]);
+    }
 
 }
 
-
-
+/** This E6 class is used to setup scripts and
+    allow the creation of new CsoundNode objects
+*/
 class CsoundNodeFactory {
 
-  /** Use to asynchronously setup AudioWorklet */
-  static importScripts(script_base='./') {
-    let actx = CSOUND_AUDIO_CONTEXT;
-    return new Promise( (resolve) => {
-      actx.audioWorklet.addModule(script_base + 'libcsound-worklet.wasm.js').then(() => {
-        actx.audioWorklet.addModule(script_base + 'libcsound-worklet.js').then(() => {
-          actx.audioWorklet.addModule(script_base + 'CsoundProcessor.js').then(() => {
-            resolve(); 
-          }) }) })      
-    }) 
-  }
+    /** 
+     * This static method is used to asynchronously setup scripts for AudioWorklet Csound
+     *
+     * @param {string} script_base A string containing the base path to scripts
+     */
+    static importScripts(script_base='./') {
+        let actx = CSOUND_AUDIO_CONTEXT;
+        return new Promise( (resolve) => {
+            actx.audioWorklet.addModule(script_base + 'libcsound-worklet.wasm.js').then(() => {
+                actx.audioWorklet.addModule(script_base + 'libcsound-worklet.js').then(() => {
+                    actx.audioWorklet.addModule(script_base + 'CsoundProcessor.js').then(() => {
+                        resolve(); 
+                    }) }) })      
+        }) 
+    }
 
-  static createNode(inputChannelCount=1, outputChannelCount=2) {
-    return new CsoundNode(CSOUND_AUDIO_CONTEXT);
-  }
+    /** 
+     * This static method creates a new CsoundNode. 
+     *  @param {number} inputChannelCount number of input channels
+     *  @param {number} outputChannelCount number of output channels
+     */
+    static createNode(inputChannelCount=1, outputChannelCount=2) {
+        var options = {};
+        options.numberOfInputs  = inputChannelCount;
+        options.numberOfOutputs  = outputChannelCount;
+        return new CsoundNode(CSOUND_AUDIO_CONTEXT, options);
+    }
 }
 
 
