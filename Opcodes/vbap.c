506cd294ab39556d3cc7a1af42ff5cbb893b2e26
veplaini <victor.lazzarini@nuim.ie>
2018-08-06 12:49:53 +0100

    fixes to uninitialised var, freeing of resources and code leftover

--- Opcodes/vbap.c
+++ Opcodes/vbap.c
@@ -581,6 +581,7 @@ static inline int32_t vbap_ls_init_sr (CSOUND *csound, int32_t dim, int32_t coun
     //dim = (int32_t) *p->dim;
     csound->Message(csound, "dim : %d\n",dim);
     if (UNLIKELY(!((dim==2) || (dim == 3)))) {
+      free(lss);
       csound->ErrorMsg(csound,
                        Str("Error in loudspeaker dimension. %d not permitted"),
                        dim);
