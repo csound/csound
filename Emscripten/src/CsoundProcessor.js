155c33511337d19f596677f697e3e44ee2175034
Steven Yi <stevenyi@gmail.com>
2018-10-15 22:14:02 -0400

    simplify WebAudio Csound by using SINGLE_FILE to embed wasm into libcsound.js; embed FileList.js with --pre-js; update AudioWorklet build to use ES6 module for CsoundProcesor.js and libcsound-worklet.js

--- Emscripten/src/CsoundProcessor.js
+++ Emscripten/src/CsoundProcessor.js
@@ -21,54 +21,54 @@
     02110-1301 USA
 */
 
-'use strict';
+import libcsound from './libcsound-worklet.js'
 
-const WAM = AudioWorkletGlobalScope.WAM;
+const CSMOD = {} 
 
-WAM["ENVIRONMENT"] = "WEB";
-WAM["print"] = (t) => console.log(t);
-WAM["printErr"] = (t) => console.log(t);
+CSMOD["ENVIRONMENT"] = "WEB";
+CSMOD["print"] = (t) => console.log(t);
+CSMOD["printErr"] = (t) => console.log(t);
 
 
 // INITIALIAZE WASM
-AudioWorkletGlobalScope.libcsound(WAM);
+libcsound(CSMOD);
 
 // SETUP FS
 
-let FS = WAM["FS"];
+let FS = CSMOD["FS"];
 
 
 
 // Get cwrap-ed functions
 const Csound = {
 
-    new: WAM.cwrap('CsoundObj_new', ['number'], null),
-    compileCSD: WAM.cwrap('CsoundObj_compileCSD', ['number'], ['number', 'string']),
-    evaluateCode: WAM.cwrap('CsoundObj_evaluateCode', ['number'], ['number', 'string']),
-    readScore: WAM.cwrap('CsoundObj_readScore', ['number'], ['number', 'string']),
+    new: CSMOD.cwrap('CsoundObj_new', ['number'], null),
+    compileCSD: CSMOD.cwrap('CsoundObj_compileCSD', ['number'], ['number', 'string']),
+    evaluateCode: CSMOD.cwrap('CsoundObj_evaluateCode', ['number'], ['number', 'string']),
+    readScore: CSMOD.cwrap('CsoundObj_readScore', ['number'], ['number', 'string']),
 
-    reset: WAM.cwrap('CsoundObj_reset', null, ['number']),
-    getOutputBuffer: WAM.cwrap('CsoundObj_getOutputBuffer', ['number'], ['number']),
-    getInputBuffer: WAM.cwrap('CsoundObj_getInputBuffer', ['number'], ['number']),
-    getControlChannel: WAM.cwrap('CsoundObj_getControlChannel', ['number'], ['number', 'string']),
-    setControlChannel: WAM.cwrap('CsoundObj_setControlChannel', null, ['number', 'string', 'number']),
-    setStringChannel: WAM.cwrap('CsoundObj_setStringChannel', null, ['number', 'string', 'string']),
-    getKsmps: WAM.cwrap('CsoundObj_getKsmps', ['number'], ['number']),
-    performKsmps: WAM.cwrap('CsoundObj_performKsmps', ['number'], ['number']),
+    reset: CSMOD.cwrap('CsoundObj_reset', null, ['number']),
+    getOutputBuffer: CSMOD.cwrap('CsoundObj_getOutputBuffer', ['number'], ['number']),
+    getInputBuffer: CSMOD.cwrap('CsoundObj_getInputBuffer', ['number'], ['number']),
+    getControlChannel: CSMOD.cwrap('CsoundObj_getControlChannel', ['number'], ['number', 'string']),
+    setControlChannel: CSMOD.cwrap('CsoundObj_setControlChannel', null, ['number', 'string', 'number']),
+    setStringChannel: CSMOD.cwrap('CsoundObj_setStringChannel', null, ['number', 'string', 'string']),
+    getKsmps: CSMOD.cwrap('CsoundObj_getKsmps', ['number'], ['number']),
+    performKsmps: CSMOD.cwrap('CsoundObj_performKsmps', ['number'], ['number']),
 
-    render: WAM.cwrap('CsoundObj_render', null, ['number']),
-    getInputChannelCount: WAM.cwrap('CsoundObj_getInputChannelCount', ['number'], ['number']),
-    getOutputChannelCount: WAM.cwrap('CsoundObj_getOutputChannelCount', ['number'], ['number']),
-    getTableLength: WAM.cwrap('CsoundObj_getTableLength', ['number'], ['number', 'number']),
-    getTable: WAM.cwrap('CsoundObj_getTable', ['number'], ['number', 'number']),
-    getZerodBFS: WAM.cwrap('CsoundObj_getZerodBFS', ['number'], ['number']),
-    compileOrc: WAM.cwrap('CsoundObj_compileOrc', 'number', ['number', 'string']),
-    setOption: WAM.cwrap('CsoundObj_setOption', null, ['number', 'string']),
-    prepareRT: WAM.cwrap('CsoundObj_prepareRT', null, ['number']),
-    getScoreTime: WAM.cwrap('CsoundObj_getScoreTime', null, ['number']),
-    setTable: WAM.cwrap('CsoundObj_setTable', null, ['number', 'number', 'number', 'number']),
-    pushMidiMessage: WAM.cwrap('CsoundObj_pushMidiMessage', null, ['number', 'number', 'number', 'number']),
-    setMidiCallbacks: WAM.cwrap('CsoundObj_setMidiCallbacks', null, ['number']),
+    render: CSMOD.cwrap('CsoundObj_render', null, ['number']),
+    getInputChannelCount: CSMOD.cwrap('CsoundObj_getInputChannelCount', ['number'], ['number']),
+    getOutputChannelCount: CSMOD.cwrap('CsoundObj_getOutputChannelCount', ['number'], ['number']),
+    getTableLength: CSMOD.cwrap('CsoundObj_getTableLength', ['number'], ['number', 'number']),
+    getTable: CSMOD.cwrap('CsoundObj_getTable', ['number'], ['number', 'number']),
+    getZerodBFS: CSMOD.cwrap('CsoundObj_getZerodBFS', ['number'], ['number']),
+    compileOrc: CSMOD.cwrap('CsoundObj_compileOrc', 'number', ['number', 'string']),
+    setOption: CSMOD.cwrap('CsoundObj_setOption', null, ['number', 'string']),
+    prepareRT: CSMOD.cwrap('CsoundObj_prepareRT', null, ['number']),
+    getScoreTime: CSMOD.cwrap('CsoundObj_getScoreTime', null, ['number']),
+    setTable: CSMOD.cwrap('CsoundObj_setTable', null, ['number', 'number', 'number', 'number']),
+    pushMidiMessage: CSMOD.cwrap('CsoundObj_pushMidiMessage', null, ['number', 'number', 'number', 'number']),
+    setMidiCallbacks: CSMOD.cwrap('CsoundObj_setMidiCallbacks', null, ['number']),
 
 }
 
@@ -81,10 +81,10 @@ class CsoundProcessor extends AudioWorkletProcessor {
     constructor(options) {
         super(options);
         let p = this.port;
-        WAM["print"] = (t) => {
+        CSMOD["print"] = (t) => {
             p.postMessage(["log", t]);
         };
-        WAM["printErr"] = (t) => {
+        CSMOD["printErr"] = (t) => {
             p.postMessage(["log", t]);
         };
 
@@ -177,9 +177,9 @@ class CsoundProcessor extends AudioWorkletProcessor {
             this.cnt = ksmps;
             
             let outputPointer = Csound.getOutputBuffer(csObj);
-            this.csoundOutputBuffer = new Float32Array(WAM.HEAP8.buffer, outputPointer, ksmps * this.nchnls);
+            this.csoundOutputBuffer = new Float32Array(CSMOD.HEAP8.buffer, outputPointer, ksmps * this.nchnls);
             let inputPointer = Csound.getInputBuffer(csObj);
-            this.csoundInputBuffer = new Float32Array(WAM.HEAP8.buffer, inputPointer, ksmps * this.nchnls_i);
+            this.csoundInputBuffer = new Float32Array(CSMOD.HEAP8.buffer, inputPointer, ksmps * this.nchnls_i);
             this.zerodBFS = Csound.getZerodBFS(csObj);
             this.started = true;
         }
@@ -271,7 +271,7 @@ class CsoundProcessor extends AudioWorkletProcessor {
         case "getTable":
             let buffer = Csound.getTable(this.csObj, data[1]);
             let len = Csound.getTableLength(this.csObj, data[1]);
-            let src = new Float32Array(WAM.HEAP8.buffer, buffer, len);
+            let src = new Float32Array(CSMOD.HEAP8.buffer, buffer, len);
             let table = new Float32Array(src);
             p.postMessage(["table", data[1], table]);
             break;
