597688519c82f8ea6d6ffaa7c7ecf2b147711a12
John ffitch <jpff@codemist.co.uk>
2018-11-14 22:16:18 +0000

    textual

--- Engine/csound_orc_compile.c
+++ Engine/csound_orc_compile.c
@@ -690,12 +690,13 @@ INSTRTXT *create_instrument0(CSOUND *csound, TREE *root,
       CS_SPRINTF(s, Str("sr = %.7g, kr = %.7g, ksmps = %.7g\n"),
                  csound->esr, csound->ekr, ensmps);
       if (UNLIKELY(csound->ksmps < 1 || FLOAT_COMPARE(ensmps, csound->ksmps))) {
-         csound->Warning(csound, Str("%s invalid ksmps value, needs to be integral."), s);
-         ensmps = csound->ksmps = floor(ensmps);
-         csound->ekr  = csound->esr/csound->ksmps;
-         csound->Warning(csound, "resetting orc parameters to: "
-                 "sr = %.7g, kr = %.7g, ksmps = %u", csound->esr, csound->ekr, 
-               csound->ksmps);
+        csound->Warning(csound,
+                        Str("%s invalid ksmps value, needs to be integral."), s);
+        ensmps = csound->ksmps = floor(ensmps);
+        csound->ekr  = csound->esr/csound->ksmps;
+        csound->Warning(csound, Str("resetting orc parameters to: "
+                                    "sr = %.7g, kr = %.7g, ksmps = %u"),
+                        csound->esr, csound->ekr, csound->ksmps);
       }
       if (UNLIKELY(csound->esr <= FL(0.0)))
         csoundDie(csound, Str("%s invalid sample rate"), s);
