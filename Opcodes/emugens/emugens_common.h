c965bf90cc12f0081fe4beb956385940ab5c8d4f
Eduardo Moguillansky <eduardo.moguillansky@gmail.com>
2019-03-31 18:28:39 +0200

    added init and perf time tabensure variants

---
+++ Opcodes/emugens/emugens_common.h
@@ -0,0 +1,57 @@
+#ifndef EMUGENS_COMMON_H
+#define EMUGENS_COMMON_H
+
+#include "csdl.h"
+
+
+#define CHECKARR1D(arr)           \
+    if((arr)->dimensions != 1)    \
+        return INITERRF(Str("Array should be of 1D, but has %d dimensions"), \
+                        (arr)->dimensions);
+
+
+// Ensure the existence and size of the array at i-time ONLY if
+// array has not been initialized, so we don't need to check intialization
+// at k-time
+static inline void 
+tabensure_init(CSOUND *csound, ARRAYDAT *p, int size)
+{
+    if (p->data == NULL || p->dimensions == 0) {
+        size_t ss;
+        if (p->data == NULL) {
+            CS_VARIABLE* var = p->arrayType->createVariable(csound, NULL);
+            p->arrayMemberSize = var->memBlockSize;
+        }
+        ss = p->arrayMemberSize*size;
+        if (p->data==NULL) {
+            p->data = (MYFLT*)csound->Calloc(csound, ss);
+            p->allocated = ss;
+        }
+        else if (ss > p->allocated) {
+            p->data = (MYFLT*) csound->ReAlloc(csound, p->data, ss);
+            p->allocated = ss;
+        }
+        if (p->dimensions==0) {
+            p->dimensions = 1;
+            p->sizes = (int32_t*)csound->Malloc(csound, sizeof(int32_t));
+        }
+        p->sizes[0] = size;
+    }
+}
+
+
+// Use this to ensure the size of the array at k-time
+static inline void
+tabensure_perf(CSOUND *csound, ARRAYDAT *p, int size)
+{
+    if(UNLIKELY(p->sizes[0] != size)) {
+        size_t ss = p->arrayMemberSize*size;
+        if (ss > p->allocated) {
+            p->data = (MYFLT*) csound->ReAlloc(csound, p->data, ss);
+            p->allocated = ss;
+        }
+    }
+    p->sizes[0] = size;
+}
+
+#endif
