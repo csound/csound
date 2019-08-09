fc17b041cdab4055a16858a193c1ef8b5287caab
Stephen Kyne <xskyne@gmail.com>
2019-08-09 02:40:04 +0100

    Fixed FindLiblo.cmake and now using this to find liblo instead of manually sourcing header and library

--- cmake/Modules/FindLIBLO.cmake
+++ cmake/Modules/FindLIBLO.cmake
@@ -15,12 +15,10 @@
 # Redistribution and use is allowed according to the terms of the New BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 #
-
-
 if (LIBLO_LIBRARIES AND LIBLO_INCLUDE_DIRS)
   # in cache already
   set(LIBLO_FOUND TRUE)
-else (LIBLO_LIBRARIES AND LIBLO_INCLUDE_DIRS)
+else ()
   find_path(LIBLO_INCLUDE_DIR
     NAMES
       lo/lo.h
@@ -34,6 +32,7 @@ else (LIBLO_LIBRARIES AND LIBLO_INCLUDE_DIRS)
   find_library(LIBLO_LIBRARY
     NAMES
       lo
+      liblo
     PATHS
       /usr/lib
       /usr/local/lib
@@ -44,25 +43,26 @@ else (LIBLO_LIBRARIES AND LIBLO_INCLUDE_DIRS)
   set(LIBLO_INCLUDE_DIRS
     ${LIBLO_INCLUDE_DIR}
   )
+  
   set(LIBLO_LIBRARIES
     ${LIBLO_LIBRARY}
-)
+  )
 
-  if (LIBLO_INCLUDE_DIRS AND LIBLO_LIBRARIES)
+  if(LIBLO_INCLUDE_DIRS AND LIBLO_LIBRARIES)
      set(LIBLO_FOUND TRUE)
-  endif (LIBLO_INCLUDE_DIRS AND LIBLO_LIBRARIES)
+  endif()
 
-  if (LIBLO_FOUND)
-    if (NOT liblo_FIND_QUIETLY)
+  if(LIBLO_FOUND)
+    if(NOT liblo_FIND_QUIETLY)
       message(STATUS "Found liblo: ${LIBLO_LIBRARIES}")
-    endif (NOT liblo_FIND_QUIETLY)
-  else (LIBLO_FOUND)
-    if (liblo_FIND_REQUIRED)
+    endif()
+  else()
+    if(liblo_FIND_REQUIRED)
       message(FATAL_ERROR "Could not find liblo")
-    endif (liblo_FIND_REQUIRED)
-  endif (LIBLO_FOUND)
+    endif()
+    set(LIBLO_FOUND TRUE)
+  endif()
 
   # show the LIBLO_INCLUDE_DIRS and LIBLO_LIBRARIES variables only in the advanced view
   mark_as_advanced(LIBLO_INCLUDE_DIRS LIBLO_LIBRARIES)
-
-endif (LIBLO_LIBRARIES AND LIBLO_INCLUDE_DIRS)
+endif()
