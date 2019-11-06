d897c867fccceba4e62c67ebfdc64e1185f3c36e
hlolli <hlolli@gmail.com>
2019-11-06 03:21:34 +0100

    add the possibility to specify gcc compiler for the hashes when gcc version matters

--- tests/soak/opcodes/gen01/gen01.csd
+++ tests/soak/opcodes/gen01/gen01.csd
@@ -1,41 +1,33 @@
-<CsoundSynthesizer> 
-<CsInstruments> 
+<CsoundSynthesizer>
+<CsInstruments>
 
-sr = 44100 
-ksmps = 32 
-nchnls = 2 
-0dbfs  = 1 
+sr = 44100
+ksmps = 32
+nchnls = 2
+0dbfs  = 1
 
-instr 1	;plays deferred and non-deferred sounds with loscil
-
-ifn = p4
-ibas = 1
-
-asig loscil 1, 1, ifn, ibas
-     outs asig, asig
-    
+instr 1
+  ifn = p4
+  ibas = 1
+  asig loscil 1, 1, ifn, ibas
+  outs asig, asig
 endin
 
-instr 2	;plays only non-deffered sound            
-
-isnd  = p4
-aread line   sr*p3, p3, 0				;play this backward
-asig  tablei aread, isnd				;use table 1
-      outs   asig, asig
-        
+instr 2
+  isnd  = p4
+  aread line   sr*p3, p3, 0
+  asig  tablei aread, isnd
+  outs   asig, asig
 endin
+
 </CsInstruments>
 <CsScore>
-f 1 0 131072 1 "beats.wav" 0 0 0			;non-deferred sound
-f 2 0    0   1 "flute.aiff" 0 0 0			;& deferred sounds in 
-f 3 0    0   1 "beats.ogg" 0 0 0			;different formats					
+f 1 0 131072 1 "beats.wav" 0 0 0
+f 2 0    0   1 "flute.aiff" 0 0 0
 
 i 1 0 1 1
 i 1 + 1 2
-i 1 + 1 3
-
-i 2 4 2 1	;non-deffered sound for instr. 2
+i 2 4 2 1
 e
 </CsScore>
 </CsoundSynthesizer>
-
--- tests/soak/test_checksum.py
+++ tests/soak/test_checksum.py
@@ -7,6 +7,7 @@ import shutil
 import os
 import os.path
 import warnings
+import platform
 from glob import glob
 from parameterized import parameterized
 from hashlib import sha256
@@ -21,9 +22,12 @@ test_data = []
 
 for test_json_path in opcodes_test_data:
     with open(test_json_path) as handle:
-        data = json.loads(handle.read())
-        data["path"] = test_json_path
-        test_data.append(data)
+        try:
+            data = json.loads(handle.read())
+            data["path"] = test_json_path
+            test_data.append(data)
+        except:
+            warnings.warn("JSON-PARSER ERROR: " + test_json_path)
 
 cli_options = ["-d", "-W", "-m0", "-b256", "-B512"]
 
@@ -55,9 +59,32 @@ def custom_name_func(testcase_func, index, params):
         ),
     )
 
+operating_system = platform.system()
+
+c_compiler = os.getenv("CC")
+
+if operating_system == "Linux":
+    if c_compiler == "gcc":
+        gcc_version = subprocess.run(
+            ["gcc", "-dumpversion"],
+            encoding="utf-8",
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            check=True,
+            env=env
+        )
+        gcc_major_version = gcc_version.stdout[:1]
+        c_compiler += gcc_major_version
+
 class SoakOpcodes(unittest.TestCase):
     @parameterized.expand(test_data_parameterized , name_func=custom_name_func)
     def test(self, path, expected):
+        # for cases where c compilers version may differ
+        if type(expected) is dict:
+            if c_compiler in expected:
+                expected = expected[c_compiler]
+            else:
+                expected = list(expected.values())
         os.makedirs("tmp", exist_ok=True)
         name = os.path.basename(path).replace(".csd.json", "")
         csd = path.replace(".json", "")
@@ -69,7 +96,6 @@ class SoakOpcodes(unittest.TestCase):
             stderr=subprocess.PIPE,
             check=True,
             env=env
-            # , stdout=subprocess.DEVNULL, stderr=stderr
         )
         output = str(proc.stdout) + str(proc.stderr)
         if "unexpected T_IDENT" in output:
@@ -80,6 +106,9 @@ class SoakOpcodes(unittest.TestCase):
         hasher = sha256()
         hasher.update(binary_data)
         actual_hashsum = hasher.hexdigest()
-        self.assertEqual(actual_hashsum, expected)
+        if type(expected) is list:
+            self.assertIn(actual_hashsum, expected)
+        else:
+            self.assertEqual(actual_hashsum, expected)
         # Make sure that audio was produced (dev)
         # self.assertFalse("overall amps:  0.00000" in output)
