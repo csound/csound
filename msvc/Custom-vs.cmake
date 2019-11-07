b87239f1e8ac74c7fd7152f35624c4f408b53444
Stephen Kyne <xskyne@gmail.com>
2019-11-07 02:28:31 +0100

    Removing remenants of manual builds. Everything is now in vcpkg or chocolatey (bar GMM)

--- msvc/Custom-vs.cmake
+++ msvc/Custom-vs.cmake
@@ -71,8 +71,6 @@ set(HAVE_BIG_ENDIAN 0)
 set(CMAKE_VERBOSE_MAKEFILE ON)
 set(CMAKE_16BIT_TYPE "unsigned short")
 set(FAIL_MISSING OFF) # Enable when packaging
-list(APPEND CMAKE_PREFIX_PATH "${PROJECT_SOURCE_DIR}\\msvc\\deps;${PROJECT_SOURCE_DIR}\\msvc\\deps\\fluidsynthdeps")
-list(APPEND CMAKE_REQUIRED_INCLUDES "${PROJECT_SOURCE_DIR}\\msvc\\deps\\include")
 
 # Disable the following warnings in msvc
 # - C4244 loss of data in conversion
--- msvc/downloadDependencies.ps1
+++ msvc/downloadDependencies.ps1
@@ -28,21 +28,11 @@ echo "vsGenerator: $vsGenerator"
 echo "Build type: $targetTriplet"
 
 $startTime = (Get-Date).TimeOfDay
-$webclient = New-Object System.Net.WebClient
+# $webclient = New-Object System.Net.WebClient
 $currentDir = Split-Path $MyInvocation.MyCommand.Path
 $cacheDir = $currentDir + "\cache\"
-$depsDir = $currentDir + "\deps\"
-$stageDir = $currentDir + "\staging\"
-$depsBinDir = $depsDir + "bin\"
-$depsLibDir = $depsDir + "lib\"
-$depsIncDir = $depsDir + "include\"
 $csoundDir = $currentDir + "\.."
 $vcpkgDir = ""
-
-# Metrics
-$vcpkgTiming = 0
-$buildTiming = 0
-$cmakeTiming = 0
 
 # Find VCPKG from path if it already exists
 # Otherwise use the local Csound version that will be installed
@@ -114,18 +104,9 @@ if (-not (Test-Path $vcpkgDir/buildtrees/portaudio/src/asiosdk)) {
     vcpkg --triplet $targetTriplet remove portaudio --overlay-triplets=.
 }
 
-vcpkg --triplet $targetTriplet install eigen3 fltk zlib libflac libogg libvorbis libsndfile libsamplerate portmidi portaudio liblo hdf5 dirent libstk --overlay-triplets=.
-
-$vcpkgTiming = (Get-Date).TimeOfDay
-
-# Comment for testing to avoid extracting if already done so
-rm -Path deps -Force -Recurse -ErrorAction SilentlyContinue
-mkdir cache -ErrorAction SilentlyContinue
-mkdir deps -ErrorAction SilentlyContinue
-mkdir $depsLibDir -ErrorAction SilentlyContinue
-mkdir $depsBinDir -ErrorAction SilentlyContinue
-mkdir $depsIncDir -ErrorAction SilentlyContinue
-mkdir staging -ErrorAction SilentlyContinue
+vcpkg --triplet $targetTriplet install `
+    eigen3 fltk zlib libflac libogg libvorbis libsndfile libsamplerate portmidi portaudio liblo hdf5 dirent libstk fluidsynth `
+    --overlay-triplets=.
 
 echo "Downloading and installing non-VCPKG packages..."
 
@@ -135,108 +116,9 @@ choco upgrade swig -y
 choco install winflexbison -y
 choco upgrade winflexbison -y
 
-# List of URIs to download and install
-$uriList=
-"http://ftp.acc.umu.se/pub/gnome/binaries/win64/dependencies/gettext-runtime_0.18.1.1-2_win64.zip",
-"http://ftp.acc.umu.se/pub/gnome/binaries/win64/dependencies/pkg-config_0.23-2_win64.zip",
-"http://ftp.acc.umu.se/pub/gnome/binaries/win64/dependencies/proxy-libintl-dev_20100902_win64.zip",
-"http://ftp.acc.umu.se/pub/gnome/binaries/win64/glib/2.26/glib-dev_2.26.1-1_win64.zip",
-"http://ftp.acc.umu.se/pub/gnome/binaries/win64/glib/2.26/glib_2.26.1-1_win64.zip",
-"http://download-mirror.savannah.gnu.org/releases/getfem/stable/gmm-5.1.tar.gz"
-
-# Appends this folder location to the 'deps' uri
-$destList=
-"fluidsynthdeps",
-"fluidsynthdeps",
-"fluidsynthdeps",
-"fluidsynthdeps",
-"fluidsynthdeps",
-""
-
-# Download list of files to cache folder
-for($i=0; $i -lt $uriList.Length; $i++)
-{
-    $fileName = Split-Path -Leaf $uriList[$i]
-    $cachedFile = $cacheDir + $fileName
-    if (Test-Path $cachedFile -PathType Leaf)
-    {
-        echo "Already downloaded file: $fileName"
-    }
-    else
-    {
-      echo "Downloading: " $uriList[$i]
-      $webclient.DownloadFile($uriList[$i], $cachedFile)
-    }
-}
-
-# Extract libs to deps directory
-for($i=0; $i -lt $uriList.Length; $i++)
-{
-    $fileName = Split-Path -Leaf $uriList[$i]
-    $cachedFile = $cacheDir + $fileName
-    $destDir = $depsDir + $destList[$i]
-    if ($PSVersionTable.PSVersion.Major -gt 3)
-    {
-        Expand-Archive $cachedFile -DestinationPath $destDir -Force
-    }
-    else
-    {
-        New-Item $destDir -ItemType directory -Force
-        Expand-Archive $cachedFile -OutputPath $destDir -Force
-    }
-    echo "Extracted $fileName to $destDir"
-}
-
-# GMM
-cd $cacheDir
-7z e -y "gmm-5.1.tar.gz"
-7z x -y "gmm-5.1.tar"
-cd ..
-copy ($cacheDir + "gmm-5.1\include\gmm\") -Destination ($depsIncDir + "gmm\") -Force -Recurse
-echo "Copied v5.1 gmm headers to deps include directory. Please note, verson 5.1 is REQUIRED, "
-echo "later versions do not function as stand-alone, header-file-only libraries."
-
-echo "FluidSynth..."
-cd $stageDir
-
-if (Test-Path "fluidsynth")
-{
-    cd fluidsynth
-    git pull
-    cd ..
-    echo "Fluidsynth already downloaded, updated"
-}
-else
-{
-    git clone --depth=1 -b v1.1.10 "https://github.com/FluidSynth/fluidsynth.git"
-}
-
-rm -Path fluidsynthbuild -Force -Recurse -ErrorAction SilentlyContinue
-mkdir fluidsynthbuild -ErrorAction SilentlyContinue
-cd fluidsynthbuild
-
-cmake ..\fluidsynth -G $vsGenerator `
-    -DCMAKE_PREFIX_PATH="$depsDir\fluidsynthdeps" `
-    -DCMAKE_INCLUDE_PATH="$depsDir\fluidsynthdeps\include\glib-2.0;$depsDir\fluidsynthdeps\lib\glib-2.0\include"
-
-cmake --build . --config Release
-copy .\src\Release\fluidsynth.exe -Destination $depsBinDir -Force
-copy .\src\Release\fluidsynth.lib -Destination $depsLibDir -Force
-copy .\src\Release\libfluidsynth-1.dll -Destination $depsBinDir -Force
-copy .\include\fluidsynth.h -Destination $depsIncDir -Force
-robocopy ..\fluidsynth\include\fluidsynth $depsIncDir\fluidsynth *.h /s /NJH /NJS
-copy .\include\fluidsynth\version.h -Destination $depsIncDir\fluidsynth\version.h -Force
-
-$buildTiming = (Get-Date).TimeOfDay
 $endTime = (Get-Date).TimeOfDay
-$buildTiming = $buildTiming - $vcpkgTiming
-$vcpkgTiming = $vcpkgTiming - $startTime
 $duration = $endTime - $startTime
 
-echo "Removed unnecessary files from dependency bin directory"
-echo " "
-echo "VCPKG duration: $($vcpkgTiming.TotalMinutes) minutes"
-echo "Build duration: $($buildTiming.TotalMinutes) minutes"
 echo "Total duration: $($duration.TotalMinutes) minutes"
 echo "-------------------------------------------------"
 echo "Generating Csound Visual Studio solution..."
