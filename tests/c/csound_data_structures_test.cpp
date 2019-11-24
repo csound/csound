e6ec65487b1dce0625e6b093d114e39eb3adb2ee
Stephen Kyne <xskyne@gmail.com>
2019-11-25 00:53:39 +0100

    Fix for gtest include

--- tests/c/csound_data_structures_test.cpp
+++ tests/c/csound_data_structures_test.cpp
@@ -10,7 +10,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "csoundCore.h"
-#include "gtest\gtest.h"
+#include "gtest/gtest.h"
 
 class CsoundDataStructuresTests : public ::testing::Test {
 public:
