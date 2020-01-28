b8e58adcdf2fd769f1b46b58ca606cbd7c17ff3c
Steven Yi <stevenyi@gmail.com>
2020-01-28 18:25:29 -0500

    added static links for libflac, libogg, and libvorbis for VCPKG and windows

--- msvc/x64-windows-csound.cmake
+++ msvc/x64-windows-csound.cmake
@@ -1,7 +1,7 @@
 set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CRT_LINKAGE dynamic)
 
-if(PORT STREQUAL "libsndfile")
+if(${PORT} MATCHES "libflac|libogg|libvorbis|libsndfile" )
 	set(VCPKG_LIBRARY_LINKAGE static)
 else()
 	set(VCPKG_LIBRARY_LINKAGE dynamic)
