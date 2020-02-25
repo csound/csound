e21e69357440a2ee54f8407829a149cf7f9bc112
Olga Ustuzhanina <me@laserbat.pw>
2020-02-25 20:06:49 +0000

    Deleted old and unused files

--- installer/misc/makespec.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-rm -f "csound.spec"
-cd ..
-TOPDIR=`pwd`
-cd -
-cat "installer/misc/csound.spec.in" | sed "s#@TOPDIR@#${TOPDIR}#g" > "csound.spec"
-find "../__csound6" \! -type d -print | sort | sed 's/^\.\.\/__csound6//' >> "csound.spec"
-
