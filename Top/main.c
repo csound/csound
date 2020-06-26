2197ab56cfe3ee9e934d2a17821266a05479ad58
Eduardo Moguillansky <eduardo.moguillansky@gmail.com>
2020-06-26 06:00:07 +0200

    Let syntax-check-only return an error if syntax failed

--- Top/main.c
+++ Top/main.c
@@ -164,6 +164,7 @@ PUBLIC int csoundCompileArgs(CSOUND *csound, int argc, const char **argv)
     volatile int     csdFound = 0;
     volatile int ac = argc;
     char    *fileDir;
+    int     compiledOk = 0;
 
 
     if ((n = setjmp(csound->exitjmp)) != 0) {
@@ -322,6 +323,9 @@ PUBLIC int csoundCompileArgs(CSOUND *csound, int argc, const char **argv)
                                       "Csound will start with no instruments"));
        }
     }
+    else {
+      compiledOk = 1;
+    }
     csound->modules_loaded = 1;
 
     s = csoundQueryGlobalVariable(csound, "_RTMIDI");
@@ -384,7 +388,10 @@ PUBLIC int csoundCompileArgs(CSOUND *csound, int argc, const char **argv)
     print_benchmark_info(csound, Str("end of score sort"));
     if (O->syntaxCheckOnly) {
       csound->Message(csound, Str("Syntax check completed.\n"));
-      return CSOUND_EXITJMP_SUCCESS;
+      // return CSOUND_EXITJMP_SUCCESS;
+      if (compiledOk)
+          return CSOUND_EXITJMP_SUCCESS;
+      return CSOUND_ERROR;
     }
     return CSOUND_SUCCESS;
 }
