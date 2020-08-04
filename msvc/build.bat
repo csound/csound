b0c296a7409e59ffd2ad755df87670e85311f7d2
Steven Yi <stevenyi@gmail.com>
2020-08-03 20:19:22 -0400

    build Release by default to match Appveyor build

--- msvc/build.bat
+++ msvc/build.bat
@@ -1,4 +1,4 @@
 @echo off
 powershell -ExecutionPolicy ByPass -File downloadDependencies.ps1 -vsGenerator "Visual Studio 16 2019"
 powershell -ExecutionPolicy ByPass -File generateProject.ps1 -vsGenerator "Visual Studio 16 2019"
-cmake --build csound-vs --config RelWithDebInfo
+cmake --build csound-vs --config Release
