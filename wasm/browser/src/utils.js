d63c9c421d52335f087f9f6f9e2aa900bc578ffa
Steven Yi <stevenyi@gmail.com>
2020-12-21 22:17:41 -0500

    fix issue with memory growth after calling allocStringMem by moving when buffer is referenced

--- wasm/browser/src/utils.js
+++ wasm/browser/src/utils.js
@@ -36,9 +36,9 @@ export const string2ptr = (wasm, string) => {
     return;
   }
 
-  const { buffer } = wasm.exports.memory;
   const stringBuf = encoder.encode(string);
   const offset = wasm.exports.allocStringMem(stringBuf.length);
+  const { buffer } = wasm.exports.memory;
   const outBuf = new Uint8Array(buffer, offset, stringBuf.length + 1);
   outBuf.set(stringBuf);
   return offset;
