6ba9e7c6074bf8de1d98830679a899abf1d1d1ef
Steven Yi <stevenyi@gmail.com>
2021-03-09 22:49:02 -0500

    fix worklet detection logic

--- wasm/browser/src/index.js
+++ wasm/browser/src/index.js
@@ -98,7 +98,7 @@ export async function Csound({
   let audioWorker;
   let csoundWasmApi;
 
-  if (!useSPN || workletSupport) {
+  if (!useSPN && workletSupport) {
     audioWorker = new AudioWorkletMainThread({ audioContext, audioContextIsProvided, autoConnect });
   } else if (spnSupport) {
     audioWorker = new ScriptProcessorNodeMainThread({
