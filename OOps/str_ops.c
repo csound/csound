dd9565e1b68bb7407d5c19a503f625010b81f3b0
Victor Lazzarini <victor.lazzarini@mu.ie>
2022-02-17 09:17:24 +0000

    removed tracing

--- OOps/str_ops.c
+++ OOps/str_ops.c
@@ -184,7 +184,7 @@ int32_t strassign_k(CSOUND *csound, STRCPY_OP *p) {
   if(p->str->timestamp == csound->GetKcounter(csound)) {
   CS_TYPE *strType = csound->GetTypeForArg(p->str);    
   strType->copyValue(csound, strType, p->r, p->str);
-  printf("copy\n");
+  //printf("copy\n")
   }
   return  OK;
 }
