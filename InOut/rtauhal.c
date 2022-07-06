5b26278a48917a7d851a0e54a5e82d30fa1ce214
Victor Lazzarini <victor.lazzarini@mu.ie>
2022-07-06 13:04:26 +0100

    fixing code

--- InOut/rtauhal.c
+++ InOut/rtauhal.c
@@ -723,14 +723,14 @@ static int rtrecord_(CSOUND *csound, MYFLT *inbuff_, int nbytes)
 {
     csdata  *cdata;
     int n = nbytes/sizeof(MYFLT);
-    int m = 0, l, w = n;
-    MYFLT sr = csound->GetSr(csound);
+    int m = 0, l;//, w = n;
+    //MYFLT sr = csound->GetSr(csound);
     cdata = (csdata *) *(csound->GetRtRecordUserData(csound));
     do{
       l = csound->ReadCircularBuffer(csound,cdata->incb,&inbuff_[m],n);
       m += l;
       n -= l;
-      if(n) usleep(MICROS*w/sr);
+      //if(n) usleep(MICROS*w/sr);
     } while(n);
     return nbytes;
 }
@@ -779,14 +779,14 @@ static void rtplay_(CSOUND *csound, const MYFLT *outbuff_, int nbytes)
 {
     csdata  *cdata;
     int n = nbytes/sizeof(MYFLT);
-    int m = 0, l, w = n;
-    MYFLT sr = csound->GetSr(csound);
+    int m = 0, l;//, w = n;
+    //MYFLT sr = csound->GetSr(csound);
     cdata = (csdata *) *(csound->GetRtPlayUserData(csound));
     do {
       l = csound->WriteCircularBuffer(csound, cdata->outcb,&outbuff_[m],n);
       m += l;
       n -= l;
-      if(n) usleep(MICROS*w/sr);
+      //if(n) usleep(MICROS*n/sr);
     } while(n);
 }
 
