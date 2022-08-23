ed56e6d47d929e6a7c750e83cdadd33c287af1fc
gesellkammer <eduardo.moguillansky@gmail.com>
2022-08-23 17:31:45 +0200

    reverted changes in timeinstk and timeinsts; fixed order of .k and .i in times and timek; provided two sets of opcodes elapsedcycles, elapsedtime and eventcycles and eventtime which act like timek, times, timeinstk and timeinsts but return the correct values instead of being one cycle late

--- OOps/ugrw1.c
+++ OOps/ugrw1.c
@@ -83,6 +83,24 @@ int32_t timesr(CSOUND *csound, RDTIME *p)
     return OK;
 }
 
+int32_t elapsedcycles(CSOUND *csound, RDTIME *p)
+{
+    IGN(csound);
+    /* Read the global variable kcounter and turn it into a float.   */
+    *p->rslt = (MYFLT) CS_KCNT - 1;
+    return OK;
+}
+
+/* timesr() */
+int32_t elapsedtime(CSOUND *csound, RDTIME *p)
+{
+    /* Read the global variable kcounter divide it by the k rate.    */
+    IGN(csound);
+    *p->rslt = (MYFLT) (CS_KCNT - 1) * CS_ONEDKR;
+    return OK;
+}
+
+
 /*-----------------------------------*/
 
 /* Subroutines to read time for this instance of the instrument. */
@@ -107,6 +125,19 @@ int32_t instimset(CSOUND *csound, RDTIME *p)
 int32_t instimek(CSOUND *csound, RDTIME *p)
 {
     IGN(csound);
+    *p->rslt = (MYFLT) (CS_KCNT - p->instartk);
+    return OK;
+}
+
+
+/* eventcycles()
+ *
+ * Read difference between the global variable kcounter and the starting
+ * time of this instance. Return it as a float.
+ */
+int32_t eventcycles(CSOUND *csound, RDTIME *p)
+{
+    IGN(csound);
     *p->rslt = (MYFLT) (CS_KCNT - p->instartk - 1);
     return OK;
 }
@@ -117,6 +148,18 @@ int32_t instimek(CSOUND *csound, RDTIME *p)
  * time of this instance.  Return it as a float in seconds.
  */
 int32_t instimes(CSOUND *csound, RDTIME *p)
+{
+    IGN(csound);
+    *p->rslt = (MYFLT) (CS_KCNT - p->instartk) * CS_ONEDKR;
+    return OK;
+}
+
+/* eventtime()
+ *
+ * Read difference between the global variable kcounter and the starting
+ * time of this instance.  Return it as a float in seconds.
+ */
+int32_t eventtime(CSOUND *csound, RDTIME *p)
 {
     IGN(csound);
     *p->rslt = (MYFLT) (CS_KCNT - p->instartk - 1) * CS_ONEDKR;
