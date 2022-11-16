663c1b15f7098c4ce38e10ea44c72ed3102a0277
vlazzarini <victor.lazzarini@mu.ie>
2022-11-16 08:26:55 +0000

    added missing initialisation for OPARMS

--- Top/csound.c
+++ Top/csound.c
@@ -956,7 +956,8 @@ static const CSOUND cenviron_ = {
       0,             /*    fft_lib */
       0,             /* echo */
       0.0,           /* limiter */
-      DFLT_SR, DFLT_KR  /* defaults */
+      DFLT_SR, DFLT_KR,  /* defaults */
+      0  /* mp3 mode */
     },
     {0, 0, {0}}, /* REMOT_BUF */
     NULL,           /* remoteGlobals        */
