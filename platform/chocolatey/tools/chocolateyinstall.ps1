c02b5180a50d645892a9959fa9ba6efadab00bd3
Stephen Kyne <xskyne@gmail.com>
2022-12-07 00:02:42 +0100

    Add Chocolatey package (#1640)
    * Added chocolatey package files
    
    * Update chocolatey package

---
+++ platform/chocolatey/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Join-Path $toolsDir 'csound_installer.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  file          = $fileLocation
+
+  softwareName  = 'csound*'
+
+  validExitCodes= @(0)
+  silentArgs = '/sp- /silent /norestart'
+}
+
+Install-ChocolateyInstallPackage @packageArgs
