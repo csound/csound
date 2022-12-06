14ab9189e60f99eb8d1c35bf99659ca6bdfb4fae
Stephen Kyne <xskyne@gmail.com>
2022-12-06 23:44:32 +0100

    Update chocolatey package

--- platform/chocolatey/tools/chocolateyinstall.ps1
+++ platform/chocolatey/tools/chocolateyinstall.ps1
@@ -10,9 +10,6 @@ $packageArgs = @{
 
   softwareName  = 'csound*'
 
-  checksum64     = '68ff3629a20fe91385c6bb8660eb95f8df5ecb6619d1d0becfa6bf4c9748435b'
-  checksumType64 = 'sha256'
-
   validExitCodes= @(0)
   silentArgs = '/sp- /silent /norestart'
 }
