91b12d0154374e35909864a89fc20285e6d8263b
Andy Fillebrown <andy@docEdub.com>
2023-04-26 23:25:54 -0400

    Move verb to front of hash table function names

--- include/csoundCore.h
+++ include/csoundCore.h
@@ -1425,13 +1425,13 @@ typedef struct _message_queue_t_ {
     MYFLT (*LPrms)(CSOUND *, void *);
     void *(*CreateThread2)(uintptr_t (*threadRoutine)(void *), unsigned int, void *userdata);
     CS_HASH_TABLE *(*CreateHashTable)(CSOUND *);
-    void *(*HashTableGet)(CSOUND *, CS_HASH_TABLE *, char *);
-    void (*HashTablePut)(CSOUND *, CS_HASH_TABLE *, char *, void *);
-    void (*HashTableRemove)(CSOUND *, CS_HASH_TABLE *, char *);
+    void *(*GetHashTableValue)(CSOUND *, CS_HASH_TABLE *, char *);
+    void (*SetHashTableValue)(CSOUND *, CS_HASH_TABLE *, char *, void *);
+    void (*RemoveHashTableKey)(CSOUND *, CS_HASH_TABLE *, char *);
     void (*DestroyHashTable)(CSOUND *, CS_HASH_TABLE *);
-    char *(*HashTableGetKey)(CSOUND *, CS_HASH_TABLE *, char *);
-    CONS_CELL *(*HashTableGetKeys)(CSOUND *, CS_HASH_TABLE *);
-    CONS_CELL *(*HashTableGetValues)(CSOUND *, CS_HASH_TABLE *);
+    char *(*GetHashTableKey)(CSOUND *, CS_HASH_TABLE *, char *);
+    CONS_CELL *(*GetHashTableKeys)(CSOUND *, CS_HASH_TABLE *);
+    CONS_CELL *(*GetHashTableValues)(CSOUND *, CS_HASH_TABLE *);
     /**@}*/
     /** @name Placeholders
         To allow the API to grow while maintining backward binary compatibility. */
