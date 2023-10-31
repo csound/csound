faf662964fe602db6e5c8ad9849bae80b5611a6a
vlazzarini <victor.lazzarini@mu.ie>
2023-10-31 23:00:59 +0000

    added option for plugin opcodes

--- Opcodes/sockrecv.c
+++ Opcodes/sockrecv.c
@@ -19,7 +19,7 @@
   License along with Csound; if not, write to the Free Software
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
   02110-1301 USA
-*/
+*/ 
 
 #ifdef  HAVE_SOCKETS 
 /* Haiku 'int32' etc definitions in net headers conflict with sysdep.h */
