446419c360a3d17fbcf876fbecd8e3793504b537
vlazzarini <victor.lazzarini@mu.ie>
2024-01-04 00:13:49 +0000

    android build restored with latest NDK

--- Android/CsoundAndroid/jni/Android.mk
+++ Android/CsoundAndroid/jni/Android.mk
@@ -8,20 +8,20 @@ LOCAL_MODULE   := csoundandroid
 LOCAL_C_INCLUDES := $(LIBSNDFILE_SRC_DIR) $(HOME)/include $(LOCAL_PATH)/../../../H $(LOCAL_PATH)/../../../include $(LOCAL_PATH)/../../../ $(LIBSNDFILE_SRC_DIR) $(LOCAL_PATH)/../../../Engine $(LOCAL_PATH)/../../../interfaces
 
 ifeq ($(NDK_TOOLCHAIN_VERSION),clang)
-LOCAL_CFLAGS := -std=c99 -O3 -DENABLE_OPCODEDIR_WARNINGS -D__BUILDING_LIBCSOUND -DENABLE_NEW_PARSER -DLINUX -DHAVE_DIRENT_H -DHAVE_FCNTL_H -DHAVE_UNISTD_H -DHAVE_STDINT_H -DHAVE_SYS_TIME_H -DHAVE_SYS_TYPES_H -DHAVE_TERMIOS_H -DHAVE_STRTOK_R -DHAVE_PTHREAD -DHAVE_ATOMIC_BUILTIN -mllvm -unroll-allow-partial -mllvm -unroll-runtime -funsafe-math-optimizations -ffast-math
+LOCAL_CFLAGS := -std=c99 -O3 -DENABLE_OPCODEDIR_WARNINGS -D__BUILDING_LIBCSOUND -DENABLE_NEW_PARSER -DLINUX -DHAVE_DIRENT_H -DHAVE_FCNTL_H -DHAVE_UNISTD_H -DHAVE_STDINT_H -DHAVE_SYS_TIME_H -DHAVE_SYS_TYPES_H -DHAVE_TERMIOS_H -DHAVE_STRTOK_R -DHAVE_PTHREAD -DHAVE_ATOMIC_BUILTIN -mllvm -unroll-allow-partial -mllvm -unroll-runtime -funsafe-math-optimizations -ffast-math -DPARCS
 else
-LOCAL_CFLAGS := -std=c99 -O3 -DENABLE_OPCODEDIR_WARNINGS -D__BUILDING_LIBCSOUND -DENABLE_NEW_PARSER -DLINUX -DHAVE_DIRENT_H -DHAVE_FCNTL_H -DHAVE_UNISTD_H -DHAVE_STDINT_H -DHAVE_SYS_TIME_H -DHAVE_SYS_TYPES_H -DHAVE_TERMIOS_H -DHAVE_STRTOK_R -DHAVE_PTHREAD -DHAVE_ATOMIC_BUILTIN -unroll-allow-partial -unroll-runtime -funsafe-math-optimizations -ffast-math -DPFFFT_SIMD_DISABLE
+LOCAL_CFLAGS := -std=c99 -O3 -DENABLE_OPCODEDIR_WARNINGS -D__BUILDING_LIBCSOUND -DENABLE_NEW_PARSER -DLINUX -DHAVE_DIRENT_H -DHAVE_FCNTL_H -DHAVE_UNISTD_H -DHAVE_STDINT_H -DHAVE_SYS_TIME_H -DHAVE_SYS_TYPES_H -DHAVE_TERMIOS_H -DHAVE_STRTOK_R -DHAVE_PTHREAD -DHAVE_ATOMIC_BUILTIN -unroll-allow-partial -unroll-runtime -funsafe-math-optimizations -ffast-math -DPFFFT_SIMD_DISABLE -DPARCS
 endif
 
 LOCAL_CPPFLAGS += -std=c++11 -pthread -frtti -fexceptions
 LOCAL_LDFLAGS += -Wl,--export-dynamic -L$(LIBSNDFILE_SRC_DIR)
 
-ifeq ($(TARGET_ARCH_ABI),$(filter $(TARGET_ARCH_ABI), armeabi-v7a arm64-v8a x86))
-LOCAL_ARM_NEON  := true
-LOCAL_CFLAGS += -DHAVE_NEON
+ifeq ($(TARGET_ARCH_ABI),$(filter $(TARGET_ARCH_ABI), armeabi-v7a arm64-v8a))
+LOCAL_ARM_NEON  := true 
+LOCAL_CFLAGS += -DHAVE_NEON -mfloat-abi=softfp
 endif # TARGET_ARCH_ABI == armeabi-v7a |arm64-v8a | x86
 
-ifeq ($(TARGET_ARCH_ABI),$(filter $(TARGET_ARCH_ABI), armeabi))
+ifeq ($(TARGET_ARCH_ABI),$(filter $(TARGET_ARCH_ABI), armeabi x86))
 LOCAL_CFLAGS += -DPFFFT_SIMD_DISABLE
 endif # TARGET_ARCH_ABI == armeabi
 ###
@@ -258,9 +258,6 @@ $(CSOUND_SRC_ROOT)/Engine/csound_orc_optimize.c \
 $(CSOUND_SRC_ROOT)/Engine/csound_orc_compile.c \
 $(CSOUND_SRC_ROOT)/Engine/new_orc_parser.c \
 $(CSOUND_SRC_ROOT)/Engine/symbtab.c \
-$(CSOUND_SRC_ROOT)/Engine/cs_new_dispatch.c \
-$(CSOUND_SRC_ROOT)/Engine/cs_par_base.c \
-$(CSOUND_SRC_ROOT)/Engine/cs_par_orc_semantic_analysis.c \
 $(CSOUND_SRC_ROOT)/Opcodes/mp3in.c \
 $(CSOUND_SRC_ROOT)/InOut/libmpadec/layer1.c \
 $(CSOUND_SRC_ROOT)/InOut/libmpadec/layer2.c \
@@ -278,7 +275,10 @@ AndroidCsound.cpp \
 $(CSOUND_SRC_ROOT)/Top/csPerfThread.cpp \
 $(CSOUND_SRC_ROOT)/Java/cs_glue.cpp \
 java_interfaceJAVA_wrap.cpp \
-$(CSOUND_SRC_ROOT)/Opcodes/paulstretch.c
+$(CSOUND_SRC_ROOT)/Opcodes/paulstretch.c \
+$(CSOUND_SRC_ROOT)/Engine/cs_new_dispatch.c \
+$(CSOUND_SRC_ROOT)/Engine/cs_par_base.c \
+$(CSOUND_SRC_ROOT)/Engine/cs_par_orc_semantic_analysis.c \
 
 #CsoundObj.cpp
 
