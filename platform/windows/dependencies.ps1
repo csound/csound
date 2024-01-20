f1bbbb5feb76c2f28c6edd6281bc4cd5bcbf3de0
werner mendizabal <nonameentername@gmail.com>
2024-01-19 19:06:37 -0600

    Update dependencies script

--- platform/windows/dependencies.ps1
+++ platform/windows/dependencies.ps1
@@ -1,6 +1,6 @@
 Write-Host "Install csound dependencies"
 
-choco install -y winflexbison3 innosetup llvm javaruntime jdk8 swig python
+choco install -y winflexbison3 innosetup javaruntime jdk8 swig python
 choco install -y cmake --installargs '"ADD_CMAKE_TO_PATH=System"'
 choco install -y visualstudio2022community --package-parameters "add Microsoft.VisualStudio.Workload.NativeDesktop --includeRecommended --includeOptional --passive --locale en-US"
 choco install -y visualstudio2022buildtools
