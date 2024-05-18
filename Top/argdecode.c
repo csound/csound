7af7831b224acf07c823b1010a19a12c1cf4c973
vlazzarini <victor.lazzarini@mu.ie>
2024-05-18 14:49:32 +0100

    fixing #1878

--- Top/argdecode.c
+++ Top/argdecode.c
@@ -1557,11 +1557,10 @@ PUBLIC int argdecode(CSOUND *csound, int argc, const char **argv_)
               break;
             }
 #endif
-           if (!decode_long(csound, s, argc, argv))
-              // shouldn't need to long jump here -
-              // - it's crashing if csoundSetOption fails
-              //csound->LongJmp(csound, 1);
-             csound->Message(csound,"\n ...ignoring \n");
+            if (!decode_long(csound, s, argc, argv)) {
+              csound->Message(csound,"\n...failing to parse options\n");
+              return 0;  // fail
+            }
             while (*(++s));
             break;
           case 'j':
