508c64c5f973ef3510eb94bb80d3d90981a972bc
vlazzarini <victor.lazzarini@mu.ie>
2024-06-18 14:44:05 +0100

    adding to API

--- include/csound_new.h
+++ include/csound_new.h
@@ -29,43 +29,69 @@ PUBLIC int 	csoundInitialize (int flags)
 PUBLIC CSOUND*  csoundCreate (void *hostData) 
 PUBLIC void 	csoundDestroy (CSOUND *) 
 
-//Performance
-PUBLIC int 	    csoundCompile (CSOUND *, int argc, const char **argv)
-PUBLIC int 	    csoundPerformKsmps (CSOUND *)
+// Performance
+// We could potentially use "argc" as a code
+// > 0 - argc/argv as main() command-line parameters
+// 0 - start csound as a daemon with no code, ignore argv
+// -1 - argv[0] is an orchestra string (csoundCompileStr)
+// -2 - argv[0] is a complete csound   (csoundCompileCsdText)
+// First call to csoundCompile() also starts Csound so we remove csoundStart()
+// async defines async operation
+PUBLIC int  csoundCompile (CSOUND *, int argc, const char **argv, int async);
+PUBLIC int  csoundPerformKsmps (CSOUND *);
 
-//Realtime Audio
-PUBLIC MYFLT * 	csoundGetSpin (CSOUND *)
-PUBLIC MYFLT * 	csoundGetSpout (CSOUND *csound)
+// Realtime Audio
+// this is now effectively -n since there is no access to buffer, only to spin.
+PUBLIC void csoundSetHostImplementedAudioIO(CSOUND *, int state);
+PUBLIC MYFLT * 	csoundGetSpin (CSOUND *);
+PUBLIC MYFLT * 	csoundGetSpout (CSOUND *csound);
 
-//Realtime MIDI
+// MIDI callbacks
 PUBLIC void 	csoundSetExternalMidiInOpenCallback (CSOUND *, int(*func)(CSOUND *, void **userData, const char *devName))
 PUBLIC void 	csoundSetExternalMidiReadCallback (CSOUND *, int(*func)(CSOUND *, void *userData, unsigned char *buf, int nBytes)) 
 PUBLIC void 	csoundSetExternalMidiInCloseCallback (CSOUND *, int(*func)(CSOUND *, void *userData))
 PUBLIC void 	csoundSetExternalMidiOutOpenCallback (CSOUND *, int(*func)(CSOUND *, void **userData, const char *devName)) 
 PUBLIC void 	csoundSetExternalMidiWriteCallback (CSOUND *, int(*func)(CSOUND *, void *userData, const unsigned char *buf, int nBytes)) 
 
-//Messages
-PUBLIC void 	csoundCreateMessageBuffer (CSOUND *csound, int toStdOut)
-PUBLIC const char * 	csoundGetFirstMessage (CSOUND *csound)
-PUBLIC void 	csoundPopFirstMessage (CSOUND *csound)
-PUBLIC int 	    csoundGetMessageCnt (CSOUND *csound)
-void PUBLIC 	csoundDestroyMessageBuffer (CSOUND *csound)
+// Messages
+PUBLIC void 	   csoundCreateMessageBuffer (CSOUND *csound, int toStdOut)
+PUBLIC const char *csoundGetFirstMessage (CSOUND *csound)
+PUBLIC void 	   csoundPopFirstMessage (CSOUND *csound)
+PUBLIC int 	   csoundGetMessageCnt (CSOUND *csound)
+void PUBLIC 	   csoundDestroyMessageBuffer (CSOUND *csound)
 
-//Channels Controls / Events
-PUBLIC void 	csoundGetAudioChannel (CSOUND *csound, const char *name, MYFLT *samples)
-PUBLIC void 	csoundSetAudioChannel (CSOUND *csound, const char *name, MYFLT *samples)
-PUBLIC void 	csoundGetStringChannel (CSOUND *csound, const char *name, char *string)
-PUBLIC void 	csoundSetStringChannel (CSOUND *csound, const char *name, char *string)
+//Channels 
+PUBLIC int csoundGetChannelPtr(CSOUND *,
+                                 MYFLT **p, const char *name, int type);
+PUBLIC int csoundListChannels(CSOUND *, controlChannelInfo_t **lst);
+PUBLIC void csoundDeleteChannelList(CSOUND *, controlChannelInfo_t *lst);
+PUBLIC MYFLT csoundGetControlChannel(CSOUND *csound, const char *name,
+                                       int *err);
+PUBLIC void csoundSetControlChannel(CSOUND *csound,
+                                      const char *name, MYFLT val);
+PUBLIC void csoundGetAudioChannel(CSOUND *csound,
+                                    const char *name, MYFLT *samples);
+PUBLIC void csoundSetAudioChannel(CSOUND *csound,
+                                    const char *name, MYFLT *samples);
+PUBLIC void csoundGetStringChannel(CSOUND *csound,
+                                     const char *name, char *string);
+PUBLIC  void csoundSetStringChannel(CSOUND *csound,
+                                      const char *name, char *string);
+PUBLIC int csoundGetChannelDatasize(CSOUND *csound, const char *name);
 
-PUBLIC void 	csoundInputMessage (CSOUND *, const char *message)
+// events
+// async selects asynchronous operation
+PUBLIC void  csoundInputMessage (CSOUND *, const char *message, int async);
 
 //Tables
-PUBLIC int 	    csoundTableLength (CSOUND *, int table)
-PUBLIC void 	csoundTableCopyIn (CSOUND *csound, int table, MYFLT *src)
-PUBLIC void 	csoundTableCopyOut (CSOUND *csound, int table, MYFLT *dest)
+PUBLIC int 	csoundTableLength (CSOUND *, int table);
+PUBLIC void 	csoundTableCopyIn (CSOUND *csound, int table, MYFLT *src);
+PUBLIC void 	csoundTableCopyOut (CSOUND *csound, int table, MYFLT *dest);
 
-//Opcodes
-PUBLIC int 	csoundAppendOpcode (CSOUND *, const char *opname, int dsblksiz, int flags, int thread, const char *outypes, const char *intypes, int(*iopadr)(CSOUND *, void *), int(*kopadr)(CSOUND *, void *), int(*aopadr)(CSOUND *, void *))
+//Opcodes - adapted for new internals
+PUBLIC int 	csoundAppendOpcode (CSOUND *, const char *opname, int dsblksiz, int flags,
+                                    const char *outypes, const char *intypes, int(*init)(CSOUND *, void *),
+                                    int(*perf)(CSOUND *, void *), int(*deinit)(CSOUND *, void *));
 
 //Threading and concurrency
 
