c035a07d2b38243c9df2a38fe233f8c936eda378
vlazzarini <victor.lazzarini@mu.ie>
2024-08-31 20:57:54 +0100

    making functions externally accessible to wasm

--- Top/csmodule.c
+++ Top/csmodule.c
@@ -230,9 +230,9 @@ static int check_plugin_compatibility(CSOUND *csound, const char *fname, int n)
   }
   return 0;
 }
-static int csoundOpenLibrary(void **library, const char *libraryPath);
-static int csoundCloseLibrary(void *library);
-static void *csoundGetLibrarySymbol(void *library, const char *symbolName);
+int csoundOpenLibrary(void **library, const char *libraryPath);
+int csoundCloseLibrary(void *library);
+void *csoundGetLibrarySymbol(void *library, const char *symbolName);
 
 
 /**
@@ -1033,25 +1033,25 @@ int csoundDestroyModules(CSOUND *csound)
 
 #if defined(WIN32)
 
-static int csoundOpenLibrary(void **library, const char *libraryPath)
+int csoundOpenLibrary(void **library, const char *libraryPath)
 {
   *library = (void*) LoadLibrary(libraryPath);
   return (*library != NULL ? 0 : -1);
 }
 
-static int csoundCloseLibrary(void *library)
+int csoundCloseLibrary(void *library)
 {
   return (int) (FreeLibrary((HMODULE) library) == FALSE ? -1 : 0);
 }
 
-static void *csoundGetLibrarySymbol(void *library, const char *procedureName)
+void *csoundGetLibrarySymbol(void *library, const char *procedureName)
 {
   return (void*) GetProcAddress((HMODULE) library, procedureName);
 }
 
 #elif  !(defined(__wasi__)) && (defined(LINUX) || defined(NEW_MACH_CODE) || defined(__HAIKU__))
 
-static int csoundOpenLibrary(void **library, const char *libraryPath)
+int csoundOpenLibrary(void **library, const char *libraryPath)
 {
   int flg = RTLD_NOW;
   if (libraryPath != NULL) {
@@ -1066,30 +1066,30 @@ static int csoundOpenLibrary(void **library, const char *libraryPath)
   return (*library != NULL ? 0 : -1);
 }
 
-static int csoundCloseLibrary(void *library)
+int csoundCloseLibrary(void *library)
 {
   return (int) dlclose(library);
 }
 
-static void *csoundGetLibrarySymbol(void *library, const char *procedureName)
+void *csoundGetLibrarySymbol(void *library, const char *procedureName)
 {
   return (void*) dlsym(library, procedureName);
 }
 
 #else /* case for platforms without shared libraries -- added 062404, akozar */
 
-static int csoundOpenLibrary(void **library, const char *libraryPath)
+int csoundOpenLibrary(void **library, const char *libraryPath)
 {
   *library = NULL;
   return -1;
 }
 
-static int csoundCloseLibrary(void *library)
+int csoundCloseLibrary(void *library)
 {
   return 0;
 }
 
-static void *csoundGetLibrarySymbol(void *library, const char *procedureName)
+void *csoundGetLibrarySymbol(void *library, const char *procedureName)
 {
   return NULL;
 }
