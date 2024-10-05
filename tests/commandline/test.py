e9f01cce38a27eeb55579dbac248bc73b273e941
Scott Sitar <scottsitar@gmail.com>
2024-10-04 23:32:34 -0700

    add test case from github issue

--- tests/commandline/test.py
+++ tests/commandline/test.py
@@ -174,6 +174,7 @@ def runTest():
     ["test_plusname.csd", "test +Name for instr name"],
     ["testnewline.csd", "test newline in statements"],
     ["testmidichannels.csd", "test use of mapped multiport channels"],
+    ["test_ambiguous_opcall.csd", "test ambiguous opcall examples"],
     ]
 
     arrayTests = [["arrays/arrays_i_local.csd", "local i[]"],
