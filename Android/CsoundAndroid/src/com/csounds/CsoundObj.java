07f918618608bef63e473c83ff582be37620b0b3
vlazzarini <victor.lazzarini@mu.ie>
2025-03-05 23:39:42 +0000

    added csound.Start()

--- Android/CsoundAndroid/src/com/csounds/CsoundObj.java
+++ Android/CsoundAndroid/src/com/csounds/CsoundObj.java
@@ -300,6 +300,8 @@ public class CsoundObj {
 		}
 		if(!isAsync) this.pause();
 		retVal = csound.Compile(f.getAbsolutePath());
+                if(retVal == 0)
+                    retVal = csound.Start();
 		Log.d("CsoundObj", "Return Value2: " + retVal);
 		if (retVal == 0) {
 			for (int i = 0; i < bindings.size(); i++) {
