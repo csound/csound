1dfc33efcbcb2ddff5c9d9f163bd9f56ac136975
vlazzarini <victor.lazzarini@mu.ie>
2025-03-07 14:26:53 +0000

    adding ndk download

--- Android/build-release.sh
+++ Android/build-release.sh
@@ -4,8 +4,8 @@ NDK_VERSION=28
 NDK_NUMBER=13004108
 NDK_RELEASE="android-ndk-r$NDK_VERSION-darwin.dmg"
 
-#wget https://dl.google.com/android/repository/$NDK_RELEASE
-#open $NDK_RELEASE
+wget https://dl.google.com/android/repository/$NDK_RELEASE
+open $NDK_RELEASE
 ditto "/Volumes/Android NDK r$NDK_VERSION/AndroidNDK$NDK_NUMBER.app" AndroidNDK.app
 export ANDROID_NDK_ROOT="$PWD/AndroidNDK.app/Contents/NDK"
 export NDK_MODULE_PATH=$PWD/modules
