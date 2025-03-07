f398fed2d238a6ab3920b0dec68d93ee459057c5
vlazzarini <victor.lazzarini@mu.ie>
2025-03-07 15:29:24 +0000

    changing the script to build in stages

--- Android/downloadNDK.sh
+++ Android/downloadNDK.sh
@@ -6,12 +6,3 @@ NDK_RELEASE="android-ndk-r$NDK_VERSION-darwin.dmg"
 wget -q https://dl.google.com/android/repository/$NDK_RELEASE
 hdiutil attach $NDK_RELEASE
 ditto "/Volumes/Android NDK r$NDK_VERSION/AndroidNDK$NDK_NUMBER.app" AndroidNDK.app
-export ANDROID_NDK_ROOT="$PWD/AndroidNDK.app/Contents/NDK"
-export NDK_MODULE_PATH=$PWD/modules
-sh downloadDependencies.sh
-cd CsoundAndroid
-sh build.sh
-cd ..
-sh update.sh
-sh release.sh
-
