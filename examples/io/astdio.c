a4210c85a831d21287afb266a340b1a2a96d541f
vlazzarini <victor.lazzarini@mu.ie>
2025-04-23 18:00:13 +0100

    formatting examples

--- examples/io/astdio.c
+++ examples/io/astdio.c
@@ -1,15 +1,18 @@
-#include <csdl.h>
 // Audio IO module example
 // V Lazzarini, Nov 2024
+#include <csdl.h>
 #include <poll.h>
 
 static int32_t open_device(CSOUND *csound, const csRtAudioParams *p){
+  // nothing to do here, so exit
   return OK;
 }
 
 static void audio_out(CSOUND *csound, const MYFLT *s, int32_t nbytes) {
   int32_t n, nsmps;
+  // number of interleaved samples in buffer
   nsmps = nbytes / sizeof(MYFLT);
+  // send each sample to stdout 
   for(n = 0; n < nsmps; n++)
     fprintf(stdout, "%f\n", s[n]);
 }
@@ -18,42 +21,53 @@ static int32_t audio_in(CSOUND *csound, MYFLT *s, int32_t nbytes) {
   struct pollfd fd = {0, POLLIN, 0};
   int32_t cnt = 0, nsmps;
   MYFLT data;
+  // buffer size in samples
   nsmps = nbytes / sizeof(MYFLT);
+  // poll for input data on stdin
   if(poll(&fd, 1, 0)) {
-   while(scanf("%f", &data) > 0 && cnt < nsmps)
-       s[cnt++] = data;
+    // read each sample from input 
+    while(scanf("%f", &data) > 0 && cnt < nsmps)
+      s[cnt++] = data;
   }
+  // return the number of samples read
   return cnt/sizeof(MYFLT);  
 }
 
-static void close_device(CSOUND *csound) { }
+static void close_device(CSOUND *csound) {
+  // nothing to do
+}
   
 
 int32_t csoundModuleCreate(CSOUND *csound){
-    csound->Message(csound, "stdio audio module\n");
-    return 0;
+  // print message announcing module
+  csound->Message(csound, "stdio audio module\n");
+  return 0;
 }
 
 int32_t csoundModuleInit(CSOUND *csound)
 {
-    char *module = (char*)
-      (csound->QueryGlobalVariable(csound, "_RTAUDIO"));
-    if (module == NULL)
-      return 0;
-    if (strcmp(module, "stdio") != 0)
-      return 0;
-    csound->Message(csound, "stdio audo module enabled\n");
-    csound->SetPlayopenCallback(csound, open_device);
-    csound->SetRecopenCallback(csound, open_device);
-    csound->SetRtplayCallback(csound, audio_out);
-    csound->SetRtrecordCallback(csound, audio_in);
-    csound->SetRtcloseCallback(csound, close_device);
+  // get requested module name
+  char *module = (char*)
+    (csound->QueryGlobalVariable(csound, "_RTAUDIO"));
+  if (module == NULL)
     return 0;
+  // module section check 
+  if (strcmp(module, "stdio") != 0)
+    return 0;
+  // print message - module selected
+  csound->Message(csound, "stdio audo module enabled\n");
+  csound->SetPlayopenCallback(csound, open_device);
+  csound->SetRecopenCallback(csound, open_device);
+  csound->SetRtplayCallback(csound, audio_out);
+  csound->SetRtrecordCallback(csound, audio_in);
+  csound->SetRtcloseCallback(csound, close_device);
+  return 0;
 }
 
+// versioning
 int32_t csoundModuleInfo(void)
 {
-    return ((CS_VERSION << 16) +
-            (CS_SUBVER << 8) +
-            (int32_t) sizeof(MYFLT));
+  return ((CS_VERSION << 16) +
+          (CS_SUBVER << 8) +
+          (int32_t) sizeof(MYFLT));
 }
--- examples/io/mstdin.c
+++ examples/io/mstdin.c
@@ -4,52 +4,60 @@
 #include <poll.h>
 
 static int32_t midi_open(CSOUND *csound,void **userData,
-                  const char *devName){
-  csound->Message(csound, "using stdin for MIDI input\n");
+                         const char *devName){
+  // nothing to do, so exit
   return OK;
 }
 
 static int32_t midi_read(CSOUND *csound, void *userData,
-                  unsigned char *buf, int32_t nBytes) {
+                         unsigned char *buf, int32_t nBytes) {
   
   struct pollfd fd = {0, POLLIN, 0};
   int32_t cnt = 0, data;
+  // poll for stdin data
   if(poll(&fd, 1, 0)) {
-   while(scanf("%d", &data) > 0 && cnt < nBytes)
-       buf[cnt++] = data;
+    // read stdin MIDI data into buffer
+    while(scanf("%d", &data) > 0 && cnt < nBytes)
+      buf[cnt++] = data;
   }
   return cnt;
 }
 
 static int32_t midi_close(CSOUND *csound,void *userData){
+  // nothing to do
   return OK;
 }
 
 
 int32_t csoundModuleCreate(CSOUND *csound)
 {
-    csound->Message(csound, "stdin MIDI module\n");
-    return 0;
+  // announce module
+  csound->Message(csound, "stdin MIDI module\n");
+  return 0;
 }
 
 int32_t csoundModuleInit(CSOUND *csound)
 {
-    char *module = (char*)
-      (csound->QueryGlobalVariable(csound, "_RTMIDI"));
-    if (module == NULL)
-      return 0;
-    if (strcmp(module, "stdin") != 0)
-      return 0;
-    csound->Message(csound, "stdin MIDI module enabled\n");
-    csound->SetExternalMidiInOpenCallback(csound, midi_open);
-    csound->SetExternalMidiReadCallback(csound, midi_read);
-    csound->SetExternalMidiInCloseCallback(csound, midi_close);
+  // get requested module name
+  char *module = (char*)
+    (csound->QueryGlobalVariable(csound, "_RTMIDI"));
+  if (module == NULL)
     return 0;
+  // check for requested module
+  if (strcmp(module, "stdin") != 0)
+    return 0;
+  // print message - module selected
+  csound->Message(csound, "stdin MIDI module enabled\n");
+  csound->SetExternalMidiInOpenCallback(csound, midi_open);
+  csound->SetExternalMidiReadCallback(csound, midi_read);
+  csound->SetExternalMidiInCloseCallback(csound, midi_close);
+  return 0;
 }
 
+// versioning
 int32_t csoundModuleInfo(void)
 {
-    return ((CS_VERSION << 16) +
-            (CS_SUBVER << 8) +
-            (int32_t) sizeof(MYFLT));
+  return ((CS_VERSION << 16) +
+          (CS_SUBVER << 8) +
+          (int32_t) sizeof(MYFLT));
 }
