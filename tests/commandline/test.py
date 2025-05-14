97651849ff003fb53ad2e24ec959a99b906f8c0e
vlazzarini <victor.lazzarini@mu.ie>
2025-05-14 10:33:14 +0100

    added midi file test

--- tests/commandline/test.py
+++ tests/commandline/test.py
@@ -236,6 +236,7 @@ def runTest():
                    ["test_schedule_instr.csd", "Test pfields on all forms of schedule with instance"],  
                    ["test_event_pfields.csd", "Test pfields on all forms of event"],
                    ["test_recursive_schedule.csd", "Test recursive events"],
+                   ["test_midifile.csd", "Test midi file input (-F)"],
                        ]     
 
     tests += arrayTests
