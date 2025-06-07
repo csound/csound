abfea28678a0d9e80a1caf3e6d9cf4f30c959981
vlazzarini <victor.lazzarini@mu.ie>
2025-06-07 18:47:26 +0100

    fixed the duplicate declaration

--- H/corfile.h
+++ H/corfile.h
@@ -54,5 +54,4 @@ void corfile_set(CORFIL *f, int32_t n);
 void corfile_seek(CORFIL *f, int32_t n, int32_t dir);
 void corfile_preputs(CSOUND *csound, const char *s, CORFIL *f);
 void add_corfile(CSOUND* csound, CORFIL *smpf, char *filename);
-CORFIL *copy_url_corefile(CSOUND *, const char *, int);
 #endif
