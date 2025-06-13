c536334d5f1dc618412988939b9030cd80fc170a
vlazzarini <victor.lazzarini@mu.ie>
2025-06-13 15:49:58 +0100

    added license

--- Daisy/DaisyCsoundExamples/DaisyCsoundMidi/daisyCsoundMidi.cpp
+++ Daisy/DaisyCsoundExamples/DaisyCsoundMidi/daisyCsoundMidi.cpp
@@ -1,11 +1,34 @@
+/*
+  daisyCsoundMidi.cpp:
+
+  Copyright (C) 2025 Aman Jagawni
+
+  This file is part of Csound.
+
+  The Csound Library is free software; you can redistribute it
+  and/or modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  Csound is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with Csound; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+  02111-1307 USA
+*/
+
 #include "daisy_seed.h"
 #include "daisysp.h"
 #include <array>
 #include <stdio.h>
 #include "daisyCsoundMidi.h"
 #include "midiBuffer.h"
-#include "csound.h"
-#include "plugin.h"
+#include <csound.h>
+#include <plugin.h>
 
 using namespace daisy;
 using namespace daisy::seed;
