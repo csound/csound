9d359d6d174b53790ef6ca83cfdeb62acf1777e0
vlazzarini <victor.lazzarini@mu.ie>
2025-09-02 13:28:25 +0100

    added example again

---
+++ iOS/Csound-for-iOS/Generator/Generator/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  Generator
+//
+//  Created by vlazzarini on 02/09/2025.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ iOS/Csound-for-iOS/Generator/GeneratorTests/GeneratorTests.m
@@ -0,0 +1,36 @@
+//
+//  GeneratorTests.m
+//  GeneratorTests
+//
+//  Created by vlazzarini on 02/09/2025.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GeneratorTests : XCTestCase
+
+@end
+
+@implementation GeneratorTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
