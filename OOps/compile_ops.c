ca08d55453de652cc8a0af5ab934adcd6ebf9059
vlazzarini <victor.lazzarini@mu.ie>
2025-10-24 15:28:47 +0100

    reverted small change

--- OOps/compile_ops.c
+++ OOps/compile_ops.c
@@ -140,10 +140,9 @@ OSC_MESS *csoundReadOSCMessage(CSOUND *csound, const char *address,
   if(p->address == NULL) return NULL;
   csoundSpinLock(lock);
   do {
-    if(p->flag) {
-      if(!strcmp(p->address, address)
+    if(p->flag &&
+      !strcmp(p->address, address)
        && !strcmp(p->type, type)) break;
-    }
   } while((p = p->nxt) != NULL);
   csoundSpinUnLock(lock);
   return p;
