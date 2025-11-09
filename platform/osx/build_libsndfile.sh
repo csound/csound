5f407548d6c4fca34697892d6e7046d3959f8ca6
vlazzarini <victor.lazzarini@mu.ie>
2025-11-09 22:12:00 +0000

    Fix LAME download URL to use redirect

--- platform/osx/build_libsndfile.sh
+++ platform/osx/build_libsndfile.sh
@@ -66,7 +66,7 @@ rm -rf "flac-${FLACVERSION}"
 echo "Downloading flac..."
 curl -sL "https://downloads.xiph.org/releases/flac/flac-${FLACVERSION}.tar.xz" | tar -xJ
 echo "Downloading lame..."
-curl -sL -o "${LAME_TARBALL}" "https://downloads.sourceforge.net/project/lame/lame/${LAMEVERSION}/lame-${LAMEVERSION}.tar.gz" 
+curl -fL -o "${LAME_TARBALL}" "https://downloads.sourceforge.net/project/lame/lame/${LAMEVERSION}/lame-${LAMEVERSION}.tar.gz/download" 
 if ! file "${LAME_TARBALL}" | grep -q 'gzip compressed data'; then
   echo "Error: Downloaded lame-3.100.tar.gz is not a valid tar.gz archive"
   exit 1
