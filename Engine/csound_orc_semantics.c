f0711613b2ddaff3845a49adae21aebcdd0d6a06
vlazzarini <victor.lazzarini@mu.ie>
2026-01-02 22:59:11 +0000

    fixed comment

--- Engine/csound_orc_semantics.c
+++ Engine/csound_orc_semantics.c
@@ -1563,7 +1563,7 @@ char* convert_internal_to_external(CSOUND* csound, char* arg) {
   while(*type != '\0') {
     if(*type == '[' && *(type+1) ==  ']') {
       // Only strip if this is NOT the internal [typename] format
-      // and NOT a single-letter array type like S[] or i[]
+      // and a single-letter array type like S[] or i[]
       if (!hasLeadingBracket && isSingleChar) {
         *type = '\0';
       }
