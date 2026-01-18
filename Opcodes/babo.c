46258502df083525d20dd350878abeeeddfa3b3c
Steven Yi <stevenyi@gmail.com>
2026-01-18 11:36:02 -0500

    chore: clarify functions as OPCODE rather than PUBLIC

--- Opcodes/babo.c
+++ Opcodes/babo.c
@@ -765,7 +765,7 @@ verify_coherence(CSOUND *csound, BABO *p)
 }
 
 /*
- * PUBLIC FUNCTIONS - baboset(), babo()
+ * OPCODE FUNCTIONS - baboset(), babo()
  *
  * these get called from the csound engine
  *
