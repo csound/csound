8487c1e2b8a899bc7cb398ffe880f39264357845
vlazzarini <victor.lazzarini@mu.ie>
2026-03-07 18:35:14 +0000

    trying to fix android build

--- Android/CsoundAndroid/build.sh
+++ Android/CsoundAndroid/build.sh
@@ -16,7 +16,7 @@ bison -d -pcsound_orc --report=itemset -o jni/csound_orcparse.c ../../Engine/cso
 rm -rf src/csnd7
 mkdir -p src/csnd7
 
-swig -java -package csnd7 -DUSE_DOUBLE=0 -D__BUILDING_LIBCSOUND -DENABLE_NEW_PARSER -DPARCS -DHAVE_DIRENT_H -DHAVE_FCNTL_H -DHAVE_UNISTD_H -DHAVE_STDINT_H -DHAVE_SYS_TIME_H -DHAVE_SYS_TYPES_H -DHAVE_TERMIOS_H -includeall -verbose -outdir src/csnd7 -c++ -I$HOME/include/c++/v1 -I/usr/local/include -I../../H -I../../include -I../../Engine -I../.. -I../../interfaces -I/System/Library/Frameworks/Python.framework/Headers -I/System/Library/Frameworks/JavaVM.framework/Headers -I./jni -o jni/java_interfaceJAVA_wrap.cpp android_interface.i
+swig -java -package csnd7 -D__BUILDING_LIBCSOUND -DENABLE_NEW_PARSER -DPARCS -DHAVE_DIRENT_H -DHAVE_FCNTL_H -DHAVE_UNISTD_H -DHAVE_STDINT_H -DHAVE_SYS_TIME_H -DHAVE_SYS_TYPES_H -DHAVE_TERMIOS_H -includeall -verbose -outdir src/csnd7 -c++ -I$HOME/include/c++/v1 -I/usr/local/include -I../../H -I../../include -I../../Engine -I../.. -I../../interfaces -I/System/Library/Frameworks/Python.framework/Headers -I/System/Library/Frameworks/JavaVM.framework/Headers -I./jni -o jni/java_interfaceJAVA_wrap.cpp android_interface.i
 
 # ADJUST SWIG CODE FOR ANDROID and DIRECTORS
 sed -i.bak "s/AttachCurrentThread((void \*\*)/AttachCurrentThread(/" jni/java_interfaceJAVA_wrap.cpp 
