04e0f7cf1200ee8ebe99421c859344c45eeb4d23
vlazzarini <victor.lazzarini@mu.ie>
2026-05-12 11:57:37 +0100

    added test

--- tests/commandline/test.py
+++ tests/commandline/test.py
@@ -613,6 +613,7 @@ def runTest():
         ["test_string_in_event.csd", "test multiple strings in realtime event"],
         ["testmidichannels.csd", "test use of mapped multiport channels"],
         ["test_midi_default.csd", "test midi default instr"],
+        ["test_midi_tempo.csd", "test tempo reading from midifile"],
         ["test_instr_type.csd", "test instr type and variables"],
         ["test_delete_instr.csd", "test creating and deleting instr"],
         ["test_create_instr.csd", "testing creating and scheduling instr"],
---
+++ tests/commandline/test_midi_tempo.csd
@@ -0,0 +1,33 @@
+<CsoundSynthesizer>
+<CsOptions>
+-n -F rain.mid -T
+</CsOptions>
+<CsInstruments>
+0dbfs=1
+nchnls=1
+ksmps=64
+
+
+
+instr 1
+  itp miditempo
+  if int(itp) != 78 then
+    print itp
+    exitnow(-1)
+  endif
+
+  kcps cpsmidib 2
+  iamp ampmidi 0dbfs
+  //print iamp
+  a2 oscili iamp, kcps
+  a2 linenr  a2, 0.1,0.1,0.01
+       	out a2*0.09
+	   	
+endin
+
+
+</CsInstruments>
+<CsScore>
+f0 30
+</CsScore>
+</CsoundSynthesizer>
