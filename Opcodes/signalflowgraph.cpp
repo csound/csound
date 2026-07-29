5b3ec9ef5ccbb8b855d7c997dcbdfd16025b51a1
Hlöðver Sigurðsson <hlolli@gmail.com>
2026-07-29 20:22:29 +0300

    fix(windows): keep directives outside macro calls

--- Opcodes/signalflowgraph.cpp
+++ Opcodes/signalflowgraph.cpp
@@ -1343,11 +1343,12 @@ static int32_t ftgenonce_(CSOUND *csound, FTGEN *p, bool isNamedGenerator,
   }
   SignalFlowGraphState *sfg_globals = nullptr;
   QueryGlobalPointer(csound, "sfg_globals", sfg_globals);
-  if (UNLIKELY(sfg_globals == nullptr
 #ifndef __wasi__
-               || sfg_globals->signal_flow_ftables_lock == nullptr
+  if (UNLIKELY(sfg_globals == nullptr ||
+               sfg_globals->signal_flow_ftables_lock == nullptr)) {
+#else
+  if (UNLIKELY(sfg_globals == nullptr)) {
 #endif
-               )) {
     return csound->InitError(csound, "%s",
                              Str("ftgenonce: not initialized"));
   }
