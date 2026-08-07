4661c5ab59f3bbaad83f7050fcbdc05a100a536f
Hlöðver Sigurðsson <hlolli@gmail.com>
2026-08-07 15:25:12 +0300

    fix: use atomic PARCS shutdown reads

--- Top/csound_perf.c
+++ Top/csound_perf.c
@@ -421,7 +421,7 @@ int32_t kperf(CSOUND *csound) {
 #ifdef PARCS_USE_THREAD_BARRIER
         csound->WaitBarrier(csound->barrier1);
 #else
-        ATOMIC_SET(csound->parflag,!csound->parflag);
+        ATOMIC_SET(csound->parflag, !ATOMIC_GET(csound->parflag));
 #endif
       }
       csoundMessage(csound, Str("end of Performance\n"));
@@ -468,7 +468,7 @@ int32_t perform_buffer(CSOUND *csound) {
 #ifdef PARCS_USE_THREAD_BARRIER
         csound->WaitBarrier(csound->barrier1);
 #else
-        ATOMIC_SET(csound->parflag,!csound->parflag);
+        ATOMIC_SET(csound->parflag, !ATOMIC_GET(csound->parflag));
 #endif
         }
         return done;
